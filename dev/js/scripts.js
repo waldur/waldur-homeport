e3ec3afda9fb77a7835cac78c7e9ab83e0702f00
Mihail Yakimenko <macgera@gmail.com>
2014-10-15 16:42:01 +0200

    title smaller, projects: search, sort

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,30 @@
 (function($) {
 
+    // sort by name – search field in projects th
+    function byNameinProjects(){
+        var searchBox = $('[data-proj-sort="box"]');
+        searchBox.each(function (){
+            var searchTrigger = $(this).find('[data-proj-sort="name"]');
+            var searchField = $(this).find('[data-proj-sort="field"]');
+            searchTrigger.click(function (){
+                $(this).toggleClass('active');
+                if(!searchField.hasClass('state')) {
+                    searchField.addClass('state');
+                    console.log('foo bar')
+                } else {
+                    searchField.removeClass('state');
+                    console.log('fuck')
+                }
+            });
+        });
+    }
+
     // document ready
     $(document).ready(function() {
+        // bs tooltips
         $('[data-toggle="tooltip"]').tooltip();
+        // sort by name – search field in projects th
+        byNameinProjects();
     });
 
     // all initial on window resize
