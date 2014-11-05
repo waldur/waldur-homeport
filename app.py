3e4bab0e93bea19b591d494e8a6eace5245bac93
Mihail Yakimenko <macgera@gmail.com>
2014-11-05 02:22:43 +0100

    bottom slogan

--- app.py
+++ app.py
@@ -24,6 +24,10 @@ freezer = Freezer(app)
 def index():
     return render_template('index.html')
 
+@app.route('/plans/')
+def plans():
+    return render_template('plans.html')
+
 @app.route('/dashboard/')
 def dashboard():
     return render_template('dashboard.html')
