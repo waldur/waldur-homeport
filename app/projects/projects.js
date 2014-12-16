c59bd71903ecc65dc9dbba49e10f41a9f68575dc
Mihail Yakimenko <macgera@gmail.com>
2014-12-16 03:13:27 +0200

    projects view

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -9,6 +9,60 @@ angular.module('ncsaas.projects', ['ngRoute'])
   });
 }])
 
-.controller('projectsCtrl', [function() {
+.controller('projectsCtrl', function($scope, $http) {
 
-}]);
+    // tempory data
+
+    $scope.projects = [
+        {
+            Name: 'G48s-11',
+            Description: 'Here goes project description.',
+            Resources: [
+                {
+                    Type: 'VM',
+                    Name: 'Res one'
+                },
+                {
+                    Type: 'Repo',
+                    Name: 'Res two'
+                }
+            ],
+            Users: [
+                {
+                    Name: 'Mihail Yakimenko'
+                },
+                {
+                    Name: 'Ilja Livenson'
+                }
+            ],
+        },
+        {
+            Name: 'G48s-23',
+            Description: 'Here goes project description.',
+            Resources: [
+                {
+                    Type: 'VM',
+                    Name: 'Res one'
+                },
+                {
+                    Type: 'Repo',
+                    Name: 'Res two'
+                }
+            ],
+            Users: [
+                {
+                    Name: 'Avdey Avdeeych'
+                },
+                {
+                    Name: 'Gera Libre'
+                }
+            ],
+        }
+    ];
+
+    // example API json parse
+    // $http.get('http://localhost:5000/api/').
+    //     success(function(data) {
+    //         $scope.projects = data;
+    //     });
+});
