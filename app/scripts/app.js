0f72b0a917c49670234e17e337f4081f961c72d0
Pavel <marchukpavelp@gmail.com>
2015-01-12 12:38:01 +0200

    sattelizer settings (saas-49)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,14 @@
 'use strict';
 
 angular
+
   // module name and dependencies
   .module('ncsaas', [
     'config',
+    'satellizer',
     'ngRoute',
     'ngCookies',])
+
   // urls
   .config(function($routeProvider) {
     $routeProvider
@@ -18,7 +21,26 @@ angular
         controller: 'AuthCtrl'
       })
 
+      .when('')
+
       .otherwise({
         redirectTo: '/'
       });
+  })
+
+  // social auth
+  .config(function($authProvider) {
+
+    $authProvider.facebook({
+      clientId: '654736081301402', // '624059410963642',
+      url: 'http://localhost:8080/api-auth/facebook/',
+      redirectUri: 'http://localhost:8000'
+    });
+
+    $authProvider.google({
+      clientId: '251636923168-9a6n6391j5vv5v4vf0m1k3n9sl9kag41.apps.googleusercontent.com',
+      url: 'http://127.0.0.1:8080/api-auth/google/',
+      redirectUri: 'http://127.0.0.1:8000'
+    });
+
   });
