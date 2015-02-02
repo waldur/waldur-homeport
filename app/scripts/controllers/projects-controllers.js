ededd523d8ced6046d3bf99380da6880f27751a7
Ilja Livenson <ilja@opennodecloud.com>
2015-02-02 10:50:50 +0400

    Cleanup
    - SAAS-73

--- app/scripts/controllers/projects-controllers.js
+++ app/scripts/controllers/projects-controllers.js
@@ -31,13 +31,13 @@
     vm.save = save;
 
     function save() {
+      // TODO: refactor this function to use named urls and uuid field instead - SAAS-108
       vm.project.$save(function() {
         var url = vm.project.url;
         var array = url.split ('/').filter(function(el) {
           return el.length != 0
         });
         var uuidNew = array[4];
-        console.log(uuidNew);
         $location.path('/projects/'+uuidNew+'/');
       });
     }
