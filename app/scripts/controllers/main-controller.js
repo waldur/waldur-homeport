2db4cf396aa03c419ab280cd6a50d05ab64e6a21
Ron Balnen <ron.balnen@gmail.com>
2015-02-16 14:41:08 +0200

    fix takin in account Pavel comments

--- app/scripts/controllers/main-controller.js
+++ app/scripts/controllers/main-controller.js
@@ -5,12 +5,9 @@
         .controller('MainController', ['$rootScope','$location', '$auth','usersService','authService', MainController]);
 
     function MainController($rootScope, $location, $auth,usersService,authService) {
-        console.log('Main controller');
         $rootScope.$on('$routeChangeSuccess', function (e, current, pre) {
-            //console.log(current);
             if (current.$$route != undefined && (current.$$route.originalPath === '/login/' || current.$$route.originalPath === '/')){
                 if (authService.getAuthCookie() != null){
-                    //console.log('auth');
                     $location.path('/dashboard');
                 }
             }
