7b86dbda1ac933f82ad65ff8f859a21183bbf74c
Pavel <marchukpavelp@gmail.com>
2015-02-23 20:15:35 +0200

    Implement templates service (saas-128)

--- app/scripts/services/templates-service.js
+++ app/scripts/services/templates-service.js
@@ -2,6 +2,23 @@
 
 (function() {
   angular.module('ncsaas')
+    .service('templatesService', ['RawTemplate', templatesService]);
+
+  function templatesService(RawTemplate) {
+    /*jshint validthis: true */
+    var vm = this;
+    vm.getTemplateList = getTemplateList;
+
+    function getTemplateList() {
+      return RawTemplate.query().$promise;
+    }
+
+  }
+
+})();
+
+(function() {
+  angular.module('ncsaas')
     .factory('RawTemplate', ['ENV', '$resource', RawTemplate]);
 
     function RawTemplate(ENV, $resource) {
