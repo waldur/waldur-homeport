32f46b39a9504470dbef3dc49bf88f573213d058
Ron Balnen <ron.balnen@gmail.com>
2015-04-20 16:20:05 +0300

    User profile. Delete my account (SAAS-215)
    Added button for deleting user account on profile page.
    http://localhost:8001/#/profile/
    Added test for account deleting. Added skip test for deleting account.

--- test/internal/profile-tests.js
+++ test/internal/profile-tests.js
@@ -64,3 +64,32 @@ for(var i=0; i < users.length; i++) {
   })(user, data);
 
 }
+
+var users = [
+  auth.getUser('Walter')
+];
+
+for(var i=0; i < users.length; i++) {
+  var user = users[i];
+
+  (function(user) {
+    describe('Test ' + user.username + ' can go to delete account:', function() {
+      it('I should be able to login', function() {
+        auth.login(user);
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/#/dashboard/');
+      });
+
+      // TODO this test can be run when functionality of adding new user will be available.
+      xit('I should be able to delete my account', function() {
+        element(by.css('li.dropdown.user-box a')).click();
+        element(by.cssContainingText('li.dropdown.user-box ul li:nth-child(1) a', 'Profile')).click();
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/#/profile/');
+        expect(element(by.css('.profile-name')).getText()).toContain(user.username);
+
+        element(by.css('ul.controls.pull-right > li:nth-child(3) > a')).click();
+        browser.switchTo().alert().accept();
+        expect(element(by.css('.tour-box .take-a-tour')).getText()).toEqual('Sign me up!');
+      });
+    });
+  })(user);
+}
