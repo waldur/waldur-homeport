8199d30354e83690343e4c779073de7dee523842
Michael Yakimenko <macgera@gmail.com>
2015-04-23 22:06:29 +0800

    Change directive loginboxcontext for vertical aligne login blovk. Remove directive loginbox.
    – saas-239

--- app/scripts/directives/login-box.js
+++ app/scripts/directives/login-box.js
@@ -3,22 +3,12 @@
 (function() {
 
   angular.module('ncsaas')
-    .directive('loginbox', function($window) {
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-          var winHeight = $window.innerHeight;
-          element.css('height', winHeight - 100 + 'px');
-        }
-      };
-    })
-
     .directive('loginboxcontext', function($window) {
       return {
         restrict: 'A',
         link: function(scope, element, attrs) {
-          var winHeight = $window.innerHeight;
-          element.css('top', winHeight / 5 + 'px');
+          var elHeight = element[0].offsetHeight;
+          element.css('margin-top', -elHeight / 2 + 'px');
         }
       };
     });
