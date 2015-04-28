e8d302f4a23e5b4357b0428776dd69ffb3035838
Michael Yakimenko <macgera@gmail.com>
2015-04-28 18:14:48 +0800

    Make new state for inital-data, for setup views: header, content, footer.
    – saas-257

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -60,28 +60,29 @@ angular
           authenticated: notLoggedCheck
         }
       })
-      // temporary, for develop only, remove after
-      .state('home.initialdatatemplate', {
-        url: 'init-test/',
-        views: {
-          'siteHeader@home' : {
-            templateUrl: 'views/partials/site-header.html',
-          },
-          'siteContent@home' : {
-            templateUrl: 'views/initial-data/initial-data.html',
-          },
-          'siteFooter@home' : {
-            templateUrl: 'views/partials/site-footer.html',
-          }
-        },
+
+      .state('initialdata', {
+        url: '/initial-data/',
+        templateUrl: 'views/initial-data/base.html',
+        abstract: true,
         resolve: {
           authenticated: notLoggedCheck
         }
       })
-      // temporary, for develop only, remove after
-      .state('initialdata', {
-        url: '/initial-data/',
-        templateUrl: 'views/initial-data.html',
+
+      .state('initialdata.view', {
+        url: '',
+        views: {
+          'siteHeader@initialdata' : {
+            templateUrl: 'views/partials/site-header-disabled.html',
+          },
+          'siteContent@initialdata' : {
+            templateUrl: 'views/initial-data/initial-data.html',
+          },
+          'siteFooter@initialdata' : {
+            templateUrl: 'views/partials/site-footer.html',
+          }
+        },
         resolve: {
           authenticated: authCheck
         }
