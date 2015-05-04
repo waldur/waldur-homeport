aded3f01e388f169e34468bf1876786239d841fd
Pavel <marchukpavelp@gmail.com>
2015-05-04 16:15:10 +0300

    Fix tests (saas-203)

--- test/internal/plan-tests.js
+++ test/internal/plan-tests.js
@@ -11,18 +11,16 @@ describe('Plans list visibility positive test for user ' + user.username + ':', 
   });
 
   it('I should be able to go to customers manage page', function() {
-    element(by.cssContainingText('a.customer-head span.customer-expl', 'active for context')).click();
+    element(by.cssContainingText('a.customer-context.drop-active span.active-context', 'Active organization')).click();
 
-    element(by.cssContainingText(
-      'ul.nav.navbar-nav.navbar-right.nav-user-area.hidden-md.hidden-sm > li:nth-child(2) > ul > li > a',
-      'Manage customers')).click();
+    element(by.cssContainingText('li.dropdown.customers > ul > li > a', 'Manage customers')).click();
     expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/#/customers/');
   });
 
   it('I should be able to see customer plans', function() {
-    element(by.cssContainingText('tr.ng-scope td.name h2.item-title a.ng-binding', customer.name)).click();
-    element(by.cssContainingText(
-      'div.col-xs-11.profile-info > div.row > div > ul > li:nth-child(2) > a', 'Plans')).click();
+    element(by.cssContainingText('div.list-item div h3.item-title a.ng-binding', customer.name)).click();
+    element(by.cssContainingText('a.button', 'actions')).click();
+    element(by.cssContainingText('a.button-simple', 'Plans')).click();
     for (var i = 0; i < expectedPlans.length; i++) {
       expect(element(by.cssContainingText('span.plan-name.ng-binding', expectedPlans[i])).isPresent()).toBe(true);
     }
@@ -34,3 +32,4 @@ describe('Plans list visibility positive test for user ' + user.username + ':', 
   });
 
 });
+// html.ng-scope body.ng-scope div#appContainer.ng-scope div.ng-scope div.ng-scope div.white-box.clear ul.object-list li.ng-scope div.list-item div h3.item-title a.ng-binding
