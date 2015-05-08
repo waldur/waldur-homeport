9ffff45202d1a99f9c3552ffae7c674832462189
Pavel <marchukpavelp@gmail.com>
2015-05-08 14:03:00 +0300

    Show real plan on initial-data page (saas-266)

--- app/scripts/controllers/initial-data-controller.js
+++ app/scripts/controllers/initial-data-controller.js
@@ -2,14 +2,17 @@
 
 (function() {
   angular.module('ncsaas')
-    .controller('InitialDataController', ['usersService', '$state', InitialDataController]);
+    .controller('InitialDataController',
+      ['usersService', 'planCustomersService', 'currentStateService', '$state', InitialDataController]);
 
-  function InitialDataController(usersService, $state) {
+  function InitialDataController(usersService, planCustomersService, currentStateService, $state) {
+    /*jshint camelcase: false */
     var vm = this;
 
     vm.user = null;
     vm.errors = {};
     vm.save = save;
+    vm.getPrettyQuotaName = getPrettyQuotaName;
 
     usersService.getCurrentUser().then(function(response) {
       vm.user = response;
@@ -17,6 +20,25 @@
         $state.go('dashboard.eventlog');
       }
     });
+
+    currentStateService.getCustomer().then(function(customer) {
+      planCustomersService.getList({customer: customer.uuid}).then(function(planCustomers) {
+        if (planCustomers.length !== 0) {
+          vm.plan = planCustomers[0].plan;
+        }
+      });
+    });
+
+    // XXX: This is quick fix, we need to get display names from backend, but currently quotas on backend do not
+    // have display names
+    function getPrettyQuotaName(name) {
+      var prettyNames = {
+        'nc_user_count': 'users',
+        'nc_resource_count': 'resources',
+        'nc_project_count': 'projects'
+      };
+      return prettyNames[name];
+    }
 
     function save() {
       if (vm.user.full_name && vm.user.email) {
