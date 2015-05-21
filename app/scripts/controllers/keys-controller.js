43d14745fde8dc4aa84d8dbd6f585879b45b0464
Ron Balnen <ron.balnen@gmail.com>
2015-05-21 18:47:43 +0300

    As a NC user I want to add my ssh key to the profile (SAAS-351)
    Created KeyAddController. Crated view for creation new ssh key.
    Added state for key creation.
    http://localhost:8001/#/keys/add/

---
+++ app/scripts/controllers/keys-controller.js
@@ -0,0 +1,18 @@
+(function() {
+  angular.module('ncsaas')
+    .controller('KeyAddController', ['baseControllerAddClass', 'keysService', KeyAddController]);
+
+  function KeyAddController(baseControllerAddClass, keysService) {
+    var controllerScope = this;
+    var Controller = baseControllerAddClass.extend({
+      init: function() {
+        this.service = keysService;
+        this.controllerScope = controllerScope;
+        this._super();
+        this.listState = 'dashboard.eventlog';
+      }
+    });
+
+    controllerScope.__proto__ = new Controller();
+  }
+})();
