f9560f4f90966ad13a7c59cac34a6ac1d4a5d0ed
Ron Balnen <ron.balnen@gmail.com>
2015-05-21 12:15:55 +0300

    Implementing baseControllerClass for PaymentMockController (SAAS-369)
    Upgraded PaymentMockController by baseControllerClass.

--- app/scripts/controllers/payments-controllers.js
+++ app/scripts/controllers/payments-controllers.js
@@ -2,31 +2,39 @@
 
 (function() {
     angular.module('ncsaas')
-    .controller('PaymentMockController', ['ordersService', '$stateParams', '$state', PaymentMockController]);
+    .controller('PaymentMockController',
+      ['ordersService', '$stateParams', '$state', 'baseControllerClass', PaymentMockController]);
 
-  function PaymentMockController(ordersService, $stateParams, $state) {
-    var vm = this;
+  function PaymentMockController(
+    ordersService, $stateParams, $state, baseControllerClass) {
+    var controllerScope = this;
+    var Controller = baseControllerClass.extend({
 
-    function executeOrder() {
-      ordersService.$get($stateParams.uuid).then(
-        function(order) {
-          ordersService.executeOrder($stateParams.uuid).then(function() {
-            var array = order.customer.split('/').filter(function(el) {
-            return el.length !== 0;
-            }),
-            customerUUID = array[4];
-            $state.go('customers.plans', {uuid:customerUUID});
-          }, function(error) {
+      init: function() {
+        this._super();
+        this.executeOrder();
+      },
+      executeOrder: function() {
+        ordersService.$get($stateParams.uuid).then(
+          function(order) {
+            ordersService.executeOrder($stateParams.uuid).then(function() {
+              // TODO refactor this function to use named customer_uuid field
+              var array = order.customer.split('/').filter(function(el) {
+                  return el.length !== 0;
+                }),
+                customerUUID = array[4];
+              $state.go('customers.plans', {uuid:customerUUID});
+            }, function(error) {
+              alert(error.data.detail);
+            });
+          },
+          function(error) {
             alert(error.data.detail);
-          });
-        },
-        function(error) {
-          alert(error.data.detail);
-        }
-      );
-    }
+          }
+        );
+      }
+    });
 
-    executeOrder();
-
+    controllerScope.__proto__ = new Controller();
   }
 })();
