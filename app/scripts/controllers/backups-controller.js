11e8dc23c3d562b7cded70f93d5ba7613bd39e0c
Ron Balnen <ron.balnen@gmail.com>
2015-06-12 19:02:22 +0300

    Backup actions (SAAS-453)
    Added array of objects for BackupListController action buttons.
    Added delete and restore operations in backupsService.
    http://localhost:8001/#/backups/

--- app/scripts/controllers/backups-controller.js
+++ app/scripts/controllers/backups-controller.js
@@ -16,7 +16,26 @@
         this.controllerScope = controllerScope;
         this._super();
         this.searchFieldName = 'name';
-        this.actionButtonsListItems = [];
+        this.actionButtonsListItems = [
+          {
+            title: 'Restore',
+            clickFunction: this.restoreBackup.bind(this.controllerScope)
+          },
+          {
+            title: 'Remove',
+            clickFunction: this.deleteBackup.bind(this.controllerScope)
+          }
+        ];
+      },
+      deleteBackup:function(backup) {
+        var vm = this;
+        vm.service.deleteBackup(backup.uuid).then(
+          vm.getList, vm.handleActionException);
+      },
+      restoreBackup:function(backup) {
+        var vm = this;
+        vm.service.restoreBackup(backup.uuid).then(
+          vm.getList, vm.handleActionException);
       }
     });
 
