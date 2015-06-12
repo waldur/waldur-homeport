ce407da5a106cc7a8649893609c619a0a6049567
Ron Balnen <ron.balnen@gmail.com>
2015-06-12 17:34:25 +0300

    Backup create page (SAAS-451)
    Added state for backup create page. Created BackupAddController.
    Created view for backup create page. Implemented BackupAddController for
    backup create page view. Added getSuccessMessage in baseControllerAddClass
    for creating success message. Rewrote getSuccessMessage in BackupAddController
    for showing description field in flash message. Created test for
    backup created page.
    http://localhost:8001/#/backups/add/

---
+++ test/internal/backup-tests.js
@@ -0,0 +1,60 @@
+var auth = require('../helpers/auth.js'),
+  helpers = require('../helpers/helpers.js');
+
+/*jshint camelcase: false */
+var testData = [
+  {
+    user: auth.getUser('Walter'),
+    description: 'backup ' + helpers.getUUID(),
+    sourceName: 'resource#0',
+    customer: 'Ministry of Bells'
+  },
+  {
+    user: auth.getUser('Walter'),
+    description: 'backup ' + helpers.getUUID(),
+    sourceName: 'resource#1',
+    customer: 'Ministry of Bells'
+  }
+];
+
+for(var i = 0; i < testData.length; i++) {
+  var data = testData[i],
+    user = data.user;
+
+  (function(user, data, customer) {
+    describe('Key add test for customer owner(' + user.username + '):', function() {
+
+      it('I should be able to login', function() {
+        auth.login(user);
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/#/dashboard/');
+        helpers.chooseCustomer(customer);
+      });
+
+      it('I should be able go to backups list page', function() {
+        browser.get('/#/backups/');
+        expect(element(by.cssContainingText('h2.app-title', 'Backups')).isPresent()).toBe(true);
+      });
+
+      it('I should be able to create backup', function() {
+        browser.get('/#/backups/add/');
+        expect(element(by.cssContainingText('h2.app-title', 'Add backup')).isPresent()).toBe(true);
+        // fill name
+        element(by.model('BackupAdd.instance.description')).sendKeys(data.description);
+
+        element(by.model('searchStr')).sendKeys(data.sourceName);
+        element(by.cssContainingText('.angucomplete-row .angucomplete-title', data.sourceName)).click();
+
+        element(by.cssContainingText('a.button-apply', 'Add backup')).click();
+
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/#/backups/');
+      });
+
+      it('I should be able to logout', function() {
+        auth.logout();
+        expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '/#/');
+      });
+
+    });
+
+  })(user, data, data.customer);
+}
