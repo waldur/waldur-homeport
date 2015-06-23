a7cb077893ab09e3da14cd643b73efa6cbcd2eef
Michael Yakimenko <macgera@gmail.com>
2015-06-23 22:25:29 +0300

    Include directive help-icon to project and views.
    – SAAS-457

--- app/scripts/directives/help-icon.js
+++ app/scripts/directives/help-icon.js
@@ -2,9 +2,9 @@
 
 (function() {
   angular.module('ncsaas')
-    .directive('helpicon', ['ENV', helpicon]);
+    .directive('helpicon', ['$document', helpicon]);
 
-  function helpicon(ENV) {
+  function helpicon($document) {
     return {
       restrict: 'E',
       templateUrl: "views/directives/help-icon.html",
@@ -15,17 +15,14 @@
       link: function ($scope, element) {
         var trigger = element;
         var text = trigger.find('span');
-
         trigger.css('position', 'relative');
         trigger.bind('click', function(){
           if (!text.hasClass('active')) {
             text.addClass('active');
-            console.log(text[0].offsetHeight);
             text.css({
               'position': 'absolute',
               'top': -(text[0].offsetHeight + 4) + 'px',
               'left': '50%',
-              // 'margin-top': -text[0].offsetHeight/2 + 'px',
               'margin-left': -text[0].offsetWidth/2 + 'px'
             })
           } else {
