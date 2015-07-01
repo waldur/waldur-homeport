7a1e07abe354c3c5214321a64c1a100d55f78a88
Victor Mireyev <victor@opennodecloud.com>
2015-07-01 10:17:39 +0300

    Split resources list into tabs (SAAS-411)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -602,6 +602,9 @@ angular
           'appContent': {
             templateUrl: 'views/resource/list.html',
           },
+          'listTemplate@resources.list': {
+            templateUrl: 'views/resource/list-template.html',
+          },
           'appHeader': {
             templateUrl: 'views/partials/app-header.html',
           }
