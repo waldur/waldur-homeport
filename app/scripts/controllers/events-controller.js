a51d391ac95d95258e3ff3e0dfa1a44f20630344
Ron Balnen <ron.balnen@gmail.com>
2015-07-02 17:10:01 +0300

    Dashboard layout extensions (SAAS-482)
    Applied animation for tab switching in a nicer way

--- app/scripts/controllers/events-controller.js
+++ app/scripts/controllers/events-controller.js
@@ -99,16 +99,6 @@
               pointHighlightFill: "#fff",
               pointHighlightStroke: "rgba(220,220,220,1)",
               data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-              label: "Alerts",
-              fillColor: "rgba(151,187,205,0.2)",
-              strokeColor: "rgba(151,187,205,1)",
-              pointColor: "rgba(151,187,205,1)",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "rgba(151,187,205,1)",
-              data: [28, 48, 40, 19, 86, 27, 90]
             }
           ]
         };
@@ -124,16 +114,6 @@
               pointHighlightFill: "#fff",
               pointHighlightStroke: "rgba(220,220,220,1)",
               data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-              label: "Alerts",
-              fillColor: "rgba(151,187,205,0.2)",
-              strokeColor: "rgba(151,187,205,1)",
-              pointColor: "rgba(151,187,205,1)",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "rgba(151,187,205,1)",
-              data: [28, 48, 40, 19, 86, 27, 90]
             }
           ]
         };
