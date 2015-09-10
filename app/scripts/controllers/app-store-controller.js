492ca1e65fa0dc7b56de0a98d751cd629f2c1ff7
Victor Mireyev <victor@opennodecloud.com>
2015-09-10 19:04:20 +0300

    Display fields in order (SAAS-633).
    Allow to filter long list of choices by name.
    
    Toggle short and full display of choices.

--- app/scripts/controllers/app-store-controller.js
+++ app/scripts/controllers/app-store-controller.js
@@ -30,8 +30,6 @@
     var Controller = baseControllerAddClass.extend({
       UNIQUE_FIELDS: {
         service_project_link: 'service_project_link',
-        ssh_public_key: 'ssh_public_key',
-        configuration: 'configuration'
       },
       FIELD_TYPES: {
         string: 'string',
@@ -45,7 +43,6 @@
       thirdStep: false,
       resourceTypesBlock: false,
 
-      activeTab: null,
       successMessage: 'Purchase of {vm_name} was successful.',
       formOptions: {},
       allFormOptions: {},
@@ -70,6 +67,9 @@
       total: 0,
       defaultPriceListItems: [],
       priceItems: [],
+
+      fields: [],
+      limitChoices: 9,
 
       init:function() {
         this.service = servicesService;
@@ -103,7 +103,7 @@
         this.selectedServiceName = service;
         this.resourceTypesBlock = true;
         this.thirdStep = false;
-        this.formOptions = {};
+        this.fields = [];
         if (this.selectedService) {
           var types = Object.keys(this.selectedService.resources);
           if (types.length === 1) {
@@ -119,11 +119,11 @@
         var vm = this;
         vm.selectedResourceType = type;
         vm.thirdStep = true;
-        vm.formOptions = {};
+        vm.fields = [];
         if (vm.selectedService.resources[vm.selectedResourceType]) {
           vm.instance = servicesService.$create(vm.selectedService.resources[vm.selectedResourceType]);
           servicesService.getOption(vm.selectedService.resources[vm.selectedResourceType]).then(function(response) {
-            vm.setFormOptions(response.actions.POST);
+            vm.setFields(response.actions.POST);
             vm.setServiceProjectLink(response.actions.POST);
           });
         }
@@ -140,22 +140,44 @@
         }
         this.instance[this.UNIQUE_FIELDS.service_project_link] = link;
       },
-      setFormOptions: function(formOptions) {
+      setFields: function(formOptions) {
         this.allFormOptions = formOptions;
-        this.activeTab = null;
         for (var name in formOptions) {
-          if (!formOptions[name].read_only && name != this.UNIQUE_FIELDS.service_project_link) {
-            this.formOptions[formOptions[name].type] = this.formOptions[formOptions[name].type] || {};
-            if (name == this.UNIQUE_FIELDS[name]) {
-              this.formOptions[name] = formOptions[name];
-            } else {
-              if (!this.activeTab && formOptions[name].type == this.FIELD_TYPES.field) {
-                this.activeTab = name;
-              }
-              this.formOptions[formOptions[name].type][name] = formOptions[name];
-            }
+          if (formOptions[name].read_only || name == this.UNIQUE_FIELDS.service_project_link) {
+            continue;
           }
+
+          var type = formOptions[name].type;
+          if (type == 'field') {
+            type = 'choice';
+          }
+
+          var required = formOptions[name].required;
+          var choices = formOptions[name].choices;
+
+          this.fields.push({
+            name: name,
+            label: formOptions[name].label,
+            type: type,
+            help_text: formOptions[name].help_text,
+            required: required,
+            choices: choices
+          });
         }
+
+      },
+      toggleChoicesLimit: function(field) {
+        if (field.limit == this.limitChoices) {
+          field.limit = field.choices.length;
+        } else {
+          field.limit = this.limitChoices;
+        }
+      },
+      isListLong: function(field) {
+        return field.choices.length > this.limitChoices;
+      },
+      isListExpanded: function(field) {
+        return field.limit == field.choices.length;
       },
       doChoice: function(name, choice) {
         var vm = this;
@@ -224,7 +246,7 @@
         vm.selectedService = {};
         vm.selectedServiceName = null;
         vm.resourceTypesBlock = false;
-        vm.formOptions = null;
+        vm.fields = [];
         vm.priceItems = [];
         vm.selectedResourceType = null;
         vm.instance = null;
