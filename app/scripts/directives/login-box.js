bd01cac9e5e8ed37e47504ef184a6faabd8c4bb4
Ron Balnen <ron.balnen@gmail.com>
2015-09-16 20:51:27 +0300

    Login form position for opened mobile keyboard (SAAS-714)

--- app/scripts/directives/login-box.js
+++ app/scripts/directives/login-box.js
@@ -8,7 +8,7 @@
         restrict: 'A',
         link: function(scope, element, attrs) {
           var elHeight = element[0].offsetHeight;
-          element.css('margin-top', -elHeight / 2 + 'px');
+          element.css('margin-top', -elHeight / 2 + 45 + 'px');
         }
       };
     });
