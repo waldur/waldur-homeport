ac5173823cf50bad467e801513f0bbb4b9c9d0d5
Ron Balnen <ron.balnen@gmail.com>
2015-10-08 16:36:56 +0300

    Implemented check-quotas directive in remaining places (SAAS-736)
    Fixed tooltip position, buttons style and status enabled/disabled

--- app/scripts/directives/check-quotas.js
+++ app/scripts/directives/check-quotas.js
@@ -2,9 +2,9 @@
 
 (function() {
   angular.module('ncsaas')
-    .directive('checkQuotas', ['currentStateService', '$state', checkQuotas]);
+    .directive('checkQuotas', ['currentStateService', '$state', '$rootScope', checkQuotas]);
 
-  function checkQuotas(currentStateService, $state) {
+  function checkQuotas(currentStateService, $state, $rootScope) {
     var directive = {
       restrict: 'A',
       replace: true,
@@ -12,7 +12,6 @@
       templateUrl: "views/directives/check-quotas.html",
       scope: {
         checkQuotas: '@',
-        showMessage: '=',
         tooltipType: '@',
         sref: '@',
         classLink: '@'
@@ -21,64 +20,109 @@
       link: checkQuotasLink
     };
 
+    /**
+     * Link function
+     * @param scope
+     * @param element
+     */
     function checkQuotasLink(scope, element) {
-
-      var refresh = function() {
-        // checkQuotas - quota type: resource, project, service, user
-        // tooltipType: listItems, emptyListItems, addUser
-        // showMessage: boolean
-        var item = scope.checkQuotas,
-          tooltipType = scope.tooltipType,
-          triangleBefore,
-          triangleAfter;
-
-        currentStateService.getCustomer().then(function(response) {
-
-          triangleAfter = triangleBefore = Math.floor(element[0].offsetWidth / 2) + 1;
-
-          scope.triangleBefore = {right: triangleBefore};
-          scope.triangleAfter = {right: triangleAfter};
-
-          if (tooltipType === 'userDropDown') {
-            scope.tooltipStyle = {top: '75px'};
-          }
-          if (tooltipType === 'projectDropDown') {
-            scope.tooltipStyle = {top: '110px'};
-          }
-          if (tooltipType === 'addUser') {
-            scope.triangleBefore = {left: '20px'};
-            scope.triangleAfter = {left: '21px'};
-            scope.tooltipStyle = {top: '115%', left:0, width: '350px'};
-          }
-
-          scope.tooltipType = tooltipType;
-          scope.plansLink = 'organizations.plans({uuid:\'' + response.uuid + '\'})';
-          scope.enable = false;
-
-          for (var i = 0; i<response.quotas.length; i++) {
-            var value = response.quotas[i];
-            value.name = value.name.replace(/nc_|_count/gi,'');
-            if (item && value.name === item && value.limit > -1 && (value.limit === value.usage || value.limit === 0)) {
-              scope.enable = true;
-              scope.showMessage = false;
-              break;
-            }
-          }
-
-          scope.classes['disabled-view'] = scope.enable;
-
-        });
-      };
-
-      refresh();
+      refresh(scope, element);
       scope.$on('checkQuotas:refresh', function() {
-        refresh();
+        refresh(scope, element);
       });
     }
 
-    checkQuotasController.$inject = ['$scope'];
-    function checkQuotasController($scope) {
+    /**
+     * Update disable state for directive element
+     * @param scope
+     * @param element
+     */
+    function refresh(scope, element) {
+      // checkQuotas - quota type: resource, project, service, user
+      // tooltipType: listItems, emptyListItems, addUser
+      var item = scope.checkQuotas;
 
+      currentStateService.getCustomer().then(function(response) {
+
+        var style = calculatePosition(element, scope.tooltipType);
+
+        scope.tooltipStyle = style.tooltipStyle;
+        scope.triangleBefore = style.triangleBefore;
+        scope.triangleAfter = style.triangleAfter;
+        scope.plansLink = 'organizations.plans({uuid:\'' + response.uuid + '\'})';
+        scope.enable = false;
+
+        for (var i = 0; i<response.quotas.length; i++) {
+          var value = response.quotas[i];
+          value.name = value.name.replace(/nc_|_count/gi,'');
+          if (item && value.name === item && value.limit > -1 && (value.limit === value.usage || value.limit === 0)) {
+            scope.enable = true;
+            scope.showMessage = false;
+            break;
+          }
+        }
+
+        scope.classes.disabled = scope.enable;
+
+      });
+    }
+
+    /**
+     * Calculate position style for tooltip
+     * @param element
+     * @param tooltipType
+     * @returns {{triangleBefore: ({right: string}|*), triangleAfter: ({right: string}|*), tooltipStyle: *}}
+     */
+    function calculatePosition(element, tooltipType) {
+      var triangleBefore,
+          triangleAfter,
+          tooltipStyle;
+
+      triangleAfter = triangleBefore = Math.floor(element[0].offsetWidth / 2) || 100;
+
+      triangleBefore = {right: triangleBefore + 'px'};
+      triangleAfter = {right: triangleAfter + 1 + 'px'};
+
+      switch (tooltipType) {
+        case 'listItems': {
+          tooltipStyle = {top: '35px'};
+          break;
+        }
+        case 'headNavAppstore': {
+          tooltipStyle = {top: '50px'};
+          break;
+        }
+        case 'userDropDown': {
+          tooltipStyle = {top: '44px'};
+          break;
+        }
+        case 'projectDropDown': {
+          tooltipStyle = {top: '10px', right: '-220px'};
+          break;
+        }
+        case 'addUser': {
+          tooltipStyle = {top: '110%', left: 0, width: '350px'};
+          triangleBefore = {left: '20px'};
+          triangleAfter = {left: '21px'};
+          break;
+        }
+      }
+
+      return {
+          triangleBefore: triangleBefore,
+          triangleAfter: triangleAfter,
+          tooltipStyle : tooltipStyle
+      };
+    }
+
+    checkQuotasController.$inject = ['$scope'];
+
+    /**
+     * Controller function
+     * @param $scope
+     */
+    function checkQuotasController($scope) {
+      $scope.showMessage = false;
       $scope.classes = {};
 
       var classes;
@@ -88,16 +132,25 @@
         });
       }
 
+      $scope.$on('checkQuotas:closeAll', function() {
+        $scope.closeTooltip();
+      });
+
       $scope.closeTooltip = function() {
         $scope.showMessage = false;
       };
 
+      $scope.openTooltip = function() {
+        $scope.showMessage = true;
+      };
+
       $scope.handleAction = function(e) {
         if ($scope.enable) {
-          $scope.showMessage = true;
+          $rootScope.$broadcast('checkQuotas:closeAll');
+          $scope.openTooltip();
           e.preventDefault();
           e.stopPropagation();
-        } else {
+        } else if ($scope.sref) {
           $state.go($scope.sref);
         }
       };
