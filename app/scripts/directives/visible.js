2a1fe1a64b38877a7f42a6a4e239f70b47c3bb23
Ron Balnen <ron.balnen@gmail.com>
2015-10-09 12:59:40 +0300

    When I click on my resource in project menu I got 404 error (SAAS-747)
    Disabled tabs by resource types. Added resourcesTypes variable to
    base-config. Set 'alerts' tab by default for project details page.

--- app/scripts/directives/visible.js
+++ app/scripts/directives/visible.js
@@ -13,7 +13,8 @@
         var doRemove = false,
           features = attrs.visible.split(',');
         for (var i = 0; i < features.length; i++) {
-          if (ENV.toBeFeatures.indexOf(features[i].trim()) !== -1) {
+          if (ENV.toBeFeatures.indexOf(features[i].trim()) !== -1
+              || (ENV.toBeFeatures.indexOf('resources') !== -1 && features[i].trim()) in ENV.resourcesTypes) {
             doRemove = true;
             break;
           }
