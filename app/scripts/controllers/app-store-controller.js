dc824feab0aa1526f3a7d034ce7aefff43a77b64
Victor Mireyev <victor@opennodecloud.com>
2015-10-28 15:29:27 +0300

    In AppStore extend list of safe states for service (SAAS-877)

--- app/scripts/controllers/app-store-controller.js
+++ app/scripts/controllers/app-store-controller.js
@@ -495,6 +495,7 @@
         vm.loadingProviders = true;
         var myBlockUI = blockUI.instances.get('store-content');
         myBlockUI.start();
+        var safeStates = ['Sync Scheduled', 'Syncing', 'In Sync'];
         currentStateService.getProject().then(function(response) {
           vm.currentProject = response;
           for (var j = 0; j < categories.length; j++) {
@@ -502,7 +503,7 @@
             vm.categoryServices[category.name] = [];
             for (var i = 0; i < vm.currentProject.services.length; i++) {
               var service = vm.currentProject.services[i];
-              if (service.state == 'In Sync'
+              if (safeStates.indexOf(service.state) != -1
                 && category.services
                 && (category.services.indexOf(service.type) + 1)
               ) {
