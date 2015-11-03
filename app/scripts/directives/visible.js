38cee4d841ac977965a3139209b1abcaa05cc4e7
Ron Balnen <ron.balnen@gmail.com>
2015-11-03 12:45:17 +0200

    Display resource costs in cost mode (SAAS-867)
    Modified 'visible' directive for modes excluding.
    Implemented excluding for costs tab in dashboard page.

--- app/scripts/directives/visible.js
+++ app/scripts/directives/visible.js
@@ -8,13 +8,16 @@
   function visible(ENV) {
     return {
       restrict: 'A',
-      scope: {},
+      scope: {
+        excludeMode: "@"
+      },
       link: function(scope, element, attrs) {
         var doRemove = false,
           features = attrs.visible.split(',');
         for (var i = 0; i < features.length; i++) {
-          if (ENV.toBeFeatures.indexOf(features[i].trim()) !== -1
-              || (ENV.toBeFeatures.indexOf('resources') !== -1 && features[i].trim()) in ENV.resourcesTypes) {
+          if ((ENV.toBeFeatures.indexOf(features[i].trim()) !== -1
+              || (ENV.toBeFeatures.indexOf('resources') !== -1 && features[i].trim()) in ENV.resourcesTypes)
+              && ENV.modeName !== attrs.excludeMode) {
             doRemove = true;
             break;
           }
