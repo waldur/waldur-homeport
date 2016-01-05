240a78eb28a92031bcf455756d0629250989cc2b
Ron Balnen <ron.balnen@gmail.com>
2016-01-05 15:43:53 +0200

    Calculate resource price in AppStore at frontend (SAAS-968)
    Added check for adjustSearchFilters method existence in list-cache-reset
    directive. Added name prefixes for AWS list. Added sorting for summary menu.
    Modified total block in summary menu to show price per hour.

--- app/scripts/controllers/app-store-controller.js
+++ app/scripts/controllers/app-store-controller.js
@@ -85,6 +85,7 @@
 
       fields: [],
       limitChoices: 10,
+      fieldsOrder: null,
 
       init:function() {
         this.service = resourcesService;
@@ -336,15 +337,19 @@
           });
           display_label = null;
         }
-        var order = [
+        this.fieldsOrder = [
           'name', 'image', 'region', 'size', 'flavor', 'system_volume_size', 'data_volume_size',
           'security_groups', 'ssh_public_key', 'description', 'user_data'
         ];
-        this.fields.sort(function(a, b) {
-          return order.indexOf(a.name) - order.indexOf(b.name);
-        });
+        this.fields.sort(this.fieldsComparator.bind(this));
         this.sortFlavors();
         this.attachIconsToImages();
+      },
+      fieldsComparator: function(a, b) {
+        return this.fieldsOrder.indexOf(a.name) - this.fieldsOrder.indexOf(b.name);
+      },
+      cartComparator: function(a, b) {
+        return this.fieldsOrder.indexOf(a.type) - this.fieldsOrder.indexOf(b.type);
       },
       attachIconsToImages: function() {
         var field = this.findFieldByName('image');
@@ -515,7 +520,7 @@
         var display_name = choice.display_name;
         var price = this.findPrice(name, display_name);
         price = price ? price : 0;
-        display_name = choice.item.description || choice.display_name;
+        display_name = choice.display_name;
         this.pushPriceItem(name, display_name, price);
       },
       findPrice: function(name, display_name) {
@@ -537,6 +542,7 @@
           name: name,
           price: price
         });
+        this.priceItems.sort(this.cartComparator.bind(this));
         this.countTotal();
       },
       deletePriceItem: function(name) {
