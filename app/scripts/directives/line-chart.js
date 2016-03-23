64be92355c67c19f4f6f51f5cedf92f058c788a2
Ron Balnen <ron.balnen@gmail.com>
2016-03-23 21:27:21 +0200

    Change y axis (SAAS-1194)
    Changed valuer range.
    Added points for line.
    Showed flashes for points by hover.

--- app/scripts/directives/line-chart.js
+++ app/scripts/directives/line-chart.js
@@ -28,13 +28,14 @@
 
         var data = scope.data.y;
         var max = d3.max(data, function(d) { return d; });
+        var min = d3.min(data, function(d) { return d; });
 
         var x = d3.time.scale().range([0, w]);
         x.domain(d3.extent(data, function(d) { return d; }));
 
         x = d3.scale.linear().domain([0, data.length - 1]).range([0, w]);
 
-        var y = d3.scale.linear().domain([0, max]).range([h, 0]);
+        var y = d3.scale.linear().domain([min, max]).range([h, 0]);
         var line = d3.svg.line()
           .x(function(d,i) {
             return x(i);
@@ -66,13 +67,48 @@
           .attr("transform", "rotate(-65)" );
 
 
-        var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+        var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left").tickFormat(d3.format("d"));;
         graph.append("svg:g")
           .attr("class", "y axis")
           .attr("transform", "translate(-25,0)")
           .call(yAxisLeft);
 
-        graph.append("svg:path").attr("d", line(data));
+        graph
+          .append('svg:g')
+          .attr("class", "line");
+
+        var pathContainers = graph.selectAll('g.line');
+
+        pathContainers
+          .append("svg:path")
+          .attr("d", line(data));
+
+        scope.data.y.forEach(function(yValue, xValue) {
+          pathContainers
+            .append('circle')
+            .attr('cx', function () { return x(xValue); })
+            .attr('cy', function () { return y(yValue); })
+            .attr('r', 3)
+            .attr('value', scope.data.x[xValue] + ': ' + yValue)
+            .on("mouseenter", flash);
+        });
+
+        function flash() {
+          var name = d3.event.toElement.getAttribute('value');
+          var translate = d3.mouse(this);
+          translate[1] = translate[1] > 0 ? translate[1] : 0;
+          graph
+            .selectAll('text.flash-chart')
+            .remove();
+          graph.append("text")
+            .attr("class", 'flash-chart')
+            .attr("transform", "translate(" + translate + ")")
+            .text(name)
+            .transition()
+            .duration(1500)
+            .style("opacity", 0)
+            .remove();
+        }
       }
     }
   }
