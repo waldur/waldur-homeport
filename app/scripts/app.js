1783f58fc9d67be83cd87d5d0f843eda26159343
Victor Mireyev <victor@opennodecloud.com>
2016-08-26 22:26:51 +0300

    Fix tests.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -868,7 +868,7 @@
 
         if ($auth.isAuthenticated()) {
           // can't use $state because its will throw recursion error
-          $location.path('/login/');
+          $location.path('/dashboard/');
         } else {
           deferred.resolve();
         }
