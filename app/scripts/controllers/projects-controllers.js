d020a58940ee42d2ca5b82324efe8daff406d1ec
Victor Mireyev <victor@opennodecloud.com>
2016-09-12 17:15:18 +0300

    Refresh customer and project workspace after user has been authenticated.

--- app/scripts/controllers/projects-controllers.js
+++ app/scripts/controllers/projects-controllers.js
@@ -452,6 +452,9 @@
       $scope.$on('currentProjectUpdated', function() {
         refreshProject();
       });
+      $scope.$on('authService:signin', function() {
+        refreshProject();
+      });
       refreshProject();
     }
 
