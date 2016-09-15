702172dc61c0530ab026363a8d63bc1d62a708ce
Victor Mireyev <victor@opennodecloud.com>
2016-09-15 21:25:56 +0300

    Separate table directive from data providers. Add export buttons (SAAS-1411)

--- app/scripts/directives/responsive-table.js
+++ app/scripts/directives/responsive-table.js
@@ -4,44 +4,74 @@
 
   angular.module('ncsaas').directive('responsiveTable', responsiveTable);
 
-  responsiveTable.$inject = ['$http', 'ENV'];
-
-  function responsiveTable($http, ENV) {
+  function responsiveTable($timeout) {
     return {
-      restrict: 'C',
+      restrict: 'E',
+      scope: {
+        controller: '=tableCtrl'
+      },
+      template: '<table class="table table-striped table-bordered table-hover"/>',
       link: function(scope, element) {
-        $(element[0]).DataTable({
-          "responsive": true,
-          "processing": true,
-          "serverSide": true,
-          "ajax": serverDataTableCallback
+        $(element.find('table')[0]).DataTable({
+          responsive: true,
+          processing: true,
+          serverSide: true,
+          ajax: serverDataTableCallback,
+          dom: '<"html5buttons"B>lTfgitp',
+          buttons: [
+            {extend: 'copy'},
+            {extend: 'csv'},
+            {extend: 'excel'},
+            {extend: 'pdf'}
+          ],
+          columns: scope.controller.columns
         });
 
         function serverDataTableCallback(request, drawCallback, settings) {
-          console.log(request)
-          settings.jqXHR = $.ajax({
-            "dataType": "json",
-            "headers": {
-              "Authorization": $http.defaults.headers.common.Authorization
-            },
-            "data": {
-              "page_size": request.length,
-            }
-            "url": ENV.apiEndpoint + 'api/projects/',
-            "success": function(json) {
-              console.log(json)
-              drawCallback({
-                "draw": 1,
-                "recordsTotal": 57,
-                "recordsFiltered": 57,
-                "data": json.map(function(item) {
-                  return [item.uuid, item.name, item.customer_name];
-                })
-              })
-            }
+          var filter = {};
+          if (request.search.value) {
+            filter[scope.controller.searchFieldName] = request.search.value;
+          }
+          var service = scope.controller.service;
+          service.pageSize = request.length;
+          service.page = Math.ceil(request.start / request.length) + 1;
+          service.getList(filter).then(function(list) {
+            scope.controller.list = list;
+            var total = service.resultCount;
+            drawCallback({
+              draw: request.draw,
+              recordsTotal: total,
+              recordsFiltered: total,
+              data: list
+            });
           });
         }
       }
     };
+  }
+
+  angular.module('ncsaas').controller('ProjectListController', ProjectListController);
+
+  ProjectListController.$inject = ['$state', 'projectsService'];
+
+  function ProjectListController($state, projectsService) {
+    this.service = projectsService;
+    this.columns = [
+      {
+        data: 'uuid',
+        title: 'ID'
+      },
+      {
+        data: 'name',
+        title: 'Project name'
+      },
+      {
+        title: 'Customer name',
+        render: function(data, type, row, meta) {
+          var href = $state.href('organization.details', {uuid: row.customer_uuid});
+          return '<a href="' + href + '">' + row.customer_name + '</a>';
+        }
+      }
+    ];
   }
 })();
