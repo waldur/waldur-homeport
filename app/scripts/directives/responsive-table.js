b33a3ba2b12c1a996ffa7f3b363870496e355c20
Victor Mireyev <victor@opennodecloud.com>
2016-09-16 14:14:01 +0300

    Refactor table directive (SAAS-1411)

--- app/scripts/directives/responsive-table.js
+++ app/scripts/directives/responsive-table.js
@@ -12,14 +12,62 @@
       },
       template: '<table class="table table-striped table-bordered table-hover"/>',
       link: function(scope, element) {
-        var rowActions = {
-          title: 'Actions',
-          orderable: false,
-          render: function(data, type, row, meta) {
-            return scope.controller.rowActions.map(function(action, index) {
-              return '<button class="btn btn-default" row-index="' + meta.row + '" action-index="' + index + '">' + action.name + '</button>';
-            }).join('');
-          }
+        var rowButtons = getRowButtons(scope.controller.rowActions);
+
+        var exportButtons = getExportButtons(
+          scope.controller.columns.length,
+          ['copyHtml5', 'csvHtml5', 'excelHtml5', 'pdfHtml5', 'print']
+        );
+
+        var table = $(element.find('table')[0]).DataTable({
+          responsive: true,
+          processing: true,
+          serverSide: true,
+          ajax: serverDataTableCallback,
+          dom: '<"html5buttons"B>lTfgitp',
+          buttons: exportButtons,
+          columns: scope.controller.columns.concat([rowButtons])
+        });
+
+        connectRowButtons(table, scope.controller);
+
+        function connectRowButtons(table, scope) {
+          table.on('click', 'button', function(event) {
+            $(this).blur();
+            var rowIndex = parseInt($(event.target).attr('row-index'));
+            var actionIndex = parseInt($(event.target).attr('action-index'));
+            var action = scope.rowActions[actionIndex];
+            var row = scope.list[rowIndex];
+            $timeout(function() {
+              action.callback.apply(scope, [row]);
+            });
+          });
+        }
+
+        function getRowButtons(spec) {
+          return {
+            title: 'Actions',
+            orderable: false,
+            render: function(data, type, row, meta) {
+              return spec.map(function(action, index) {
+                return '<button class="btn btn-default" row-index="' + meta.row + '" action-index="' + index + '">' + action.name + '</button>';
+              }).join('');
+            }
+          };
+        }
+
+        function getExportButtons(columnsCount, formats) {
+          var title = document.title + ' - ' + moment().format('YYYY-MM-DD');
+          var exportOptions = {
+            columns: range(columnsCount)
+          };
+          return formats.map(function(format) {
+            return {
+              extend: format,
+              exportOptions: exportOptions,
+              title: title
+            };
+          });
         }
 
         function range(n) {
@@ -29,52 +77,6 @@
           }
           return x;
         }
-
-        var exportOptions = {
-          columns: range(scope.controller.columns.length)
-        };
-
-        var table = $(element.find('table')[0]).DataTable({
-          responsive: true,
-          processing: true,
-          serverSide: true,
-          ajax: serverDataTableCallback,
-          dom: '<"html5buttons"B>lTfgitp',
-          buttons: [
-            {
-              extend: 'copyHtml5',
-              exportOptions: exportOptions
-            },
-            {
-              extend: 'csvHtml5',
-              exportOptions: exportOptions
-            },
-            {
-              extend: 'excelHtml5',
-              exportOptions: exportOptions
-            },
-            {
-              extend: 'pdfHtml5',
-              exportOptions: exportOptions
-            },
-            {
-              extend: 'print',
-              exportOptions: exportOptions
-            }
-          ],
-          columns: scope.controller.columns.concat([rowActions])
-        });
-
-        table.on('click', 'button', function(event) {
-          $(this).blur();
-          var rowIndex = $(event.target).attr('row-index');
-          var actionIndex = $(event.target).attr('action-index');
-          var action = scope.controller.rowActions[actionIndex];
-          var row = scope.controller.list[rowIndex];
-          $timeout(function() {
-            action.callback.apply(scope.controller, [row]);
-          });
-        });
 
         function serverDataTableCallback(request, drawCallback, settings) {
           var filter = {};
