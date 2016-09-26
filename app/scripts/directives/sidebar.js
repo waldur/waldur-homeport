a40b54fbf02fb55eae0fd3c522184084399a56ec
Victor Mireyev <victor@opennodecloud.com>
2016-09-26 13:08:15 +0300

    Make expanded/collapsed menu behaviour consistent (SAAS-1423)

--- app/scripts/directives/sidebar.js
+++ app/scripts/directives/sidebar.js
@@ -13,8 +13,24 @@
         context: '='
       },
       templateUrl: 'views/directives/sidebar.html',
-      link: function(scope) {
-        scope.$state = $state;
+      link: function(scope, element, attrs) {
+        scope.onMenuClick = function(event, item) {
+          if (item.children) {
+            item.expanded = !item.expanded;
+            event.preventDefault();
+          }
+        }
+        function syncMenu() {
+          angular.forEach(scope.items, function(item) {
+            item.expanded = $state.includes(item.link);
+          });
+        }
+        scope.$on('$stateChangeSuccess', function() {
+          syncMenu();
+        });
+        attrs.$observe('items', function() {
+          syncMenu();
+        });
       }
     };
   }
