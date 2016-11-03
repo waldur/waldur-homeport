13eca26e8c021f73ae2558bcc98d07b45fbd84c8
Balnen <ron.balnen@gmail.com>
2016-11-03 19:50:36 +0200

    Volume detail issues fixed (WAL-116)
     enter the commit message for your changes. Lines starting

--- app/scripts/controllers/resources-controllers.js
+++ app/scripts/controllers/resources-controllers.js
@@ -476,7 +476,7 @@
         controllerScope.getModel().then(function(model) {
           controllerScope.model = model;
         }, function(error) {
-          if (error.status == 404) {
+          if (error.status === 404) {
             ncUtilsFlash.error('Resource is gone.');
             this.modelNotFound();
           }
@@ -484,6 +484,7 @@
       },
 
       afterActivate: function() {
+        this.viewHeaderLabel = this.model.resource_type.split('.').join(' ');
         this.updateMenu();
         this.setCounters();
         this.updateResourceTab();
