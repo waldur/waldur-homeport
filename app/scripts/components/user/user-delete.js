aa13631dea6197c554bc07a50fea393dbc2b3768
Balnen <ron.balnen@gmail.com>
2016-11-06 13:03:20 +0200

    Added user data to user manage tab (WAL-104)
    User data is now editable.

--- app/scripts/components/user/user-delete.js
+++ app/scripts/components/user/user-delete.js
@@ -10,12 +10,41 @@ export default function userDelete() {
 }
 
 // @ngInject
-function UserDeleteController(baseControllerClass, $state) {
+function UserDeleteController(baseControllerClass, usersService, $q, $state) {
   var controllerScope = this;
   var DeleteController = baseControllerClass.extend({
     init: function() {
       this.controllerScope = controllerScope;
       this._super();
+      this.getUser();
+    },
+    saveUser: function() {
+      var vm = this;
+      var i;
+      var requiredFields = {
+        email: 'Email is required',
+        full_name: 'Full name is required'
+      };
+      vm.user.errors = {};
+      for (i in requiredFields) {
+        if (requiredFields.hasOwnProperty(i) && !vm.user[i]) {
+          vm.user.errors[i] = requiredFields[i];
+        }
+      }
+      if (Object.keys(vm.user.errors).length !== 0) {
+        return $q.reject();
+      }
+      return vm.user.$update(function() {
+        usersService.currentUser = null;
+      }, function(response) {
+        vm.user.errors = response.data;
+      });
+    },
+    getUser: function() {
+      var vm = this;
+      usersService.getCurrentUser().then(function(response) {
+        vm.user = response;
+      });
     },
     removeUser: function () {
       $state.go('support.create', {type: 'remove_user'});
