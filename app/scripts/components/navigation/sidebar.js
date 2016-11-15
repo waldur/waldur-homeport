640bf474faf833430a0fc93b46ba4fea29c414e1
Victor Mireyev <victor@opennodecloud.com>
2016-11-15 15:50:27 +0300

    Move sidebar to navigation module (WAL-174)

---
+++ app/scripts/components/navigation/sidebar.js
@@ -0,0 +1,43 @@
+import template from './sidebar.html';
+
+export default function sidebar() {
+  return {
+    restrict: 'E',
+    scope: {
+      items: '=',
+      context: '='
+    },
+    template: template,
+    controller: SidebarController,
+    controllerAs: '$ctrl',
+    bindToController: true
+  };
+}
+
+// @ngInject
+class SidebarController {
+  constructor(ENV, $state, $scope) {
+    this.brandName = ENV.shortPageTitle;
+    this.$state = $state;
+    $scope.$on('$stateChangeSuccess', this.syncMenu.bind(this));
+    $scope.$watch('items', this.syncMenu.bind(this));
+  }
+
+  onMenuClick(event, item) {
+    if (item.children) {
+      item.expanded = !item.expanded;
+      event.preventDefault();
+    }
+  }
+
+  syncMenu() {
+    var data = this.$state.$current.data;
+    this.items.map(item => {
+      if (data && data.sidebarState) {
+        item.expanded = item.link === data.sidebarState;
+        return;
+      }
+      item.expanded = this.$state.includes(item.link);
+    });
+  }
+}
