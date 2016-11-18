a0ad26cd12161872a9c5183ea799c2b0b83f3924
Victor Mireyev <victor@opennodecloud.com>
2016-11-18 16:40:21 +0300

    Add daily, monthly and annual price estimates for OpenStack packages (WAL-171)

--- app/scripts/components/form/openstack-tenant-template-field.js
+++ app/scripts/components/form/openstack-tenant-template-field.js
@@ -14,36 +14,19 @@ export default function openstackTenantTemplateField() {
   }
 }
 
-function parsePackages(choices) {
-  /* Output is list of items which have following format:
-  {
-    "url": "https://example.com/api/package-templates/2/",
-    "name": "Minimal VPC package",
-    "price": 300,
-    "ram": 20240,
-    "cores": 20,
-    "storage": 51200,
-  }
-  */
-  return choices.map(choice => {
-    const components = choice.item.components.reduce((map, item) => {
-      map[item.type] = item.amount;
-      return map;
-    }, {});
-    return angular.extend({
-      url: choice.item.url,
-      name: choice.item.name,
-      description: choice.item.description,
-      price: choice.item.price,
-    }, components);
-  });
-}
-
 // @ngInject
 class FieldController {
   constructor($uibModal) {
     this.$uibModal = $uibModal;
-    this.packages = parsePackages(this.field.choices);
+    this.packages = this.field.choices.map(parsePackage);
+  }
+
+  isMissing() {
+    return this.field.required && !this.hasChoices();
+  }
+
+  hasChoices() {
+    return this.packages.length > 0;
   }
 
   getLabel() {
@@ -59,6 +42,7 @@ class FieldController {
     const vm = this;
     this.$uibModal.open({
       component: 'openstackTenantTemplateDialog',
+      size: 'lg',
       resolve: {
         field: () => vm.field,
         model: () => vm.model,
@@ -66,4 +50,36 @@ class FieldController {
       }
     });
   }
+}
+
+function parsePackage(choice) {
+  /* Output is list of items which have following format:
+  {
+    "url": "https://example.com/api/package-templates/2/",
+    "name": "Minimal VPC package",
+    "price": {
+      "day": 1,
+      "month": 30,
+      "year": 365
+    },
+    "ram": 20240,
+    "cores": 20,
+    "storage": 51200,
+  }
+  */
+  const components = choice.item.components.reduce((map, item) => {
+    map[item.type] = item.amount;
+    return map;
+  }, {});
+  var dailyPrice = choice.item.price * 24;
+  return angular.extend({
+    url: choice.item.url,
+    name: choice.item.name,
+    description: choice.item.description,
+    price: {
+      day: dailyPrice,
+      month: dailyPrice * 30,
+      year: dailyPrice * 365,
+    }
+  }, components);
 }
