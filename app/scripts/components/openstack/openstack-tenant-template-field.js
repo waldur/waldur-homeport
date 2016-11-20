d4675048fee897e1546880ebd65ac5c6eb6e4d27
Victor Mireyev <victor@opennodecloud.com>
2016-11-20 18:47:41 +0300

    Extract utils (WAL-171)

--- app/scripts/components/openstack/openstack-tenant-template-field.js
+++ app/scripts/components/openstack/openstack-tenant-template-field.js
@@ -1,4 +1,5 @@
 import template from './openstack-tenant-template-field.html';
+import { formatPackageDetails, parsePackage } from './utils';
 
 export default function openstackTenantTemplateField() {
   return {
@@ -22,7 +23,7 @@ class FieldController {
     this.packages = this.field.choices.map(parsePackage);
   }
 
-  isMissing() {
+  renderWarning() {
     return this.field.required && !this.hasChoices();
   }
 
@@ -50,43 +51,4 @@ class FieldController {
     }
     return formatPackageDetails(this.$filter, value);
   }
-}
-
-function formatPackageDetails($filter, value) {
-  const ram = $filter('filesize')(value.ram);
-  const storage = $filter('filesize')(value.storage);
-  const props = `${value.cores} vCPU, ${ram} RAM, ${storage} storage`;
-  return `${value.name} (${props})`;
-}
-
-function parsePackage(choice) {
-  /* Output is item with the following format:
-  {
-    "url": "https://example.com/api/package-templates/2/",
-    "name": "Minimal VPC package",
-    "price": {
-      "day": 1,
-      "month": 30,
-      "year": 365
-    },
-    "ram": 20240,
-    "cores": 20,
-    "storage": 51200,
-  }
-  */
-  const components = choice.item.components.reduce((map, item) => {
-    map[item.type] = item.amount;
-    return map;
-  }, {});
-  var dailyPrice = choice.item.price * 24;
-  return angular.extend({
-    url: choice.item.url,
-    name: choice.item.name,
-    description: choice.item.description,
-    price: {
-      day: dailyPrice,
-      month: dailyPrice * 30,
-      year: dailyPrice * 365,
-    }
-  }, components);
 }
---
+++ app/scripts/components/openstack/utils.js
@@ -0,0 +1,38 @@
+export function formatPackageDetails($filter, value) {
+  const ram = $filter('filesize')(value.ram);
+  const storage = $filter('filesize')(value.storage);
+  const props = `${value.cores} vCPU, ${ram} RAM, ${storage} storage`;
+  return `${value.name} (${props})`;
+}
+
+export function parsePackage(choice) {
+  /* Output is item with the following format:
+  {
+    "url": "https://example.com/api/package-templates/2/",
+    "name": "Minimal VPC package",
+    "price": {
+      "day": 1,
+      "month": 30,
+      "year": 365
+    },
+    "ram": 20240,
+    "cores": 20,
+    "storage": 51200,
+  }
+  */
+  const components = choice.item.components.reduce((map, item) => {
+    map[item.type] = item.amount;
+    return map;
+  }, {});
+  var dailyPrice = choice.item.price * 24;
+  return angular.extend({
+    url: choice.item.url,
+    name: choice.item.name,
+    description: choice.item.description,
+    price: {
+      day: dailyPrice,
+      month: dailyPrice * 30,
+      year: dailyPrice * 365,
+    }
+  }, components);
+}
