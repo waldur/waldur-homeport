e4e7fbe42b157568fea340eb7485765bbfb0858f
Victor Mireyev <victor@opennodecloud.com>
2016-12-02 17:25:50 +0300

    Fix render of resource state tooltip (WAL-163)

--- app/scripts/components/resource/resource-utils-service.js
+++ app/scripts/components/resource/resource-utils-service.js
@@ -75,57 +75,66 @@ export default function resourceUtils(ncUtils, ncServiceUtils, authService, $fil
       return '/static/images/appstore/icon-' + service_type.toLowerCase() + '.png';
     },
     getResourceState: function(resource) {
-      var runtimeShutdownStates = [];
-      var runtimeErrorStates = [];
-      var resourceType = this.formatResourceType(resource);
-      var config = ResourceStateConfiguration[resource.resource_type];
-      if (config) {
-        runtimeShutdownStates = config.shutdown_states || [];
-        runtimeErrorStates = config.error_states || [];
-      }
-      var context = {};
-      context.className = null;
-      var movement = false;
-      context.label = null;
-      context.tooltip = null;
+      let resourceType = this.formatResourceType(resource);
+      let config = ResourceStateConfiguration[resource.resource_type] || {};
+      let runtimeShutdownStates = config.shutdown_states || [];
+      let runtimeErrorStates = config.error_states || [];
+      let context = {
+        className: '',
+        label: '',
+        tooltip: '',
+        movementClassName: ''
+      };
+      let showRuntimeState = false;
 
       if (runtimeErrorStates.indexOf(resource.runtime_state) !== -1) {
         context.className = 'progress-bar-danger';
       }
-      if(resource.state.toLowerCase() === 'ok') {
+      if (resource.state.toLowerCase() === 'ok') {
         if (runtimeShutdownStates.indexOf(resource.runtime_state) !== -1) {
           context.className = 'progress-bar-plain';
         }
         context.label = resource.runtime_state || resource.state;
         if (resource.service_settings_state.toLowerCase() !== 'erred') {
           context.className = context.className || 'progress-bar-primary';
-          context.tooltip = `Resource in sync on backend, currently in state ${resource.runtime_state}`;
+          context.tooltip = `Resource is in sync`;
+          showRuntimeState = true;
         } else {
-          var errorMessage = resource.service_settings_error_message || ENV.defaultErrorMessage;
+          let errorMessage = resource.service_settings_error_message;
           context.className = context.className || 'progress-bar-warning';
-          context.tooltip = `Service settings of this resource are in state erred, error message received: ${errorMessage}`;
+          context.tooltip = `Service settings of this resource are in state erred`;
+          if (errorMessage) {
+            context.tooltip += `, error message: ${errorMessage}`;
+          }
         }
       } else if (resource.state.toLowerCase() === 'erred') {
         context.className = 'progress-bar-warning';
         context.label = resource.runtime_state || resource.state;
-        context.tooltip = `Failed to operate with backend, ${resource.error_message}`;
+        context.tooltip = `Failed to operate with backend`;
+        let errorMessage = resource.error_message;
+        if (errorMessage) {
+          context.tooltip += `, error message: ${errorMessage}`;
+        }
       } else {
-        movement = true;
+        showRuntimeState = true;
         context.className = 'progress-bar-primary';
+        context.movementClassName = 'progress-striped active';
         if (resource.action) {
-          var descriptionState = resource.action_details.message || `${resource.action} ${resourceType}`;
           context.label = resource.action;
-          context.tooltip = `${descriptionState}, current state on backend: ${resource.runtime_state}`;
+          context.tooltip = resource.action_details.message || `${resource.action} ${resourceType}`;
         } else {
           context.label = resource.state;
           if (runtimeErrorStates.indexOf(resource.state) !== -1) {
-            context.tooltip = `${resourceType} has state ${resource.state}, current state on backend: ${resource.runtime_state}`;
+            context.tooltip = `${resourceType} has state ${resource.state}`;
           } else {
-            context.tooltip = `${resource.state} ${resourceType}, current state on backend: ${resource.runtime_state}`;
+            context.tooltip = `${resource.state} ${resourceType}`;
           }
         }
       }
-      context.movementClassName = movement ? 'progress-striped active' : '';
+
+      if (showRuntimeState && resource.runtime_state) {
+        context.tooltip += `, current state on backend: ${resource.runtime_state}`;
+      }
 
       return context;
     }
