e95270541e2e2cb410403922a369a63817e178fc
Victor Mireyev <victor@opennodecloud.com>
2016-12-05 00:46:46 +0300

    WAL-193. Make resource details feature aware. Make feature toggle directive compliant with transclusion.

--- app/scripts/components/visibleIf/visibleIf.js
+++ app/scripts/components/visibleIf/visibleIf.js
@@ -1,14 +1,23 @@
 // @ngInject
 export default function visibleIf(ENV, $parse) {
   return {
+    transclude: 'element',
+    priority: 599,
+    terminal: true,
     restrict: 'A',
-    link: function(scope, element, attrs) {
-      if (ENV.featuresVisible || !attrs.visibleIf) {
-        return;
+    $$tlb: true,
+    compile: (element, attrs) => ($scope, element, attrs, ctrl, $transclude) => {
+      let visible = true;
+      if (!ENV.featuresVisible && attrs.visibleIf) {
+        let feature = $parse(attrs.visibleIf)($scope);
+        visible = ENV.toBeFeatures.indexOf(feature) === -1;
       }
-      var feature = $parse(attrs.visibleIf)(scope);
-      if (ENV.toBeFeatures.indexOf(feature) !== -1) {
-        element.remove();
+
+      if (visible) {
+        let childScope = $scope.$new();
+        $transclude(childScope, clone => {
+          element.after(clone).remove();
+        });
       }
     }
   };
