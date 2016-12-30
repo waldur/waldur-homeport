791be7186db2ad87dcf35c9c51d12c1123286e26
kiryaev <kiryaev.k@gmail.com>
2016-12-30 17:40:50 +0200

    Added icon to user dashboard that is shown if he is staff (WAL-291).
    Added Organization owner field in organizations table in user dashboard.
    Added organization name field in projects table.

--- app/scripts/components/user/user-projects.js
+++ app/scripts/components/user/user-projects.js
@@ -30,6 +30,13 @@ function UserProjectsController(
             }
           },
           {
+            title: 'Organization',
+            className: 'all',
+            render: function(row) {
+              return row.customer_name;
+            }
+          },
+          {
             title: 'Role',
             className: 'all',
             render: function(row) {
