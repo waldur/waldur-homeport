1484da91ca2f96e58fe435f992cf70b49b0828df
Victor Mireyev <victor@opennodecloud.com>
2017-01-05 13:51:55 +0300

    Allow to edit subnet (WAL-294)

--- app/scripts/components/openstack/openstack-network/module.js
+++ app/scripts/components/openstack/openstack-network/module.js
@@ -52,6 +52,22 @@ function actionConfig(ActionConfigurationProvider, DEFAULT_EDIT_ACTION) {
       },
     },
   });
+
+  ActionConfigurationProvider.register('OpenStack.SubNet', {
+    order: [
+      'edit',
+      'pull',
+      'destroy'
+    ],
+    options: {
+      edit: angular.merge({}, DEFAULT_EDIT_ACTION, {
+        successMessage: 'Subnet has been updated'
+      }),
+      pull: {
+        title: 'Synchronise'
+      },
+    }
+  });
 }
 
 // @ngInject
