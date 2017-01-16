738d8b18b96d0ace4c8c91548a1e87c4863217ba
Victor Mireyev <victor@opennodecloud.com>
2017-01-16 20:15:01 +0300

    Check user before logout.

--- app/scripts/components/auth/store-state.js
+++ app/scripts/components/auth/store-state.js
@@ -1,12 +1,14 @@
 // @ngInject
 export default function storeLastState($rootScope, $state, $injector, usersService, UserSettings) {
   $rootScope.$on('logoutStart', function() {
-    const user_uuid = usersService.currentUser.uuid;
-    const $stateParams = $injector.get('$stateParams');
-    UserSettings.setSettings(user_uuid, {
-      name: $state.current.name,
-      params: $stateParams,
-    });
+    if (usersService.currentUser) {
+      const user_uuid = usersService.currentUser.uuid;
+      const $stateParams = $injector.get('$stateParams');
+      UserSettings.setSettings(user_uuid, {
+        name: $state.current.name,
+        params: $stateParams,
+      });
+    }
   });
 }
 
