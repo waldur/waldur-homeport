4c4da9b0fc23b55d0b8eee85eeeda1a1f927c1ac
kiryaev <kiryaev.k@gmail.com>
2017-01-18 09:59:12 +0200

    MR fixes

--- app/scripts/components/providers/module.js
+++ app/scripts/components/providers/module.js
@@ -1,11 +1,9 @@
-import providerUtils from './provider-utils-service';
 import providerState from './provider-state';
 import ProviderListController from './list';
 import providerCreate from './create';
 import detailsModule from './details/module';
 
 export default module => {
-  module.service('providerUtils', providerUtils);
   module.component('providerState', providerState);
   module.controller('ProviderListController', ProviderListController);
   module.directive('providerCreate', providerCreate);
--- app/scripts/components/providers/provider-state.js
+++ app/scripts/components/providers/provider-state.js
@@ -11,8 +11,26 @@ const providerState = {
 export default providerState;
 
 // @ngInject
-function ProviderStateController(providerUtils, $scope) {
+function ProviderStateController($scope, ENV) {
   $scope.$watch(() => this.provider, () =>
-    this.context = providerUtils.getProviderState(this.provider)
+    this.context = getProviderState(this.provider)
   );
+
+  function getProviderState(provider) {
+    let context = {
+      className: '',
+      label: '',
+      tooltip: '',
+      movementClassName: ''
+    };
+    context.label = provider.state;
+
+    if (ENV.servicesStateColorClasses[provider.state] === 'processing') {
+      context.movementClassName = 'progress-striped active';
+    } else if (ENV.servicesStateColorClasses[provider.state] === 'erred') {
+      context.className = 'progress-bar-danger';
+      context.tooltip = provider.error_message;
+    }
+    return context;
+  }
 }
