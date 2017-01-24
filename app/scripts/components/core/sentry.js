2a5220bd8829a9a15c2f024bebd917a05cb24804
Victor Mireyev <victor@opennodecloud.com>
2017-01-24 16:38:35 +0300

    Connect Sentry Raven to Waldur HomePort (WAL-399)

---
+++ app/scripts/components/core/sentry.js
@@ -0,0 +1,53 @@
+import Raven from 'raven-js';
+
+export default module => {
+  module.factory('$exceptionHandler', exceptionHandlerFactory);
+  module.run(attachSentry);
+};
+
+// @ngInject
+function exceptionHandlerFactory($log, ENV) {
+  return function(exception, cause) {
+    if (ENV.SENTRY_DSN) {
+      Raven.captureException(exception, {
+        extra: {
+          cause: cause
+        }
+      });
+    }
+    $log.warn(exception, cause);
+  };
+}
+
+// @ngInject
+function attachSentry(ENV) {
+  if (ENV.SENTRY_DSN) {
+    Raven.config(ENV.SENTRY_DSN).install();
+    Raven.setDataCallback(function(data, original) {
+      _normalizeData(data);
+      original && original(data);
+    });
+  }
+}
+
+// See https://github.com/angular/angular.js/blob/v1.4.7/src/minErr.js
+var angularPattern = /^\[((?:[$a-zA-Z0-9]+:)?(?:[$a-zA-Z0-9]+))\] (.*?)\n?(\S+)$/;
+
+function _normalizeData(data) {
+  // We only care about mutating an exception
+  var exception = data.exception;
+  if (exception) {
+    exception = exception.values[0];
+    var matches = angularPattern.exec(exception.value);
+
+    if (matches) {
+      // This type now becomes something like: $rootScope:inprog
+      exception.type = matches[1];
+      exception.value = matches[2];
+
+      data.message = exception.type + ': ' + exception.value;
+      // auto set a new tag specifically for the angular error url
+      data.extra.angularDocs = matches[3].substr(0, 250);
+    }
+  }
+}
