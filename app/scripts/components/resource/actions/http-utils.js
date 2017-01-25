274feafb38d1912f7dd6c923129da5dfe29ce356
Victor Mireyev <victor@opennodecloud.com>
2017-01-25 16:33:54 +0300

    Put getNextPageUrl inside of HttpUtils for consistency with AngularJS (WAL-409)

--- app/scripts/components/resource/actions/http-utils.js
+++ app/scripts/components/resource/actions/http-utils.js
@@ -1,19 +1,9 @@
-// Extract next page URL from header links
-export const getNextPageUrl = response => {
-  const link = response.headers('link');
-  if (!link) {
-    return null;
-  }
-
-  const nextLink = link.split(', ').filter(s => s.indexOf('rel="next"') > -1)[0];
-  if (!nextLink) {
-    return null;
-  }
-
-  return nextLink.split(';')[0].slice(1, -1);
-};
-
 export default class HttpUtils {
+  /*
+    This class provides utilities for fetching data from REST API.
+    Ideally it should not be a class, because it doesn't have any internal state.
+    The only reason to implement this class is to use AngularJS dependency injection.
+  */
   constructor($http) {
     this.$http = $http;
   }
@@ -29,7 +19,7 @@ export default class HttpUtils {
       because it is expected that number of pages is not big
     */
     return this.$http.get(url).then(response => {
-      const nextPageUrl = getNextPageUrl(response);
+      const nextPageUrl = this.getNextPageUrl(response);
       accum = [...accum, ...response.data];
       if (nextPageUrl) {
         return this.fetchNextPage(accum, nextPageUrl);
@@ -37,5 +27,20 @@ export default class HttpUtils {
         return accum;
       }
     });
+  }
+
+  getNextPageUrl(response) {
+    // Extract next page URL from header links
+    const link = response.headers('link');
+    if (!link) {
+      return null;
+    }
+
+    const nextLink = link.split(', ').filter(s => s.indexOf('rel="next"') > -1)[0];
+    if (!nextLink) {
+      return null;
+    }
+
+    return nextLink.split(';')[0].slice(1, -1);
   }
 }
--- app/scripts/components/resource/actions/http-utils.spec.js
+++ app/scripts/components/resource/actions/http-utils.spec.js
@@ -1,33 +1,38 @@
-import HttpUtils, { getNextPageUrl } from './http-utils';
-
-describe('getNextPageUrl', () => {
-  const fakeResponse = links => ({
-    headers: () => links
-  });
-  it('parses links header and returns next URL', () => {
-    const links = '<https://example.com/api/customers/>; rel="first", ' +
-                  '<https://example.com/api/customers/?page=2>; rel="next", ' +
-                  '<https://example.com/api/customers/?page=13>; rel="last"';
-    expect(getNextPageUrl(fakeResponse(links))).toBe('https://example.com/api/customers/?page=2');
-  });
-
-  it('returns null if header does not contain links', () => {
-    expect(getNextPageUrl(fakeResponse(null))).toBe(null);
-  });
-
-  it('returns null if headers link do not contain next url', () => {
-    const links = '<https://example.com/api/customers/>; rel="first"';
-    expect(getNextPageUrl(fakeResponse(links))).toBe(null);
-  });
-});
+import HttpUtils from './http-utils';
 
 describe('HttpUtils', () => {
   let $httpBackend, httpUtils;
-
   beforeEach(inject((_$http_, _$httpBackend_) => {
     $httpBackend = _$httpBackend_;
     httpUtils = new HttpUtils(_$http_);
   }));
+
+  describe('getNextPageUrl', () => {
+    const fakeResponse = links => ({
+      headers: () => links
+    });
+    it('parses links header and returns next URL', () => {
+      const links = '<https://example.com/api/customers/>; rel="first", ' +
+                    '<https://example.com/api/customers/?page=2>; rel="next", ' +
+                    '<https://example.com/api/customers/?page=13>; rel="last"';
+      const response = fakeResponse(links);
+      const nextPage = httpUtils.getNextPageUrl(response);
+      expect(nextPage).toBe('https://example.com/api/customers/?page=2');
+    });
+
+    it('returns null if header does not contain links', () => {
+      const response = fakeResponse(null);
+      const nextPage = httpUtils.getNextPageUrl(response);
+      expect(nextPage).toBe(null);
+    });
+
+    it('returns null if headers link do not contain next url', () => {
+      const links = '<https://example.com/api/customers/>; rel="first"';
+      const response = fakeResponse(links);
+      const nextPage = httpUtils.getNextPageUrl(response);
+      expect(nextPage).toBe(null);
+    });
+  });
 
   it('fetches and merges all pages in correct order using header links', () => {
     let result;
