f82a13aec00f207c2c0a36c3a20ad354285ebc4d
Victor Mireyev <victor@opennodecloud.com>
2017-01-31 21:44:04 +0300

    Display a more clear error message than -1 if connection with backend fails (WAL-407)

---
+++ app/scripts/components/core/error-message-formatter.js
@@ -0,0 +1,44 @@
+const NETWORK_ERROR_MESSAGE = 'Unfortunately, connection to server has failed. Please check if you can connect to {apiEndpoint} from your browser and contact support if the error continues.';
+
+export default class ErrorMessageFormatter {
+
+  constructor(ENV, $filter) {
+    this.ENV = ENV;
+    this.$filter = $filter;
+  }
+
+  format(response) {
+    /*
+    Status code -1 denotes network error.
+    Usually it is caused by one of the following reasons:
+
+    - client is not connected to the internet;
+    - server is down;
+    - server is inaccessible via VPN;
+    - server is located in another domain and didn't return valid CORS headers;
+    - request has timed out;
+    - request was aborted, ie manually cancelled by the client.
+
+    See also: https://fetch.spec.whatwg.org/#concept-filtered-response
+    */
+
+    let message;
+
+    if (response.status === -1) {
+      message = NETWORK_ERROR_MESSAGE.replace('{apiEndpoint}', this.ENV.apiEndpoint);
+    } else {
+      message = response.status + ': ' + response.statusText;
+    }
+
+    if (response.data) {
+      if (response.data.non_field_errors) {
+        message += ' ' + response.data.non_field_errors;
+      }
+      if (response.data.detail) {
+        message += ' ' + response.data.detail;
+      }
+    }
+
+    return message;
+  }
+}
