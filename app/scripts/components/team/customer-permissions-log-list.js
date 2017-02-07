9575610d2b1b40d93c642970e8762ead0ad4ba09
Victor Mireyev <victor@opennodecloud.com>
2017-02-07 20:12:05 +0300

    Use arrow functions in table column declaration (WAL-316)

--- app/scripts/components/team/customer-permissions-log-list.js
+++ app/scripts/components/team/customer-permissions-log-list.js
@@ -21,37 +21,27 @@ function customerPermissionsLogListController(
           {
             title: 'User',
             className: 'all',
-            render: function(row) {
-              return row.user_full_name || row.user_username;
-            }
+            render: row => row.user_full_name || row.user_username
           },
           {
             title: 'User email',
             className: 'desktop',
-            render: function(row) {
-              return row.user_email;
-            }
+            render: row => row.user_email
           },
           {
             title: 'Role',
             className: 'min-tablet-l',
-            render: function(row) {
-              return $filter('translate')(row.role);
-            }
+            render: row => $filter('translate')(row.role)
           },
           {
             title: 'Created',
             className: 'min-tablet-l',
-            render: function(row) {
-              return $filter('dateTime')(row.created);
-            }
+            render: row => $filter('dateTime')(row.created)
           },
           {
             title: 'Expiration time',
             className: 'min-tablet-l',
-            render: function(row) {
-              return $filter('dateTime')(row.expiration_time);
-            }
+            render: row => $filter('dateTime')(row.expiration_time)
           }
         ],
       };
