78bf8386fdfe709ed2814cc41b2ef1613951bd05
Victor Mireyev <victor@opennodecloud.com>
2017-03-20 22:16:50 +0300

    Remove old sizing components [WAL-628]

--- app/scripts/components/cost-planning/module.js
+++ app/scripts/components/cost-planning/module.js
@@ -1,5 +1,3 @@
-import customerSizing from './customer-sizing';
-import sizing from './sizing';
 import costPlansList from './cost-plans-list';
 import costPlanDialog from './cost-plan-dialog';
 import costPlanItems from './cost-plan-items';
@@ -8,8 +6,6 @@ import costPresetsService from './cost-presets-service';
 import certificationsService from './certifications-service';
 
 export default module => {
-  module.component('customerSizing', customerSizing);
-  module.directive('sizing', sizing);
   module.component('costPlansList', costPlansList);
   module.component('costPlanDialog', costPlanDialog);
   module.component('costPlanItems', costPlanItems);
