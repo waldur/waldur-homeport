3068769c3446e3b2ad8567c6cda159f36e326fbd
Victor Mireyev <victor@opennodecloud.com>
2017-03-22 16:37:57 +0300

    Prevent selection of the same component twice [WAL-628]

--- app/scripts/components/cost-planning/cost-plan-items.js
+++ app/scripts/components/cost-planning/cost-plan-items.js
@@ -7,6 +7,17 @@ const costPlanItems = {
     presets: '<',
     onDelete: '&',
     onAdd: '&',
+  },
+  controller: class ComponentController {
+    getFreePresets(current_choice) {
+      let used_choices = {};
+      angular.forEach(this.items, item => {
+        if (item.preset && item.preset !== current_choice) {
+          used_choices[item.preset.url] = true;
+        }
+      });
+      return this.presets.filter(preset => !used_choices[preset.url]);
+    }
   }
 };
 
