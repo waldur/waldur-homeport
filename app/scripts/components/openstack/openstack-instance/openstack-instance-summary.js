de64e2134e1993540f150fd371685c6b392f2ddc
Victor Mireyev <victor@opennodecloud.com>
2017-03-27 17:25:17 +0300

    [WAL-652] Extract virtual machine summary. Use it in both OpenStack instance and DigitalOcean droplet.

--- app/scripts/components/openstack/openstack-instance/openstack-instance-summary.js
+++ app/scripts/components/openstack/openstack-instance/openstack-instance-summary.js
@@ -1,25 +1,10 @@
 import template from './openstack-instance-summary.html';
 
-export const openstackInstanceSummary = {
+const openstackInstanceSummary = {
   template,
   bindings: {
     resource: '<'
   },
-  controller: class ResourceSummaryController {
-    // @ngInject
-    constructor(resourceUtils) {
-      this.resourceUtils = resourceUtils;
-    }
-
-    $onChanges(changes) {
-      let resource = changes.resource.currentValue;
-      if (!resource) {
-        return;
-      }
-
-      this.resourceUtils.setAccessInfo(resource);
-      resource.summary = this.resourceUtils.getSummary(resource);
-      resource.uptime = this.resourceUtils.getUptime(resource);
-    }
-  }
 };
+
+export default openstackInstanceSummary;
