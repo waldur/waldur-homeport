14027a0359c4b3fd070fc439a005b249b7c8bb5b
kiryaev <kiryaev.k@gmail.com>
2017-03-30 11:44:38 +0300

    Added extra fields to backup restore actions (WAL-549)

--- app/scripts/components/openstack/openstack-backup/actions.js
+++ app/scripts/components/openstack/openstack-backup/actions.js
@@ -28,6 +28,35 @@ export default function actionConfig(ActionConfigurationProvider, DEFAULT_EDIT_A
           summary: {
             component: 'openstackBackupRestoreSummary',
             formGroupClass: 'm-b-n'
+          },
+          security_groups: {
+            type: 'multiselect',
+            placeholder: gettext('Select security groups...'),
+            resource_default_value: true,
+            serializer: items => items.map(item => ({url: item.value}))
+          },
+          internal_ips_set: {
+            type: 'multiselect',
+            label: gettext('Connected subnets'),
+            placeholder: gettext('Select subnets to connect to...'),
+            resource_default_value: true,
+            serializer: items => items.map(item => ({ subnet: item.value })),
+            formatter: ($filter, item) => internalIpFormatter(item),
+            modelParser: (field, items) => items.map(item => ({
+              url: item.subnet,
+              name: item.subnet_name,
+              cidr: item.subnet_cidr,
+            })),
+            value_field: 'url',
+          },
+          floating_ips: {
+            resource_default_value: true,
+            component: 'openstackInstanceFloatingIps',
+            init: (field, resource) => {
+              field.internal_ips_set = resource.internal_ips_set;
+            },
+            display_name_field: 'address',
+            value_field: 'url',
           }
         }
       }
