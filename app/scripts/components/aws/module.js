7cbdd2dd2ed0f322612ad11f377d4970ff7fa2cc
Victor Mireyev <victor@opennodecloud.com>
2017-04-13 12:42:44 +0300

    Implement adapter for AWS cost plan [WAL-669]

---
+++ app/scripts/components/aws/module.js
@@ -0,0 +1,16 @@
+export default module => {
+  module.run(costPlanningConfig);
+};
+
+// @ngInject
+function costPlanningConfig(costPlanOptimizerService) {
+  costPlanOptimizerService.pushPostprocessor(plan => {
+    if (plan.service_settings_type === 'Amazon' && plan.optimized_presets) {
+      angular.forEach(plan.optimized_presets, item => {
+        item.flavor = item.size;
+        item.preset.disk = item.preset.storage;
+      });
+    }
+    return plan;
+  });
+}
