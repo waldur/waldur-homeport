f276d8138e9dd7dcd42d54cfda66acd0b1f84685
Victor Mireyev <victor@opennodecloud.com>
2017-04-17 20:56:42 +0300

    Add unit test for service store resource loader.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -17,6 +17,9 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/scripts/class.js',
       'test/test_index.js',
     ],
 
