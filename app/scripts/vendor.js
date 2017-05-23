cd42ae98ee339200efedc27e8a4beca9399b33dd
tarasmatsyk <tamatsyk@gmail.com>
2017-05-23 13:50:20 +0300

    Register missing images in leaflet [WAL-673]

--- app/scripts/vendor.js
+++ app/scripts/vendor.js
@@ -1,5 +1,23 @@
 import 'font-awesome/css/font-awesome.css';
+// leaflet does not load images correctly.
+// see more details:
+// https://github.com/PaulLeCam/react-leaflet/issues/255
+// https://github.com/Leaflet/Leaflet/issues/4968
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+// remove to avoid 414 error.
+delete L.Icon.Default.prototype._getIconUrl;
+// load missing images
+import 'leaflet/dist/images/marker-icon-2x.png';
+import 'leaflet/dist/images/marker-shadow.png';
+
+// register images
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'static/images/marker-icon-2x.png',
+    iconUrl: 'static/images/marker-icon.png',
+    shadowUrl: 'static/images/marker-shadow.png',
+});
+
 import 'world-flags-sprite/stylesheets/flags16.css';
 import 'html5shiv';
 import 'npm-font-open-sans';
