664fe086368df45268b11ef3f5baef7610792ccf
tarasmatsyk <tamatsyk@gmail.com>
2017-06-07 14:49:07 +0300

    Add missing tab parameter [WAL-800]

--- app/scripts/components/resource/tabs/resource-tabs.js
+++ app/scripts/components/resource/tabs/resource-tabs.js
@@ -6,15 +6,18 @@ const resourceTabs = {
     resource: '<'
   },
   controller: class ResourceTabs {
-    constructor(ResourceTabsConfiguration, DEFAULT_RESOURCE_TABS) {
+    constructor(ResourceTabsConfiguration, DEFAULT_RESOURCE_TABS, $stateParams) {
       // @ngInject
       this.registry = ResourceTabsConfiguration;
       this.defaults = DEFAULT_RESOURCE_TABS;
+      this.selectedTabName = $stateParams.tab;
     }
 
     $onInit() {
       const config = this.registry[this.resource.resource_type] || this.defaults;
       this.tabs = config.order.map(tab => angular.extend({name: tab}, config.options[tab]));
+      let selectedTab = this.tabs.filter(tab => tab.name === this.selectedTabName)[0] || this.tabs[0];
+      this.active = selectedTab.name;
     }
   }
 };
