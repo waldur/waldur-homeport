eeadfba65026cff9ece119f8fb19c7846ce8b51d
Victor Mireyev <victor@opennodecloud.com>
2017-07-20 20:31:47 +0300

    [WAL-982] In HomePort allow customer owner to terminate expert contract.

---
+++ app/scripts/components/experts/requests/expert-request-cancel.js
@@ -0,0 +1,37 @@
+import template from './expert-request-cancel.html';
+
+const expertRequestCancel = {
+  template,
+  bindings: {
+    expertRequest: '<'
+  },
+  controller: class ExpertRequestCancelController {
+    // @ngInject
+    constructor(expertRequestsService, customersService, ncUtilsFlash, $rootScope) {
+      this.expertRequestsService = expertRequestsService;
+      this.customersService = customersService;
+      this.ncUtilsFlash = ncUtilsFlash;
+      this.$rootScope = $rootScope;
+    }
+
+    $onInit() {
+      this.customersService.isOwnerOrStaff()
+        .then(canManageRequest => this.canManageRequest = canManageRequest);
+    }
+
+    canCancelRequest() {
+      return this.canManageRequest && this.expertRequest.state === 'Active';
+    }
+
+    cancelRequest() {
+      return this.expertRequestsService.cancel(this.expertRequest).then(() => {
+        this.ncUtilsFlash.success(gettext('Expert request has been cancelled.'));
+        this.$rootScope.$broadcast('refreshExpertDetails');
+      }).catch(() => {
+        this.ncUtilsFlash.error(gettext('Unable to cancel expert request.'));
+      });
+    }
+  }
+}
+
+export default expertRequestCancel;
