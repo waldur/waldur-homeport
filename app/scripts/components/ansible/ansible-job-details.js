557301db0a2427a9d07d957120055dcdd68cb35c
Victor Mireyev <victor@opennodecloud.com>
2017-08-08 14:18:30 +0300

    Implement periodic polling in Ansible job details view [WAL-1081]

--- app/scripts/components/ansible/ansible-job-details.js
+++ app/scripts/components/ansible/ansible-job-details.js
@@ -4,21 +4,31 @@ const ansbileJobDetails = {
   template: template,
   controller: class AnsbileJobDetailsController {
     // ngInject
-    constructor($stateParams, AnsibleJobsService) {
+    constructor($stateParams, $interval, ENV, AnsibleJobsService) {
       this.$stateParams = $stateParams;
+      this.$interval = $interval;
+      this.ENV = ENV;
       this.AnsibleJobsService = AnsibleJobsService;
     }
 
     $onInit() {
       this.loading = true;
-      this.loadAnsibleJob().finally(() => {
-        this.loading = false;
-      });
+      this.pollingPromise = this.startPolling();
     }
 
-    loadAnsibleJob() {
-      return this.AnsibleJobsService.$get(this.$stateParams.jobId)
-        .then(job => this.job = job);
+    $onDestroy() {
+      this.$interval.cancel(this.pollingPromise);
+    }
+
+    startPolling() {
+      this.fetchJob();
+      return this.$interval(this.fetchJob.bind(this), this.ENV.singleResourcePollingTimeout);
+    }
+
+    fetchJob() {
+      this.AnsibleJobsService.$get(this.$stateParams.jobId)
+        .then(job => this.job = job)
+        .finally(() => this.loading = false);
     }
   }
 };
