73950b0bb90392a5e6681b2e04c3be1bc05fd97b
Victor Mireyev <victor@opennodecloud.com>
2017-08-10 17:31:38 +0300

    Implement lazy loading for resource analytics module [WAL-1091]

--- app/scripts/components/analytics/module.js
+++ app/scripts/components/analytics/module.js
@@ -4,14 +4,12 @@ import resourceAnalysis from './resource-analysis-new';
 import resourceBarChart from './resource-bar-chart';
 import resourcePieChart from './resource-pie-chart';
 import ResourceChartService from './resource-chart-service';
-import analyticsRoutes from './routes';
 
-export default module => {
-  module.directive('dashboardCostChart', dashboardCostChart);
-  module.directive('costAnalysis', costAnalysis);
-  module.component('resourceAnalysis', resourceAnalysis);
-  module.component('resourceBarChart', resourceBarChart);
-  module.directive('resourcePieChart', resourcePieChart);
-  module.service('ResourceChartService', ResourceChartService);
-  module.config(analyticsRoutes);
-};
+const module = angular.module('waldur.analytics', []);
+module.directive('dashboardCostChart', dashboardCostChart);
+module.directive('costAnalysis', costAnalysis);
+module.component('resourceAnalysis', resourceAnalysis);
+module.component('resourceBarChart', resourceBarChart);
+module.directive('resourcePieChart', resourcePieChart);
+module.service('ResourceChartService', ResourceChartService);
+export default module.name;
