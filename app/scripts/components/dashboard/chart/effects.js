ff14b64d4bcb75f7001eed5e841e6ac11844cb85
Victor Mireyev <victor@opennodecloud.com>
2017-09-06 12:19:06 +0300

    Fix dashboard chart update after customer switch. Temporary disable chart pulling.

--- app/scripts/components/dashboard/chart/effects.js
+++ app/scripts/components/dashboard/chart/effects.js
@@ -7,15 +7,19 @@ import actions from './actions';
 
 const CHART_PULLING_INTERVAL = 60 * 1000;
 
+function* fetchDashboardChart(chartId, scope) {
+  try {
+    const charts = yield call(api.fetchChart, chartId, scope);
+    yield put(actions.dashboardChartSuccess(chartId, charts));
+  } catch(error) {
+    yield put(actions.dashboardChartError(chartId, error));
+  }
+}
+
 function* pullDashboardChart(chartId, scope) {
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    try {
-      const charts = yield call(api.fetchChart, chartId, scope);
-      yield put(actions.dashboardChartSuccess(chartId, charts));
-    } catch(error) {
-      yield put(actions.dashboardChartError(chartId, error));
-    }
+    fetchDashboardChart(chartId, scope);
     yield call(delay, CHART_PULLING_INTERVAL);
   }
 }
@@ -33,7 +37,7 @@ function* watchEmit() {
 }
 
 function* rootSaga():any {
-  yield takeEvery(actions.DASHBOARD_CHARTS_START, startDashboardChart);
+  yield takeEvery(actions.DASHBOARD_CHARTS_START, ({chartId, scope}) => fetchDashboardChart(chartId, scope));
   yield fork(watchEmit);
 }
 
