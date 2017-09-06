b478c638274fa214dd21e18e8d0ca283a6981de4
Victor Mireyev <victor@opennodecloud.com>
2017-09-06 11:02:09 +0300

    Render CPU limit and usage fields in SLURM allocation details view.

--- app/scripts/components/slurm/module.js
+++ app/scripts/components/slurm/module.js
@@ -4,6 +4,7 @@ import slurmAllocationConfig from './slurm-allocation-config';
 import registerSidebarExtension from './sidebar';
 import SlurmAllocationService from './slurm-allocation-service';
 import slurmAllocationList from './slurm-allocation-list';
+import slurmAllocationSummary from './slurm-allocation-summary';
 
 export default module => {
   module.config(slurmRoutes);
@@ -12,4 +13,5 @@ export default module => {
   module.run(registerSidebarExtension);
   module.service('SlurmAllocationService', SlurmAllocationService);
   module.component('slurmAllocationList', slurmAllocationList);
+  module.component('slurmAllocationSummary', slurmAllocationSummary);
 };
