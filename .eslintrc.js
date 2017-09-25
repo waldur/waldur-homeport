95bdb48d93710a77b4e4e6aedcdae704ef0c46ec
Victor Mireyev <victor@opennodecloud.com>
2017-09-25 14:24:19 +0300

    Allow template literals [WAL-1166]

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,10 @@ module.exports = {
     ],
     'quotes': [
       'error',
-      'single'
+      'single',
+      {
+        'allowTemplateLiterals': true
+      }
     ],
     'semi': [
       'error',
