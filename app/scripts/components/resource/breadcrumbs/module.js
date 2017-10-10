26f4a7f54a6a6b165fe08f9853b1d2c89fbe390a
Victor Mireyev <victor@opennodecloud.com>
2017-10-10 12:35:01 +0300

    Reuse breadcrumbs container in resource details view.

--- app/scripts/components/resource/breadcrumbs/module.js
+++ app/scripts/components/resource/breadcrumbs/module.js
@@ -1,9 +1,7 @@
-import { resourceBreadcrumbs } from './resource-breadcrumbs';
 import ResourceBreadcrumbsService from './resource-breadcrumbs-service';
 import { CATEGORY_ITEMS } from './resource-categories';
 
 export default module => {
-  module.component('resourceBreadcrumbs', resourceBreadcrumbs);
   module.service('ResourceBreadcrumbsService', ResourceBreadcrumbsService);
   module.constant('CATEGORY_ITEMS', CATEGORY_ITEMS);
 };
