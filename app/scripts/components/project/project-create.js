ea007ae6fea684f46f99330bbf7820bd0340cf64
Dimitry Lutsenko <333333y@gmail.com>
2017-11-15 21:06:37 +0530

    Add project name validation, prevent project name duplication [WAL-1017].

--- app/scripts/components/project/project-create.js
+++ app/scripts/components/project/project-create.js
@@ -1,75 +1,103 @@
 import template from './project-create.html';
+import {BaseControllerAddClass} from './../controllers/base-controller-class-es6';
+
+class ProjectAddController extends BaseControllerAddClass {
+  // @ngInject
+  constructor(projectsService,
+              certificationsService,
+              projectValidationService,
+              $q,
+              ncUtils,
+              $injector) {
+    super($injector);
+    this.$injector = $injector;
+    this.certificationsService = certificationsService;
+    this.projectsService = projectsService;
+    this.projectValidationService = projectValidationService;
+    this.$q = $q;
+    this.ncUtils = ncUtils;
+    this.detailsState = 'project.details';
+    this.redirectToDetailsPage = true;
+    this.certifications = [];
+    this.projectCertifications = [];
+    this.projectsAll = [];
+  }
+
+  $onInit() {
+    super.$onInit(this.projectsService);
+    this.project = this.instance;
+    this.activate();
+  }
+
+  activate() {
+    this.loading = true;
+    this.$q.all([
+      this.loadCustomer(),
+      this.loadCertificates()
+    ]).finally(() => {
+      this.loading = false;
+    });
+  }
+
+  loadCustomer() {
+    return this.currentStateService.getCustomer().then(customer => {
+      this.project.customer = customer.url;
+      this.projectsAll = customer.projects;
+      if (this.ncUtils.isCustomerQuotaReached(customer, 'project')) {
+        this.$state.go('errorPage.limitQuota');
+      }
+    });
+  }
+
+  loadCertificates() {
+    return this.certificationsService.getAll().then(certifications => {
+      this.certifications = certifications;
+    });
+  }
+
+  validate() {
+    let response = this.projectValidationService.validate({
+      projects: this.projectsAll,
+      form: this.form
+    });
+    this.errors = {};
+
+    if (response === true) {
+      return true;
+    }
+    this.errors = response;
+    return false;
+  }
+
+  beforeSave() {
+    this.project.certifications = this.projectCertifications.map(item => ({url: item.url}));
+  }
+
+  save() {
+    if (!this.validate()) return;
+    super.save();
+  }
+
+  afterSave(project) {
+    this.$rootScope.$broadcast('refreshProjectList', {
+      model: project, new: true, current: true
+    });
+  }
+
+  onError(errorObject) {
+    this.ncUtilsFlash.errorFromResponse(errorObject, gettext('Project could not be created.'));
+  }
+
+  cancel() {
+    this.currentStateService.getCustomer().then((customer) => {
+      this.$state.go('organization.projects', {uuid: customer.uuid});
+    });
+  }
+}
 
 const projectCreate = {
   template: template,
-  controller: ProjectAddController,
-  controllerAs: 'ProjectAdd',
+  controller: ProjectAddController
 };
 
 export default projectCreate;
-
-// @ngInject
-function ProjectAddController(
-  projectsService,
-  currentStateService,
-  certificationsService,
-  baseControllerAddClass,
-  $q,
-  $rootScope,
-  $state,
-  ncUtils,
-  ncUtilsFlash) {
-  let controllerScope = this;
-  let ProjectController = baseControllerAddClass.extend({
-    userRole: 'admin',
-    init: function() {
-      this.service = projectsService;
-      this.controllerScope = controllerScope;
-      this._super();
-      this.detailsState = 'project.details';
-      this.redirectToDetailsPage = true;
-      this.project = this.instance;
-      this.certifications = [];
-      this.projectCertifications = [];
-    },
-    activate: function() {
-      this.loading = true;
-      $q.all([
-        this.loadCustomer(),
-        this.loadCertificates()
-      ]).finally(() => this.loading = false);
-    },
-    loadCustomer: function() {
-      return currentStateService.getCustomer().then(customer => {
-        this.project.customer = customer.url;
-        if (ncUtils.isCustomerQuotaReached(customer, 'project')) {
-          $state.go('errorPage.limitQuota');
-        }
-      });
-    },
-    loadCertificates: function() {
-      return certificationsService.getAll().then(certifications => {
-        this.certifications = certifications;
-      });
-    },
-    beforeSave: function() {
-      this.project.certifications = this.projectCertifications.map(item => ({url: item.url}));
-    },
-    afterSave: function(project) {
-      $rootScope.$broadcast('refreshProjectList', {
-        model: project, new: true, current: true
-      });
-      this._super();
-    },
-    onError: function(errorObject) {
-      ncUtilsFlash.error(errorObject.data.detail);
-    },
-    cancel: function() {
-      currentStateService.getCustomer().then(function(customer) {
-        $state.go('organization.projects', {uuid: customer.uuid});
-      });
-    }
-  });
-
-  controllerScope.__proto__ = new ProjectController();
-}
