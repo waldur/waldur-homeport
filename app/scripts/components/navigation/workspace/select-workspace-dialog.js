6591bf79dad4ee4a3c3fbd6da1e951b056f1e5a1
Victor Mireyev <victor@opennodecloud.com>
2018-01-05 15:19:29 +0300

    Skip quotas loading in workspace selector.

--- app/scripts/components/navigation/workspace/select-workspace-dialog.js
+++ app/scripts/components/navigation/workspace/select-workspace-dialog.js
@@ -111,7 +111,7 @@ function SelectWorkspaceDialogController(
       }),
 
       customersService.getAll({
-        field: ['name', 'uuid', 'projects', 'owners', 'quotas', 'abbreviation', 'is_expert_provider'],
+        field: ['name', 'uuid', 'projects', 'owners', 'abbreviation', 'is_expert_provider'],
         o: 'name'
       }).then(function(organizations) {
         if (ctrl.selectedOrganization) {
