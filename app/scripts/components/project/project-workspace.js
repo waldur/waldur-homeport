3bc8d16f192cd32a712c378ab829b933eddc8967
Victor Mireyev <victor@opennodecloud.com>
2018-05-07 18:42:38 +0300

    Disable marketplace by default [WAL-1522]

--- app/scripts/components/project/project-workspace.js
+++ app/scripts/components/project/project-workspace.js
@@ -72,6 +72,7 @@ export default function ProjectWorkspaceController(
         key: 'marketplace',
         icon: 'fa-shopping-cart',
         label: gettext('Marketplace'),
+        feature: 'marketplace',
         link: 'marketplace-list({uuid: $ctrl.context.project.uuid})',
         index: 210,
       },
