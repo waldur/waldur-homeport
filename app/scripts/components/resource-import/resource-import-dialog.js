f94eacd3ce5f84e608b997b45b303994314c4e07
Victor Mireyev <victor@opennodecloud.com>
2018-05-18 18:51:37 +0300

    [WAL-1551] Clear cache for importable resources service when import action is executed.

--- app/scripts/components/resource-import/resource-import-dialog.js
+++ app/scripts/components/resource-import/resource-import-dialog.js
@@ -57,6 +57,7 @@ const resourceImportDialog = {
         return;
       }
 
+      this.service.clearAllCacheForCurrentEndpoint();
       return this.service.importResources(this.selectedProvider, this.selectedItems).then(() => {
         this.ncUtilsFlash.success(gettext('Resources import has been initiated'));
         this.close();
