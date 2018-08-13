8059759dc2a3201dc8101599399273fa25317a81
Victor Mireyev <victor@opennodecloud.com>
2018-08-13 08:57:17 -0400

    Render timezone field for backup and snapshot schedule in summary component.

--- app/scripts/components/openstack/openstack-instance/types.tsx
+++ app/scripts/components/openstack/openstack-instance/types.tsx
@@ -8,6 +8,5 @@ export interface OpenStackInstance extends VirtualMachine {
   flavor_name: string;
   internal_ips_set: InternalIp[];
   runtime_state: 'SHUTOFF' | 'ACTIVE';
-  url: string;
-  service_settings_uuid: string;
+  security_groups?: string[];
 }
