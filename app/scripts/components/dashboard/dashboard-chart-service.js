f65ce4f18ae692cacba3356c343a16368cb8a2bf
Victor Mireyev <victor@opennodecloud.com>
2018-08-15 18:14:23 +0300

    Fix volume and backup size calculation in project and organization dashboard.

--- app/scripts/components/dashboard/dashboard-chart-service.js
+++ app/scripts/components/dashboard/dashboard-chart-service.js
@@ -105,14 +105,14 @@ export default class DashboardChartService {
     let formatter = x => x;
     let units;
 
-    const maxValue = items.sort((c1, c2) => c1.value < c2.value)[0].value;
+    const maxValue = items.map(c => c.value).sort()[items.length - 1];
     if (chart.type === 'filesize') {
       if (maxValue > 1024 * 1024) {
-        formatter = x => Math.round(x / 1024 * 1024);
+        formatter = x => Math.round(10 * x / 1024 / 1024) / 10;
         units = 'TB';
       }
       else if (maxValue > 1024) {
-        formatter = x => Math.round(x / 1024);
+        formatter = x => Math.round(10 * x / 1024) / 10;
         units = 'GB';
       }
       else {
