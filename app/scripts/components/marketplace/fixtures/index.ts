9648f8b77b0eeccb2dffda8312ba599678175c02
Victor Mireyev <victor@opennodecloud.com>
2018-08-20 16:38:45 +0300

    Remove old fixtures for marketplace [WAL-1734]

--- app/scripts/components/marketplace/fixtures/index.ts
+++ app/scripts/components/marketplace/fixtures/index.ts
@@ -1,6 +1,1 @@
-import { fillFeatures } from './features';
-import { products as rawProducts } from './products';
 export { categories } from './categories';
-export { sections } from './sections';
-
-export const products = rawProducts.map(fillFeatures);
