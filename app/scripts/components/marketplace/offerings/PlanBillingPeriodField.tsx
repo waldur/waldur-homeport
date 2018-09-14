d3809c7fd48348cb35465e7a6ce27be5da8d6749
Victor Mireyev <victor@opennodecloud.com>
2018-09-14 13:04:46 +0300

    Exclude quantity from billing period choices for billing plan.

--- app/scripts/components/marketplace/offerings/PlanBillingPeriodField.tsx
+++ app/scripts/components/marketplace/offerings/PlanBillingPeriodField.tsx
@@ -2,19 +2,21 @@ import * as React from 'react';
 import Select from 'react-select';
 import { Field } from 'redux-form';
 
-import { withTranslation } from '@waldur/i18n';
+import { getBillingPeriods } from './constants';
 
-import { PRICE_UNITS } from './constants';
+interface PlanBillingPeriodFieldProps {
+  plan: string;
+}
 
-export const PriceUnitField = withTranslation(props => (
+export const PlanBillingPeriodField = (props: PlanBillingPeriodFieldProps) => (
   <Field
     name={`${props.plan}.unit`}
     component={fieldProps => (
       <Select
         value={fieldProps.input.value}
         onChange={value => fieldProps.input.onChange(value)}
-        options={PRICE_UNITS}
+        options={getBillingPeriods()}
       />
     )}
   />
-));
+);
