9c18f44028cbb7d9322ece535d2528d60f78d367
Victor Mireyev <victor@opennodecloud.com>
2018-11-13 14:04:03 +0300

    Fix typing for price field component.

--- src/marketplace/offerings/plan/PriceField.tsx
+++ src/marketplace/offerings/plan/PriceField.tsx
@@ -4,7 +4,7 @@ import { defaultCurrency } from '@waldur/core/services';
 
 import { connectPlanComponents } from './utils';
 
-export const PriceField = connectPlanComponents(props => (
+export const PriceField = connectPlanComponents((props: {total: number}) => (
   <div className="form-control-static">
     {defaultCurrency(props.total)}
   </div>
