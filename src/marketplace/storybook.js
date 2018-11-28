e09de30c6032e01390ff4cd998237e25e0f638e6
Faradj Musaev <faradj.musaev@gmail.com>
2018-11-28 07:20:30 -0500

    [WAL-2001] Migrate openstackInstanceNetworks component to ReactJS.

--- src/marketplace/storybook.js
+++ src/marketplace/storybook.js
@@ -1,7 +1,9 @@
 import OpenstackInstanceSecurityGroupsStory from '@waldur/openstack/openstack-instance/OpenstackInstanceSecurityGroupsStory';
+import OpenstackInstanceNetworksStory from '@waldur/openstack/openstack-instance/OpenstackInstanceNetworksStory';
 
 export default module => {
   module.component('openstackInstanceSecurityGroupsStory', OpenstackInstanceSecurityGroupsStory);
+  module.component('openstackInstanceNetworksStory', OpenstackInstanceNetworksStory);
   module.config(storyBookRoutes);
 };
 
@@ -11,5 +13,9 @@ function storyBookRoutes($stateProvider) {
     .state('openstack-instance-security-groups-story', {
       url: '/openstack-instance-security-groups-story/',
       template: '<openstack-instance-security-groups-story></openstack-instance-security-groups-story>',
+    })
+    .state('openstack-instance-networks-story', {
+      url: '/openstack-instance-networks-story/',
+      template: '<openstack-instance-networks-story></openstack-instance-networks-story>'
     });
 }
--- src/openstack/openstack-instance/storyFixtures.ts
+++ src/openstack/openstack-instance/storyFixtures.ts
@@ -93,3 +93,84 @@ export const securityGroups = [
     ],
   },
 ];
+
+export const subnets = [
+  {
+    url: 'http://localhost:8000/api/openstacktenant-subnets/557eaef2c2774a2da89976e59f5f09c1/',
+    uuid: '557eaef2c2774a2da89976e59f5f09c1',
+    name: '123-sub-net',
+    cidr: '192.168.42.0/24',
+    gateway_ip: '192.168.42.1',
+    allocation_pools: [
+        {
+            start: '192.168.42.10',
+            end: '192.168.42.200',
+        },
+    ],
+    ip_version: 4,
+    enable_dhcp: true,
+    dns_nameservers: [],
+    network: 'http://localhost:8000/api/openstacktenant-networks/8773466ef002433881cc2cd28e095026/',
+  },
+  {
+      url: 'http://localhost:8000/api/openstacktenant-subnets/3a6d62de0ad54f5bbb9bdb5dcce11abf/',
+      uuid: '3a6d62de0ad54f5bbb9bdb5dcce11abf',
+      name: 'awdawd-sub-net',
+      cidr: '192.168.42.0/24',
+      gateway_ip: '192.168.42.1',
+      allocation_pools: [
+          {
+              start: '192.168.42.10',
+              end: '192.168.42.200',
+          },
+      ],
+      ip_version: 4,
+      enable_dhcp: true,
+      dns_nameservers: [],
+      network: 'http://localhost:8000/api/openstacktenant-networks/a4c80b032cc54166822b741b1d64e8b9/',
+  },
+  {
+      url: 'http://localhost:8000/api/openstacktenant-subnets/8a07dca67f71459daab5f7f02147a367/',
+      uuid: '8a07dca67f71459daab5f7f02147a367',
+      name: 'baip-demo-sub-net',
+      cidr: '192.168.42.0/24',
+      gateway_ip: '192.168.42.1',
+      allocation_pools: [
+          {
+              start: '192.168.42.10',
+              end: '192.168.42.200',
+          },
+      ],
+      ip_version: 4,
+      enable_dhcp: true,
+      dns_nameservers: [],
+      network: 'http://localhost:8000/api/openstacktenant-networks/9b8fa00872fc4c0599b0c0d8ea41f473/',
+  },
+];
+
+export const floatingIps = [
+  {
+    url: 'http://localhost:8000/api/openstacktenant-floating-ips/7892347016d74ba2872a6d6b24877319/',
+    uuid: '7892347016d74ba2872a6d6b24877319',
+    settings: 'http://localhost:8000/api/service-settings/0989b23135f348ae8a30f0ec32d9551c/',
+    address: '185.82.124.65',
+    runtime_state: 'DOWN',
+    is_booked: false,
+  },
+  {
+    url: 'http://localhost:8000/api/openstacktenant-floating-ips/a195eaf0e3ba403bb212aeacaeb30b03/',
+    uuid: 'a195eaf0e3ba403bb212aeacaeb30b03',
+    settings: 'http://localhost:8000/api/service-settings/ff0e48285018422fba79c909db547dba/',
+    address: '185.82.124.71',
+    runtime_state: 'DOWN',
+    is_booked: false,
+  },
+  {
+    url: 'http://localhost:8000/api/openstacktenant-floating-ips/e5a4336b2958446ca6d508fca9065961/',
+    uuid: 'e5a4336b2958446ca6d508fca9065961',
+    settings: 'http://localhost:8000/api/service-settings/ff0e48285018422fba79c909db547dba/',
+    address: '185.82.124.84',
+    runtime_state: 'DOWN',
+    is_booked: false,
+  },
+];
