793ff0d3a73413054edeac34568977ab2f018f12
Victor Mireyev <victor@opennodecloud.com>
2018-12-21 15:28:29 +0300

    Remove excessive comments.

--- src/openstack/api.ts
+++ src/openstack/api.ts
@@ -3,23 +3,19 @@ import { $http } from '@waldur/core/services';
 import { Flavor, FloatingIp, Subnet, ServiceComponent, SshKey } from '@waldur/openstack/openstack-instance/types';
 import { SecurityGroup } from '@waldur/openstack/openstack-security-groups/types';
 
-// tslint:disable-next-line:variable-name
+// tslint:disable:variable-name
 export const loadFlavors = (settings_uuid: string) =>
   getAll<Flavor>('/openstacktenant-flavors/', {params: {settings_uuid}});
 
-// tslint:disable-next-line:variable-name
 export const loadImages = (settings_uuid: string) =>
   getAll<ServiceComponent>('/openstacktenant-images/', {params: {settings_uuid}});
 
-// tslint:disable-next-line:variable-name
 export const loadSecurityGroups = (settings_uuid: string) =>
   getAll<SecurityGroup>('/openstacktenant-security-groups/', {params: {settings_uuid}});
 
-// tslint:disable-next-line:variable-name
 export const loadSubnets = (settings_uuid: string) =>
   getAll<Subnet>('/openstacktenant-subnets/', {params: {settings_uuid}});
 
-// tslint:disable-next-line:variable-name
 export const loadFloatingIps = (settings_uuid: string) =>
   getAll<FloatingIp>('/openstacktenant-floating-ips/', {params: {
     is_booked: 'False',
@@ -27,11 +23,9 @@ export const loadFloatingIps = (settings_uuid: string) =>
     settings_uuid,
   }});
 
-// tslint:disable-next-line:variable-name
 export const loadSshKeys = (user_uuid: string) =>
   getAll<SshKey>('/keys/', {params: {user_uuid}});
 
-// tslint:disable-next-line:variable-name
 export const loadProjectQuotas = (settings_uuid: string, project_uuid: string) =>
   getFirst('/openstacktenant-service-project-link/', {
     params: {
