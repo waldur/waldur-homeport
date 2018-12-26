22c1f402678375f39950347ce33ab14e52eeab07
Faradj Musaev <faradj.musaev@gmail.com>
2018-12-26 05:11:26 -0500

    [WAL-1990] Migrate OpenStack volume provisioning form to marketplace.

---
+++ src/openstack/openstack-volume/OpenstackVolumeCreateForm.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { Field } from 'redux-form';
+
+import { getLatinNameValidators } from '@waldur/core/validators';
+import { StringField, NumberField, TextField } from '@waldur/form-react';
+import { translate } from '@waldur/i18n';
+import { OfferingConfigurationFormProps } from '@waldur/marketplace/types';
+import { OpenstackInstanceFormGroup } from '@waldur/openstack/openstack-instance/OpenstackInstanceCreateForm';
+
+export class OpenstackVolumeCreateForm extends React.Component<OfferingConfigurationFormProps> {
+  componentDidMount() {
+    this.props.initialize({ attributes: {size: 1024} });
+  }
+  render() {
+    return (
+      <form className="form-horizontal">
+        <OpenstackInstanceFormGroup
+          label={translate('Volume name')}
+          required={true}>
+          <Field
+            name="attributes.name"
+            component={StringField}
+            validate={getLatinNameValidators()}
+          />
+        </OpenstackInstanceFormGroup>
+        <OpenstackInstanceFormGroup label={translate('Size')}>
+            <div className="input-group">
+              <Field
+                name="attributes.size"
+                component={fieldProps =>
+                  <NumberField
+                    min={0}
+                    max={1 * 4096}
+                    {...fieldProps.input}/>
+                }
+                format={v => v ? v / 1024 : 0}
+                normalize={v => Number(v) * 1024}
+              />
+              <span className="input-group-addon">
+                GB
+              </span>
+          </div>
+        </OpenstackInstanceFormGroup>
+        <OpenstackInstanceFormGroup label={translate('Description')}>
+          <Field
+            name="attributes.description"
+            component={fieldProps =>
+              <TextField
+                maxLength={500}
+                {...fieldProps.input}
+              />
+            }
+          />
+        </OpenstackInstanceFormGroup>
+      </form>
+    );
+  }
+}
