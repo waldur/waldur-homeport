6b78c7997a9a9dd57f8325ee086a9da645b3de23
Victor Mireyev <victor@opennodecloud.com>
2018-12-26 18:04:51 +0300

    Fix MR comments [WAL-2062]

--- src/user/support/api.ts
+++ src/user/support/api.ts
@@ -1,22 +1,20 @@
-import { post } from '@waldur/core/api';
-import { $http, ENV } from '@waldur/core/services';
+import { patch } from '@waldur/core/api';
 
 export const updateUser = user =>
-  $http
-    .patch(`${ENV.apiEndpoint}api/users/${user.uuid}/`, {
-      full_name: user.full_name,
-      native_name: user.native_name,
-      email: user.email,
-      organization: user.organization,
-      job_title: user.job_title,
-      description: user.description,
-      phone_number: user.phone_number,
-      agree_with_policy: user.agree_with_policy,
-      token_lifetime: user.token_lifetime.value,
-    });
+  patch(`/users/${user.uuid}/`, {
+    full_name: user.full_name,
+    native_name: user.native_name,
+    email: user.email,
+    organization: user.organization,
+    job_title: user.job_title,
+    description: user.description,
+    phone_number: user.phone_number,
+    agree_with_policy: user.agree_with_policy,
+    token_lifetime: user.token_lifetime.value,
+  });
 
 export const activateUser = userUuid =>
-    post(`/users/${userUuid}/activate/`);
+  patch(`/users/${userUuid}/`, {is_active: true});
 
 export const deactivateUser = userUuid =>
-  post(`/users/${userUuid}/deactivate/`);
+  patch(`/users/${userUuid}/`, {is_active: false});
