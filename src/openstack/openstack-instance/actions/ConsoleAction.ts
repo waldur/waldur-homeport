55dc7e1ef6fb2452639594c6eaff95b769d331af
Victor Mireyev <victor@opennodecloud.com>
2019-01-29 15:47:21 +0300

    Add permission validation for OpenStack console action.

--- src/openstack/openstack-instance/actions/ConsoleAction.ts
+++ src/openstack/openstack-instance/actions/ConsoleAction.ts
@@ -1,11 +1,22 @@
 import { get } from '@waldur/core/api';
 import { format } from '@waldur/core/ErrorMessageFormatter';
+import { ENV } from '@waldur/core/services';
 import { translate } from '@waldur/i18n';
 import { validateState } from '@waldur/resource/actions/base';
-import { ResourceAction } from '@waldur/resource/actions/types';
+import { ResourceAction, ActionContext } from '@waldur/resource/actions/types';
 
 const getConsoleURL = (id: string) =>
   get(`/openstacktenant-instances/${id}/console/`);
+
+const validatePermissions = (ctx: ActionContext) => {
+  if (ctx.user.is_staff) {
+    return;
+  }
+  if (!ctx.user.is_support && ENV.plugins.WALDUR_OPENSTACK_TENANT.ALLOW_CUSTOMER_USERS_OPENSTACK_CONSOLE_ACCESS) {
+    return;
+  }
+  return translate('Only staff and organization users are allowed to open console.');
+};
 
 export default function createAction(): ResourceAction {
   return {
@@ -21,6 +32,6 @@ export default function createAction(): ResourceAction {
         alert(message);
       });
     },
-    validators: [validateState('OK')],
+    validators: [validateState('OK'), validatePermissions],
   };
 }
