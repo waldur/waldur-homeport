777a523e207285cffcc94438e8eec9fd405610e0
Victor Mireyev <victor@opennodecloud.com>
2019-02-11 11:09:57 +0300

    Expose IPs for Azure VM.

--- src/azure/vm/AzureVirtualMachineSummary.tsx
+++ src/azure/vm/AzureVirtualMachineSummary.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { AzureVirtualMachine } from '@waldur/azure/common/types';
 import { withTranslation } from '@waldur/i18n';
 import { Field, ResourceSummaryProps } from '@waldur/resource/summary';
+import { formatIpList } from '@waldur/resource/summary/VirtualMachineSummary';
 import { UserPassword } from '@waldur/resource/UserPassword';
 import { formatSummary } from '@waldur/resource/utils';
 
@@ -28,6 +29,14 @@ const PureAzureVirtualMachineSummary = (props: ResourceSummaryProps<AzureVirtual
       <Field
         label={translate('Size')}
         value={resource.size_name}
+      />
+      <Field
+        label={translate('Internal IP')}
+        value={formatIpList(props.resource.internal_ips)}
+      />
+      <Field
+        label={translate('External IP')}
+        value={formatIpList(props.resource.external_ips)}
       />
     </>
   );
