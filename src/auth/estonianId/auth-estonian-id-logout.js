f2af38ff03db4cf536597bb3bc8db9c78a681499
Victor Mireyev <victor@opennodecloud.com>
2019-03-07 13:36:30 +0300

    Skip logout if OpenID authentication is not enabled.

--- src/auth/estonianId/auth-estonian-id-logout.js
+++ src/auth/estonianId/auth-estonian-id-logout.js
@@ -1,5 +1,8 @@
 // @ngInject
 export default function estonianIdLogout(ENV, $rootScope) {
+  if (!ENV.plugins.WALDUR_CORE.AUTHENTICATION_METHODS.includes('ESTONIAN_ID')) {
+    return;
+  }
   $rootScope.$on('logoutStart', function() {
     const popup = $('<iframe></iframe>');
     popup.hide();
