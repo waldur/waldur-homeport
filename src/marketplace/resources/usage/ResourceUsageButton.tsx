dac670087a0c13b966859ca7a64093ce39fea921
Victor Mireyev <victor@opennodecloud.com>
2019-03-14 15:19:12 +0300

    Disable marketplace usage buttons if related offering is not usage-based.

--- src/marketplace/resources/usage/ResourceUsageButton.tsx
+++ src/marketplace/resources/usage/ResourceUsageButton.tsx
@@ -1,15 +1,26 @@
+import * as classNames from 'classnames';
 import * as React from 'react';
+
+import { translate } from '@waldur/i18n';
+import { wrapTooltip } from '@waldur/table-react/ActionButton';
 
 import { ResourceCreateUsageButton } from './ResourceCreateUsageButton';
 import { ResourceShowUsageButton } from './ResourceShowUsageButton';
 
-export const ResourceUsageButton = ({ row }) => (
-  <div className="btn-group">
-    <ResourceShowUsageButton resource={row.uuid}/>
-    <ResourceCreateUsageButton
-      offering_uuid={row.offering_uuid}
-      resource_uuid={row.uuid}
-      plan_unit={row.plan_unit}
-    />
-  </div>
-);
+export const ResourceUsageButton = ({ row }) => {
+  const body = (
+    <div className={classNames('btn-group', {disabled: !row.is_usage_based})}>
+      <ResourceShowUsageButton resource={row.uuid}/>
+      <ResourceCreateUsageButton
+        offering_uuid={row.offering_uuid}
+        resource_uuid={row.uuid}
+        plan_unit={row.plan_unit}
+      />
+    </div>
+  );
+  if (!row.is_usage_based) {
+    return wrapTooltip(translate('Resource does not have usage-based components.'), body);
+  } else {
+    return body;
+  }
+};
