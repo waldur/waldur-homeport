71f4ae2ecd905cf01ac8bea225022f03f1d34d17
Victor Mireyev <victor@opennodecloud.com>
2019-03-27 16:02:02 +0300

    Report usage should include only inputs for usage-based components [WAL-2255]

--- src/marketplace/resources/usage/ResourceUsageContainer.tsx
+++ src/marketplace/resources/usage/ResourceUsageContainer.tsx
@@ -6,7 +6,6 @@ import { translate } from '@waldur/i18n';
 
 import { getOffering } from '@waldur/marketplace/common/api';
 import { PlanUnit } from '@waldur/marketplace/orders/types';
-import { Offering } from '@waldur/marketplace/types';
 
 import { ResourceUsageForm } from './ResourceUsageForm';
 
@@ -16,14 +15,23 @@ export interface ResourceUsageContainerProps {
   submitting: boolean;
 }
 
+// tslint:disable-next-line: variable-name
+const getUsageComponents = (offering_uuid: string) => {
+  return getOffering(offering_uuid).then(offering => {
+    return {
+      components: offering.components.filter(component => component.billing_type === 'usage'),
+    };
+  });
+};
+
 export const ResourceUsageContainer = (props: ResourceUsageContainerProps) => (
-  <Query loader={getOffering} variables={props.offering_uuid}>
-    {({ loading, error, data }: {loading: boolean, error?: any, data?: Offering}) => {
+  <Query loader={getUsageComponents} variables={props.offering_uuid}>
+    {({ loading, error, data }) => {
       if (loading) {
         return <LoadingSpinner/>;
       } else if (error) {
         return <h3>{translate('Unable to load marketplace offering details.')}</h3>;
-      } else if (data.components.filter(component => component.billing_type === 'usage').length === 0) {
+      } else if (data.components.length === 0) {
         return <h3>{translate('Marketplace offering does not have any usage-based components.')}</h3>;
       } else {
         return (
