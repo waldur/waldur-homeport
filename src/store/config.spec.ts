609288376177296a98bc62d5ede13959d1ae3611
Victor Mireyev <victor@opennodecloud.com>
2019-04-25 10:24:53 +0300

    Fix visibility logic [WAL-2347]

--- src/store/config.spec.ts
+++ src/store/config.spec.ts
@@ -1,25 +1,43 @@
 import { initConfig, reducer, isVisible } from './config';
 
 describe('Configuration reducer', () => {
-  const disabledFeatures = {experts: true, paypal: true};
+  const disabledFeatures = {freeipa: true, support: true};
+  const enabledFeatures = {billing: true, marketplace: true};
 
   it('should merge experimental and disabled features', () => {
     const actual = reducer(undefined, initConfig({
-      disabledFeatures: ['experts'],
-      toBeFeatures: ['paypal'],
+      disabledFeatures: ['freeipa'],
+      toBeFeatures: ['support'],
     }));
     expect(actual.disabledFeatures).toEqual(disabledFeatures);
   });
 
-  it('should check disabled feature', () => {
-    const state = {config: {disabledFeatures, featuresVisible: false}};
-    expect(isVisible(state, 'experts')).toBe(false);
-    expect(isVisible(state, 'billing')).toBe(true);
+  it('should parse enabled features', () => {
+    const actual = reducer(undefined, initConfig({
+      enabledFeatures: ['billing', 'marketplace'],
+    }));
+    expect(actual.enabledFeatures).toEqual(enabledFeatures);
   });
 
-  it('should skip check if all features are visible', () => {
-    const state = {config: {disabledFeatures, featuresVisible: true}};
-    expect(isVisible(state, 'experts')).toBe(true);
+  it('should check disabled features', () => {
+    const state = {config: {disabledFeatures, enabledFeatures}};
+    expect(isVisible(state, 'freeipa')).toBe(false);
+    expect(isVisible(state, 'support')).toBe(false);
+  });
+
+  it('should check enabled features', () => {
+    const state = {config: {disabledFeatures, enabledFeatures}};
     expect(isVisible(state, 'billing')).toBe(true);
+    expect(isVisible(state, 'marketplace')).toBe(true);
+  });
+
+  it('should check visibility for all other features 1', () => {
+    const state = {config: {disabledFeatures, enabledFeatures, featuresVisible: true}};
+    expect(isVisible(state, 'visibleFeature')).toBe(true);
+  });
+
+  it('should check visibility for all other features 2', () => {
+    const state = {config: {disabledFeatures, enabledFeatures, featuresVisible: false}};
+    expect(isVisible(state, 'hiddenFeature')).toBe(false);
   });
 });
