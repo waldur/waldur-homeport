e4942ec1629aa38df841f0130ac3fe88f83da839
Victor Mireyev <victor@opennodecloud.com>
2019-05-03 12:02:41 +0300

    Fix cypress tests.

--- cypress/integration/support/commands.js
+++ cypress/integration/support/commands.js
@@ -3,5 +3,6 @@ Cypress.Commands.add('visitOrganizations', () => {
     .log('visit /support/organizations/')
     .route('http://localhost:8080/api/customers/?**', 'fixture:customers/alice_bob_web.json')
     .route('http://localhost:8080/api/billing-total-cost/**', 'fixture:customers/billing_total_cost.json')
+    .route('http://localhost:8080/api/invoices/**', 'fixture:customers/invoices.json')
     .visit('/#/support/organizations/', {log: false});
 });
