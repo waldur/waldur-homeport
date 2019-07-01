6aaa3fa9ad968c58e14dd7d3148b9dcc71286b0f
Victor Mireyev <victor@opennodecloud.com>
2019-07-01 14:20:08 +0300

    Render detail view for VMware disk [WAL-2514]

--- src/resource/actions/dialog/action-dialog.js
+++ src/resource/actions/dialog/action-dialog.js
@@ -32,7 +32,7 @@ function ActionDialogController(
           if (field.default_value) {
             $scope.form[name] = field.default_value;
           }
-          if (!field.init && (field.resource_default_value || $scope.action.name === 'update')) {
+          if (!field.init && (field.resource_default_value || $scope.action.title === 'Update')) {
             $scope.form[name] = angular.copy($scope.resource[name]);
           }
           if (field.modelParser) {
