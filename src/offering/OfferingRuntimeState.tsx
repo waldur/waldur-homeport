6343fc7a788590c54c262aea203ece71fe6e6df0
Victor Mireyev <victor@opennodecloud.com>
2019-07-02 11:03:58 +0300

    Expose Resource state in request-based item view [WAL-2522]

---
+++ src/offering/OfferingRuntimeState.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import { StateIndicator } from '@waldur/core/StateIndicator';
+import { ResourceState } from '@waldur/marketplace/resources/types';
+
+interface Props {
+  state: ResourceState;
+}
+
+export const OfferingRuntimeState = (props: Props) => (
+  <StateIndicator
+    label={props.state}
+    variant={
+      props.state === 'Erred' ? 'danger' :
+      props.state === 'Terminated' ? 'warning' :
+      'primary'
+    }
+    active={['Creating', 'Updating', 'Terminating'].includes(props.state)}
+  />
+);
