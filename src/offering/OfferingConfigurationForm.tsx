6df6b9f0992bd3b40d3c0e3ad368c24a85247f1b
Victor Mireyev <victor@opennodecloud.com>
2019-07-12 14:57:18 +0300

    Improvements for OpenStack tenant and instance fields [WAL-2531]
    * Filter resource by name.
    * Expose project name in resource choice.
    * Fetch all resources matching filter specification.

--- src/offering/OfferingConfigurationForm.tsx
+++ src/offering/OfferingConfigurationForm.tsx
@@ -17,7 +17,7 @@ import { ProjectField } from '@waldur/marketplace/details/ProjectField';
 import { OfferingConfigurationFormProps } from '@waldur/marketplace/types';
 import { getCustomer } from '@waldur/workspace/selectors';
 
-import { loadTenantOptions, loadInstanceOptions } from './utils';
+import { fetchTenantOptions, fetchInstanceOptions } from './api';
 
 export class PureOfferingConfigurationForm extends React.Component<OfferingConfigurationFormProps> {
 
@@ -111,14 +111,14 @@ export class PureOfferingConfigurationForm extends React.Component<OfferingConfi
               case 'select_openstack_tenant':
                 OptionField = AsyncSelectField;
                 params = {
-                  loadOptions: loadTenantOptions(props.customer.uuid),
+                  loadOptions: query => fetchTenantOptions(query, props.customer.uuid),
                   placeholder: translate('Select tenant...'),
                 };
                 break;
               case 'select_openstack_instance':
                 OptionField = AsyncSelectField;
                 params = {
-                  loadOptions: loadInstanceOptions(props.customer.uuid),
+                  loadOptions: query => fetchInstanceOptions(query, props.customer.uuid),
                   placeholder: translate('Select instance...'),
                 };
                 break;
@@ -140,7 +140,7 @@ export class PureOfferingConfigurationForm extends React.Component<OfferingConfi
               name="attributes.schedules"
               excludedEvents={this.state.availableDates}
               label={translate('Select dates')}
-              />
+            />
           }
         </FormContainer>
       </form>
--- src/offering/api.ts
+++ src/offering/api.ts
@@ -1,29 +1,33 @@
-import { getList } from '@waldur/core/api';
+import { getAll } from '@waldur/core/api';
 
-export const fetchTenantOptions = ({ customerId }) =>
-  getList('/openstack-tenants/', {
-    field: ['name', 'backend_id'],
+export const fetchTenantOptions = (query, customerId) =>
+  getAll('/openstack-tenants/', {params: {
+    field: ['name', 'backend_id', 'project_name'],
     customer_uuid: customerId,
-  })
+    name: query,
+    o: ['project_name', 'name'],
+  }})
   .then(options => ({
     options: options.map(tenantSerializer),
   }));
 
-const tenantSerializer = ({ name, backend_id }) => ({
-  name,
+const tenantSerializer = ({ name, backend_id, project_name }) => ({
+  name: `${project_name} / ${name}`,
   value: `Tenant UUID: ${backend_id}. Name: ${name}`,
 });
 
-export const fetchInstanceOptions = ({ customerId }) =>
-  getList('/openstacktenant-instances/', {
-    field: ['name', 'backend_id'],
+export const fetchInstanceOptions = (query, customerId) =>
+  getAll('/openstacktenant-instances/', {params: {
+    field: ['name', 'backend_id', 'project_name'],
     customer_uuid: customerId,
-  })
+    name: query,
+    o: ['project_name', 'name'],
+  }})
   .then(options => ({
     options: options.map(instanceSerializer),
   }));
 
-const instanceSerializer = ({ name, backend_id }) => ({
-  name,
+const instanceSerializer = ({ name, backend_id, project_name }) => ({
+  name: `${project_name} / ${name}`,
   value: `Instance UUID: ${backend_id}. Name: ${name}`,
 });
