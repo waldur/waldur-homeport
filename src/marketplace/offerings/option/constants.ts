b982bf0b026fd70a63bd66cedd853e71e2fcf131
Victor Mireyev <victor@opennodecloud.com>
2019-07-12 15:06:24 +0300

    Implement widget to select multiple OpenStack instances in offering form [WAL-2531]

--- src/marketplace/offerings/option/constants.ts
+++ src/marketplace/offerings/option/constants.ts
@@ -38,6 +38,10 @@ export const FIELD_TYPES: Array<{value: FieldType, label: string}> = [
     label: 'Select OpenStack instance',
   },
   {
+    value: 'select_multiple_openstack_instances',
+    label: 'Select multiple OpenStack instances',
+  },
+  {
     value: 'date',
     label: 'Date',
   },
