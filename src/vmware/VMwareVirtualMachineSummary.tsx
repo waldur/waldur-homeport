24bd77682dfe59eebf4c723ac91fd51b53a22b13
Victor Mireyev <victor@opennodecloud.com>
2019-07-18 15:57:48 +0300

    Add "basic" mode or VMware provisioning with automated choices of elements [WAL-2553]

--- src/vmware/VMwareVirtualMachineSummary.tsx
+++ src/vmware/VMwareVirtualMachineSummary.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
+import { ENV } from '@waldur/core/services';
 import { withTranslation } from '@waldur/i18n';
 import { Field, PureResourceSummaryBase } from '@waldur/resource/summary';
 import { formatSummary } from '@waldur/resource/utils';
 
 const PureVMwareVirtualMachineSummary = props => {
   const { translate, resource } = props;
+  const advancedMode = !ENV.plugins.WALDUR_VMWARE.BASIC_MODE;
   return (
     <>
       <PureResourceSummaryBase {...props}/>
@@ -17,6 +19,26 @@ const PureVMwareVirtualMachineSummary = props => {
         label={translate('Guest OS')}
         value={resource.guest_os_name}
       />
+      {advancedMode && (
+        <>
+          <Field
+            label={translate('Template')}
+            value={resource.template_name}
+          />
+          <Field
+            label={translate('Cluster')}
+            value={resource.cluster_name}
+          />
+          <Field
+            label={translate('Datastore')}
+            value={resource.datastore_name}
+          />
+          <Field
+            label={translate('Networks')}
+            value={resource.networks.map(network => network.name).join(', ')}
+          />
+        </>
+      )}
     </>
   );
 };
