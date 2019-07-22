f316e9b4bf61696da7b81c3a9ead8d30fd5af727
Victor Mireyev <victor@opennodecloud.com>
2019-07-22 10:33:49 -0400

    Improvements related to VMware billing [WAL-2550]

--- src/vmware/VMwareVirtualMachineForm.tsx
+++ src/vmware/VMwareVirtualMachineForm.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { formValues } from 'redux-form';
+import { formValues, Field } from 'redux-form';
 
 import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
 import { Query } from '@waldur/core/Query';
@@ -18,6 +18,7 @@ interface Template {
   cores: number;
   cores_per_socket: number;
   ram: number;
+  disk: number;
 }
 
 const GuestOSField = formValues<any>({template: 'attributes.template'})(props => props.template ? (
@@ -34,6 +35,138 @@ interface Props extends OfferingConfigurationFormProps {
   };
 }
 
+const initAttributes = props => {
+  React.useEffect(() => {
+    const attributes = {...props.initialAttributes};
+    const initialData: Record<string, any> = {attributes};
+    if (props.plan) {
+      initialData.plan = props.plan;
+    } else if (props.offering.plans.length === 1) {
+      initialData.plan = props.offering.plans[0];
+    }
+    if (props.data.templates.length > 0) {
+      const template = props.data.templates[0];
+      initialData.attributes.template = template;
+      initialData.limits = {
+        cpu: template.cores,
+        ram: template.ram,
+        disk: template.disk,
+      };
+      initialData.attributes.cores_per_socket = template.cores_per_socket;
+    }
+    props.initialize(initialData);
+  }, []);
+};
+
+const StaticDiskField = () => (
+  <Field
+    name="limits.disk"
+    component={fieldProps => fieldProps.input.value ? (
+      <StaticField
+        label={translate('Storage size in MiB')}
+        value={fieldProps.input.value}
+        labelClass="col-sm-3"
+        controlClass="col-sm-9"
+      />
+    ) : null}
+  />
+);
+
+const FormComponent = props => {
+  const advancedMode = !ENV.plugins.WALDUR_VMWARE.BASIC_MODE;
+  initAttributes(props);
+  return (
+    <form className="form-horizontal">
+      <FormContainer
+        submitting={false}
+        labelClass="col-sm-3"
+        controlClass="col-sm-9">
+        <ProjectField/>
+        <StringField
+          label={translate('Name')}
+          name="attributes.name"
+          description={translate('This name will be visible in accounting data.')}
+          validate={required}
+          required={true}
+        />
+        {props.data.templates.length > 0 && (
+          <SelectField
+            label={translate('Template')}
+            name="attributes.template"
+            required={true}
+            clearable={false}
+            validate={required}
+            options={props.data.templates}
+            labelKey="name"
+            valueKey="url"
+            onChange={(value: Template) => {
+              props.change('limits.cpu', value.cores);
+              props.change('limits.ram', value.ram);
+              props.change('limits.disk', value.disk);
+              props.change('attributes.cores_per_socket', value.cores_per_socket);
+            }}
+          />
+        )}
+        <NumberField
+          label={translate('Number of cores in a VM')}
+          name="limits.cpu"
+        />
+        <NumberField
+          label={translate('Number of CPU cores per socket')}
+          name="attributes.cores_per_socket"
+        />
+        <NumberField
+          label={translate('Memory size in MiB')}
+          name="limits.ram"
+        />
+        <StaticDiskField/>
+        <GuestOSField/>
+        {advancedMode && props.data.clusters.length > 0 && (
+          <SelectField
+            label={translate('Cluster')}
+            name="attributes.cluster"
+            options={props.data.clusters}
+            labelKey="name"
+            valueKey="url"
+          />
+        )}
+        {advancedMode && props.data.datastores.length > 0 && (
+          <SelectField
+            label={translate('Datastore')}
+            name="attributes.datastore"
+            options={props.data.datastores}
+            labelKey="name"
+            valueKey="url"
+          />
+        )}
+        {advancedMode && props.data.folders.length > 0 && (
+          <SelectField
+            label={translate('Folder')}
+            name="attributes.folder"
+            options={props.data.folders}
+            labelKey="name"
+            valueKey="url"
+          />
+        )}
+        {advancedMode && props.data.networks.length > 0  && (
+          <SelectField
+            label={translate('Networks')}
+            name="attributes.networks"
+            options={props.data.networks}
+            labelKey="name"
+            valueKey="url"
+            multi={true}
+          />
+        )}
+        <TextField
+          label={translate('Description')}
+          name="attributes.description"
+        />
+      </FormContainer>
+    </form>
+  );
+};
+
 export const VMwareVirtualMachineForm = connector((props: Props) => (
   <Query variables={props.variable} loader={loadFormOptions}>
     {({ loading, error, data }) => {
@@ -43,93 +176,7 @@ export const VMwareVirtualMachineForm = connector((props: Props) => (
       if (error) {
         return <span>{translate('Unable to load form options.')}</span>;
       }
-      const advancedMode = !ENV.plugins.WALDUR_VMWARE.BASIC_MODE;
-      return (
-        <form className="form-horizontal">
-          <FormContainer
-            submitting={false}
-            labelClass="col-sm-3"
-            controlClass="col-sm-9">
-            <ProjectField/>
-            <StringField
-              label={translate('Name')}
-              name="attributes.name"
-              description={translate('This name will be visible in accounting data.')}
-              validate={required}
-              required={true}
-            />
-            <SelectField
-              label={translate('Template')}
-              name="attributes.template"
-              required={true}
-              clearable={false}
-              validate={required}
-              options={data.templates}
-              labelKey="name"
-              valueKey="url"
-              onChange={(value: Template) => {
-                props.change('attributes.cores', value.cores);
-                props.change('attributes.cores_per_socket', value.cores_per_socket);
-                props.change('attributes.ram', value.ram);
-              }}
-            />
-            <GuestOSField/>
-            <NumberField
-              label={translate('Number of cores in a VM')}
-              name="attributes.cores"
-            />
-            <NumberField
-              label={translate('Number of CPU cores per socket')}
-              name="attributes.cores_per_socket"
-            />
-            <NumberField
-              label={translate('Memory size in MiB')}
-              name="attributes.ram"
-            />
-            {advancedMode && data.clusters.length > 0 && (
-              <SelectField
-                label={translate('Cluster')}
-                name="attributes.cluster"
-                options={data.clusters}
-                labelKey="name"
-                valueKey="url"
-              />
-            )}
-            {advancedMode && data.datastores.length > 0 && (
-              <SelectField
-                label={translate('Datastore')}
-                name="attributes.datastore"
-                options={data.datastores}
-                labelKey="name"
-                valueKey="url"
-              />
-            )}
-            {advancedMode && data.folders.length > 0 && (
-              <SelectField
-                label={translate('Folder')}
-                name="attributes.folder"
-                options={data.folders}
-                labelKey="name"
-                valueKey="url"
-              />
-            )}
-            {advancedMode && data.networks.length > 0  && (
-              <SelectField
-                label={translate('Networks')}
-                name="attributes.networks"
-                options={data.networks}
-                labelKey="name"
-                valueKey="url"
-                multi={true}
-              />
-            )}
-            <TextField
-              label={translate('Description')}
-              name="attributes.description"
-            />
-          </FormContainer>
-        </form>
-      );
+      return <FormComponent {...props} data={data}/>;
     }}
   </Query>
 ));
