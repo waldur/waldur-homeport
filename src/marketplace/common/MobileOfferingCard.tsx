cb79dab8248de82e3375b9195b16119d21e2936d
Victor Mireyev <victor@opennodecloud.com>
2019-08-21 13:23:21 +0300

    Render reason why offering has been paused as a tooltip [WAL-2613]

--- src/marketplace/common/MobileOfferingCard.tsx
+++ src/marketplace/common/MobileOfferingCard.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 import { OfferingLogo } from '@waldur/marketplace/common/OfferingLogo';
 import { OfferingLink } from '@waldur/marketplace/links/OfferingLink';
+import { wrapTooltip } from '@waldur/table-react/ActionButton';
 
 import { Offering } from '../types';
 
@@ -10,7 +11,8 @@ interface MobileOfferingCardProps {
   offering: Offering;
 }
 
-export const MobileOfferingCard = (props: MobileOfferingCardProps) => (
+export const MobileOfferingCard = (props: MobileOfferingCardProps) => wrapTooltip(
+  props.offering.paused_reason, (
   <tr className={classNames({disabled: props.offering.state !== 'Active'})}>
     <td className="img-md">
       <OfferingLink offering_uuid={props.offering.uuid}>
@@ -26,4 +28,4 @@ export const MobileOfferingCard = (props: MobileOfferingCardProps) => (
       {props.offering.description}
     </td>
   </tr>
-);
+));
