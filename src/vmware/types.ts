ebca3c72dcef18597726949015d0f930bfd3a5fe
Victor Mireyev <victor@opennodecloud.com>
2019-08-27 13:20:46 +0300

    Use VMware tools state from SOAP client.

--- src/vmware/types.ts
+++ src/vmware/types.ts
@@ -2,6 +2,7 @@ import { Resource } from '@waldur/resource/types';
 
 export interface VMwareVirtualMachine extends Resource {
   guest_power_state: string;
+  tools_state: string;
   guest_os_name: string;
   template_name?: string;
   cluster_name?: string;
