97f2aba91cfe7f19409ee8930e258f3e094b9afc
Victor Mireyev <victor@opennodecloud.com>
2019-09-24 15:51:06 +0300

    Allow to specify limits for builtin components [WAL-2633]

--- src/marketplace/offerings/create/AccountingStepContainer.tsx
+++ src/marketplace/offerings/create/AccountingStepContainer.tsx
@@ -5,13 +5,14 @@ import { withTranslation } from '@waldur/i18n';
 import { showComponentsList } from '@waldur/marketplace/common/registry';
 
 import { removeOfferingComponent, removeOfferingQuotas } from '../store/actions';
-import { getType } from '../store/selectors';
+import { getType, getOfferingComponents } from '../store/selectors';
 import { AccountingStep } from './AccountingStep';
 
 const mapStateToProps = state => {
   const type = getType(state);
   const showComponents = type && showComponentsList(type);
-  return {showComponents, type};
+  const builtinComponents = getOfferingComponents(state, type);
+  return {showComponents, type, builtinComponents};
 };
 
 const mapStateToDispatch = {
