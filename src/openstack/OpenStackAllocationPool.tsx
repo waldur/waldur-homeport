46f50eca26a13631dc0f47474d1270470ff2200d
Victor Mireyev <victor@opennodecloud.com>
2019-11-14 19:09:57 +0300

    Add possibility to create arbitrary subnets conforming to RFC1918 [WAL-2631]

--- src/openstack/OpenStackAllocationPool.tsx
+++ src/openstack/OpenStackAllocationPool.tsx
@@ -3,22 +3,14 @@ import { formValues } from 'redux-form';
 
 import { FormField } from '@waldur/form-react/types';
 
-import { extractSubnet } from './utils';
-
-interface OpenStackAllocationPoolProps extends FormField {
-  range: string;
-}
-
-const enhance = formValues<any, any, OpenStackAllocationPoolProps>('attributes.subnet_cidr');
+const enhance = formValues<any, any, FormField>('attributes.subnet_cidr');
 
 export const OpenStackAllocationPool = enhance(props => {
-  let subnetCidr = props['attributes.subnet_cidr'];
-  if (isNaN(subnetCidr)) {
-    subnetCidr = extractSubnet(subnetCidr);
-  }
+  const subnetCidr = props['attributes.subnet_cidr'] || '';
+  const prefix = subnetCidr.split('.').slice(0, 3).join('.');
   return (
     <div className="form-control-static">
-      {subnetCidr ? props.range.replace(/X/g, subnetCidr) : '-'}
+      {prefix ? `${prefix}.10 - ${prefix}.200` : '-'}
     </div>
   );
 });
