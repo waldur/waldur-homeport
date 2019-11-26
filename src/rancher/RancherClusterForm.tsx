6825ab06a4bcd9a3925d261d41c47de9c081b7d4
Victor Mireyev <victor@opennodecloud.com>
2019-11-26 19:22:20 +0300

    Allow selecting target Subnet when deploying a Rancher cluster [WAL-2699]

--- src/rancher/RancherClusterForm.tsx
+++ src/rancher/RancherClusterForm.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { FieldArray } from 'redux-form';
 
+import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
+import { useQuery } from '@waldur/core/useQuery';
 import { required } from '@waldur/core/validators';
 import { FormContainer, StringField, TextField } from '@waldur/form-react';
 import { translate } from '@waldur/i18n';
@@ -8,50 +10,65 @@ import { PlanDetailsTable } from '@waldur/marketplace/details/plan/PlanDetailsTa
 import { PlanField } from '@waldur/marketplace/details/plan/PlanField';
 import { ProjectField } from '@waldur/marketplace/details/ProjectField';
 import { OfferingConfigurationFormProps } from '@waldur/marketplace/types';
+import { loadSubnets } from '@waldur/openstack/api';
 
 import { DEFAULT_CLUSTER_CONFIGURATION } from './constants';
 import { NodeList } from './NodeList';
 import { rancherClusterName } from './utils';
 
-export class RancherClusterForm extends React.Component<OfferingConfigurationFormProps> {
-  componentDidMount() {
-    const { project, plan } = this.props;
+const fetchResource = serviceSettings => loadSubnets(serviceSettings)
+  .then(subnets => subnets.map(subnet => ({
+    label: `${subnet.network_name} / ${subnet.name}`,
+    value: subnet.url,
+  })));
+
+export const RancherClusterForm: React.FC<OfferingConfigurationFormProps> = props => {
+  React.useEffect(() => {
+    const { project, plan } = props;
     const initialData = {
       project,
       plan,
       attributes: DEFAULT_CLUSTER_CONFIGURATION,
     };
-    if (!plan && this.props.offering.plans.length === 1) {
-      initialData.plan = this.props.offering.plans[0];
+    if (!plan && props.offering.plans.length === 1) {
+      initialData.plan = props.offering.plans[0];
     }
-    this.props.initialize(initialData);
+    props.initialize(initialData);
+  }, []);
+
+  const {state: resourceProps, call: loadResource} = useQuery(fetchResource, props.offering.scope_uuid);
+  React.useEffect(loadResource, []);
+
+  if (!resourceProps.loaded) {
+    return <LoadingSpinner/>;
   }
 
-  render() {
-    const props = this.props;
-    return (
-      <form className="form-horizontal">
-        <FormContainer
-          submitting={props.submitting}
-          labelClass="col-sm-3"
-          controlClass="col-sm-9">
-          <ProjectField/>
-          <StringField
-            label={translate('Cluster name')}
-            name="attributes.name"
-            description={translate('This name will be visible in accounting data.')}
-            validate={[required, rancherClusterName]}
-            required={true}
-          />
-          <PlanField offering={props.offering}/>
-          <PlanDetailsTable offering={props.offering} limits={props.limits}/>
-          <TextField
-            label={translate('Cluster description')}
-            name="attributes.description"
-          />
-          <FieldArray name="attributes.nodes" component={NodeList}/>
-        </FormContainer>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="form-horizontal">
+      <FormContainer
+        submitting={props.submitting}
+        labelClass="col-sm-3"
+        controlClass="col-sm-9">
+        <ProjectField/>
+        <StringField
+          label={translate('Cluster name')}
+          name="attributes.name"
+          description={translate('This name will be visible in accounting data.')}
+          validate={[required, rancherClusterName]}
+          required={true}
+        />
+        <PlanField offering={props.offering}/>
+        <PlanDetailsTable offering={props.offering} limits={props.limits}/>
+        <TextField
+          label={translate('Cluster description')}
+          name="attributes.description"
+        />
+        <FieldArray
+          name="attributes.nodes"
+          component={NodeList}
+          subnetChoices={resourceProps.data}
+        />
+      </FormContainer>
+    </form>
+  );
+};
