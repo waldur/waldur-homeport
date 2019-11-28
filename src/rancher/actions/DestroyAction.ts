674fdfcea7663f11cc5f9c5d8053fae42e332028
Victor Mireyev <victor@opennodecloud.com>
2019-11-28 21:20:11 +0300

    Add support for linking Rancher K8S nodes with OpenStack instances [WAL-2698]

---
+++ src/rancher/actions/DestroyAction.ts
@@ -0,0 +1,14 @@
+import { translate } from '@waldur/i18n';
+import { validateState } from '@waldur/resource/actions/base';
+import { ResourceAction } from '@waldur/resource/actions/types';
+
+export default function createAction(): ResourceAction {
+  return {
+    name: 'destroy',
+    type: 'form',
+    method: 'DELETE',
+    destructive: true,
+    title: translate('Destroy'),
+    validators: [validateState('OK', 'Erred')],
+  };
+}
--- src/rancher/module.js
+++ src/rancher/module.js
@@ -1,5 +1,34 @@
+import actions from './actions';
 import './marketplace';
 import './provider';
+import rancherNodesService from './rancher-nodes-service';
+import rancherClusterNodes from './rancher-cluster-nodes';
 
-export default () => {
+export default module => {
+  module.service('rancherNodesService', rancherNodesService);
+  module.component('rancherClusterNodes', rancherClusterNodes);
+  module.config(actionsConfig);
+  module.config(tabsConfig);
 };
+
+
+// @ngInject
+function actionsConfig(ActionConfigurationProvider) {
+  ActionConfigurationProvider.register('Rancher.Node', actions);
+}
+
+// @ngInject
+function tabsConfig(ResourceTabsConfigurationProvider, DEFAULT_RESOURCE_TABS) {
+  ResourceTabsConfigurationProvider.register('Rancher.Cluster', {
+    order: [
+      'nodes',
+      ...DEFAULT_RESOURCE_TABS.order,
+    ],
+    options: angular.merge({}, DEFAULT_RESOURCE_TABS.options, {
+      snapshots: {
+        heading: gettext('Nodes'),
+        component: 'rancherClusterNodes'
+      },
+    })
+  });
+}
