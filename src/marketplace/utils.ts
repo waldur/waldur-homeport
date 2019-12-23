0022be544feb6e8638cd9b0109304c43ae81085a
Victor Mireyev <victor@opennodecloud.com>
2019-12-23 13:19:41 +0300

    Resource tab menu should be expanded with category highlighted in resource detail view [WAL-2442]

--- src/marketplace/utils.ts
+++ src/marketplace/utils.ts
@@ -1,3 +1,5 @@
 import { ngInjector } from '@waldur/core/services';
 
 export const marketplaceIsVisible = () => ngInjector.get('features').isVisible('marketplace');
+
+export const getCategoryLink = id => `marketplace-project-resources({uuid: $ctrl.context.project.uuid, category_uuid: '${id}'})`;
