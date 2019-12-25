27ec11f90f4ce80bf1b42a9f1e5d64efab80ce9d
Victor Mireyev <victor@opennodecloud.com>
2019-12-25 13:27:50 +0300

    Expose access link to Rancher self-service for rancher clusters [WAL-2757]

---
+++ src/rancher/RancherClusterSummary.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import { withTranslation } from '@waldur/i18n';
+import { Field, ResourceSummaryProps, PureResourceSummaryBase } from '@waldur/resource/summary';
+import { ResourceAccessInfo } from '@waldur/resource/summary/ResourceAccessInfo';
+
+const PureRancherClusterSummary = (props: ResourceSummaryProps) => {
+  const { translate } = props;
+  return (
+    <>
+      <PureResourceSummaryBase {...props}/>
+      <Field
+        label={translate('Access')}
+        value={<ResourceAccessInfo {...props}/>}
+      />
+    </>
+  );
+};
+
+export const RancherClusterSummary = withTranslation(PureRancherClusterSummary);
