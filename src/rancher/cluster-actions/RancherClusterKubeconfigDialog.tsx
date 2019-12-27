1b0e36d219dce255d13ded0ea07eac50bce0e382
Victor Mireyev <victor@opennodecloud.com>
2019-12-27 10:48:38 +0300

    Fix MR comments.

--- src/rancher/cluster-actions/RancherClusterKubeconfigDialog.tsx
+++ src/rancher/cluster-actions/RancherClusterKubeconfigDialog.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { get } from '@waldur/core/api';
 import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
@@ -8,27 +9,40 @@ import { translate } from '@waldur/i18n';
 import { CloseDialogButton } from '@waldur/modal/CloseDialogButton';
 import { ModalDialog } from '@waldur/modal/ModalDialog';
 import { connectAngularComponent } from '@waldur/store/connect';
+import { showSuccess } from '@waldur/store/coreSaga';
 
 const getKubeconfigFile = resourceId =>
   get(`/rancher-clusters/${resourceId}/kubeconfig_file/`)
   .then(response => response.data.config);
 
-const KubeconfigFilePanel = props => (
-  <>
-    <p>
-      {translate('Put this into ~/.kube/config:')}
-    </p>
-    <pre style={{height: 200}}>{props.config}</pre>
-    <a onClick={() => copyToClipboard(props.config)}>
-      {translate('Copy to clipboard')}
-    </a>
-    <p>
-      <a href="http://kubernetes.io/docs/user-guide/prereqs/">
-        {translate('Then download (if needed) and run kubectl')}
-      </a>
-    </p>
-  </>
-);
+const KubeconfigFilePanel = props => {
+  const dispatch = useDispatch();
+  const onClick = React.useCallback(() => {
+    copyToClipboard(props.config);
+    dispatch(showSuccess(translate('File has been copied')));
+  }, []);
+
+  return (
+    <>
+      <p>
+        {translate('Put this into ~/.kube/config:')}
+      </p>
+      <pre style={{height: 200}}>{props.config}</pre>
+      <p className="m-b-sm m-t-sm">
+        <a onClick={onClick}>
+          <i className="fa fa-copy"/>
+          {' '}
+          {translate('Copy to clipboard')}
+        </a>
+      </p>
+      <p>
+        <a href="http://kubernetes.io/docs/user-guide/prereqs/" target="_blank">
+          {translate('Then download (if needed) and run kubectl')}
+        </a>
+      </p>
+    </>
+  );
+};
 
 export const RancherClusterKubeconfigDialog = props => {
   const {state: resourceProps, call: loadResource} = useQuery(getKubeconfigFile, props.resolve.resource.uuid);
