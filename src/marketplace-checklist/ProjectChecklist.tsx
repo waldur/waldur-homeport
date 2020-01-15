cff390647c0b8b2b92963275bbe78fc2aba7da24
Victor Mireyev <victor@opennodecloud.com>
2020-01-15 20:04:08 +0300

    Replace checkbox with toggle button group [WAL-2778]

--- src/marketplace-checklist/ProjectChecklist.tsx
+++ src/marketplace-checklist/ProjectChecklist.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as Button from 'react-bootstrap/lib/Button';
 import * as Table from 'react-bootstrap/lib/Table';
+import * as ToggleButton from 'react-bootstrap/lib/ToggleButton';
+import * as ToggleButtonGroup from 'react-bootstrap/lib/ToggleButtonGroup';
 import { useSelector } from 'react-redux';
 import Select from 'react-select';
 
@@ -11,6 +13,91 @@ import { connectAngularComponent } from '@waldur/store/connect';
 import { getProject } from '@waldur/workspace/selectors';
 
 import { useChecklist } from './useChecklist';
+
+const AnswerGroup = ({ answers, question, setAnswers }) => (
+  <ToggleButtonGroup
+    value={{true: 'true', false: 'false', null: 'null'}[answers[question.uuid]]}
+    onChange={value => setAnswers({
+      ...answers,
+      [question.uuid]: {true: true, false: false, null: null}[value],
+    })}
+    type="radio"
+    name={`checklist-${question.uuid}`}
+    defaultValue="null">
+    <ToggleButton value="true">
+      {translate('Yes')}
+    </ToggleButton>
+    <ToggleButton value="false">
+      {translate('No')}
+    </ToggleButton>
+    <ToggleButton value="null">
+      {translate('Not provided')}
+    </ToggleButton>
+  </ToggleButtonGroup>
+);
+
+const SubmitButton = ({ submitting, submit }) => (
+  <Button
+    onClick={() => submit()}
+    bsStyle="primary"
+    bsSize="small"
+    disabled={submitting}>
+    {submitting && <><i className="fa fa-spinner fa-spin"/>{' '}</>}
+    {translate('Submit')}
+  </Button>
+);
+
+const CategoryLink = ({ question }) =>
+  question.category_uuid ? (
+    <p>
+      <Link
+        state="marketplace-category"
+        params={{category_uuid: question.category_uuid}}
+        label={translate('Go to marketplace')}
+      />
+    </p>
+  ) : null;
+
+const AnswersTable = ({ checklist, answers, setAnswers }) => (
+  <Table
+    responsive={true}
+    bordered={true}
+    striped={true}
+    className="m-t-md"
+  >
+    <thead>
+      <tr>
+        <th className="col-sm-1">
+          #
+        </th>
+        <th>{translate('Question')}</th>
+        <th className="col-sm-3 text-center">
+          {translate('Answer')}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      {checklist.questions.map((question, index) => (
+        <tr key={question.uuid}>
+          <td>
+            {index + 1}
+          </td>
+          <td>
+            {question.description}
+            <CategoryLink question={question}/>
+          </td>
+          <td className="text-center">
+            <AnswerGroup
+              question={question}
+              answers={answers}
+              setAnswers={setAnswers}
+            />
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
+);
 
 const ProjectChecklist = () => {
   const project = useSelector(getProject);
@@ -37,64 +124,16 @@ const ProjectChecklist = () => {
           onChange={state.setChecklist}
           options={state.data.questions}
         />
-        <Table
-          responsive={true}
-          bordered={true}
-          striped={true}
-          className="m-t-md"
-        >
-          <thead>
-            <tr>
-              <th className="col-sm-1">
-                #
-              </th>
-              <th>{translate('Question')}</th>
-              <th className="col-sm-1">
-                {translate('Answer')}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {state.checklist.questions.map((question, index) => (
-              <tr key={question.uuid}>
-                <td>
-                  {index + 1}
-                </td>
-                <td>
-                  {question.description}
-                  {question.category_uuid && (
-                    <p>
-                      <Link
-                        state="marketplace-category"
-                        params={{category_uuid: question.category_uuid}}
-                        label={translate('Go to marketplace')}
-                      />
-                    </p>
-                  )}
-                </td>
-                <td>
-                  <input
-                    type="checkbox"
-                    checked={state.answers[question.uuid] || false}
-                    onChange={event => state.
-                      setAnswers({
-                      ...state.answers,
-                      [question.uuid]: event.target.checked,
-                    })}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        <AnswersTable
+          checklist={state.checklist}
+          answers={state.answers}
+          setAnswers={state.setAnswers}
+        />
         <p>
-          <Button
-            onClick={() => state.submit()}
-            bsStyle="primary"
-            disabled={state.submitting}>
-            {state.submitting && <><i className="fa fa-spinner fa-spin"/>{' '}</>}
-            {translate('Submit')}
-          </Button>
+          <SubmitButton
+            submit={state.submit}
+            submitting={state.submitting}
+          />
         </p>
       </>
     );
