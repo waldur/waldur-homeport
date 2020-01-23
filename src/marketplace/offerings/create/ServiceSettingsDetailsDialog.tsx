853611b537a63ed23e9ea45278c20016eb717cfe
Victor Mireyev <victor@opennodecloud.com>
2020-01-23 16:55:59 +0300

    Allow user to edit service settings from marketplace [WAL-2792]

---
+++ src/marketplace/offerings/create/ServiceSettingsDetailsDialog.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
+import { $http } from '@waldur/core/services';
+import { useQuery } from '@waldur/core/useQuery';
+import { translate } from '@waldur/i18n';
+import { CloseDialogButton } from '@waldur/modal/CloseDialogButton';
+import { ModalDialog } from '@waldur/modal/ModalDialog';
+import * as actions from '@waldur/providers/actions';
+import { ProviderUpdateForm } from '@waldur/providers/ProviderUpdateForm';
+import { findProvider } from '@waldur/providers/registry';
+import { connectAngularComponent } from '@waldur/store/connect';
+
+import { getOffering } from '../store/selectors';
+
+export const ServiceSettingsDetailsDialog = () => {
+  const offering = useSelector(getOffering);
+  const {state, call} = useQuery(async () => {
+    const provider = (await $http.get(offering.offering.scope)).data;
+    return {
+      initialValues: {
+        name: provider.name,
+        type: findProvider(provider.type),
+        details: provider,
+      },
+      type: findProvider(provider.type),
+    };
+  });
+  React.useEffect(call, []);
+
+  const dispatch = useDispatch();
+  const updateProvider = React.useCallback(data => actions.updateProvider(data, dispatch), []);
+
+  return (
+    <ModalDialog
+      title={translate('Provider details')}
+      footer={<CloseDialogButton />}
+    >
+      {state.loading ? <LoadingSpinner/> :
+       state.error ? <h3>{translate('Unable to load provider details.')}</h3> :
+       !state.loaded ? null : (
+        <ProviderUpdateForm
+          {...state.data}
+          updateProvider={updateProvider}
+          translate={translate}
+        />
+       )
+      }
+    </ModalDialog>
+  );
+};
+
+export default connectAngularComponent(ServiceSettingsDetailsDialog);
