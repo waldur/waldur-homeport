8a730b611f466640f1d497c01b46400bfc53f833
Victor Mireyev <victor@opennodecloud.com>
2020-02-08 03:15:31 +0300

    Allow to request new email in initial data view [WAL-2841]

--- src/user/support/UserEmailChangeDialog.tsx
+++ src/user/support/UserEmailChangeDialog.tsx
@@ -2,33 +2,17 @@ import * as React from 'react';
 import * as Button from 'react-bootstrap/lib/Button';
 import { useDispatch } from 'react-redux';
 
-import { post } from '@waldur/core/api';
-import { format } from '@waldur/core/ErrorMessageFormatter';
 import { translate } from '@waldur/i18n';
 import { closeModalDialog } from '@waldur/modal/actions';
 import { ModalDialog } from '@waldur/modal/ModalDialog';
 import { connectAngularComponent } from '@waldur/store/connect';
-import { showError, showSuccess } from '@waldur/store/coreSaga';
-import { userUpdated } from '@waldur/workspace/actions';
+
+import { useEmailChange } from './useEmailChange';
 
 export const UserEmailChangeDialog = ({ resolve: {user} }) => {
-  const [email, setEmail] = React.useState('');
-  const [submitting, setSubmitting] = React.useState(false);
   const dispatch = useDispatch();
 
-  const handleSubmit = React.useCallback(async () => {
-    setSubmitting(false);
-    try {
-      await post(`/users/${user.uuid}/change_email/`,  {email});
-    } catch (error) {
-      const errorMessage = `${translate('Unable to change email.')} ${format(error)}`;
-      dispatch(showError(errorMessage));
-      return;
-    }
-    dispatch(showSuccess(translate('Email verification has been sent. Please check your inbox.')));
-    dispatch(closeModalDialog());
-    dispatch(userUpdated({...user, requested_email: email}));
-  }, [email]);
+  const {handleSubmit, submitting, email, setEmail} = useEmailChange(user);
 
   const handleClose = React.useCallback(() => {
     dispatch(closeModalDialog());
