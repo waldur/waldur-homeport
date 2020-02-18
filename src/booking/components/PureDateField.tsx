cbdaf0ba929c00be83625bc57ae63e18d52dbe30
Olaf Viikna <olaf.viikna@ut.ee>
2020-02-18 17:18:42 +0200

    Remove clearing button and add translation

--- src/booking/components/PureDateField.tsx
+++ src/booking/components/PureDateField.tsx
@@ -4,30 +4,35 @@ import * as React from 'react';
 import * as DatePicker from 'react-16-bootstrap-date-picker';
 
 import {TimeSelectField} from '@waldur/booking/components/TimeSelectField';
+import {translate} from '@waldur/i18n';
 
 import { PureDateProps } from '../store/types';
 
-const handleChange = (newDate, newTime): Date => moment(`${newDate} ${newTime}`, 'DD-MM-YYYY HH:mm', true).toDate();
+const handleChange = (newDate, newTime): Date =>
+  moment(`${newDate} ${newTime}`, 'DD-MM-YYYY HH:mm', true).toDate();
 
 export const PureDateField = (props: PureDateProps) => (
   <>
-    {props.label && <label className="col-sm-1 control-label" htmlFor={`${props.name}-date-picker`}>{props.label}</label>}
-    <DatePicker
-      bsClass={classNames(props.className,
-        'date-input-group',
-        props.label ? 'col-sm-5' : 'col-sm-6',
-        props.withTime ? 'col-sm-6' : 'col-sm-6 col-sm-center',
-        { disabled: props.isDisabled })}
-      name={`${props.name}-date-picker`}
-      weekStartsOn={1}
-      showTodayButton={true}
-      todayButtonLabel={'Today'}
-      dateFormat="DD-MM-YYYY"
-      value={moment(props.value).format()}
-      onChange={ (_, formattedValue) => props.onChange(
-        handleChange( formattedValue, moment(props.value).format('HH:mm') )
-      )}
-    />
+    {props.label && <label className="col-xs-2 control-label" htmlFor={`${props.name}-date-picker`}>{props.label}</label>}
+    <div className={classNames(
+      props.className,
+      props.label ? 'col-xs-10 col-md-4' : 'col-xs-12 col-md-6',
+      props.withTime ? 'col-sm-6' : '',
+      { disabled: props.isDisabled }
+      )}>
+      <DatePicker
+        name={`${props.name}-date-picker`}
+        weekStartsOn={1}
+        showClearButton={false}
+        showTodayButton={true}
+        todayButtonLabel={translate('Today')}
+        dateFormat="DD-MM-YYYY"
+        value={moment(props.value).format()}
+        onChange={ (_, formattedValue) => props.onChange(
+          handleChange( formattedValue, moment(props.value).format('HH:mm') )
+        )}
+      />
+    </div>
     {props.withTime &&
       <TimeSelectField
         {...props.withTime}
