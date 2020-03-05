f7e60642f8db5b1befc6ed62a06d65f496da32f2
Victor Mireyev <victor@opennodecloud.com>
2020-03-05 14:56:27 +0300

    Refactor autoformatting.

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,12 @@ module.exports = {
   singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
+  "overrides": [
+    {
+      "files": "src/**/*.html",
+      "options": {
+        "parser": "angular"
+      }
+    }
+  ]
 };
