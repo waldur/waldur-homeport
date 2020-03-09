78d552ae697442b096d86185480cbffc914a5f02
Victor Mireyev <victor@opennodecloud.com>
2020-03-09 17:45:32 +0300

    Drop service project link management.

--- src/services/module.js
+++ src/services/module.js
@@ -1,17 +1,10 @@
 import currentStateService from './current-state-service';
-import joinService from './join-service';
-import joinServiceProjectLinkService from './join-service-project-link-service';
 import priceEstimationService from './price-estimation-service';
 import projectPermissionsService from './project-permissions-service';
 import quotasService from './quotas-service';
 
 export default module => {
   module.service('currentStateService', currentStateService);
-  module.service('joinService', joinService);
-  module.service(
-    'joinServiceProjectLinkService',
-    joinServiceProjectLinkService,
-  );
   module.service('priceEstimationService', priceEstimationService);
   module.service('projectPermissionsService', projectPermissionsService);
   module.service('quotasService', quotasService);
