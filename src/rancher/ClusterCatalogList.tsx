9cc78e7cc0b004b5f8cb8f4df0dca4da02be9ea2
Victor Mireyev <victor@opennodecloud.com>
2020-03-10 12:52:57 +0300

    Allow to list cluster's catalogs [WAL-2859]

---
+++ src/rancher/ClusterCatalogList.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+import { connectAngularComponent } from '@waldur/store/connect';
+import { Table, connectTable, createFetcher } from '@waldur/table-react';
+
+const TableComponent = props => {
+  const { translate } = props;
+  return (
+    <Table
+      {...props}
+      columns={[
+        {
+          title: translate('Name'),
+          render: ({ row }) => <span>{row.name}</span>,
+        },
+        {
+          title: translate('Description'),
+          render: ({ row }) => <span>{row.description}</span>,
+        },
+        {
+          title: translate('URL'),
+          render: ({ row }) => <span>{row.catalog_url}</span>,
+        },
+      ]}
+      verboseName={translate('catalogs')}
+    />
+  );
+};
+
+const TableOptions = {
+  table: 'rancher-catalogs',
+  fetchData: createFetcher('rancher-catalogs'),
+  exportFields: ['name', 'description', 'catalog_url'],
+  exportRow: row => [row.name, row.description, row.catalog_url],
+  mapPropsToFilter: props => ({
+    cluster_uuid: props.resource.uuid,
+  }),
+};
+
+const ClusterCatalogList = connectTable(TableOptions)(TableComponent);
+
+export default connectAngularComponent(ClusterCatalogList, ['resource']);
