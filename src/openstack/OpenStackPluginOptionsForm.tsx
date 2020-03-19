1d0968d6fb91711d686d8a76ec0c27753c82696f
Victor Mireyev <victor@opennodecloud.com>
2020-03-19 22:42:05 +0300

    Fix MR comments.

--- src/openstack/OpenStackPluginOptionsForm.tsx
+++ src/openstack/OpenStackPluginOptionsForm.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
+import { formValueSelector } from 'redux-form';
 
 import { FormContainer, SelectField, NumberField } from '@waldur/form-react';
 import { translate } from '@waldur/i18n';
+import { FORM_ID } from '@waldur/marketplace/offerings/store/constants';
+
+const pluginOptionsSelector = state =>
+  formValueSelector(FORM_ID)(state, 'plugin_options');
 
 export const OpenStackPluginOptionsForm = ({ container }) => {
   const STORAGE_MODE_OPTIONS = React.useMemo(
@@ -20,6 +26,8 @@ export const OpenStackPluginOptionsForm = ({ container }) => {
     [],
   );
 
+  const pluginOptions = useSelector(pluginOptionsSelector);
+
   return (
     <FormContainer {...container}>
       <SelectField
@@ -29,11 +37,16 @@ export const OpenStackPluginOptionsForm = ({ container }) => {
         simpleValue={true}
         required={true}
       />
-      <NumberField
-        name="snapshot_size_limit_gb"
-        label={translate('Snapshot size limit')}
-        unit="GB"
-      />
+      {pluginOptions.storage_mode == 'dynamic' && (
+        <NumberField
+          name="snapshot_size_limit_gb"
+          label={translate('Snapshot size limit')}
+          unit="GB"
+          description={translate(
+            'Additional space to apply to storage quota to be used by snapshots.',
+          )}
+        />
+      )}
     </FormContainer>
   );
 };
