6106630635d3a20529a5a970decd9878af14090b
Michael Alex <michaelaleks.dev@gmail.com>
2020-03-21 11:56:57 -0400

    Make "Running accounting" default value for a filter in Financial overview [WAL-2927]

--- src/customer/CustomerListFilter.tsx
+++ src/customer/CustomerListFilter.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import * as Col from 'react-bootstrap/lib/Col';
 import * as Row from 'react-bootstrap/lib/Row';
+import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
+
+import { translate } from '@waldur/i18n';
 
 import { AccountingPeriodField } from './AccountingPeriodField';
 import { AccountingRunningField } from './AccountingRunningField';
@@ -11,7 +14,7 @@ interface Props {
   accountingPeriods: AccountingPeriodOption[];
 }
 
-export const PureCustomerListFilter = (props: Props) => (
+const PureCustomerListFilter = (props: Props) => (
   <div className="ibox">
     <div className="ibox-content m-b-sm border-bottom">
       <form className="form-inline">
@@ -28,6 +31,19 @@ export const PureCustomerListFilter = (props: Props) => (
   </div>
 );
 
-export const CustomerListFilter = reduxForm<{}, Props>({
+let CustomerListFilter: any = reduxForm<{}, Props>({
   form: 'customerListFilter',
 })(PureCustomerListFilter);
+
+const mapStateToProps = () => ({
+  initialValues: {
+    accounting_is_running: {
+      value: true,
+      label: translate('Running accounting'),
+    },
+  },
+});
+
+CustomerListFilter = connect(mapStateToProps)(CustomerListFilter);
+
+export { CustomerListFilter };
