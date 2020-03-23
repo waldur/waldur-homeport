1bf7ff1444c37647d2be3e2619b9773c7d689f3b
Victor Mireyev <victor@opennodecloud.com>
2020-03-24 01:16:29 +0300

    Drop leftover.

--- src/team/module.js
+++ src/team/module.js
@@ -1,20 +1,13 @@
-import userSelector from './user-selector';
 import addProjectMember from './add-project-member';
 import addTeamMember from './add-team-member';
 import customerUsersList from './customer-users-list';
-import { projectUsers } from './project-users-list';
-import customerPermissionsLogService from './customer-permissions-log-service';
-import projectPermissionsLogService from './project-permissions-log-service';
 import customerPermissionsLogList from './CustomerPermissionsLogList';
+import { projectUsers } from './project-users-list';
 import projectPermissionsLogList from './ProjectPermissionsLogList';
+import userSelector from './user-selector';
 import './events';
 
 export default module => {
-  module.service(
-    'customerPermissionsLogService',
-    customerPermissionsLogService,
-  );
-  module.service('projectPermissionsLogService', projectPermissionsLogService);
   module.directive('userSelector', userSelector);
   module.component('addProjectMember', addProjectMember);
   module.component('addTeamMember', addTeamMember);
