4480d65ed723418356338bcb9a434226ffccee50
Victor Mireyev <victor@opennodecloud.com>
2020-04-01 14:45:33 +0300

    Improve layour of pie chart header for compliance feature.

--- src/marketplace-checklist/ComplianceChecklists.tsx
+++ src/marketplace-checklist/ComplianceChecklists.tsx
@@ -6,10 +6,10 @@ import { useSelector } from 'react-redux';
 import { get } from '@waldur/core/api';
 import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
 import { useQuery } from '@waldur/core/useQuery';
-import { DashboardCounter } from '@waldur/dashboard/DashboardCounter';
 import { translate } from '@waldur/i18n';
 import { getProject } from '@waldur/workspace/selectors';
 
+import { ChartHeader } from './ChartHeader';
 import { PieChart } from './PieChart';
 
 export const ComplianceChecklists = () => {
@@ -44,10 +44,7 @@ export const ComplianceChecklists = () => {
     <Row>
       {state.data.map(checklist => (
         <Col key={checklist.uuid} md={3}>
-          <DashboardCounter
-            label={`${checklist.score} %`}
-            value={checklist.name}
-          />
+          <ChartHeader label={`${checklist.score} %`} value={checklist.name} />
           <PieChart
             positive={checklist.positive_count}
             negative={checklist.negative_count}
