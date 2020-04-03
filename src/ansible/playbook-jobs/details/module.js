4ccbfe71ffa990ec7362c160b5d42a40a0e9a3e1
Victor Mireyev <victor@opennodecloud.com>
2020-04-03 16:57:35 +0300

    Drop ansible plugin as it has been supersed by marketplace script plugin.

--- src/ansible/playbook-jobs/details/module.js
+++
@@ -1,9 +0,0 @@
-import ansibleJobDetails from './ansible-job-details';
-import ansibleJobResourcesList from './ansible-job-resources';
-import ansibleJobSummary from './ansible-job-summary';
-
-export default module => {
-  module.component('ansibleJobDetails', ansibleJobDetails);
-  module.component('ansibleJobResourcesList', ansibleJobResourcesList);
-  module.component('ansibleJobSummary', ansibleJobSummary);
-};
