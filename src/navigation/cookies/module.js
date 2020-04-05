838ac364589cd9d91f27a6919ad4f121d0bca18b
Victor Mireyev <victor@opennodecloud.com>
2020-04-05 22:43:20 +0300

    Migrate CookiesConsent to ReactJS.

--- src/navigation/cookies/module.js
+++ src/navigation/cookies/module.js
@@ -1,5 +1,7 @@
-import cookiesConsent from './cookies-consent';
+import { connectAngularComponent } from '@waldur/store/connect';
+
+import { CookiesConsent } from './CookiesConsent';
 
 export default module => {
-  module.component('cookiesConsent', cookiesConsent);
+  module.component('cookiesConsent', connectAngularComponent(CookiesConsent));
 };
