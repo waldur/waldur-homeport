18559b13589d9126473569b347b28da2e6c5e698
Victor Mireyev <victor@opennodecloud.com>
2020-04-07 23:02:32 +0300

    Drop leftover class name.

--- src/navigation/sidebar/Sidebar.tsx
+++ src/navigation/sidebar/Sidebar.tsx
@@ -15,12 +15,12 @@ import { MenuItemType } from './types';
 export const Sidebar = ({ items }: { items: MenuItemType[] }) => (
   <nav className="navbar-default navbar-static-side" role="navigation">
     <Scrollbars style={{ height: '100%' }} className="sidebar-collapse">
-      <ul className="nav metismenu" id="side-menu">
+      <ul className="nav" id="side-menu">
         <BrandName />
         <UserDropdownMenu />
         <MenuItemList items={items} />
       </ul>
-      <ul className="nav metismenu visible-xs">
+      <ul className="nav visible-xs">
         <SupportLink />
         <LanguageSelectorMenuItem />
         <DocsLink />
