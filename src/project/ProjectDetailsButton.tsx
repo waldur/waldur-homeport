8570d25e0d1612b2dee46aae4f7842f023358048
Victor Mireyev <victor@opennodecloud.com>
2020-04-07 20:07:21 +0300

    Fix typings.

--- src/project/ProjectDetailsButton.tsx
+++ src/project/ProjectDetailsButton.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { translate } from '@waldur/i18n/translate';
 import { openModalDialog } from '@waldur/modal/actions';
 import { connectAngularComponent } from '@waldur/store/connect';
 import { ActionButton } from '@waldur/table-react/ActionButton';
-import { getProject } from '@waldur/workspace/selectors';
 import { Project } from '@waldur/workspace/types';
 
 const openProjectDialog = (project: Project) =>
   openModalDialog('projectDialog', { resolve: { project }, size: 'lg' });
 
-export const ProjectDetailsButton = () => {
+export const ProjectDetailsButton = ({ project }: { project?: Project }) => {
   const dispatch = useDispatch();
-  const project = useSelector(getProject);
   const callback = React.useCallback(
     () => dispatch(openProjectDialog(project)),
     [project],
