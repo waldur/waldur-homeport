50f97bcae75ad343f0cded05da003441f264f66d
Victor Mireyev <victor@opennodecloud.com>
2020-04-07 16:07:58 +0300

    Migrate sidebar toggle component to ReactJS.

--- src/navigation/sidebar/module.js
+++ src/navigation/sidebar/module.js
@@ -1,9 +1,7 @@
 import sidebar from './sidebar';
-import sidebarToggle from './sidebar-toggle';
 import SidebarExtensionService from './sidebar-extension-service';
 
 export default module => {
   module.service('SidebarExtensionService', SidebarExtensionService);
   module.component('sidebar', sidebar);
-  module.component('sidebarToggle', sidebarToggle);
 };
