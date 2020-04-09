492e39c5549b272d707c1c5b9b71afffb100b81b
Victor Mireyev <victor@opennodecloud.com>
2020-04-09 16:13:59 +0300

    Fix prettier.

--- src/resource/utils.spec.ts
+++ src/resource/utils.spec.ts
@@ -47,8 +47,6 @@ describe('formatFlavor', () => {
       gigabytes_ssd: 0,
       gigabytes_gpfs: 300,
     };
-    expect(formatFlavor(flavor)).toBe(
-      '1 vCPU, 2 GB RAM, 300 GB GPFS',
-    );
+    expect(formatFlavor(flavor)).toBe('1 vCPU, 2 GB RAM, 300 GB GPFS');
   });
 });
