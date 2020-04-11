5cce822f0eb55d93b0c8d21c854ab26c84f7a1be
Victor Mireyev <victor@opennodecloud.com>
2020-04-12 01:05:29 +0300

    Migrate cluster nodes list to ReactJS.

---
+++ src/resource/actions/NestedListActions.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import * as Button from 'react-bootstrap/lib/Button';
+import useEffectOnce from 'react-use/esm/useEffectOnce';
+
+import { ngInjector } from '@waldur/core/services';
+
+export const NestedListActions = ({ resource, tab }) => {
+  const [actions, setActions] = React.useState({});
+  const controller = {
+    handleActionException: () => {
+      console.log('handleActionException');
+    },
+    reInitResource: () => {
+      console.log('reInitResource');
+    },
+  };
+
+  const actionUtilsService = ngInjector.get('actionUtilsService');
+
+  useEffectOnce(() => {
+    actionUtilsService
+      .loadNestedActions(controller, resource, tab)
+      .then(setActions);
+  });
+
+  return (
+    <>
+      {Object.keys(actions).map(key => (
+        <Button
+          bsSize="sm"
+          disabled={actions[key].disabled}
+          key={key}
+          onClick={() => actions[key].callback()}
+        >
+          {actions[key].iconClass ? (
+            <>
+              <i className={actions[key].iconClass}></i>{' '}
+            </>
+          ) : null}
+          {actions[key].title}
+        </Button>
+      ))}
+    </>
+  );
+};
