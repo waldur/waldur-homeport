26ae81fa14fa28723d1ba22fe3816874abad2c77
Victor Mireyev <victor@opennodecloud.com>
2020-04-17 16:39:15 +0300

    Migrate JiraProjectsList to React.

--- src/jira/project/module.js
+++ src/jira/project/module.js
@@ -1,14 +1,13 @@
-import tabsConfig from './tabs';
-import JiraProjectService from './jira-project-service';
-import jiraProjectsList from './jira-projects-list';
-import jiraProjectCheckoutSummary from './jira-project-checkout-summary';
-import { JiraProjectSummary } from './JiraProjectSummary';
 import * as ResourceSummary from '@waldur/resource/summary/registry';
+
+import jiraProjectCheckoutSummary from './jira-project-checkout-summary';
+import jiraProjectsList from './JiraProjectsList';
+import { JiraProjectSummary } from './JiraProjectSummary';
+import tabsConfig from './tabs';
 
 export default module => {
   ResourceSummary.register('JIRA.Project', JiraProjectSummary);
   module.config(tabsConfig);
-  module.service('JiraProjectService', JiraProjectService);
   module.component('jiraProjectsList', jiraProjectsList);
   module.component('jiraProjectCheckoutSummary', jiraProjectCheckoutSummary);
 };
