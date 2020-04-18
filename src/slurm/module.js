2c3b1ad942a76da48848b75d4cc0cdfc3539ea67
Victor Mireyev <victor@opennodecloud.com>
2020-04-18 16:52:48 +0300

    Drop table-extension-service

--- src/slurm/module.js
+++ src/slurm/module.js
@@ -2,7 +2,6 @@ import actionConfig from './actions';
 import detailsModule from './details/module';
 import quotaPie from './quota-pie';
 import SlurmPackagesService from './slurm-packages-service';
-import registerTableExtension from './table-extension';
 import tabsConfig from './tabs';
 import './provider';
 import './register-header';
@@ -11,7 +10,6 @@ import './marketplace';
 export default module => {
   module.config(actionConfig);
   module.config(tabsConfig);
-  module.run(registerTableExtension);
   module.service('SlurmPackagesService', SlurmPackagesService);
   module.directive('quotaPie', quotaPie);
   detailsModule(module);
--- src/table/module.js
+++ src/table/module.js
@@ -1,12 +1,10 @@
 import baseControllerListClass from './base-list-controller';
-import responsiveTable from './responsive-table';
 import filterList from './filter-list';
 import filterSelector from './filter-selector';
-import TableExtensionService from './table-extension-service';
+import responsiveTable from './responsive-table';
 
 export default module => {
   module.service('baseControllerListClass', baseControllerListClass);
-  module.service('TableExtensionService', TableExtensionService);
   module.directive('responsiveTable', responsiveTable);
   module.component('filterList', filterList);
   module.component('filterSelector', filterSelector);
