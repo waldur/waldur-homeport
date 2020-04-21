cb68cd4ee402d48ef93b580feadaab7c536c7c68
Victor Mireyev <victor@opennodecloud.com>
2020-04-21 03:54:10 +0300

    Drop leftover.

--- src/customer/services/customer-permissions-service.js
+++ src/customer/services/customer-permissions-service.js
@@ -1,10 +1,6 @@
 // @ngInject
-export default function customerPermissionsService(
-  $q,
-  baseServiceClass,
-  ncUtils,
-) {
-  let ServiceClass = baseServiceClass.extend({
+export default function customerPermissionsService(baseServiceClass, ncUtils) {
+  const ServiceClass = baseServiceClass.extend({
     init: function() {
       this._super();
       this.endpoint = '/customer-permissions/';
@@ -15,30 +11,6 @@ export default function customerPermissionsService(
     },
 
     getPermissionKey: ncUtils.getUUID,
-
-    userHasCustomerRole: function(username, role, customerUUID) {
-      let deferred = $q.defer(),
-        filter = { username: username };
-      if (customerUUID) {
-        filter.customer = customerUUID;
-      }
-
-      this.getList(filter).then(
-        function(permissions) {
-          for (let i = 0; i < permissions.length; i++) {
-            if (permissions[i].role === role) {
-              deferred.resolve(true);
-            }
-          }
-          deferred.resolve(false);
-        },
-        function(err) {
-          deferred.reject(err);
-        },
-      );
-
-      return deferred.promise;
-    },
   });
   return new ServiceClass();
 }
