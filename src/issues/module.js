f6f6515a583c84b95beb4dd2107845556604d14b
Victor Mireyev <victor@opennodecloud.com>
2020-04-23 17:03:34 +0300

    Remove leftover.

--- src/issues/module.js
+++ src/issues/module.js
@@ -1,14 +1,13 @@
-import issuesService from './issues-service';
-import issueDetail from './issue-detail';
-import issueRoutes from './routes';
 import issueAttachmentsModule from './attachments/module';
 import issueCommentsModule from './comments/module';
 import issueCreateModule from './create/module';
+import issueDetail from './issue-detail';
+import issuesService from './issues-service';
 import issueListModule from './list/module';
+import requestServiceButton from './request-service-button';
+import issueRoutes from './routes';
 import issueTypesModule from './types/module';
 import issueWorkspaceModule from './workspace/module';
-import requestServiceButton from './request-service-button';
-import registerExtensionPoint from './extend-appstore-selector';
 import './events';
 
 export default module => {
@@ -16,7 +15,6 @@ export default module => {
   module.component('issueDetail', issueDetail);
   module.component('requestServiceButton', requestServiceButton);
   module.config(issueRoutes);
-  module.run(registerExtensionPoint);
   issueTypesModule(module);
   issueCommentsModule(module);
   issueCreateModule(module);
