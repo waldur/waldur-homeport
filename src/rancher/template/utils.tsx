f4c41005e90eb1eccb03bf3302077df408ec868f
Victor Mireyev <victor@opennodecloud.com>
2020-04-24 14:20:34 +0300

    Rancher application deployment form should have a proper breadcrumbs nagivation [WAL-3014]

--- src/rancher/template/utils.tsx
+++ src/rancher/template/utils.tsx
@@ -1,4 +1,6 @@
 import { getById, get, getAll } from '@waldur/core/api';
+import { ngInjector } from '@waldur/core/services';
+import { translate } from '@waldur/i18n';
 
 import { Question } from './types';
 
@@ -25,6 +27,8 @@ export const groupByN = (n, data) => {
 const getTemplate = templateUuid =>
   getById('/rancher-templates/', templateUuid);
 
+const getCluster = clusterUuid => getById('/rancher-clusters/', clusterUuid);
+
 interface TemplateVersion {
   readme: string;
   questions: Question[];
@@ -38,8 +42,51 @@ const getTemplateVersion = (templateUuid, versionUuid) =>
 const getProjects = clusterUuid =>
   getAll('/rancher-projects/', { params: { cluster_uuid: clusterUuid } });
 
+const refreshBreadcrumbs = (cluster, template) => {
+  const BreadcrumbsService = ngInjector.get('BreadcrumbsService');
+  BreadcrumbsService.items = [
+    {
+      label: translate('Project workspace'),
+      state: 'project.details',
+    },
+    {
+      label: translate('Resources'),
+      state: 'marketplace-project-resources',
+      params: {
+        category_uuid: cluster.marketplace_category_uuid,
+      },
+    },
+    {
+      label: cluster.name,
+      state: 'resources.details',
+      params: {
+        resource_type: 'Rancher.Cluster',
+        uuid: cluster.uuid,
+        tab: 'catalogs',
+      },
+    },
+    {
+      label: translate('Application catalogues'),
+    },
+    {
+      label: template.catalog_name,
+      state: 'rancher-catalog-details',
+      params: {
+        clusterUuid: cluster.uuid,
+        catalogUuid: template.catalog_uuid,
+      },
+    },
+    {
+      label: translate('Application templates'),
+    },
+  ];
+  BreadcrumbsService.activeItem = template.name;
+};
+
 export const loadData = async (templateUuid, clusterUuid) => {
   const template: any = await getTemplate(templateUuid);
+  const cluster: any = await getCluster(clusterUuid);
+  refreshBreadcrumbs(cluster, template);
   const version = await getTemplateVersion(
     template.uuid,
     template.default_version,
