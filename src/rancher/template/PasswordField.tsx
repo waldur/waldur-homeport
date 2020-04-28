ddef2f61bf0113aa8b923c976b92bf8a2e0b7786
Victor Mireyev <victor@opennodecloud.com>
2020-04-28 14:55:10 +0300

    Allow to generate password in Rancher application provision form.

--- src/rancher/template/PasswordField.tsx
+++ src/rancher/template/PasswordField.tsx
@@ -1,10 +1,42 @@
+import generator from 'generate-password';
 import * as React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import InputGroup from 'react-bootstrap/lib/InputGroup';
+import { useDispatch } from 'react-redux';
+import { change } from 'redux-form';
 
-import { SecretField } from '@waldur/form-react';
+import { translate } from '@waldur/i18n';
 
+import { FORM_ID } from './constants';
 import { DecoratedField } from './DecoratedField';
 import { FieldProps } from './types';
 
-export const PasswordField: React.FC<FieldProps> = props => (
-  <DecoratedField {...props} component={SecretField} />
-);
+export const PasswordField: React.FC<FieldProps> = props => {
+  const dispatch = useDispatch();
+
+  const setGeneratedPassword = React.useCallback(() => {
+    const password = generator.generate({
+      length: 10,
+      numbers: true,
+    });
+
+    dispatch(change(FORM_ID, `answers.${props.variable}`, password));
+  }, [dispatch, props.variable]);
+
+  const renderControl = React.useCallback(
+    props => (
+      <InputGroup>
+        <FormControl {...props.input} />
+        <InputGroup.Button>
+          <Button onClick={setGeneratedPassword}>
+            {translate('Generate password')}
+          </Button>
+        </InputGroup.Button>
+      </InputGroup>
+    ),
+    [setGeneratedPassword],
+  );
+
+  return <DecoratedField {...props} component={renderControl} />;
+};
