2ad9b1b431d5f0febeafbad8dcd90e33aad46c97
Victor Mireyev <victor@opennodecloud.com>
2020-05-12 16:42:52 +0300

    Fix deepscan warn.

--- src/rancher/module.ts
+++ src/rancher/module.ts
@@ -1,12 +1,11 @@
+import './node/module';
 import './provider';
 import catalogModule from './catalog/module';
 import clusterModule from './cluster/module';
-import nodeModule from './node/module';
 import rancherRoutes from './routes';
 
 export default module => {
   module.config(rancherRoutes);
   clusterModule(module);
-  nodeModule(module);
   catalogModule(module);
 };
