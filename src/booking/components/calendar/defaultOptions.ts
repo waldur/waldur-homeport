4a8e445af291fa05a45c6056020705e385681dd7
Olaf <olaf.viikna@ut.ee>
2020-05-12 09:53:22 +0300

    Add booking modal and callendar default options

---
+++ src/booking/components/calendar/defaultOptions.ts
@@ -0,0 +1,47 @@
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+import { OptionsInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import momentPlugin from '@fullcalendar/moment';
+import momentTimezonePlugin from '@fullcalendar/moment-timezone';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+export const defaultOptions: OptionsInput = {
+  plugins: [
+    dayGridPlugin,
+    timeGridPlugin,
+    interactionPlugin,
+    bootstrapPlugin,
+    momentTimezonePlugin,
+    momentPlugin,
+    listPlugin,
+  ],
+  height: 'auto',
+  themeSystem: 'bootstrap',
+  handleWindowResize: true,
+  aspectRatio: 1.25,
+  header: {
+    left: 'prev,today,next',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay',
+  },
+  eventTimeFormat: {
+    hour: 'numeric',
+    minute: '2-digit',
+    meridiem: 'short',
+    hour12: false,
+    omitZeroMinute: true,
+  },
+  firstDay: 1,
+  timeZone: 'local',
+  progressiveEventRendering: true,
+  lazyFetching: false,
+  eventLimit: 6,
+  views: {
+    agenda: {
+      eventLimit: 3,
+    },
+  },
+  slotEventOverlap: false,
+};
---
+++ src/booking/components/modal/BookingModal.tsx
@@ -0,0 +1,142 @@
+import moment from 'moment-timezone';
+import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import * as ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
+import * as Modal from 'react-bootstrap/lib/Modal';
+
+import { BookingModalProps, BookingProps } from '@waldur/booking/types';
+import { translate } from '@waldur/i18n';
+
+import { DateAndTimeSelectField } from './DateAndTimeSelect';
+
+const BookingModal = ({
+  isOpen,
+  toggle,
+  onSuccess,
+  onDelete,
+  event,
+}: BookingModalProps) => {
+  const [newEvent, setNewEvent] = React.useState({
+    title: event.title,
+    start: event.start,
+    end: event.end,
+    allDay: event.allDay,
+  });
+
+  const handleDelete = () => {
+    onDelete();
+    toggle();
+  };
+  const handleSubmit = () => {
+    const { id, extendedProps } = event;
+    const payload = {
+      oldID: id as BookingProps['id'],
+      event: {
+        id,
+        extendedProps,
+        ...newEvent,
+      },
+    };
+    onSuccess(payload);
+    toggle();
+  };
+  const handleChange = (name, value) =>
+    setNewEvent({ ...newEvent, [name]: value });
+
+  return (
+    <Modal show={isOpen} onHide={toggle}>
+      <Modal.Header style={{ backgroundColor: '#19b394' }}>
+        <Modal.Title style={{ color: 'white', paddingLeft: 12 }}>
+          {event.extendedProps.type === ('availability' || 'Availability')
+            ? translate('Edit an availability')
+            : translate('Edit booking event')}
+          <button
+            type="button"
+            className="close"
+            aria-label="Close"
+            onClick={toggle}
+            style={{ color: 'white', fontSize: 22 }}
+          >
+            <span aria-hidden="true" style={{ padding: '1rem' }}>
+              &times;
+            </span>
+          </button>
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <form className="form-horizontal">
+          <div className="form-group">
+            <label className="control-label col-sm-2">
+              {translate('Title')}
+            </label>
+            <div className="col-sm-9">
+              <input
+                name="title"
+                type="text"
+                className="form-control"
+                value={newEvent.title}
+                onChange={({ target }) =>
+                  handleChange(target.name, target.value)
+                }
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label className="control-label col-sm-2">
+              {translate('All day')}
+            </label>
+            <div className="col-sm-9" style={{ paddingTop: 7 }}>
+              <div className="checkbox-toggle">
+                <input
+                  id="AllDay"
+                  type="checkbox"
+                  checked={newEvent.allDay}
+                  onChange={() => handleChange('allDay', !newEvent.allDay)}
+                />
+                <label htmlFor="AllDay">All-day toggler</label>
+              </div>
+            </div>
+          </div>
+
+          <DateAndTimeSelectField
+            name="start"
+            minuteStep={30}
+            label={translate('Start')}
+            isDisabled={newEvent.allDay}
+            currentTime={moment(newEvent.start, 'DD/MM/YYYY HH:mm', true)}
+            onChange={newDateValue => handleChange('start', newDateValue)}
+          />
+
+          <DateAndTimeSelectField
+            name="end"
+            label={translate('End')}
+            minuteStep={30}
+            isDisabled={newEvent.allDay}
+            currentTime={moment(newEvent.end, 'DD/MM/YYYY HH:mm', true)}
+            onChange={newDateValue => handleChange('end', newDateValue)}
+          />
+
+          <ButtonGroup
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              justifyItems: 'center',
+              marginTop: 30,
+            }}
+          >
+            <Button bsStyle="delete" onClick={handleDelete}>
+              {translate('Delete')}
+            </Button>
+            <Button bsStyle="primary" onClick={handleSubmit}>
+              {translate('Save')}
+            </Button>
+          </ButtonGroup>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default BookingModal;
