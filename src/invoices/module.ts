1d13c2f2759e3b43c6000458000f8c5d12dca287
Victor Mireyev <victor@opennodecloud.com>
2020-05-12 23:05:22 +0300

    Drop invoicesService.

--- src/invoices/module.ts
+++ src/invoices/module.ts
@@ -1,10 +1,8 @@
 import eventsModule from './events/module';
-import invoicesService from './invoices-service';
 import billingRoutes from './routes';
 import './events';
 
 export default module => {
-  module.service('invoicesService', invoicesService);
   module.config(billingRoutes);
   eventsModule(module);
 };
