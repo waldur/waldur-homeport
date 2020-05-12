49fa45940085e69333c01c9b3d6dc543b94a7004
Victor Mireyev <victor@opennodecloud.com>
2020-05-12 17:05:57 +0300

    Remove leftover.

--- src/paypal/module.ts
+++ src/paypal/module.ts
@@ -1,8 +1,6 @@
-import paypalInvoicesService from './paypal-invoices-service';
 import paymentRoutes from './routes';
 import './events';
 
 export default module => {
-  module.service('paypalInvoicesService', paypalInvoicesService);
   module.config(paymentRoutes);
 };
