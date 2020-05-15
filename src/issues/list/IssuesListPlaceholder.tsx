eea35ef53ab7b6070ee3d8680c3b5e7f96e726a3
Victor Mireyev <victor@opennodecloud.com>
2020-05-15 19:48:24 +0300

    Fix issues list rendering if support is disabled.

--- src/issues/list/IssuesListPlaceholder.tsx
+++ src/issues/list/IssuesListPlaceholder.tsx
@@ -5,7 +5,7 @@ import { ImageTablePlaceholder } from '@waldur/table-react/ImageTablePlaceholder
 
 const Illustration = require('@waldur/images/table-placeholders/undraw_empty_xct9.svg');
 
-export const IssuesListTablePlaceholder = () => (
+export const IssuesListPlaceholder = () => (
   <ImageTablePlaceholder
     illustration={Illustration}
     title={translate(`You haven't created any issues yet.`)}
