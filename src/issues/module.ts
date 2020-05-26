cc69ceffed40e156de87456b69b712c58e074712
Victor Mireyev <victor@opennodecloud.com>
2020-05-26 21:01:50 +0300

    Remove issues service.

--- src/issues/module.ts
+++ src/issues/module.ts
@@ -1,11 +1,9 @@
-import issuesService from './issues-service';
 import issueListModule from './list/module';
 import issueRoutes from './routes';
 import issueWorkspaceModule from './workspace/module';
 import './events';
 
 export default module => {
-  module.service('issuesService', issuesService);
   module.config(issueRoutes);
   issueListModule(module);
   issueWorkspaceModule(module);
