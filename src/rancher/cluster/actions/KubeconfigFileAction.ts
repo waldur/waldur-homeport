3fb19d7e3ec173cd9cf614b9bc3a26d724dc06c8
Michael Alex <michaelaleks.dev@gmail.com>
2020-05-26 14:30:29 -0400

    Disable "Generate kubeconfig" action if cluster is still being created [WAL-3093]

--- src/rancher/cluster/actions/KubeconfigFileAction.ts
+++ src/rancher/cluster/actions/KubeconfigFileAction.ts
@@ -1,15 +1,28 @@
 import { translate } from '@waldur/i18n';
 import { openModalDialog } from '@waldur/modal/actions';
+import { OpenStackInstance } from '@waldur/openstack/openstack-instance/types';
 import { ActionContext, ResourceAction } from '@waldur/resource/actions/types';
 import store from '@waldur/store/store';
 
 import { RancherClusterKubeconfigDialog } from './RancherClusterKubeconfigDialog';
+
+function validate(ctx: ActionContext<OpenStackInstance>): string {
+  if (
+    ctx.resource.state === 'Creating' ||
+    ctx.resource.state === 'Creation Scheduled'
+  ) {
+    return translate(
+      'Instance should not be Creating, or Creation Scheduled. Please contact support.',
+    );
+  }
+}
 
 export function kubeconfigAction(ctx: ActionContext): ResourceAction {
   return {
     name: 'kubeconfig',
     title: translate('Generate Kubeconfig file'),
     type: 'callback',
+    validators: [validate],
     execute: () => {
       store.dispatch(
         openModalDialog(RancherClusterKubeconfigDialog, {
