992a8dd94182555d859a78b84fde7ba2d48cb982
Victor Mireyev <victor@opennodecloud.com>
2020-05-27 09:57:55 +0300

    Drop quotas service.

--- src/services/module.ts
+++ src/services/module.ts
@@ -1,9 +1,7 @@
 import currentStateService from './current-state-service';
 import projectPermissionsService from './project-permissions-service';
-import quotasService from './quotas-service';
 
 export default module => {
   module.service('currentStateService', currentStateService);
   module.service('projectPermissionsService', projectPermissionsService);
-  module.service('quotasService', quotasService);
 };
