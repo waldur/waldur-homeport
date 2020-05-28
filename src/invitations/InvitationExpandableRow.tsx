d342c7bb014579b058e4c32302924a3ef178e347
Michael Alex <michaelaleks.dev@gmail.com>
2020-05-28 07:53:10 -0400

    Expose civil number in collapsable section of invitation list [WAL-3148]

---
+++ src/invitations/InvitationExpandableRow.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import { translate } from '@waldur/i18n';
+
+export const InvitationExpandableRow: React.FC<{
+  row;
+}> = ({ row }) => (
+  <>
+    <p>
+      <b>{translate('Invitation link')}: </b>
+      {row.link_template.replace('{uuid}', row.uuid)}
+    </p>
+    {row.civil_number ? (
+      <p>
+        <b>{translate('Civil number')}: </b>
+        {row.civil_number}
+      </p>
+    ) : null}
+  </>
+);
