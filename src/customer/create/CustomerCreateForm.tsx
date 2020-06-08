11faf91ef2dd3047586c554f2860bac227fa96bf
Victor Mireyev <victor@opennodecloud.com>
2020-06-08 13:48:19 +0300

    Hide steps list in organization creation dialog if there's only one step [WAL-3180]

--- src/customer/create/CustomerCreateForm.tsx
+++ src/customer/create/CustomerCreateForm.tsx
@@ -10,18 +10,17 @@ const WizardForms = [WizardFormFirstPage, WizardFormSecondPage];
 
 export const CustomerCreateForm = props => {
   const [step, setStep] = React.useState(1);
-  const stepCount = ENV.hideOrganizationBillingStep ? 1 : 2;
-  const isLast = step === stepCount;
+  const steps = [translate('General information')];
+  if (!ENV.hideOrganizationBillingStep) {
+    steps.push(translate('Billing details'));
+  }
+  const isLast = step === steps.length;
   const nextStep = () => setStep(step + 1);
   const prevStep = () => setStep(step - 1);
   const submitLabel = isLast
     ? translate('Create organization')
     : translate('Next');
 
-  const steps = [
-    translate('General information'),
-    translate('Billing details'),
-  ];
   const stepTitle = steps[step - 1];
 
   return React.createElement(WizardForms[step - 1], {
