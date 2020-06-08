cb68ba1f47da788250ed8e0646152534c9364baf
Michael Alex <michaelalex.dev@gmail.com>
2020-06-08 09:44:19 +0400

    delay page refresh for 1 sec

--- src/i18n/useLanguageSelector.tsx
+++ src/i18n/useLanguageSelector.tsx
@@ -23,7 +23,9 @@ export const useLanguageSelector = () => {
   const setLanguage = React.useCallback((language: Language) => {
     setCurrentLanguage(language);
     service.setCurrentLanguage(language);
-    // location.reload(); // fixme
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
   }, []);
 
   return { currentLanguage, languageChoices, setLanguage };
