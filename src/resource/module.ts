967b458460cd4a5269c8707a4684dabda69648f2
Victor Mireyev <victor@opennodecloud.com>
2020-06-11 18:14:10 +0300

    Migrate resource details container.

--- src/resource/module.ts
+++ src/resource/module.ts
@@ -1,10 +1,6 @@
 import actionsModule from './actions/module';
-import resourceHeader from './resource-header';
-import resourceDetails from './ResourceDetails';
 import './events';
 
 export default module => {
-  module.component('resourceDetails', resourceDetails);
-  module.component('resourceHeader', resourceHeader);
   actionsModule(module);
 };
