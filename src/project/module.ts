24e745ea808e015f8ce61aacbd4098ff0297863d
Victor Mireyev <victor@opennodecloud.com>
2020-06-15 02:49:53 +0300

    Remove leftover.

--- src/project/module.ts
+++ src/project/module.ts
@@ -1,13 +1,9 @@
-import { connectAngularComponent } from '@waldur/store/connect';
-
 import projectPolicies from './project-policies';
-import { ProjectSidebar } from './ProjectSidebar';
 import teamModule from './team/module';
 
 import './events';
 
 export default module => {
-  module.component('projectSidebar', connectAngularComponent(ProjectSidebar));
   module.component('projectPolicies', projectPolicies);
   teamModule(module);
 };
