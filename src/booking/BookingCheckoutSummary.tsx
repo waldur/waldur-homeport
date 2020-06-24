8216f0609a8639a3e013d2ffba1630c5a6ae0bb8
Michael Alex <michaelaleks.dev@gmail.com>
2020-06-24 14:35:41 -0400

    Extend summary for booking offerings with selected periods [WAL-3201]

---
+++ src/booking/BookingCheckoutSummary.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import { translate } from '@waldur/i18n';
+import { OfferingPeriodsRenderer } from '@waldur/marketplace/details/OfferingPeriodsRenderer';
+import { OrderSummary } from '@waldur/marketplace/details/OrderSummary';
+
+const BookingExtraComponent = (props) => (
+  <>
+    {props.formData &&
+    Array.isArray(props.formData.attributes.schedules) &&
+    props.formData.attributes.schedules.length ? (
+      <tr>
+        <td>
+          {props.formData.attributes.schedules.length === 1 ? (
+            <strong>{translate('Period')}</strong>
+          ) : (
+            <strong>{translate('Periods')}</strong>
+          )}
+        </td>
+        <td>
+          <OfferingPeriodsRenderer
+            schedules={props.formData.attributes.schedules}
+          />
+        </td>
+      </tr>
+    ) : null}
+  </>
+);
+
+export const BookingCheckoutSummary = (props) => (
+  <OrderSummary {...props} extraComponent={BookingExtraComponent} />
+);
