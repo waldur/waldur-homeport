fe3bb5c0789932b95d23fce947093897b06a6842
Victor Mireyev <victor@opennodecloud.com>
2020-07-02 14:30:04 +0300

    Drop customer and project policies components.

--- src/customer/details/module.ts
+++
@@ -1,7 +0,0 @@
-import customerPoliciesPanel from './customer-policies';
-import customerThreshold from './customer-threshold';
-
-export default (module) => {
-  module.component('customerPoliciesPanel', customerPoliciesPanel);
-  module.component('customerThreshold', customerThreshold);
-};
--- src/project/module.ts
+++ src/project/module.ts
@@ -1,9 +1,7 @@
-import projectPolicies from './project-policies';
 import teamModule from './team/module';
 
 import './events';
 
 export default (module) => {
-  module.component('projectPolicies', projectPolicies);
   teamModule(module);
 };
