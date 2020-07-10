3eabcd4aedfc4561cb4bbf66d7143805dee2e1d9
Victor Mireyev <victor@opennodecloud.com>
2020-07-10 08:40:38 +0300

    Fix leftover.

--- src/digitalocean/help.ts
+++ src/digitalocean/help.ts
@@ -1,8 +1,10 @@
+import { translate } from '@waldur/i18n';
+
 export const helpData = {
   type: 'providers',
   key: 'DigitalOcean',
-  name: gettext('DigitalOcean provider'),
-  title: gettext('How to obtain credentials for DigitalOcean provider'),
+  name: translate('DigitalOcean provider'),
+  title: translate('How to obtain credentials for DigitalOcean provider'),
   link:
     'https://www.digitalocean.com/community/tutorials/how-to-use-the-digitalocean-api-v2',
 };
