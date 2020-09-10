56113f094a61efc16fa546bd4f164c05ad69bd46
Sergei Zaiaev <serjzv@gmail.com>
2020-09-10 03:03:01 -0400

    [WAL-3406] Refactor CI pipline and migrate to rules

--- docker_push.sh
+++ docker_push.sh
@@ -7,6 +7,9 @@ else
   export VERSION=${1#v}
 fi
 
+DOCKER_PASSWORD=${DOCKER_PASSWORD:-$WALDUR_DOCKER_HUB_PASSWORD}
+DOCKER_USERNAME=${DOCKER_USERNAME:-$WALDUR_DOCKER_HUB_USER}
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t opennode/waldur-homeport:$VERSION . --build-arg VERSION
 docker push "opennode/waldur-homeport:$VERSION"
