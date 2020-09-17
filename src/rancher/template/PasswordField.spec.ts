17bc28a3985241c5c28ad614d6813b50b4353012
Victor Mireyev <victor.mireyev@gmail.com>
2020-09-17 21:55:18 +0300

    Fix lint.

--- src/rancher/template/PasswordField.spec.ts
+++ src/rancher/template/PasswordField.spec.ts
@@ -1,9 +1,9 @@
-import {generatePassword} from './PasswordField';
+import { generatePassword } from './PasswordField';
 
 describe('Password generator', () => {
   it('generates string with given length', () => {
     const password = generatePassword(10);
     expect(password.length).toBe(10);
     expect(typeof password).toBe('string');
-  })
-})
+  });
+});
