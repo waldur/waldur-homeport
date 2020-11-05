7580cb31d9578486bb401f66b8432fa38d2d6132
Michael Alex <michaelaleks.dev@gmail.com>
2020-11-05 07:59:43 -0500

    [WAL-3576] Extend usage reporting dialog with additional data

--- src/marketplace/resources/usage/types.ts
+++ src/marketplace/resources/usage/types.ts
@@ -22,6 +22,9 @@ export interface UsageReportContext {
   resource_uuid: string;
   resource_name: string;
   offering_uuid: string;
+  customer_name?: string;
+  project_name?: string;
+  backend_id?: string;
 }
 
 export interface ComponentUsage {
