a87e1616c29c234b0e1a51cae07b5655db931ed5
Victor Mireyev <victor.mireyev@gmail.com>
2020-11-16 15:15:32 +0300

    Remove leftover AngularJS wrapper from CustomerPermissionsService.

--- src/customer/services/CustomerPermissionsService.ts
+++ src/customer/services/CustomerPermissionsService.ts
@@ -1,18 +1,17 @@
 import Axios from 'axios';
 
 import { post } from '@waldur/core/api';
-import { $q } from '@waldur/core/services';
 
 export const CustomerPermissionsService = {
   create(payload) {
-    return $q.when(post('/customer-permissions/', payload));
+    return post('/customer-permissions/', payload);
   },
 
   delete(url) {
-    return $q.when(Axios.delete(url));
+    return Axios.delete(url);
   },
 
   update(url, payload) {
-    return $q.when(Axios.patch(url, payload));
+    return Axios.patch(url, payload);
   },
 };
