ec6af938631351306c88cf1c3dcc0f5c601d7447
Victor Mireyev <victor.mireyev@gmail.com>
2020-11-23 11:24:08 +0300

    Fix typo [WAL-3657]

--- src/openstack/openstack-backup/BackupsList.tsx
+++ src/openstack/openstack-backup/BackupsList.tsx
@@ -24,7 +24,7 @@ const TableComponent = (props) => {
         },
         {
           title: translate('Keep until'),
-          render: ({ row }) => formatDateTime(row.created),
+          render: ({ row }) => formatDateTime(row.kept_until),
         },
         {
           title: translate('State'),
