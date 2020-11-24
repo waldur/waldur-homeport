f27cdbb37ea8d4d25037ff46ade08c6345e46a42
Michael Alex <michaelaleks.dev@gmail.com>
2020-11-24 10:31:31 -0500

    Hide Booking management panel if there are no offerings

--- src/customer/dashboard/CustomerBookingManagement.tsx
+++ src/customer/dashboard/CustomerBookingManagement.tsx
@@ -2,17 +2,39 @@ import * as React from 'react';
 import * as Col from 'react-bootstrap/lib/Col';
 import * as Row from 'react-bootstrap/lib/Row';
 import { useSelector } from 'react-redux';
+import useAsync from 'react-use/lib/useAsync';
 
 import { BookingsFilter } from '@waldur/booking/BookingsFilter';
 import { BookingsList } from '@waldur/booking/BookingsList';
+import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
 import { Panel } from '@waldur/core/Panel';
 import { BookingsCalendar } from '@waldur/customer/dashboard/BookingsCalendar';
 import { translate } from '@waldur/i18n';
+import { getOfferingsCount } from '@waldur/marketplace/common/api';
 import { getCustomer } from '@waldur/workspace/selectors';
+
+const loadBookingOfferingsCount = async (customerUuid: string) =>
+  await getOfferingsCount({
+    params: {
+      customer_uuid: customerUuid,
+      type: 'Marketplace.Booking',
+      state: ['Active', 'Paused'],
+    },
+  });
 
 export const CustomerBookingManagement = () => {
   const customer = useSelector(getCustomer);
-  return customer.is_service_provider ? (
+  const { loading, value: offeringsCount, error } = useAsync(
+    () => loadBookingOfferingsCount(customer.uuid),
+    [customer],
+  );
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+  if (error) {
+    return <h3>{translate('Unable to load booking offerings.')}</h3>;
+  }
+  return customer.is_service_provider && offeringsCount ? (
     <Panel title={translate('Booking management')}>
       <Row style={{ marginBottom: '30px' }}>
         <Col md={8} mdOffset={2}>
