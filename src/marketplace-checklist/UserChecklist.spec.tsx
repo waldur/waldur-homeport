4f205752200c3584a82451e12ad1b590d1d5ba49
Victor Mireyev <victor@opennodecloud.com>
2020-12-09 06:13:56 -0500

    Extend tests for user checklist component.

--- src/marketplace-checklist/UserChecklist.spec.tsx
+++ src/marketplace-checklist/UserChecklist.spec.tsx
@@ -1,19 +1,34 @@
 import { mount } from 'enzyme';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
 import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
+import { actWait, updateWrapper } from '@waldur/core/testUtils';
+import { Await } from '@waldur/core/types';
+import { SET_TITLE } from '@waldur/navigation/title';
+
+jest.mock('./api');
 
 import { AnswersSummary } from './AnswersSummary';
 import { AnswersTable } from './AnswersTable';
+import * as api from './api';
+import * as fixtures from './fixtures';
 import { PieChart } from './PieChart';
 import { UserChecklist } from './UserChecklist';
 
+const apiMock = api as jest.Mocked<typeof api>;
+
+jest.mock('@waldur/core/services', () => ({
+  ENV: 'localhost',
+}));
+
 jest.mock('@waldur/store/coreSaga', () => ({
-  showSuccess: jest.fn(() => ({ type: 'mock-success' })),
-  showError: jest.fn(() => ({ type: 'mock-error' })),
+  showSuccess: jest.fn((message) => ({
+    type: 'success',
+    payload: { message },
+  })),
+  showError: jest.fn((message) => ({ type: 'error', payload: { message } })),
 }));
 
 jest.mock('@waldur/core/EChart', () => ({
@@ -31,61 +46,6 @@ jest.mock('@waldur/i18n', () => ({
   translate: jest.fn().mockImplementation((arg) => arg),
 }));
 
-const checklist = [
-  {
-    uuid: '51525237e3bc40879c437f5d4d3be850',
-    name: 'Best practices (2 questions)',
-    description: '',
-    questions_count: 2,
-    category_name: 'Project checklist',
-    category_uuid: '7fd33c0055d8453d84e4da5371dd8fab',
-  },
-  {
-    uuid: '407296c2821345aab0f02a583293a476',
-    name: 'Cyberhygiene (2 questions)',
-    description: '',
-    questions_count: 2,
-    category_name: 'Project checklist',
-    category_uuid: '7fd33c0055d8453d84e4da5371dd8fab',
-  },
-];
-
-const questions = [
-  {
-    uuid: '554b3673d2f944299057143fa62be3ef',
-    description: 'I assign PIDs to all my generated resources',
-    solution: 'Please see RDA recommendations here and here',
-    correct_answer: true,
-  },
-  {
-    uuid: '4472d7df987644f39804cb7dbdbc2e6e',
-    description: 'I preserve data in a storage with SLAs on preservation',
-    solution: 'Find a service provider to use for keeping artifacts.',
-    category_uuid: '6dfcdbe3c18241dc87526ef411bf6064',
-    correct_answer: true,
-  },
-];
-
-const answers = [
-  {
-    question_uuid: '554b3673d2f944299057143fa62be3ef',
-    value: false,
-  },
-  {
-    question_uuid: '4472d7df987644f39804cb7dbdbc2e6e',
-    value: true,
-  },
-];
-
-const category = {
-  uuid: '7fd33c0055d8453d84e4da5371dd8fab',
-  icon: null,
-  url: 'http://rest-mock.test',
-  name: 'Project checklist',
-  description: '',
-  checklists_count: 2,
-};
-
 const initialExpectAnswers = {
   '554b3673d2f944299057143fa62be3ef': false,
   '4472d7df987644f39804cb7dbdbc2e6e': true,
@@ -96,113 +56,239 @@ const updatedExpectAnswers = {
   '4472d7df987644f39804cb7dbdbc2e6e': true,
 };
 
-jest.mock('./api', () => ({
-  getChecklists: jest.fn(() => Promise.resolve(checklist)),
-  getQuestions: jest.fn(() => Promise.resolve(questions)),
-  getAnswers: jest.fn(() => Promise.resolve(answers)),
-  getCategory: jest.fn(() => Promise.resolve(category)),
-  getStats: jest.fn(() => Promise.resolve()),
-  postAnswers: jest.fn(() => Promise.resolve()),
-}));
+const initialPieChartProps = { positive: 1, negative: 1, unknown: 0 };
 
-const mockStore = configureStore();
-const store = {
-  workspace: {
-    user: {
-      uuid: 'uuid',
-    },
-  },
-};
-
-const renderUserChecklist = () =>
-  mount(
-    <Provider store={mockStore(store)}>
-      <UserChecklist />
+const renderChecklist = async (
+  store,
+  extraProps: React.ComponentProps<typeof UserChecklist> = {},
+) => {
+  const wrapper = mount<typeof UserChecklist>(
+    <Provider store={store}>
+      <UserChecklist {...extraProps} />
     </Provider>,
   );
+  await actWait();
 
-const updateComponent = async (component) => {
-  return await act(() => {
-    component.update();
-    return Promise.resolve();
-  });
+  expect(wrapper.find(LoadingSpinner)).toBeTruthy();
+
+  await updateWrapper(wrapper);
+
+  return wrapper;
 };
 
-const initComponent = async () => {
-  let component;
+type ChecklistWrapper = Await<ReturnType<typeof renderChecklist>>;
 
-  await act(() => {
-    component = renderUserChecklist();
-    return Promise.resolve();
-  });
-
-  expect(component.find(LoadingSpinner)).toBeTruthy();
-
-  await updateComponent(component);
-
-  return Promise.resolve(component);
-};
-
-const expectPieChart = (component, { positive, negative, unknown }) => {
-  const pieChart = component.find(PieChart);
+const expectPieChart = (
+  wrapper: ChecklistWrapper,
+  { positive, negative, unknown },
+) => {
+  const pieChart = wrapper.find(PieChart);
 
   expect(pieChart.prop('positive')).toBe(positive);
   expect(pieChart.prop('negative')).toBe(negative);
   expect(pieChart.prop('unknown')).toBe(unknown);
 };
 
-const expectPropsQuestionsAndAnswers = (component, wrapper) => {
-  const found = component.find(wrapper);
+const changeAnswer = async (wrapper: ChecklistWrapper) => {
+  const btnAnswer = wrapper.find("input[type='radio']");
+  btnAnswer.at(0).simulate('change');
+  await updateWrapper(wrapper);
+};
 
-  expect(found).toBeTruthy();
-  expect(found.prop('questions')).toEqual(questions);
-  expect(found.prop('answers')).toEqual(initialExpectAnswers);
+const submit = async (wrapper: ChecklistWrapper) => {
+  wrapper.find('button').simulate('click');
+  await updateWrapper(wrapper);
+  await updateWrapper(wrapper);
 };
 
 describe('UserChecklist', () => {
-  it('initial component', async () => {
-    const component = await initComponent();
+  let store;
 
-    expectPropsQuestionsAndAnswers(component, AnswersTable);
-    expectPropsQuestionsAndAnswers(component, AnswersSummary);
+  beforeEach(() => {
+    apiMock.getChecklists.mockResolvedValue(fixtures.checklists);
+    apiMock.getQuestions.mockResolvedValue(fixtures.questions);
+    apiMock.getAnswers.mockResolvedValue(fixtures.answers);
+    apiMock.getCategory.mockResolvedValue(fixtures.category);
+    apiMock.getStats.mockResolvedValue([]);
+    apiMock.postAnswers.mockResolvedValue(null);
 
-    expectPieChart(component, { positive: 1, negative: 1, unknown: 0 });
+    const mockStore = configureStore();
+    const storeData = {
+      workspace: {
+        user: {
+          uuid: 'current-user-uuid',
+        },
+      },
+    };
+
+    store = mockStore(storeData);
   });
 
-  it('should change active answer for button in component AnswersTable after click', async () => {
-    const component = await initComponent();
+  it('renders empty message if there are no checklist in given category yet', async () => {
+    apiMock.getChecklists.mockResolvedValue([]);
+    const wrapper = await renderChecklist(store);
+    expect(wrapper.html()).toBe('There are no checklist yet.');
+  });
 
-    const btnAnswer = component.find("input[type='radio']");
+  it('renders error message if it is unable to load checklists', async () => {
+    apiMock.getChecklists.mockRejectedValue(null);
+    const wrapper = await renderChecklist(store);
+    expect(wrapper.html()).toBe('Unable to load checklists.');
+  });
 
-    btnAnswer.at(0).simulate('change');
+  it('renders error message if it is unable to load questions', async () => {
+    apiMock.getQuestions.mockRejectedValue(null);
+    const wrapper = await renderChecklist(store);
+    expect(wrapper.html()).toContain('Unable to load questions and answers.');
+  });
 
-    await updateComponent(component);
+  it('renders error message if it is unable to load questions', async () => {
+    apiMock.getAnswers.mockRejectedValue(null);
+    const wrapper = await renderChecklist(store);
+    expect(wrapper.html()).toContain('Unable to load questions and answers.');
+  });
 
-    const answersTable = component.find(AnswersTable);
+  it('renders error message if it is unable to load category', async () => {
+    apiMock.getCategory.mockRejectedValue(null);
+    const wrapper = await renderChecklist(store);
+    expect(wrapper.html()).toContain('Unable to load questions and answers.');
+  });
 
+  it('renders error notification if it is unable to load questions', async () => {
+    // Arrange
+    apiMock.getAnswers.mockRejectedValue(null);
+
+    // Act
+    await renderChecklist(store);
+
+    // Assert
+    const actions = store.getActions();
+    expect(actions[0].type).toBe('error');
+  });
+
+  it('renders answers table', async () => {
+    const wrapper = await renderChecklist(store);
+
+    const node = wrapper.find(AnswersTable);
+    expect(node.prop('questions')).toEqual(fixtures.questions);
+    expect(node.prop('answers')).toEqual(initialExpectAnswers);
+  });
+
+  it('renders answers summary', async () => {
+    const wrapper = await renderChecklist(store);
+
+    const node = wrapper.find(AnswersSummary);
+    expect(node.prop('questions')).toEqual(fixtures.questions);
+    expect(node.prop('answers')).toEqual(initialExpectAnswers);
+  });
+
+  it('renders pie chart', async () => {
+    const wrapper = await renderChecklist(store);
+    expectPieChart(wrapper, initialPieChartProps);
+  });
+
+  it('sets document title from category name', async () => {
+    await renderChecklist(store);
+
+    const actions = store.getActions();
+    expect(actions[0].type).toBe(SET_TITLE);
+  });
+
+  it('fetches questions for current checklist', async () => {
+    await renderChecklist(store);
+    expect(apiMock.getQuestions).toBeCalledWith(fixtures.checklists[0].uuid);
+  });
+
+  it('fetches category by state param', async () => {
+    await renderChecklist(store);
+    expect(apiMock.getCategory).toBeCalledWith('mock-category');
+  });
+
+  it('fetches answers for current user and current checklist', async () => {
+    await renderChecklist(store);
+    expect(apiMock.getAnswers).toBeCalledWith(
+      'current-user-uuid',
+      fixtures.checklists[0].uuid,
+    );
+  });
+
+  it('updates summary when answer is changed', async () => {
+    // Arrange
+    const wrapper = await renderChecklist(store);
+
+    // Act
+    await changeAnswer(wrapper);
+
+    // Assert
+    const answersTable = wrapper.find(AnswersTable);
     expect(answersTable.prop('answers')).toEqual(updatedExpectAnswers);
   });
 
-  it('should change component PieChart after submit', async () => {
-    const component = await initComponent();
+  it('sends API request when answers are submitted', async () => {
+    const wrapper = await renderChecklist(store);
+    await submit(wrapper);
+    expect(apiMock.postAnswers).toBeCalled();
+  });
 
-    const btnAnswer = component.find("input[type='radio']");
+  it('updates pie chart after submit', async () => {
+    // Arrange
+    const wrapper = await renderChecklist(store);
 
-    btnAnswer.at(0).simulate('change');
+    // Act
+    await changeAnswer(wrapper);
+    await submit(wrapper);
 
-    await updateComponent(component);
+    // Assert
+    expectPieChart(wrapper, { positive: 2, negative: 0, unknown: 0 });
+  });
 
-    const answersTable = component.find(AnswersTable);
+  it('renders success notification after submit', async () => {
+    // Arrange
+    const wrapper = await renderChecklist(store);
 
-    expect(answersTable.prop('answers')).toEqual(updatedExpectAnswers);
+    // Act
+    await changeAnswer(wrapper);
+    await submit(wrapper);
 
-    const btnSubmit = component.find('button');
+    // Assert
+    const actions = store.getActions();
+    expect(actions[1].type).toBe('success');
+  });
 
-    btnSubmit.simulate('click');
+  it('does not update pie chart if submit failed', async () => {
+    // Arrange
+    apiMock.postAnswers.mockRejectedValue(null);
+    const wrapper = await renderChecklist(store);
 
-    await updateComponent(component);
-    await updateComponent(component);
+    // Act
+    await changeAnswer(wrapper);
+    await submit(wrapper);
 
-    expectPieChart(component, { positive: 2, negative: 0, unknown: 0 });
+    // Assert
+    expectPieChart(wrapper, initialPieChartProps);
+  });
+
+  it('renders error notification if submit failed', async () => {
+    // Arrange
+    apiMock.postAnswers.mockRejectedValue(null);
+    const wrapper = await renderChecklist(store);
+
+    // Act
+    await changeAnswer(wrapper);
+    await submit(wrapper);
+
+    // Assert
+    const actions = store.getActions();
+    expect(actions[1].type).toBe('error');
+  });
+
+  it('conceals submit button if checklist is read-only', async () => {
+    const wrapper = await renderChecklist(store, { readOnly: true });
+    expect(wrapper.contains('button')).toBe(false);
+  });
+
+  it('conceals input elements if checklist is read-only', async () => {
+    const wrapper = await renderChecklist(store, { readOnly: true });
+    expect(wrapper.contains(`input[type='radio']`)).toBe(false);
   });
 });
--- src/marketplace-checklist/fixtures.ts
+++ src/marketplace-checklist/fixtures.ts
@@ -1,4 +1,4 @@
-import { ChecklistStats } from './types';
+import { Checklist, ChecklistStats, Question, Answer, Category } from './types';
 
 export const initCustomer = (props = {}): ChecklistStats => {
   return {
@@ -9,4 +9,58 @@ export const initCustomer = (props = {}): ChecklistStats => {
     longitude: 95.63666172955419,
     ...props,
   };
+};
+
+export const checklists: Checklist[] = [
+  {
+    uuid: '51525237e3bc40879c437f5d4d3be850',
+    name: 'Best practices (2 questions)',
+    description: '',
+    questions_count: 2,
+    category_name: 'Project checklist',
+    category_uuid: '7fd33c0055d8453d84e4da5371dd8fab',
+  },
+  {
+    uuid: '407296c2821345aab0f02a583293a476',
+    name: 'Cyberhygiene (2 questions)',
+    description: '',
+    questions_count: 2,
+    category_name: 'Project checklist',
+    category_uuid: '7fd33c0055d8453d84e4da5371dd8fab',
+  },
+];
+
+export const questions: Question[] = [
+  {
+    uuid: '554b3673d2f944299057143fa62be3ef',
+    description: 'I assign PIDs to all my generated resources',
+    solution: 'Please see RDA recommendations here and here',
+    category_uuid: '6dfcdbe3c18241dc87526ef411bf6064',
+    correct_answer: true,
+  },
+  {
+    uuid: '4472d7df987644f39804cb7dbdbc2e6e',
+    description: 'I preserve data in a storage with SLAs on preservation',
+    solution: 'Find a service provider to use for keeping artifacts.',
+    category_uuid: '6dfcdbe3c18241dc87526ef411bf6064',
+    correct_answer: true,
+  },
+];
+
+export const answers: Answer[] = [
+  {
+    question_uuid: '554b3673d2f944299057143fa62be3ef',
+    value: false,
+  },
+  {
+    question_uuid: '4472d7df987644f39804cb7dbdbc2e6e',
+    value: true,
+  },
+];
+
+export const category: Category = {
+  uuid: '7fd33c0055d8453d84e4da5371dd8fab',
+  icon: null,
+  name: 'Project checklist',
+  checklists_count: 2,
 };
