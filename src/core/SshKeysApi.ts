7c3ac01b9c1bd76e132a417eb55071d60141a914
Victor Mireyev <victor@opennodecloud.com>
2020-12-11 00:34:27 +0300

    Remove leftover.

--- src/core/SshKeysApi.ts
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios';
-
-import { ENV } from '@waldur/core/services';
-
-export const findPublicKeyByUuid = (uuid: string) =>
-  Axios.get(`${ENV.apiEndpoint}api/keys/${uuid}/`).then(
-    (response) => response.data,
-  );
