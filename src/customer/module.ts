46cb5091f4016782e997d74ed6ed0b9afe617609
Victor Mireyev <victor@opennodecloud.com>
2020-12-11 02:16:54 +0300

    Restore types.

--- src/customer/module.ts
+++ src/customer/module.ts
@@ -1,6 +1,1 @@
-import customerWorkspaceModule from './workspace/module';
 import './events';
-
-export default (module) => {
-  customerWorkspaceModule(module);
-};
--- src/customer/workspace/CustomerWorkspace.tsx
+++ src/customer/workspace/CustomerWorkspace.tsx
@@ -1,12 +1,22 @@
 import { useCurrentStateAndParams } from '@uirouter/react';
 import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useAsync } from 'react-use';
 
 import { translate } from '@waldur/i18n';
 import { useBreadcrumbsFn } from '@waldur/navigation/breadcrumbs/store';
 import { BreadcrumbItem } from '@waldur/navigation/breadcrumbs/types';
 import { Layout } from '@waldur/navigation/Layout';
-import { getCustomer } from '@waldur/workspace/selectors';
+import { stateGo } from '@waldur/store/coreSaga';
+import {
+  setCurrentCustomer,
+  setCurrentProject,
+  setCurrentWorkspace,
+} from '@waldur/workspace/actions';
+import { getCustomer, getUser } from '@waldur/workspace/selectors';
+import { ORGANIZATION_WORKSPACE } from '@waldur/workspace/types';
+
+import { CustomersService } from '../services/CustomersService';
 
 import { CustomerSidebar } from './CustomerSidebar';
 
@@ -28,7 +38,32 @@ export const CustomerWorkspace = () => {
   const [pageClass, setPageClass] = useState<string>();
   const [hideBreadcrumbs, setHideBreadcrumbs] = useState<boolean>();
   const customer = useSelector(getCustomer);
+  const currentUser = useSelector(getUser);
   const { state, params } = useCurrentStateAndParams();
+  const customerId = params?.uuid;
+
+  const dispatch = useDispatch();
+  useAsync(async () => {
+    if (!customerId) {
+      dispatch(stateGo('errorPage.notFound'));
+    } else {
+      try {
+        const currentCustomer = await CustomersService.get(customerId);
+        dispatch(setCurrentCustomer(currentCustomer));
+        dispatch(setCurrentProject(null));
+        dispatch(setCurrentWorkspace(ORGANIZATION_WORKSPACE));
+
+        if (
+          !CustomersService.checkCustomerUser(currentCustomer, currentUser) &&
+          !currentUser.is_support
+        ) {
+          dispatch(stateGo('errorPage.notFound'));
+        }
+      } catch {
+        dispatch(stateGo('errorPage.notFound'));
+      }
+    }
+  }, [customerId]);
 
   function refreshState() {
     const data = state?.data;
@@ -40,11 +75,11 @@ export const CustomerWorkspace = () => {
 
   useEffect(refreshState, [state, params]);
 
-  return (
+  return customer ? (
     <Layout
       sidebar={<CustomerSidebar />}
       pageClass={pageClass}
       hideBreadcrumbs={hideBreadcrumbs}
     />
-  );
+  ) : null;
 };
