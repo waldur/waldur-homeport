8d454b9a99f5db0c828b560290c2b078a1bc9586
Michael Alex <michaelaleks.dev@gmail.com>
2021-02-24 10:07:17 -0500

    Staff should be able to see Google Calendar actions even if it's not an owner

--- src/marketplace/offerings/actions/GoogleCalendarActions.ts
+++ src/marketplace/offerings/actions/GoogleCalendarActions.ts
@@ -2,34 +2,29 @@ import { OFFERING_TYPE_BOOKING } from '@waldur/booking/constants';
 import { translate } from '@waldur/i18n';
 import { ARCHIVED } from '@waldur/marketplace/offerings/store/constants';
 
+const isVisible = (ownProps, stateProps): boolean =>
+  ownProps.offering.type === OFFERING_TYPE_BOOKING &&
+  ![ARCHIVED].includes(ownProps.offering.state) &&
+  (stateProps.user.is_staff || stateProps.isOwner);
+
 export const googleCalendarActions = (dispatchProps, ownProps, stateProps) => [
   {
     label: translate('Sync with Google Calendar'),
     handler: () => dispatchProps.syncGoogleCalendar(ownProps.offering),
-    visible:
-      ownProps.offering.type === OFFERING_TYPE_BOOKING &&
-      ![ARCHIVED].includes(ownProps.offering.state) &&
-      stateProps.user.is_staff &&
-      stateProps.isOwner,
+    visible: isVisible(ownProps, stateProps),
   },
   {
     label: translate('Publish as Google Calendar'),
     handler: () => dispatchProps.publishGoogleCalendar(ownProps.offering),
     visible:
       !ownProps.offering.google_calendar_is_public &&
-      ownProps.offering.type === OFFERING_TYPE_BOOKING &&
-      ![ARCHIVED].includes(ownProps.offering.state) &&
-      stateProps.user.is_staff &&
-      stateProps.isOwner,
+      isVisible(ownProps, stateProps),
   },
   {
     label: translate('Unpublish as Google Calendar'),
     handler: () => dispatchProps.unpublishGoogleCalendar(ownProps.offering),
     visible:
       ownProps.offering.google_calendar_is_public &&
-      ownProps.offering.type === OFFERING_TYPE_BOOKING &&
-      ![ARCHIVED].includes(ownProps.offering.state) &&
-      stateProps.user.is_staff &&
-      stateProps.isOwner,
+      isVisible(ownProps, stateProps),
   },
 ];
