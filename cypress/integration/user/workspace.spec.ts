8d94e729b36c64fae9df4b035af77f2d7cfce09c
Michael Alex <michaelaleks.dev@gmail.com>
2021-03-03 23:45:21 +0400

    Fixed cypress bugs in user workspace

--- cypress/integration/user/workspace.spec.ts
+++ cypress/integration/user/workspace.spec.ts
@@ -4,22 +4,22 @@ describe('User workspace', function () {
   });
 
   it('Should go to audit log', () => {
-    cy.get('a').contains('Audit logs').click();
+    cy.get('a').contains('Audit logs').click({ force: true });
     cy.title().should('contain', 'Audit logs');
   });
 
   it('Should go to SSH keys list', () => {
-    cy.get('a').contains('SSH Keys').click();
+    cy.get('a').contains('SSH Keys').click({ force: true });
     cy.title().should('contain', 'SSH keys');
   });
 
   it('Should go to notifications list', () => {
-    cy.get('a').contains('Notifications').click();
+    cy.get('a').contains('Notifications').click({ force: true });
     cy.title().should('contain', 'Notifications');
   });
 
   it('Should go to manage user tab', () => {
-    cy.get('a').contains('Manage').click();
+    cy.get('a').contains('Manage').click({ force: true });
     cy.title().should('contain', 'Manage');
   });
 });
