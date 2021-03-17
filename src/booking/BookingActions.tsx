7b75e61993f14cad7502a1e2b54b3cf86f80a1b0
Michael Alex <michaelaleks.dev@gmail.com>
2021-03-17 16:15:17 +0400

    edit, change plan, termiante

--- src/booking/BookingActions.tsx
+++ src/booking/BookingActions.tsx
@@ -1,6 +1,9 @@
 import { useBoolean } from 'react-use';
 
+import { ChangePlanAction } from '@waldur/marketplace/resources/change-plan/ChangePlanAction';
 import { SubmitReportAction } from '@waldur/marketplace/resources/report/SubmitReportAction';
+import { EditAction } from '@waldur/openstack/openstack-tenant/actions/EditAction';
+import { TerminateTenantAction } from '@waldur/openstack/openstack-tenant/actions/TerminateTenantAction';
 import { ResourceActionComponent } from '@waldur/resource/actions/ResourceActionComponent';
 
 import { AcceptAction } from './AcceptAction';
@@ -8,10 +11,13 @@ import { CancelAction } from './CancelAction';
 import { RejectAction } from './RejectAction';
 
 const ActionsList = [
+  EditAction,
   AcceptAction,
   RejectAction,
+  ChangePlanAction,
   CancelAction,
   SubmitReportAction,
+  TerminateTenantAction,
 ];
 
 export const BookingActions = ({ resource, reInitResource }) => {
