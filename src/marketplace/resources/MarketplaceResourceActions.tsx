4d9a5577ded57859d4542cdd5f8bf70c67e73ca6
Michael Alex <michaelaleks.dev@gmail.com>
2021-03-18 12:09:53 +0400

    terminate

--- src/marketplace/resources/MarketplaceResourceActions.tsx
+++ src/marketplace/resources/MarketplaceResourceActions.tsx
@@ -5,8 +5,8 @@ import { CancelAction } from '@waldur/booking/CancelAction';
 import { RejectAction } from '@waldur/booking/RejectAction';
 import { ChangePlanAction } from '@waldur/marketplace/resources/change-plan/ChangePlanAction';
 import { SubmitReportAction } from '@waldur/marketplace/resources/report/SubmitReportAction';
+import { TerminateAction } from '@waldur/marketplace/resources/terminate/TerminateAction';
 import { EditAction } from '@waldur/openstack/openstack-tenant/actions/EditAction';
-import { TerminateTenantAction } from '@waldur/openstack/openstack-tenant/actions/TerminateTenantAction';
 import { ResourceActionComponent } from '@waldur/resource/actions/ResourceActionComponent';
 
 const ActionsList = [
@@ -16,7 +16,7 @@ const ActionsList = [
   ChangePlanAction,
   CancelAction,
   SubmitReportAction,
-  TerminateTenantAction,
+  TerminateAction,
 ];
 
 export const MarketplaceResourceActions = ({ resource, reInitResource }) => {
