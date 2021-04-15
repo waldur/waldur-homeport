754f05af445aa3c0669092231816b597af315bc8
Michael Alex <michaelaleks.dev@gmail.com>
2021-04-15 13:16:19 +0400

    Feedback rating should be loaded only for staff and support users

--- src/issues/SupportIssues.tsx
+++ src/issues/SupportIssues.tsx
@@ -1,16 +1,19 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 
 import { Panel } from '@waldur/core/Panel';
 import { translate } from '@waldur/i18n';
 import { CustomerSupportRating } from '@waldur/issues/CustomerSupportRating';
 import { IssuesList } from '@waldur/issues/list/IssuesList';
 import { useTitle } from '@waldur/navigation/title';
+import { isStaffOrSupport as isStaffOrSupportSelector } from '@waldur/workspace/selectors';
 
 export const SupportIssues: FunctionComponent = () => {
   useTitle(translate('Issues'));
+  const isStaffOrSupport = useSelector(isStaffOrSupportSelector);
   return (
     <>
-      <CustomerSupportRating />
+      {isStaffOrSupport && <CustomerSupportRating />}
       <Panel>
         <IssuesList />
       </Panel>
