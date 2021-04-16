e4419a4d7833ef7f2e5de6922bc0408e8d27af4b
Victor Mireyev <victor@opennodecloud.com>
2021-04-16 10:42:55 +0300

    Introduce offering option to enable service provider to push offering users [WAL-3969]

--- src/support/OfferingPluginSecretOptionsForm.tsx
+++ src/support/OfferingPluginSecretOptionsForm.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from 'react';
 
 import { FormContainer, TextField } from '@waldur/form';
+import { AwesomeCheckboxField } from '@waldur/form/AwesomeCheckboxField';
 import { translate } from '@waldur/i18n';
 
 export const OfferingPluginSecretOptionsForm: FunctionComponent<{
@@ -10,6 +11,11 @@ export const OfferingPluginSecretOptionsForm: FunctionComponent<{
     <TextField
       name="template_confirmation_comment"
       label={translate('Confirmation notification template')}
+    />
+    <AwesomeCheckboxField
+      name="service_provider_can_create_offering_user"
+      label={translate('Allow service provider to create offering users.')}
+      hideLabel={true}
     />
   </FormContainer>
 );
