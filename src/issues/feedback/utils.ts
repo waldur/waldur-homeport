ae565d51b609ee0aa3b77fabe8f8dda06740fccf
Michael Alex <michaelaleks.dev@gmail.com>
2021-04-26 12:32:35 +0000

    [WAL-4054] Change feedback collection to use 0-10 points instead of 1-5

--- src/issues/feedback/utils.ts
+++ src/issues/feedback/utils.ts
@@ -1,9 +1,15 @@
 import { translate } from '@waldur/i18n';
 
 export const feedbackOptions = () => [
-  { value: 5, label: translate('Very positive') },
-  { value: 4, label: translate('Positive') },
-  { value: 3, label: translate('Neutral') },
-  { value: 2, label: translate('Negative') },
-  { value: 1, label: translate('Very negative') },
+  { value: 10, label: translate('10 - Very positive') },
+  { value: 9, label: '9' },
+  { value: 8, label: '8' },
+  { value: 7, label: '7' },
+  { value: 6, label: '6' },
+  { value: 5, label: '5' },
+  { value: 4, label: '4' },
+  { value: 3, label: '3' },
+  { value: 2, label: '2' },
+  { value: 1, label: '1' },
+  { value: 0, label: translate('0 - Very negative') },
 ];
