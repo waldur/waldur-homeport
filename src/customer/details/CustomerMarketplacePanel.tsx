101d3ad633d7baa7edea9cfd6bed2d65fd44ee6f
Michael Alex <michaelaleks.dev@gmail.com>
2021-05-06 12:54:42 +0400

    Call for registration should not be shown for a registered service provider

--- src/customer/details/CustomerMarketplacePanel.tsx
+++ src/customer/details/CustomerMarketplacePanel.tsx
@@ -1,16 +1,23 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 
 import { translate } from '@waldur/i18n';
 import { ServiceProviderManagement } from '@waldur/marketplace/service-providers/ServiceProviderManagement';
+import { getCustomer } from '@waldur/workspace/selectors';
 
-export const CustomerMarketplacePanel: FunctionComponent = () => (
-  <div className="highlight">
-    <h3>{translate('Marketplace service provider')}</h3>
-    <p>
-      {translate(
-        'You can register organization as a marketplace service provider by pressing the button below',
+export const CustomerMarketplacePanel: FunctionComponent = () => {
+  const customer = useSelector(getCustomer);
+  return (
+    <div className="highlight">
+      <h3>{translate('Marketplace service provider')}</h3>
+      {!customer.is_service_provider && (
+        <p>
+          {translate(
+            'You can register organization as a marketplace service provider by pressing the button below',
+          )}
+        </p>
       )}
-    </p>
-    <ServiceProviderManagement />
-  </div>
-);
+      <ServiceProviderManagement />
+    </div>
+  );
+};
