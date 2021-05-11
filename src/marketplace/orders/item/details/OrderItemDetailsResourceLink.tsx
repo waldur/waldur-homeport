9a152c49b8e3efe8e0190d9be5c9dc9fc8afc4fe
Victor Mireyev <victor@opennodecloud.com>
2021-05-11 10:47:04 +0300

    Fix resource link in order item detail view for resource without scope [WAL-4095]

---
+++ src/marketplace/orders/item/details/OrderItemDetailsResourceLink.tsx
@@ -0,0 +1,37 @@
+import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+
+import { Link } from '@waldur/core/Link';
+import { ResourceReference } from '@waldur/marketplace/resources/types';
+import {
+  getCustomer,
+  getProject,
+  getWorkspace,
+} from '@waldur/workspace/selectors';
+import { ORGANIZATION_WORKSPACE } from '@waldur/workspace/types';
+
+interface OrderItemDetailsResourceLinkProps {
+  item: ResourceReference;
+  children?: React.ReactNode;
+}
+
+export const OrderItemDetailsResourceLink: FunctionComponent<OrderItemDetailsResourceLinkProps> = (
+  props,
+) => {
+  const workspace = useSelector(getWorkspace);
+  const customer = useSelector(getCustomer);
+  const project = useSelector(getProject);
+  let state, params;
+  if (workspace === ORGANIZATION_WORKSPACE) {
+    state = 'marketplace-public-resource-details';
+    params = { uuid: customer.uuid, resource_uuid: props.item.resource_uuid };
+  } else {
+    state = 'marketplace-project-resource-details';
+    params = { uuid: project.uuid, resource_uuid: props.item.resource_uuid };
+  }
+  return (
+    <>
+      <Link state={state} params={params} label={props.children} />
+    </>
+  );
+};
