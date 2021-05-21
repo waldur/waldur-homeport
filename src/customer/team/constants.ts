4bdcadc0b13d9e0de1a64b35684b0c93ebc3012e
Michael Alex <michaelaleks.dev@gmail.com>
2021-05-21 11:53:53 +0000

    [WAL-3687] Allow to manage expiration time for service manager permission

--- src/customer/team/constants.ts
+++ src/customer/team/constants.ts
@@ -1,3 +1,5 @@
 export const CUSTOMER_USERS_LIST_FILTER_FORM_ID = 'CustomerUsersListFilter';
+export const UPDATE_OFFERING_PERMISSION_EXPIRATION_TIME_FORM_ID =
+  'updateOfferingPermissionExpirationTime';
 
 export const OFFERING_PERMISSIONS_LIST_ID = 'marketplace-offering-permissions';
