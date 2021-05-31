6081016ac031b77eeaa2419b300a9d3d59dd876f
Victor Mireyev <victor@opennodecloud.com>
2021-05-31 14:46:07 +0300

    Implement asynchronous loading of sizes dependent on locations and zones.

--- src/azure/vm/utils.ts
+++ src/azure/vm/utils.ts
@@ -1,34 +1,82 @@
-import { getLocations, getImages, getSizes } from '@waldur/azure/common/api';
-import { Size, Option, Image } from '@waldur/azure/common/types';
+import { Size, Image } from '@waldur/azure/common/types';
+import { ENV } from '@waldur/configs/default';
+import { getSelectData } from '@waldur/core/api';
+import { returnReactSelectAsyncPaginateObject } from '@waldur/core/utils';
 import { formatFlavor } from '@waldur/resource/utils';
 
-const formatSizeOption = (size: Size): Option => {
+export const getSizeLabel = (size: Size): string => {
   const summary = formatFlavor({
     disk: size.os_disk_size_in_mb + size.resource_disk_size_in_mb,
     cores: size.number_of_cores,
     ram: size.memory_in_mb,
   });
   const name = size.name.replace(/_/g, ' ');
-  return {
-    url: size.url,
-    name: `${name} (${summary})`,
-  };
+  return `${name} (${summary})`;
 };
 
-const formatImageOption = (image: Image): Option => {
-  return {
-    url: image.url,
-    name: `${image.publisher} ${image.name} ${image.sku}`,
-  };
+export const getImageLabel = (image: Image): string =>
+  `${image.publisher} ${image.name} ${image.sku}`;
+
+export const loadLocationOptions = async (
+  settings_uuid: string,
+  query: string,
+  prevOptions,
+  currentPage: number,
+) => {
+  const response = await getSelectData('/azure-locations/', {
+    settings_uuid,
+    name: query,
+    page: currentPage,
+    page_size: ENV.pageSize,
+    has_sizes: true,
+  });
+  return returnReactSelectAsyncPaginateObject(
+    response,
+    prevOptions,
+    currentPage,
+  );
 };
 
-export const loadData = (settings_uuid: string) =>
-  Promise.all([
-    getLocations(settings_uuid),
-    getImages(settings_uuid),
-    getSizes(settings_uuid),
-  ]).then(([locations, images, sizes]) => ({
-    locations,
-    images: images.map(formatImageOption),
-    sizes: sizes.map(formatSizeOption),
-  }));
+export const loadSizeOptions = async (
+  settings_uuid: string,
+  location_uuid: string,
+  zone: number,
+  query: string,
+  prevOptions,
+  currentPage: number,
+) => {
+  const response = await getSelectData('/azure-sizes/', {
+    settings_uuid,
+    location_uuid,
+    zone,
+    name: query,
+    page: currentPage,
+    page_size: ENV.pageSize,
+    has_sizes: true,
+  });
+  return returnReactSelectAsyncPaginateObject(
+    response,
+    prevOptions,
+    currentPage,
+  );
+};
+
+export const loadImageOptions = async (
+  settings_uuid: string,
+  query: string,
+  prevOptions,
+  currentPage: number,
+) => {
+  const response = await getSelectData('/azure-images/', {
+    settings_uuid,
+    name: query,
+    page: currentPage,
+    page_size: ENV.pageSize,
+    has_sizes: true,
+  });
+  return returnReactSelectAsyncPaginateObject(
+    response,
+    prevOptions,
+    currentPage,
+  );
+};
