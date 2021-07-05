c8bc8008c0ef99b2f7ed4f4f2cdcd90bf1399b13
Victor Mireyev <victor.mireyev@gmail.com>
2021-07-05 11:51:06 +0300

    Make set backend ID action visibility consistent with backend [WAL-4187]

--- src/marketplace/resources/SetBackendIdAction.tsx
+++ src/marketplace/resources/SetBackendIdAction.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
 
 import { lazyComponent } from '@waldur/core/lazyComponent';
 import { translate } from '@waldur/i18n';
-import { validateStaffAction } from '@waldur/marketplace/resources/actions/utils';
 import { DialogActionItem } from '@waldur/resource/actions/DialogActionItem';
+import {
+  isOwnerOrStaff as isOwnerOrStaffSelector,
+  isServiceManagerSelector,
+} from '@waldur/workspace/selectors';
 
 const SetBackendIdDialog = lazyComponent(
   () =>
@@ -11,14 +15,18 @@ const SetBackendIdDialog = lazyComponent(
   'SetBackendIdDialog',
 );
 
-const validators = [validateStaffAction];
-
-export const SetBackendIdAction: FC<any> = ({ resource, reInitResource }) => (
-  <DialogActionItem
-    validators={validators}
-    title={translate('Set backend ID')}
-    modalComponent={SetBackendIdDialog}
-    extraResolve={{ reInitResource }}
-    resource={resource}
-  />
-);
+export const SetBackendIdAction: FC<any> = ({ resource, reInitResource }) => {
+  const isOwnerOrStaff = useSelector(isOwnerOrStaffSelector);
+  const isServiceManager = useSelector(isServiceManagerSelector);
+  if (!isOwnerOrStaff && !isServiceManager) {
+    return null;
+  }
+  return (
+    <DialogActionItem
+      title={translate('Set backend ID')}
+      modalComponent={SetBackendIdDialog}
+      extraResolve={{ reInitResource }}
+      resource={resource}
+    />
+  );
+};
