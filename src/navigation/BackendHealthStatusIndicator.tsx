94270b54cbf1202abee9e4d64bd0c5a9870a341f
Michael Alex <michaelaleks.dev@gmail.com>
2021-07-12 14:41:30 +0000

    [WAL-4112] Health status in Homeport

---
+++ src/navigation/BackendHealthStatusIndicator.tsx
@@ -0,0 +1,73 @@
+import Axios from 'axios';
+import { FunctionComponent } from 'react';
+import { connect } from 'react-redux';
+import { useAsync } from 'react-use';
+
+import { ENV } from '@waldur/configs/default';
+import { lazyComponent } from '@waldur/core/lazyComponent';
+import { Tooltip } from '@waldur/core/Tooltip';
+import { translate } from '@waldur/i18n';
+import { openModalDialog } from '@waldur/modal/actions';
+
+const BackendHealthStatusDialog = lazyComponent(
+  () =>
+    import(
+      /* webpackChunkName: "BackendHealthStatusDialog" */ './BackendHealthStatusDialog'
+    ),
+  'BackendHealthStatusDialog',
+);
+
+export const getBackendHealthStatus = () =>
+  Axios.get(`${ENV.apiEndpoint}health-check/`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+const isWorking = (data): boolean => {
+  for (const item of Object.values(data)) {
+    if (item !== 'working') {
+      return false;
+    }
+  }
+  return true;
+};
+
+const openBackendHealthStatusDialog = () =>
+  openModalDialog(BackendHealthStatusDialog);
+
+export const PureBackendHealthStatusIndicator: FunctionComponent<any> = (
+  props,
+) => {
+  const { value, error } = useAsync(getBackendHealthStatus, []);
+  return (
+    <span className="m-r-sm">
+      {error ? (
+        <Tooltip
+          label={translate('Unable to load backend health status')}
+          id="backend-status-tooltip"
+        >
+          <i className="fa fa-info-circle text-danger" />
+        </Tooltip>
+      ) : value ? (
+        <a onClick={() => props.openDialog()}>
+          {isWorking(value.data) ? (
+            <i className="fa fa-check-circle text-success" />
+          ) : (
+            <i className="fa fa-times-circle text-danger" />
+          )}
+        </a>
+      ) : null}
+    </span>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  openDialog: () => dispatch(openBackendHealthStatusDialog()),
+});
+
+const enhance = connect(null, mapDispatchToProps);
+
+export const BackendHealthStatusIndicator = enhance(
+  PureBackendHealthStatusIndicator,
+);
