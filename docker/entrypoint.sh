42d77a3ba786da9d44927629c30afb8a8801415e
Yefim Gasel <yefim.gasel@gmail.com>
2021-09-13 11:01:19 +0000

    WAL-4221: Move docker entrypoint to a shell script and change HTML page title during docker container boot

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p /usr/share/nginx/html/scripts/configs/ && envsubst < /usr/share/nginx/config.template.json > /usr/share/nginx/html/scripts/configs/config.json
+
+cp /usr/share/nginx/html/index.orig.html /usr/share/nginx/html/index.html
+sed -i "s/<title>Waldur | Cloud Service Management<\/title>/<title>${TITLE}<\/title>/" /usr/share/nginx/html/index.html
+
+nginx -g 'daemon off;'
