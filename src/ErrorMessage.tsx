4c05635c2d24424626fb0fd91f6be8a3f3142548
Victor Mireyev <victor.mireyev@gmail.com>
2021-09-16 13:58:25 +0300

    Render error stack after error message.

--- src/ErrorMessage.tsx
+++ src/ErrorMessage.tsx
@@ -7,14 +7,17 @@ import { ImageTablePlaceholder } from '@waldur/table/ImageTablePlaceholder';
 const Illustration = require('@waldur/images/table-placeholders/undraw_fixing_bugs_w7gi.svg');
 
 export const ErrorMessage: FallbackRender = (props) => (
-  <ImageTablePlaceholder
-    illustration={Illustration}
-    title={translate(`An error has occurred.`)}
-    description={props.error.message}
-    action={
-      <Button onClick={() => location.reload()} bsStyle="success">
-        <i className="fa fa-refresh" /> {translate('Reload')}
-      </Button>
-    }
-  />
+  <>
+    <ImageTablePlaceholder
+      illustration={Illustration}
+      title={translate(`An error has occurred.`)}
+      description={props.error.message}
+      action={
+        <Button onClick={() => location.reload()} bsStyle="success">
+          <i className="fa fa-refresh" /> {translate('Reload')}
+        </Button>
+      }
+    />
+    <pre className="m-t-md">{props.componentStack}</pre>
+  </>
 );
