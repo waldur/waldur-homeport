4dfa119f14e5230ebc06d7332f4d47b24d8b4173
ilja <ilja@opennodecloud.com>
2021-09-22 13:26:20 +0300

    Change labels for external IdPs [WAL-4342]

--- src/auth/AuthButton.tsx
+++ src/auth/AuthButton.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
 
-import { translate } from '@waldur/i18n';
-
 export interface AuthButtonProps {
   providerKey: string;
   btnClass: string;
   iconClass: string;
   label: string;
-  mode: string;
   onClick(dispatch: Dispatch): void;
 }
 
@@ -17,21 +14,16 @@ export const AuthButton: React.FC<AuthButtonProps> = ({
   btnClass,
   iconClass,
   label,
-  mode,
   onClick,
 }) => {
   const dispatch = useDispatch();
-  const caption =
-    mode === 'register'
-      ? translate('Register with {label}', { label })
-      : translate('Sign in with {label}', { label });
   return (
     <div className="m-b-sm">
       <button
         onClick={() => onClick(dispatch)}
-        className={`btn ${btnClass} btn-block`}
+        className={`btn ${btnClass} btn-block text-center`}
       >
-        <i className={`fa ${iconClass}`} aria-hidden="true" /> {caption}
+        <i className={`fa ${iconClass}`} aria-hidden="true" /> {label}
       </button>
     </div>
   );
