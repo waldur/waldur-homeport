4a8dadadb6f435f7215c6fcf5c84ea771f32058e
Michael Alex <michaelaleks.dev@gmail.com>
2021-10-18 19:41:27 +0000

    [WAL-4388] Expose user keys and remote users in user support view

--- src/user/keys/KeysList.tsx
+++ src/user/keys/KeysList.tsx
@@ -6,12 +6,18 @@ import { useTitle } from '@waldur/navigation/title';
 import { router } from '@waldur/router';
 import { RootState } from '@waldur/store/reducers';
 import { Table, createFetcher, connectTable } from '@waldur/table';
+import { KeysListExpandableRow } from '@waldur/user/keys/KeysListExpandableRow';
 import { KeysListTablePlaceholder } from '@waldur/user/keys/KeysListTablePlaceholder';
 import { getUser } from '@waldur/workspace/selectors';
+import { UserDetails } from '@waldur/workspace/types';
 
 import { KeyCreateButton } from './KeyCreateButton';
 import { KeyRemoveButton } from './KeyRemoveButton';
 import { isStaffOrSelfSelectorCreator } from './selectors';
+
+interface OwnProps {
+  user: UserDetails;
+}
 
 const TableComponent: FunctionComponent<any> = (props) => {
   const { translate } = props;
@@ -45,6 +51,7 @@ const TableComponent: FunctionComponent<any> = (props) => {
       actions={props.isStaffOrSelf && <KeyCreateButton />}
       placeholderComponent={<KeysListTablePlaceholder />}
       enableExport={true}
+      expandableRow={KeysListExpandableRow}
     />
   );
 };
@@ -61,8 +68,8 @@ const TableOptions = {
   queryField: 'name',
 };
 
-const mapStateToProps = (state: RootState) => ({
-  user: getUser(state),
+const mapStateToProps = (state: RootState, ownProps: OwnProps) => ({
+  user: ownProps.user || getUser(state),
   isStaffOrSelf: isStaffOrSelfSelectorCreator(router.globals.params)(state),
 });
 
