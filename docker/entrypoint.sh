c08e3678524f5ca8999566f32c90032dd0d98c5a
Victor Mireyev <victor.mireyev@gmail.com>
2021-12-09 11:59:54 +0000

    Apply title only if it is defined.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,6 @@
 mkdir -p /usr/share/nginx/html/scripts/configs/ && envsubst < /usr/share/nginx/config.template.json > /usr/share/nginx/html/scripts/configs/config.json
 
 cp /usr/share/nginx/html/index.orig.html /usr/share/nginx/html/index.html
-sed -i "s/<title>Waldur | Cloud Service Management<\/title>/<title>${TITLE}<\/title>/" /usr/share/nginx/html/index.html
+[[ -z "${TITLE}" ]] && sed -i "s/<title>Waldur | Cloud Service Management<\/title>/<title>${TITLE}<\/title>/" /usr/share/nginx/html/index.html
 
 nginx -g 'daemon off;'
