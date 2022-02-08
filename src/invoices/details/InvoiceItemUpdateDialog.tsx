ead44764b05dd3b8d6e7b2e3f17c89fc4fa11e3b
Victor Mireyev <victor.mireyev@gmail.com>
2022-02-08 16:06:15 +0200

    Allow editing of start and end date by staff of invoice items [WAL-4638]

--- src/invoices/details/InvoiceItemUpdateDialog.tsx
+++ src/invoices/details/InvoiceItemUpdateDialog.tsx
@@ -7,6 +7,13 @@ import { showSuccess, showErrorResponse } from '@waldur/store/notify';
 
 import { updateInvoiceItem } from '../api';
 
+interface InvoiceItemUpdatePayload {
+  article_code?: string;
+  start?: string;
+  end?: string;
+  quantity?: number;
+}
+
 export const InvoiceItemUpdateDialog = ({
   resolve: { resource, refreshInvoiceItems },
 }) => {
@@ -18,20 +25,44 @@ export const InvoiceItemUpdateDialog = ({
       required: false,
       type: 'string',
     },
-    {
+  ];
+  if (resource.billing_type === 'fixed') {
+    fields.push({
+      name: 'start',
+      label: translate('Date and time when item usage has started'),
+      required: true,
+      type: 'datetime',
+    });
+    fields.push({
+      name: 'end',
+      label: translate('Date and time when item usage has ended'),
+      required: true,
+      type: 'datetime',
+    });
+  } else {
+    fields.push({
       name: 'quantity',
       label: translate('Quantity'),
       required: false,
       type: 'integer',
-    },
-  ];
+    });
+  }
+  const initialValues: InvoiceItemUpdatePayload = {
+    article_code: resource.article_code,
+  };
+  if (resource.billing_type === 'fixed') {
+    initialValues.start = resource.start;
+    initialValues.end = resource.end;
+  } else {
+    initialValues.quantity = resource.quantity;
+  }
   return (
     <ResourceActionDialog
       dialogTitle={translate('Update invoice item {name}', {
         name: resource.name,
       })}
       formFields={fields}
-      submitForm={async (formData) => {
+      submitForm={async (formData: InvoiceItemUpdatePayload) => {
         try {
           await updateInvoiceItem(resource.uuid, formData);
           dispatch(showSuccess(translate('Invoice item has been updated.')));
@@ -43,9 +74,7 @@ export const InvoiceItemUpdateDialog = ({
           );
         }
       }}
-      initialValues={{
-        article_code: resource.article_code,
-      }}
+      initialValues={initialValues}
     />
   );
 };
