b9de6b7d0086cd684bc7c4211d97ba90089a7510
Andrey Izman <andreyizman9@gmail.com>
2022-02-11 10:35:53 +0000

    [WAL-4625] Adding a functionality to filter divisions in Offerings

--- src/marketplace/offerings/service-providers/CategorySectionsFilterForm.tsx
+++ src/marketplace/offerings/service-providers/CategorySectionsFilterForm.tsx
@@ -1,14 +1,16 @@
 import { FunctionComponent } from 'react';
 import { reduxForm } from 'redux-form';
 
+import { AttributeFilterDivision } from '@waldur/marketplace/category/filters/AttributeFilterDivision';
 import { prepareSections } from '@waldur/marketplace/category/filters/AttributeFilterList';
 import { AttributeFilterSection } from '@waldur/marketplace/category/filters/AttributeFilterSection';
 import { CategorySectionsFilterFormActions } from '@waldur/marketplace/offerings/service-providers/CategorySectionsFilterFormActions';
 import { OFFERING_CATEGORY_SECTION_FORM_ID } from '@waldur/marketplace/offerings/service-providers/constants';
-import { Section } from '@waldur/marketplace/types';
+import { Division, Section } from '@waldur/marketplace/types';
 import './CategorySectionsFilterForm.scss';
 
 interface OwnProps {
+  divisions: Division[];
   sections: Section[];
   closeDropdown: () => void;
 }
@@ -16,6 +18,9 @@ interface OwnProps {
 const PureCategorySectionsFilterForm: FunctionComponent<any> = (props) => (
   <div className="categorySectionsFilterForm">
     <form>
+      {props.divisions?.length > 0 && (
+        <AttributeFilterDivision divisions={props.divisions} />
+      )}
       {prepareSections(props.sections).map(
         (section: Section, index: number) => (
           <AttributeFilterSection key={index} section={section} />
