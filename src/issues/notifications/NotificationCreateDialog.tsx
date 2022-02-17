9953a10c7fee7c8251705781060cb4ba413bd224
Andrey Izman <andreyizman9@gmail.com>
2022-02-17 13:57:45 +0000

    [WAL-4660] Simplify broadcast form

--- src/issues/notifications/NotificationCreateDialog.tsx
+++ src/issues/notifications/NotificationCreateDialog.tsx
@@ -9,20 +9,9 @@ import { useDispatch } from 'react-redux';
 import { reduxForm } from 'redux-form';
 
 import { SubmitButton } from '@waldur/auth/SubmitButton';
-import { ENV } from '@waldur/configs/default';
 import { post } from '@waldur/core/api';
-import {
-  CUSTOMER_OWNER_ROLE,
-  PROJECT_ADMIN_ROLE,
-  PROJECT_MANAGER_ROLE,
-} from '@waldur/core/constants';
 import { required } from '@waldur/core/validators';
-import {
-  FormContainer,
-  SelectField,
-  StringField,
-  TextField,
-} from '@waldur/form';
+import { FormContainer, StringField, TextField } from '@waldur/form';
 import { AsyncSelectField } from '@waldur/form/AsyncSelectField';
 import { reactSelectMenuPortaling } from '@waldur/form/utils';
 import { translate } from '@waldur/i18n';
@@ -35,8 +24,6 @@ import {
 import { closeModalDialog } from '@waldur/modal/actions';
 import { CloseDialogButton } from '@waldur/modal/CloseDialogButton';
 import { showErrorResponse, showSuccess } from '@waldur/store/notify';
-
-import { projectAutocomplete } from './utils';
 
 export const NotificationCreateDialog = reduxForm({
   form: NOTIFICATION_CREATE_FORM_ID,
@@ -52,9 +39,6 @@ export const NotificationCreateDialog = reduxForm({
           body: formData.body,
           query: {
             customers: formData.customers?.map((c) => c.uuid),
-            projects: formData.projects?.map((c) => c.uuid),
-            customer_roles: formData.customer_roles?.map((c) => c.value),
-            project_roles: formData.project_roles?.map((c) => c.value),
             offerings: formData.offerings?.map((c) => c.uuid),
           },
         });
@@ -85,6 +69,21 @@ export const NotificationCreateDialog = reduxForm({
       </ModalHeader>
       <ModalBody>
         <FormContainer submitting={submitting}>
+          <AsyncSelectField
+            name="offerings"
+            label={translate('Offerings')}
+            placeholder={translate('Select offerings...')}
+            loadOptions={(query, prevOptions, page) =>
+              offeringsAutocomplete(
+                { name: query, shared: true },
+                prevOptions,
+                page,
+              )
+            }
+            isMulti={true}
+            {...reactSelectMenuPortaling()}
+            onChange={(e) => setFetchNumber(e)}
+          />
           <StringField
             name="subject"
             label={translate('Subject')}
@@ -102,68 +101,6 @@ export const NotificationCreateDialog = reduxForm({
             label={translate('Organizations')}
             placeholder={translate('Select organizations...')}
             loadOptions={organizationAutocomplete}
-            isMulti={true}
-            {...reactSelectMenuPortaling()}
-            onChange={(e) => setFetchNumber(e)}
-          />
-          <SelectField
-            label={translate('Organization roles')}
-            name="customer_roles"
-            placeholder={translate('Select role')}
-            options={[
-              {
-                label: translate('Owner'),
-                value: CUSTOMER_OWNER_ROLE,
-              },
-              {
-                label: translate('Support'),
-                value: 'support',
-              },
-            ]}
-            isMulti={true}
-            noUpdateOnBlur={true}
-            onChange={(e) => setFetchNumber(e)}
-          />
-          <AsyncSelectField
-            name="projects"
-            label={translate('Projects')}
-            placeholder={translate('Select projects...')}
-            loadOptions={(query, prevOptions, page) =>
-              projectAutocomplete(query, prevOptions, page)
-            }
-            isMulti={true}
-            {...reactSelectMenuPortaling()}
-            onChange={(e) => setFetchNumber(e)}
-          />
-          <SelectField
-            label={translate('Project roles')}
-            name="project_roles"
-            placeholder={translate('Select role')}
-            options={[
-              {
-                label: translate(ENV.roles.manager),
-                value: PROJECT_MANAGER_ROLE,
-              },
-              {
-                label: translate(ENV.roles.admin),
-                value: PROJECT_ADMIN_ROLE,
-              },
-            ]}
-            isMulti={true}
-            noUpdateOnBlur={true}
-            onChange={(e) => setFetchNumber(e)}
-          />
-          <AsyncSelectField
-            name="offerings"
-            label={translate('Offerings')}
-            placeholder={translate('Select offerings...')}
-            loadOptions={(query, prevOptions, page) =>
-              offeringsAutocomplete(
-                { name: query, shared: true },
-                prevOptions,
-                page,
-              )
-            }
             isMulti={true}
             {...reactSelectMenuPortaling()}
             onChange={(e) => setFetchNumber(e)}
