254b9a3ec37e344be7b30d15b435ea4096cdf0fe
Victor Mireyev <victor.mireyev@gmail.com>
2022-02-18 16:07:59 +0200

    Fix rendering of price estimate for current month [SENTRY-5470]

--- src/invoices/list/EstimatedCost.tsx
+++ src/invoices/list/EstimatedCost.tsx
@@ -1,21 +1,40 @@
 import { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
+import { useAsync } from 'react-use';
 
+import { getById } from '@waldur/core/api';
 import { defaultCurrency } from '@waldur/core/formatCurrency';
 import { translate } from '@waldur/i18n';
 import { getActiveFixedPricePaymentProfile } from '@waldur/invoices/details/utils';
 import { getCustomer } from '@waldur/workspace/selectors';
+
+const getPriceEstimate = (uuid: string) =>
+  getById<{ billing_price_estimate: { total: number } }>(
+    '/financial-reports/',
+    uuid,
+  );
+
+const AsyncEstimatedCost = ({ customer }) => {
+  const { value } = useAsync(() => getPriceEstimate(customer.uuid));
+  if (!value) {
+    return null;
+  }
+  return (
+    <p>
+      {translate('Estimated cost for the current month: {cost}', {
+        cost: defaultCurrency(value.billing_price_estimate.total),
+      })}
+    </p>
+  );
+};
 
 export const EstimatedCost: FunctionComponent = () => {
   const customer = useSelector(getCustomer);
   if (!customer) {
     return null;
   }
-  return !getActiveFixedPricePaymentProfile(customer.payment_profiles) ? (
-    <p>
-      {translate('Estimated cost for the current month: {cost}', {
-        cost: defaultCurrency(customer.billing_price_estimate.total),
-      })}
-    </p>
-  ) : null;
+  if (getActiveFixedPricePaymentProfile(customer.payment_profiles)) {
+    return null;
+  }
+  return <AsyncEstimatedCost customer={customer} />;
 };
