d08cebab7685488cd6463a7147d27c5e22f93329
Andrey Izman <andreyizman9@gmail.com>
2022-02-22 06:53:23 +0000

    [WAL-4550] Add resize functionality on the public offering page

---
+++ src/marketplace/offerings/details/SidebarResizer.tsx
@@ -0,0 +1,53 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+export const SidebarResizer = () => {
+  const sidebarRef = useRef(
+    document.getElementsByClassName('sidebarEditor')[0] as HTMLElement,
+  );
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState(600);
+
+  useEffect(() => {
+    const el = document.getElementsByClassName(
+      'sidebarEditor',
+    )[0] as HTMLElement;
+    if (el) sidebarRef.current = el;
+  });
+
+  const startResizing = useCallback(() => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = useCallback(
+    (mouseMoveEvent) => {
+      if (isResizing && sidebarRef.current) {
+        setSidebarWidth(
+          sidebarRef.current.getBoundingClientRect().right -
+            mouseMoveEvent.clientX,
+        );
+      }
+    },
+    [isResizing],
+  );
+
+  useEffect(() => {
+    window.addEventListener('mousemove', resize);
+    window.addEventListener('mouseup', stopResizing);
+    return () => {
+      window.removeEventListener('mousemove', resize);
+      window.removeEventListener('mouseup', stopResizing);
+    };
+  }, [resize, stopResizing]);
+
+  useEffect(() => {
+    if (sidebarRef.current) {
+      sidebarRef.current.style.width = sidebarWidth + 'px';
+    }
+  }, [sidebarWidth]);
+
+  return <div className="sidebar-resizer" onMouseDown={startResizing} />;
+};
