9e6e471b2a08d70b8299adc8af3e8a797d29c18d
Victor Mireyev <victor.mireyev@gmail.com>
2022-04-07 22:04:11 +0300

    Fix date field

--- src/form/DateField.tsx
+++ src/form/DateField.tsx
@@ -1,7 +1,16 @@
+import { DateTime } from 'luxon';
 import { FunctionComponent } from 'react';
 import DatePicker from 'react-datepicker';
 
-export const DateField: FunctionComponent<any> = (props) => {
-  const { input, ...rest } = props;
-  return <DatePicker {...input} dateFormat="YYYY-MM-DD" {...rest} />;
-};
+export const DateField: FunctionComponent<any> = (props) => (
+  <DatePicker
+    dateFormat="yyyy-MM-dd"
+    selected={
+      typeof props.input.value === 'string'
+        ? DateTime.fromISO(props.input.value).toJSDate()
+        : props.input.value
+    }
+    onChange={props.input.onChange}
+    className="form-control"
+  />
+);
