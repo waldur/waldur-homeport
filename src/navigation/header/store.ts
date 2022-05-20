7356392452149f3dd0b7ee85e11202a8413bf404
Andrey Izman <andreyizman9@gmail.com>
2022-05-20 06:03:40 +0000

    WAL-4826 Fix dark theme switcher issues

--- src/navigation/header/store.ts
+++ src/navigation/header/store.ts
@@ -2,24 +2,44 @@ import { AnyAction } from 'redux';
 
 export interface Theme {
   theme: 'dark' | 'light';
+  themes: Record<'dark' | 'light', string> | {};
 }
 
 export const ThemeAction = {
   UPDATE_THEME: 'UPDATE_THEME',
+  ADD_THEME: 'ADD_THEME',
+};
+
+const getInitialTheme: any = () => {
+  let currentTheme = localStorage.getItem('themeMode');
+  if (!currentTheme) {
+    const isDarkPrefers = window.matchMedia(
+      '(prefers-color-scheme:dark)',
+    ).matches;
+    currentTheme = isDarkPrefers ? 'dark' : 'light';
+  }
+  localStorage.setItem('themeMode', currentTheme);
+  return currentTheme;
 };
 
 const INITIAL_STATE: Theme = {
-  theme: 'dark',
+  theme: getInitialTheme(),
+  themes: {},
 };
 
 export const updateThemeMode = (payload) => {
   return { type: ThemeAction.UPDATE_THEME, payload };
 };
+export const addTheme = (payload: Pick<Theme, 'themes'>) => {
+  return { type: ThemeAction.ADD_THEME, payload };
+};
 
 export const reducer = (state = INITIAL_STATE, action: AnyAction) => {
   switch (action.type) {
     case ThemeAction.UPDATE_THEME:
-      return { theme: action.payload };
+      return { ...state, theme: action.payload };
+    case ThemeAction.ADD_THEME:
+      return { ...state, themes: action.payload };
     default:
       return state;
   }
--- webpack.utils.js
+++ webpack.utils.js
@@ -3,5 +3,6 @@ const path = require('path');
 const isProd = process.env.NODE_ENV === 'production';
 const target = isProd ? 'build' : 'build.dev';
 const formatPath = (fragment) => path.resolve(`./${target}`, fragment);
+const darkModeEnabledInDevEnv = true;
 
-module.exports = { isProd, target, formatPath };
+module.exports = { isProd, target, formatPath, darkModeEnabledInDevEnv };
