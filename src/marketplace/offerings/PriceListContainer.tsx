1330421dfa82c9f9734a888352859f9665e9aa81
Andrey Izman <andreyizman9@gmail.com>
2022-06-01 07:06:06 +0000

    [WAL-4832] Fix difference of looking on the pricelist table

--- src/marketplace/offerings/PriceListContainer.tsx
+++ src/marketplace/offerings/PriceListContainer.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent } from 'react';
 
-import { Panel } from '@waldur/core/Panel';
 import { translate } from '@waldur/i18n';
 import { useReportingBreadcrumbs } from '@waldur/issues/workspace/SupportWorkspace';
 import { useTitle } from '@waldur/navigation/title';
@@ -10,9 +9,5 @@ import { PriceList } from './PriceList';
 export const PriceListContainer: FunctionComponent = () => {
   useTitle(translate('Pricelist'));
   useReportingBreadcrumbs();
-  return (
-    <Panel>
-      <PriceList />
-    </Panel>
-  );
+  return <PriceList />;
 };
