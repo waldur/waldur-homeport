3dd3be11dd3a486552667ef0b6660f6730b80156
Victor Mireyev <victor.mireyev@gmail.com>
2022-06-14 14:36:42 +0300

    Implement support for dark theme in notification system.

--- src/NotificationContainer.tsx
+++ src/NotificationContainer.tsx
@@ -2,15 +2,19 @@ import { FunctionComponent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import NotificationsSystem, {
   bootstrapTheme,
+  atalhoTheme,
   dismissNotification,
 } from 'reapop';
+
+import { RootState } from './store/reducers';
 
 export const NotificationContainer: FunctionComponent = () => {
   const dispatch = useDispatch();
   const notifications = useSelector((state: any) => state.notifications);
+  const theme = useSelector((state: RootState) => state.theme.theme);
   return (
     <NotificationsSystem
-      theme={bootstrapTheme}
+      theme={theme === 'dark' ? atalhoTheme : bootstrapTheme}
       notifications={notifications}
       dismissNotification={(id) => dispatch(dismissNotification(id))}
     />
