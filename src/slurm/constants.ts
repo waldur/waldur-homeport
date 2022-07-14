db1ee3b5c197570dce6d21a2eba87c68b688aec6
Andrey Izman <andreyizman9@gmail.com>
2022-07-14 21:18:53 +0000

    [WAL-4867] Adding support for the new marketplace SLURM plugin (Old)

--- src/slurm/constants.ts
+++ src/slurm/constants.ts
@@ -1,3 +1,4 @@
 export const ICON_CLASS = 'fa-tasks';
 export const FEATURE = 'slurm';
 export const SLURM_PLUGIN = 'SlurmInvoices.SlurmPackage';
+export const SLURM_REMOTE_PLUGIN = 'Marketplace.Slurm';
--- src/slurm/marketplace.ts
+++ src/slurm/marketplace.ts
@@ -2,7 +2,7 @@ import { lazyComponent } from '@waldur/core/lazyComponent';
 import { translate } from '@waldur/i18n';
 import { registerOfferingType } from '@waldur/marketplace/common/registry';
 import { Attribute } from '@waldur/marketplace/types';
-import { SLURM_PLUGIN } from '@waldur/slurm/constants';
+import { SLURM_PLUGIN, SLURM_REMOTE_PLUGIN } from '@waldur/slurm/constants';
 
 const AllocationForm = lazyComponent(
   () =>
@@ -53,5 +53,16 @@ registerOfferingType({
   component: AllocationForm,
   providerType: 'SLURM',
   attributes: ServiceSettingsAttributes,
+  allowToUpdateService: true,
+});
+
+registerOfferingType({
+  type: SLURM_REMOTE_PLUGIN,
+  get label() {
+    return translate('SLURM remote allocation');
+  },
+  component: AllocationForm,
+  providerType: 'SLURM remote',
+  attributes: (): Attribute[] => [],
   allowToUpdateService: true,
 });
