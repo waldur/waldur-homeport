4915b3a386c34bae60f05bc3c419b34ae7cd1eff
Adil Shirinov <adil.shirinov.99@gmail.com>
2022-07-26 10:16:54 +0000

    Add support for instances to OpenStack server groups detail view in HomePort [WAL-4950]

--- src/openstack/openstack-server-groups/module.ts
+++ src/openstack/openstack-server-groups/module.ts
@@ -2,6 +2,7 @@ import { lazyComponent } from '@waldur/core/lazyComponent';
 import * as ResourceSummary from '@waldur/resource/summary/registry';
 
 import './actions';
+import './tabs';
 
 const OpenStackServerGroupSummary = lazyComponent(
   () =>
