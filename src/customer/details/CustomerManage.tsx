f99a80f385dde6543f3694e1eaa10ab80f498901
Ilja Livenson <livenson@users.noreply.github.com>
2022-09-21 10:05:07 +0200

    Idea for page titles

--- src/customer/details/CustomerManage.tsx
+++ src/customer/details/CustomerManage.tsx
@@ -1,14 +1,20 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 
 import { translate } from '@waldur/i18n';
 import { useTitle } from '@waldur/navigation/title';
+import { getCustomer } from '@waldur/workspace/selectors';
 
 import { CustomerDetailsPanel } from './CustomerDetailsPanel';
 import { CustomerMarketplacePanel } from './CustomerMarketplacePanel';
 import { CustomerRemovePanel } from './CustomerRemovePanel';
 
 export const CustomerManage: FunctionComponent = () => {
-  useTitle(translate('Manage organization'));
+  const customer = useSelector(getCustomer);
+  useTitle(
+    translate('Settings'),
+    translate('Organization') + ': ' + customer.display_name,
+  );
 
   return (
     <>
--- src/customer/details/CustomerPaymentProfiles.tsx
+++ src/customer/details/CustomerPaymentProfiles.tsx
@@ -1,11 +1,18 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 
 import { translate } from '@waldur/i18n';
 import { useTitle } from '@waldur/navigation/title';
+import { getCustomer } from '@waldur/workspace/selectors';
 
 import { PaymentProfilesPanel } from '../payment-profiles/PaymentProfilesPanel';
 
 export const CustomerPaymentProfiles: FunctionComponent = () => {
-  useTitle(translate('Payment profiles'));
+  const customer = useSelector(getCustomer);
+  useTitle(
+    translate('Payment profiles'),
+    translate('Organization') + ': ' + customer.display_name,
+  );
+
   return <PaymentProfilesPanel />;
 };
