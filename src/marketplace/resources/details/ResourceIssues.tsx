ecf47e91126cc0381d0d91f903f0e89d9e8e5dce
Viktor Mirieiev <victor.mireyev@gmail.com>
2022-10-13 16:20:15 +0300

    Implement infinite list for resource issues.

--- src/marketplace/resources/details/ResourceIssues.tsx
+++ src/marketplace/resources/details/ResourceIssues.tsx
@@ -1,63 +1,47 @@
-import { useAsync } from 'react-use';
+import { QueryFunction, useInfiniteQuery } from 'react-query';
 
-import { get } from '@waldur/core/api';
-import { formatDateTime } from '@waldur/core/dateUtils';
-import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
+import { fixURL } from '@waldur/core/api';
 import { translate } from '@waldur/i18n';
 import { Issue } from '@waldur/issues/list/types';
-import { IssueTypeIcon } from '@waldur/issues/types/IssueTypeIcon';
+import { parseResponse } from '@waldur/table/api';
+
+import { InfiniteList } from './InfiniteList';
+import { IssueRow } from './IssueRow';
+
+interface DataPage {
+  data: Issue[];
+  nextPage?: number;
+}
 
 export const ResourceIssues = ({ resource }) => {
-  const { loading, error, value } = useAsync(() =>
-    get<Issue[]>('/support-issues/', { params: { resource: resource.url } }),
+  const loadData: QueryFunction<DataPage> = async (context) => {
+    const response = await parseResponse(
+      fixURL('/support-issues/'),
+      {
+        resource: resource.url,
+        page: context.pageParam,
+      },
+      { signal: context.signal },
+    );
+    return {
+      data: response.rows,
+      nextPage: response.nextPage,
+    };
+  };
+
+  const context = useInfiniteQuery<any, any, DataPage>(
+    'resource-issues',
+    loadData,
+    {
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+    },
   );
-  if (loading) {
-    return <LoadingSpinner />;
-  }
-  if (error) {
-    return (
-      <p className="text-center">
-        {translate('Unable to fetch resource issues.')}
-      </p>
-    );
-  }
-  if (value.data.length === 0) {
-    return (
-      <p className="text-center">
-        {translate('There are no resource issues.')}
-      </p>
-    );
-  }
 
   return (
-    <>
-      <div className="timeline">
-        {value.data.map((issue, index) => {
-          return (
-            <div key={index} className="timeline-item">
-              <div className="timeline-line w-40px"></div>
-              <div className="timeline-icon symbol symbol-circle symbol-40px me-4">
-                <div className="symbol-label bg-light">
-                  <IssueTypeIcon type={issue.type} />
-                </div>
-              </div>
-              <div className="timeline-content mb-10 mt-n1">
-                <div className="pe-3 mb-5">
-                  <div className="fs-5 fw-semibold mb-2">{issue.summary}</div>
-                  <div className="d-flex align-items-center mt-1 fs-6">
-                    <div className="text-muted me-2 fs-7">
-                      {formatDateTime(issue.modified)}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <p className="text-center">
-        <a>{translate('See more')}</a>
-      </p>
-    </>
+    <InfiniteList
+      RowComponent={IssueRow}
+      context={context}
+      emptyMessage={translate('There are no resource issues.')}
+    />
   );
 };
--- src/marketplace/resources/details/ResourceTimeline.tsx
+++ src/marketplace/resources/details/ResourceTimeline.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { QueryFunction, useInfiniteQuery } from 'react-query';
 
 import { fixURL } from '@waldur/core/api';
@@ -7,13 +6,14 @@ import { translate } from '@waldur/i18n';
 import { parseResponse } from '@waldur/table/api';
 
 import { EventRow } from './EventRow';
+import { InfiniteList } from './InfiniteList';
 
 interface DataPage {
   data: Event[];
   nextPage?: number;
 }
 
-const useResourceTimeline = (resource) => {
+export const ResourceTimeline = ({ resource }) => {
   const loadData: QueryFunction<DataPage> = async (context) => {
     const response = await parseResponse(
       fixURL('/events/'),
@@ -29,55 +29,19 @@ const useResourceTimeline = (resource) => {
     };
   };
 
-  return useInfiniteQuery<any, any, DataPage>('resource-events', loadData, {
-    getNextPageParam: (lastPage) => lastPage.nextPage,
-  });
-};
+  const context = useInfiniteQuery<any, any, DataPage>(
+    'resource-events',
+    loadData,
+    {
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+    },
+  );
 
-export const ResourceTimeline = ({ resource }) => {
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status,
-  } = useResourceTimeline(resource);
-
-  return status === 'loading' ? (
-    <p className="text-center">{translate('Loading')}</p>
-  ) : status === 'error' ? (
-    <p className="text-center">{translate('Error')}</p>
-  ) : data.pages[0].data.length === 0 ? (
-    <p className="text-center">{translate('There are no resource events.')}</p>
-  ) : (
-    <>
-      <div className="timeline">
-        {data.pages.map((page, i) => (
-          <Fragment key={i}>
-            {page.data.map((event, index) => (
-              <EventRow event={event} key={index} />
-            ))}
-          </Fragment>
-        ))}
-      </div>
-      <p className="text-center">
-        {hasNextPage && (
-          <div>
-            <button
-              onClick={() => fetchNextPage()}
-              disabled={isFetchingNextPage}
-            >
-              {isFetchingNextPage
-                ? translate('Loading more...')
-                : translate('Load more')}
-            </button>
-          </div>
-        )}
-        <div>
-          {isFetching && !isFetchingNextPage ? translate('Fetching...') : null}
-        </div>
-      </p>
-    </>
+  return (
+    <InfiniteList
+      RowComponent={EventRow}
+      context={context}
+      emptyMessage={translate('There are no resource events.')}
+    />
   );
 };
