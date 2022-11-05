8f2589c45e87a04f9a84f216b5430ab48f74c8c5
Viktor Mirieiev <victor.mireyev@gmail.com>
2022-11-05 13:16:16 +0200

    Normalize quotas in resource details page.

--- src/marketplace/resources/details/ResourceComponents.tsx
+++ src/marketplace/resources/details/ResourceComponents.tsx
@@ -1,18 +1,34 @@
 import { Row } from 'react-bootstrap';
 
+import { Limits } from '@waldur/marketplace/common/registry';
+import { OfferingComponent } from '@waldur/marketplace/types';
+
 import { QuotaCell } from './QuotaCell';
 
-export const ResourceComponents = ({ resource, components }) => (
-  <Row>
-    {components.map((component) => (
-      <QuotaCell
-        key={component.type}
-        usage={resource.current_usages[component.type] || 0}
-        limit={resource.limits[component.type]}
-        units={component.measured_unit}
-        title={component.name}
-        description={component.description}
-      />
-    ))}
-  </Row>
-);
+export const ResourceComponents = ({
+  resource,
+  components,
+}: {
+  resource: { current_usages: Limits; limits: Limits };
+  components: OfferingComponent[];
+}) => {
+  const normalize = (value: number, factor: number) =>
+    ((value || 0) / (factor || 1)).toFixed();
+  return (
+    <Row>
+      {components.map((component) => (
+        <QuotaCell
+          key={component.type}
+          usage={normalize(
+            resource.current_usages[component.type],
+            component.factor,
+          )}
+          limit={normalize(resource.limits[component.type], component.factor)}
+          units={component.measured_unit}
+          title={component.name}
+          description={component.description}
+        />
+      ))}
+    </Row>
+  );
+};
--- src/marketplace/resources/details/ShortResourceHeader.tsx
+++ src/marketplace/resources/details/ShortResourceHeader.tsx
@@ -1,10 +1,33 @@
-export const ShortResourceHeader = ({ resource, components }) => {
+import { translate } from '@waldur/i18n';
+import { Limits } from '@waldur/marketplace/common/registry';
+import { OfferingComponent } from '@waldur/marketplace/types';
+
+export const ShortResourceHeader = ({
+  resource,
+  components,
+}: {
+  resource: {
+    name: string;
+    customer_name: string;
+    project_name: string;
+    current_usages: Limits;
+    limits: Limits;
+  };
+  components: OfferingComponent[];
+}) => {
+  const normalize = (value: number, factor: number) =>
+    ((value || 0) / (factor || 1)).toFixed();
   const componentsList = components
-    .map(
-      (component) =>
-        `${resource.current_usages[component.type] || 0} / ${
-          resource.limits[component.type] || 0
-        } ${component.measured_unit} ${component.name}`,
+    .map((component) =>
+      translate('{usage} out of {limit} {unit} {name}', {
+        usage: normalize(
+          resource.current_usages[component.type],
+          component.factor,
+        ),
+        limit: normalize(resource.limits[component.type], component.factor),
+        unit: component.measured_unit,
+        name: component.name,
+      }),
     )
     .join(', ');
   return (
