820a437571c67a35c74feb6a1581e079d40d2992
Viktor Mirieiev <victor.mireyev@gmail.com>
2022-11-07 15:52:27 +0200

    Expose VM tabs as nested [WAL-5092]

--- src/openstack/openstack-volume/tabs.ts
+++ src/openstack/openstack-volume/tabs.ts
@@ -1,6 +1,7 @@
 import { lazyComponent } from '@waldur/core/lazyComponent';
 import { translate } from '@waldur/i18n';
 import { getDefaultResourceTabs } from '@waldur/resource/tabs/constants';
+import { NestedResourceTabsConfiguration } from '@waldur/resource/tabs/NestedResourceTabsConfiguration';
 import { ResourceTabsConfiguration } from '@waldur/resource/tabs/ResourceTabsConfiguration';
 
 import { VOLUME_TYPE } from '../constants';
@@ -32,4 +33,22 @@ ResourceTabsConfiguration.register(VOLUME_TYPE, () => [
     component: SnapshotSchedulesList,
   },
   ...getDefaultResourceTabs(),
+]);
+
+NestedResourceTabsConfiguration.register(VOLUME_TYPE, () => [
+  {
+    title: translate('Snapshots'),
+    children: [
+      {
+        key: 'snapshots',
+        title: translate('Snapshots'),
+        component: VolumeSnapshotsList,
+      },
+      {
+        key: 'snapshot_schedules',
+        title: translate('Snapshot schedules'),
+        component: SnapshotSchedulesList,
+      },
+    ],
+  },
 ]);
