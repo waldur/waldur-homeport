beb86a15fd454410c6f1f9ce709c76e199016009
Ilja Livenson <ilja@opennodecloud.com>
2022-12-03 10:01:33 +0000

    Revert "Merge branch 'feature/WAL-5206' into 'next'"

--- src/project/ShortEventsList.tsx
+++ src/project/ShortEventsList.tsx
@@ -1,53 +1,75 @@
-import { FunctionComponent } from 'react';
+import { Fragment, FunctionComponent, useState } from 'react';
 import { Card } from 'react-bootstrap';
-import { connect } from 'react-redux';
+import { useAsync } from 'react-use';
 
+import { formatDateTime } from '@waldur/core/dateUtils';
 import { Link } from '@waldur/core/Link';
-import { getEventsList } from '@waldur/events/BaseEventsList';
+import { Tip } from '@waldur/core/Tooltip';
+import { truncate } from '@waldur/core/utils';
+import { Event } from '@waldur/events/types';
 import { translate } from '@waldur/i18n';
-import { RootState } from '@waldur/store/reducers';
-import { getProject } from '@waldur/workspace/selectors';
+import { LoadingScreen } from '@waldur/LoadingScreen';
 import { Project } from '@waldur/workspace/types';
 
-const PureProjectEvents = getEventsList({
-  getDefaultFilter: () => ({
-    page_size: 3,
-  }),
-  mapPropsToFilter: (props) => {
-    return {
-      scope: props.project ? props.project.url : undefined,
-      feature: ['projects', 'resources'],
-    };
-  },
-  mapPropsToTableId: (props) => ['project-events', props.project?.uuid],
-});
+import { fetchLatestEvents } from './api';
 
-const mapStateToProps = (state: RootState) => ({
-  project: getProject(state),
-});
-
-const ProjectEvents = connect(mapStateToProps)(PureProjectEvents);
+const EventItemTr = (item: Event, expanded, onClick: (item) => void) => {
+  const date = formatDateTime(item.created);
+  return (
+    <Fragment key={item.uuid}>
+      <tr>
+        <td className="text-decoration-underline text-dark w-100 py-3">
+          <span onClick={() => onClick(item)}>
+            <Tip label={item.message} id={`event-${item.uuid}`}>
+              {truncate(item.message)}{' '}
+              {expanded === item.uuid ? (
+                <i className="fa fa-chevron-up" />
+              ) : (
+                <i className="fa fa-chevron-down" />
+              )}
+            </Tip>
+          </span>
+        </td>
+        <td className="text-nowrap">{date}</td>
+      </tr>
+      {expanded === item.uuid && (
+        <tr>
+          <td colSpan={3}>{item.message}</td>
+        </tr>
+      )}
+    </Fragment>
+  );
+};
 
 export const ShortEventsList: FunctionComponent<{ project: Project }> = ({
   project,
 }) => {
+  const [expanded, setExpanded] = useState('');
+  const { loading, error, value } = useAsync(() =>
+    fetchLatestEvents(project, 3),
+  );
+
+  const toggleExpand = (event) => {
+    if (expanded === event.uuid) setExpanded('');
+    else setExpanded(event.uuid);
+  };
+
+  if (loading) {
+    return <LoadingScreen loading={loading} error={error} />;
+  }
   return (
     <Card>
       <Card.Body>
-        <div className="d-flex flex-column justify-content-between h-100">
-          <ProjectEvents
-            hasQuery={false}
-            title={translate('Events')}
-            verboseName={translate('events')}
-            actions={undefined}
-            enableExport={false}
-            fullWidth={true}
-          />
-
+        <div className="d-flex flex-column justify-content-between align-items-end h-100">
+          <table className="table align-middle fs-8">
+            <tbody>
+              {value.map((event) => EventItemTr(event, expanded, toggleExpand))}
+            </tbody>
+          </table>
           <Link
             state="project.events"
             params={{ uuid: project.uuid }}
-            className="btn btn-light btn-sm min-w-100px align-self-end"
+            className="btn btn-light btn-sm min-w-100px"
           >
             {translate('Audit log')}
           </Link>
