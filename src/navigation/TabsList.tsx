4c20822ab5546e936b1f5170b66d2bfc040e82c5
Viktor Mirieiev <victor.mireyev@gmail.com>
2022-12-12 18:28:26 +0200

    Fix warning in tabs list component.

--- src/navigation/TabsList.tsx
+++ src/navigation/TabsList.tsx
@@ -43,51 +43,49 @@ export const TabsList: FunctionComponent = () => {
     <>
       {tabs.map((parentTab, parentIndex) =>
         parentTab.children?.length > 0 ? (
-          <MenuLink key={parentIndex} to={parentTab.to}>
-            <a
-              data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
-              data-kt-menu-placement="bottom-start"
-              className={classNames(
-                'menu-item menu-lg-down-accordion menu-sub-lg-down-indention me-0 me-lg-2',
-                { here: parentTab === activeTab },
-              )}
-            >
-              <span className="menu-link">
+          <span
+            data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
+            data-kt-menu-placement="bottom-start"
+            className={classNames(
+              'menu-item menu-lg-down-accordion menu-sub-lg-down-indention me-0 me-lg-2',
+              { here: parentTab === activeTab },
+            )}
+            key={parentIndex}
+          >
+            <MenuLink to={parentTab.to}>
+              <a className="menu-link">
                 <span className="menu-title">{parentTab.title}</span>
                 <span className="menu-arrow d-lg-none"></span>
-              </span>
-              <div className="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown px-lg-2 py-lg-4 w-lg-200px">
-                {parentTab.children.map((childTab, childIndex) => (
-                  <UISrefActive class="showing" key={childIndex}>
-                    <UISref to={childTab.to} params={childTab.params}>
-                      <a className="menu-item" data-kt-menu-trigger="click">
-                        <span className="menu-link">
-                          <span className="menu-title">{childTab.title}</span>
-                        </span>
-                      </a>
-                    </UISref>
-                  </UISrefActive>
-                ))}
-              </div>
-            </a>
-          </MenuLink>
+              </a>
+            </MenuLink>
+            <div className="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown px-lg-2 py-lg-4 w-lg-200px">
+              {parentTab.children.map((childTab, childIndex) => (
+                <UISrefActive class="showing" key={childIndex}>
+                  <UISref to={childTab.to} params={childTab.params}>
+                    <a className="menu-item" data-kt-menu-trigger="click">
+                      <span className="menu-link">
+                        <span className="menu-title">{childTab.title}</span>
+                      </span>
+                    </a>
+                  </UISref>
+                </UISrefActive>
+              ))}
+            </div>
+          </span>
         ) : parentTab.to ? (
-          <MenuLink
+          <span
             key={parentIndex}
-            to={parentTab.to}
-            params={parentTab.params}
+            className={classNames('menu-item text-nowrap', {
+              here: parentTab === activeTab,
+            })}
+            data-kt-menu-trigger="click"
           >
-            <a
-              className={classNames('menu-item text-nowrap', {
-                here: parentTab === activeTab,
-              })}
-              data-kt-menu-trigger="click"
-            >
-              <span className="menu-link py-3">
+            <MenuLink to={parentTab.to} params={parentTab.params}>
+              <a className="menu-link py-3">
                 <span className="menu-title">{parentTab.title}</span>
-              </span>
-            </a>
-          </MenuLink>
+              </a>
+            </MenuLink>
+          </span>
         ) : null,
       )}
     </>
