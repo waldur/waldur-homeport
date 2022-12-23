3064f2ea9c39d35edc7eb4761c027a8be154139a
Andrey Izman <andreyizman9@gmail.com>
2022-12-23 16:42:22 +0000

    [WAL-5345] Fix the marketplace link not working issue on the sidebar

--- src/marketplace/links/LandingLink.tsx
+++ src/marketplace/links/LandingLink.tsx
@@ -1,8 +1,10 @@
+import { useCurrentStateAndParams } from '@uirouter/react';
 import { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Link } from '@waldur/core/Link';
 import { getWorkspace } from '@waldur/workspace/selectors';
+import { ORGANIZATION_WORKSPACE } from '@waldur/workspace/types';
 
 import { WORKSPACE_LANDING } from '../constants';
 
@@ -11,10 +13,15 @@ interface OwnProps {
 }
 
 export const LandingLink: FunctionComponent<OwnProps> = (props) => {
+  const { params } = useCurrentStateAndParams();
   const workspace = useSelector(getWorkspace);
   const state = WORKSPACE_LANDING[workspace] || 'public.marketplace-landing';
+  let stateParams;
+  if (workspace === ORGANIZATION_WORKSPACE) {
+    stateParams = { uuid: params.uuid };
+  }
   return (
-    <Link state={state} className={props.className}>
+    <Link state={state} className={props.className} params={stateParams}>
       {props.children}
     </Link>
   );
