576de07530bab81e4c72bd5b908fa53be6d477bb
Andrey Izman <andreyizman9@gmail.com>
2023-02-06 08:31:38 +0000

    [WAL-5475] Fix marketplace link shortcut broken issue on the sidebar

--- src/marketplace/links/LandingLink.tsx
+++ src/marketplace/links/LandingLink.tsx
@@ -3,6 +3,7 @@ import { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Link } from '@waldur/core/Link';
+import { isDescendantOf } from '@waldur/navigation/useTabs';
 import { getWorkspace } from '@waldur/workspace/selectors';
 import { ORGANIZATION_WORKSPACE } from '@waldur/workspace/types';
 
@@ -13,12 +14,21 @@ interface OwnProps {
 }
 
 export const LandingLink: FunctionComponent<OwnProps> = (props) => {
-  const { params } = useCurrentStateAndParams();
+  const { state: currentState, params } = useCurrentStateAndParams();
   const workspace = useSelector(getWorkspace);
-  const state = WORKSPACE_LANDING[workspace] || 'public.marketplace-landing';
+  let state = WORKSPACE_LANDING[workspace] || 'public.marketplace-landing';
   let stateParams;
-  if (workspace === ORGANIZATION_WORKSPACE) {
+  if (workspace === ORGANIZATION_WORKSPACE && params.uuid) {
     stateParams = { uuid: params.uuid };
+  }
+  if (!state || !stateParams) {
+    if (
+      isDescendantOf('reporting', currentState) ||
+      isDescendantOf('admin', currentState) ||
+      isDescendantOf('support', currentState)
+    ) {
+      state = 'public.marketplace-landing';
+    }
   }
   return (
     <Link state={state} className={props.className} params={stateParams}>
