b5d81b33791e703284fd4e711b6b05862d4e2cf3
Andrey Izman <andreyizman9@gmail.com>
2023-02-17 07:38:37 +0000

    [WAL-5510] Update sidebar context selector to contain only organization information

--- src/navigation/workspace/context-selector/utils.ts
+++ src/navigation/workspace/context-selector/utils.ts
@@ -1,8 +1,13 @@
 import { getAbbreviation } from '@waldur/core/utils';
 
 export const getItemAbbreviation = (item) => {
+  if (typeof item !== 'object') {
+    return item || '';
+  }
   if (item.abbreviation) {
     return item.abbreviation.toUpperCase().substr(0, 4);
+  } else if (item.name) {
+    return getAbbreviation(item.name).substr(0, 4);
   }
-  return getAbbreviation(item.name).substr(0, 4);
+  return '';
 };
