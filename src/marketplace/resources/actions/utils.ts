1a57099bea2a0826f9ab25445098c4a88991bc81
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-02-20 14:11:23 +0200

    Fix crash on redirect to login view [WAL-5536]

--- src/marketplace/resources/actions/utils.ts
+++ src/marketplace/resources/actions/utils.ts
@@ -2,7 +2,7 @@ import { translate } from '@waldur/i18n/translate';
 import { ActionContext } from '@waldur/resource/actions/types';
 
 export const validateStaffAction = (ctx: ActionContext) => {
-  if (ctx.user.is_staff) {
+  if (ctx?.user.is_staff) {
     return;
   }
   return translate(`Only staff users are allowed to perform this action.`);
