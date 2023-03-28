80d66708ab71d6d5b02438a8ca76811eaebb74d1
ShirinovAdil <dev.adil.shirinov@gmail.com>
2023-03-28 15:36:12 +0300

    Allow editing bootable for volumes

--- src/openstack/openstack-volume/actions/EditDialog.tsx
+++ src/openstack/openstack-volume/actions/EditDialog.tsx
@@ -4,16 +4,31 @@ import {
   createLatinNameField,
   createDescriptionField,
 } from '@waldur/resource/actions/base';
+import { ActionField } from '@waldur/resource/actions/types';
 import { UpdateResourceDialog } from '@waldur/resource/actions/UpdateResourceDialog';
+
+export function createBooleanField(): ActionField {
+  return {
+    name: 'bootable',
+    label: translate('Bootable'),
+    required: false,
+    type: 'boolean',
+  };
+}
 
 export const EditDialog = ({ resolve: { resource } }) => {
   return (
     <UpdateResourceDialog
-      fields={[createLatinNameField(), createDescriptionField()]}
+      fields={[
+        createLatinNameField(),
+        createDescriptionField(),
+        createBooleanField(),
+      ]}
       resource={resource}
       initialValues={{
         name: resource.name,
         description: resource.description,
+        bootable: resource.bootable,
       }}
       updateResource={updateVolume}
       verboseName={translate('OpenStack volume')}
