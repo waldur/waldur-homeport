e2b5201b5b153d6659659f537baebd095bcf5710
Adil Shirinov <dev.adil.shirinov@gmail.com>
2023-03-30 13:39:39 +0000

    [WAL-5538] Allow editing bootable for a volume (next)

--- src/openstack/openstack-volume/actions/EditDialog.tsx
+++ src/openstack/openstack-volume/actions/EditDialog.tsx
@@ -6,19 +6,33 @@ import {
   createLatinNameField,
   createDescriptionField,
 } from '@waldur/resource/actions/base';
-import { ActionDialogProps } from '@waldur/resource/actions/types';
+import { ActionDialogProps, ActionField } from '@waldur/resource/actions/types';
 import { UpdateResourceDialog } from '@waldur/resource/actions/UpdateResourceDialog';
+
+function createBootableField(): ActionField {
+  return {
+    name: 'bootable',
+    label: translate('Bootable'),
+    required: false,
+    type: 'boolean',
+  };
+}
 
 export const EditDialog: FC<ActionDialogProps> = ({
   resolve: { resource, refetch },
 }) => {
   return (
     <UpdateResourceDialog
-      fields={[createLatinNameField(), createDescriptionField()]}
+      fields={[
+        createLatinNameField(),
+        createDescriptionField(),
+        createBootableField(),
+      ]}
       resource={resource}
       initialValues={{
         name: resource.name,
         description: resource.description,
+        bootable: resource.bootable,
       }}
       updateResource={updateVolume}
       refetch={refetch}
