26679a624ad770dd81c07f71f867b8bac39ab4a6
Ilja Livenson <ilja@opennodecloud.com>
2023-03-31 12:18:18 +0000

    Revert "Merge branch 'feature/ssh-eduteams' into 'next'"

--- src/user/keys/KeyCreateButton.tsx
+++ src/user/keys/KeyCreateButton.tsx
@@ -1,32 +1,18 @@
 import { FunctionComponent, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { translate } from '@waldur/i18n/translate';
 import { ActionButton } from '@waldur/table/ActionButton';
-import { getUser } from '@waldur/workspace/selectors';
 
 import { keyCreateDialog } from './actions';
 
 export const KeyCreateButton: FunctionComponent = () => {
   const dispatch = useDispatch();
-  const user = useSelector(getUser);
-  const openFormDialog = useCallback(() => {
-    if (user.registration_method === 'eduteams') {
-      document.location =
-        'https://mms.myaccessid.org/fed-apps/profile/#settings_sshkeys';
-    } else {
-      dispatch(keyCreateDialog());
-    }
-  }, [user, dispatch]);
+  const openFormDialog = useCallback(() => dispatch(keyCreateDialog()), []);
 
   return (
     <ActionButton
       title={translate('Add key')}
-      tooltip={
-        user.registration_method === 'eduteams'
-          ? translate('SSH keys are managed externally via MyAccessID.')
-          : undefined
-      }
       action={openFormDialog}
       icon="fa fa-plus"
       variant="primary"
