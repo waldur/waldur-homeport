374b11fffa8070fba7407af4bb4f2ea9d7f8a9ff
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-04-14 14:57:10 +0300

    Render project users in resource details view [WAL-5641]

---
+++ src/project/ProjectUsersBadge.tsx
@@ -0,0 +1,85 @@
+import { useMemo } from 'react';
+import { Col, Form, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useAsync } from 'react-use';
+
+import { ENV } from '@waldur/configs/default';
+import {
+  PROJECT_ADMIN_ROLE,
+  PROJECT_MANAGER_ROLE,
+  PROJECT_MEMBER_ROLE,
+} from '@waldur/core/constants';
+import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
+import { SymbolsGroup } from '@waldur/customer/dashboard/SymbolsGroup';
+import { translate } from '@waldur/i18n';
+import { getProject } from '@waldur/workspace/selectors';
+
+import { fetchAllProjectUsers } from './api';
+
+export const ProjectUsersBadge = () => {
+  const project = useSelector(getProject);
+  const {
+    loading,
+    error,
+    value: users,
+  } = useAsync(async () => await fetchAllProjectUsers(project.uuid), [project]);
+
+  const managers = useMemo(
+    () =>
+      users ? users.filter((user) => user.role === PROJECT_MANAGER_ROLE) : null,
+    [users],
+  );
+  const members = useMemo(
+    () =>
+      users ? users.filter((user) => user.role === PROJECT_MEMBER_ROLE) : null,
+    [users],
+  );
+  const admins = useMemo(
+    () =>
+      users ? users.filter((user) => user.role === PROJECT_ADMIN_ROLE) : null,
+    [users],
+  );
+
+  return (
+    <div className="d-flex justify-content-start align-items-xl-center flex-xl-row flex-column gap-xl-6">
+      {loading ? (
+        <LoadingSpinner />
+      ) : error ? (
+        <>{translate('Unable to load users')}</>
+      ) : (
+        <>
+          {managers && managers.length > 0 && (
+            <Form.Group as={Row} className="mb-1">
+              <Form.Label column xs="auto">
+                {translate(ENV.roles.manager)}:
+              </Form.Label>
+              <Col>
+                <SymbolsGroup items={managers} max={6} />
+              </Col>
+            </Form.Group>
+          )}
+          {admins && admins.length > 0 && (
+            <Form.Group as={Row} className="mb-1">
+              <Form.Label column xs="auto">
+                {translate(ENV.roles.admin)}:
+              </Form.Label>
+              <Col>
+                <SymbolsGroup items={admins} max={6} />
+              </Col>
+            </Form.Group>
+          )}
+          {members && members.length > 0 && (
+            <Form.Group as={Row} className="mb-1">
+              <Form.Label column xs="auto">
+                {translate(ENV.roles.member)}:
+              </Form.Label>
+              <Col>
+                <SymbolsGroup items={members} max={6} />
+              </Col>
+            </Form.Group>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
