ba60d754f68dc0753fa636790b627acaf866c13a
Adil Shirinov <dev.adil.shirinov@gmail.com>
2023-05-10 14:50:39 +0000

    [WAL-5702] Allow overriding font color of a login link

--- src/auth/types.ts
+++ src/auth/types.ts
@@ -35,6 +35,7 @@ export interface ExternalLink {
 interface CoreConfiguration {
   MASTERMIND_URL: string;
   BRAND_COLOR: string;
+  BRAND_LABEL_COLOR: string;
   HERO_LINK_URL: string;
   HERO_LINK_LABEL: string;
   HERO_IMAGE: string;
