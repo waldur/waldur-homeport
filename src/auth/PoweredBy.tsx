f24699c38336fa280769a0a73518957431ff3ab0
Andrey Izman <andreyizman9@gmail.com>
2023-05-11 11:04:08 +0000

    [WAL-5674] Fix powered by logo broken issue on the login page

--- src/auth/PoweredBy.tsx
+++ src/auth/PoweredBy.tsx
@@ -1,9 +1,9 @@
 import { FunctionComponent } from 'react';
 
 import { ENV } from '@waldur/configs/default';
-import './PoweredBy.scss';
 import { fixURL } from '@waldur/core/api';
 import { translate } from '@waldur/i18n';
+import './PoweredBy.scss';
 
 export const PoweredBy: FunctionComponent = () =>
   ENV.plugins.WALDUR_CORE.POWERED_BY_LOGO ? (
