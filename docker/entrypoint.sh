e078c3da234067f0ccd1246bfcd5130f3359419d
Ilja Livenson <ilja@opennodecloud.com>
2023-05-12 07:15:46 +0000

    Revert "[WAL-5703] Overridden favicon is not respected"
    This reverts commit f204a4c130d738d8ebbe05d4fb30bfe56f94197b

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,6 @@ mkdir -p /usr/share/nginx/html/scripts/configs/ && envsubst < /usr/share/nginx/c
 cp /usr/share/nginx/html/index.orig.html /usr/share/nginx/html/index.html
 [[ -z "${TITLE}" ]] && sed -i "s/<title>Waldur | Cloud Service Management<\/title>/<title>${TITLE}<\/title>/" /usr/share/nginx/html/index.html
 
-[[ -n "${API_URL}" ]] && sed -i "s#href=\".*favicon.ico.*\" type=\"image/x-icon\"#href=\"${API_URL}api/icons/favicon/\" type=\"image/x-icon\"#" /usr/share/nginx/html/index.html
+[[ -n "${API_URL}" ]] && sed -i "s#href=\"images/favicon.ico.*\" type=\"image/x-icon\"#href=\"${API_URL}api/icons/favicon/\" type=\"image/x-icon\"#" /usr/share/nginx/html/index.html
 
 nginx -g 'daemon off;'
