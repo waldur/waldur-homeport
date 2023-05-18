f1f17f7254d0eafae5b40126bdb9e3436eea9f51
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-05-18 10:53:35 +0300

    Conceal edit/delete actions for robot account if it has backend ID

--- src/marketplace/resources/details/RobotAccountActions.tsx
+++ src/marketplace/resources/details/RobotAccountActions.tsx
@@ -1,9 +1,12 @@
 import { RobotAccountDeleteButton } from './RobotAccountDeleteButton';
 import { RobotAccountEditButton } from './RobotAccountEditButton';
 
-export const RobotAccountActions = ({ row, refetch }) => (
-  <>
-    <RobotAccountDeleteButton row={row} refetch={refetch} />
-    <RobotAccountEditButton row={row} refetch={refetch} />
-  </>
-);
+export const RobotAccountActions = ({ row, refetch }) =>
+  row.backend_id ? (
+    <>N/A</>
+  ) : (
+    <>
+      <RobotAccountDeleteButton row={row} refetch={refetch} />
+      <RobotAccountEditButton row={row} refetch={refetch} />
+    </>
+  );
