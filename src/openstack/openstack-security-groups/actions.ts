0d13d598b1a8ae254f1d24a164bcdd31541a658d
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-06-09 09:08:00 +0000

    Fix duplicate Edit menu for OS SG and networks [WAL-5823]

--- src/openstack/openstack-security-groups/actions.ts
+++ src/openstack/openstack-security-groups/actions.ts
@@ -1,12 +1,10 @@
 import { ActionRegistry } from '@waldur/resource/actions/registry';
 
 import { DestroySecurityGroupAction } from './DestroySecurityGroupAction';
-import { EditAction } from './EditAction';
 import { PullSecurityGroupAction } from './PullSecurityGroupAction';
 import { SetRulesAction } from './SetRulesAction';
 
 ActionRegistry.register('OpenStack.SecurityGroup', [
-  EditAction,
   SetRulesAction,
   DestroySecurityGroupAction,
   PullSecurityGroupAction,
