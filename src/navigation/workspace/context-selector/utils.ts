3d57b73c8e9b86ac93989b085ed1e1d7ec51c881
Adil Shirinov <dev.adil.shirinov@gmail.com>
2023-06-16 14:47:04 +0000

    [WAL-5831] Default avatar of Organization and Project should follow the same logic in all places

--- src/navigation/workspace/context-selector/utils.ts
+++ src/navigation/workspace/context-selector/utils.ts
@@ -8,6 +8,10 @@ export const getItemAbbreviation = (item) => {
     return item.abbreviation.toUpperCase().substr(0, 4);
   } else if (item.name) {
     return getAbbreviation(item.name).substr(0, 4);
+  } else if (item.customer_abbreviation) {
+    return item.customer_abbreviation.toUpperCase().substr(0, 4);
+  } else if (item.customer_name) {
+    return item.customer_name.toUpperCase().substr(0, 4);
   }
   return '';
 };
