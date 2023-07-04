f519aa4414929b9ee8236dace7b7b26e8e69c73d
Adil Shirinov <dev.adil.shirinov@gmail.com>
2023-07-04 09:49:45 +0000

    [WAL-5891] Org owner is not able to manage project membership under that org if he/she is part of the project

--- src/project/team/RoleGroup.tsx
+++ src/project/team/RoleGroup.tsx
@@ -1,41 +1,61 @@
 import { FunctionComponent } from 'react';
 import { Form } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import { Field } from 'redux-form';
 
 import { ENV } from '@waldur/configs/default';
 import { isFeatureVisible } from '@waldur/features/connect';
 import { translate } from '@waldur/i18n';
+import {
+  checkCustomerUser,
+  getCustomer,
+  getUser,
+} from '@waldur/workspace/selectors';
 
 export const RoleGroup: FunctionComponent<{ isProjectManager }> = ({
   isProjectManager,
-}) =>
-  isProjectManager ? (
-    <Form.Group>
-      <p>
-        <strong>{translate('Role')}</strong>: {translate(ENV.roles.manager)}
-      </p>
-    </Form.Group>
-  ) : (
-    <>
+}) => {
+  const currentUser = useSelector(getUser);
+  const currentCustomer = useSelector(getCustomer);
+  const canChangeRole = checkCustomerUser(currentCustomer, currentUser);
+
+  if (isProjectManager && !canChangeRole) {
+    return (
       <Form.Group>
-        <label>
-          <Field name="role" component="input" type="radio" value="manager" />{' '}
-          {translate(ENV.roles.manager)}
-        </label>
+        <p>
+          <strong>{translate('Role')}</strong>: {translate(ENV.roles.manager)}
+        </p>
       </Form.Group>
-      <Form.Group>
-        <label>
-          <Field name="role" component="input" type="radio" value="admin" />{' '}
-          {translate(ENV.roles.admin)}
-        </label>
-      </Form.Group>
-      {isFeatureVisible('project.member_role') && (
+    );
+  } else {
+    return (
+      <>
         <Form.Group>
           <label>
-            <Field name="role" component="input" type="radio" value="member" />{' '}
-            {translate(ENV.roles.member)}
+            <Field name="role" component="input" type="radio" value="manager" />{' '}
+            {translate(ENV.roles.manager)}
           </label>
         </Form.Group>
-      )}
-    </>
-  );
+        <Form.Group>
+          <label>
+            <Field name="role" component="input" type="radio" value="admin" />{' '}
+            {translate(ENV.roles.admin)}
+          </label>
+        </Form.Group>
+        {isFeatureVisible('project.member_role') && (
+          <Form.Group>
+            <label>
+              <Field
+                name="role"
+                component="input"
+                type="radio"
+                value="member"
+              />{' '}
+              {translate(ENV.roles.member)}
+            </label>
+          </Form.Group>
+        )}
+      </>
+    );
+  }
+};
