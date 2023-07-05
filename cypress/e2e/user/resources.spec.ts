229390aaa9b66d4006acc7718a344ffde96f0c92
Andrey Izman <andreyizman9@gmail.com>
2023-07-05 18:50:12 +0000

    [WAL-5884] Add a test to see resources in personal space

---
+++ cypress/e2e/user/resources.spec.ts
@@ -0,0 +1,40 @@
+describe('Resources in personal space', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/marketplace-resources/**', {
+      fixture: 'marketplace/resources.json',
+    })
+      .mockUser()
+      .mockChecklists()
+      .mockPermissions()
+      .mockCustomers()
+      .setToken()
+      .visit('/profile/user-resources/')
+      .waitForPage();
+  });
+
+  it('Assure the resources list is visible', () => {
+    cy.get(
+      '#kt_content_container .card-toolbar .card-title:contains(Resources)',
+    ).should('be.visible');
+
+    cy.get('.card-table .card-body tbody tr').should('have.length', 2);
+  });
+
+  it('Assure the table expansion is working', () => {
+    cy.get('.card-table .card-body tbody tr').first().click();
+
+    cy.get('.card-table .card-body tbody tr')
+      .first()
+      .should('have.class', 'expanded');
+
+    cy.get('.card-table .card-body tbody tr').should('have.length', 3);
+
+    cy.fixture('marketplace/resources.json').then((resources) => {
+      const firstResource = resources.at(0);
+      cy.get('.card-table .card-body tbody tr.expanded')
+        .next()
+        .contains(firstResource.uuid)
+        .should('be.visible');
+    });
+  });
+});
