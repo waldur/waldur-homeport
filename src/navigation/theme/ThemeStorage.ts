7f6f7b611c73d35c3a522bf0c99415c0f7bfe94c
Adil Shirinov <dev.adil.shirinov@gmail.com>
2023-07-26 16:29:30 +0000

    [WAL-5951] Move theme selection and redirects to local storage

--- src/navigation/theme/ThemeStorage.ts
+++ src/navigation/theme/ThemeStorage.ts
@@ -3,7 +3,9 @@ import { getItem, setItem } from '@waldur/auth/AuthStorage';
 import { ThemeName } from './types';
 
 const key = 'waldur/theme/name';
+const STORAGE_FOR_THEMES = 'localStorage';
 
-export const getTheme = () => getItem(key) as ThemeName;
+export const getTheme = () => getItem(key, STORAGE_FOR_THEMES) as ThemeName;
 
-export const setTheme = (value: ThemeName) => setItem(key, value);
+export const setTheme = (value: ThemeName) =>
+  setItem(key, value, STORAGE_FOR_THEMES);
