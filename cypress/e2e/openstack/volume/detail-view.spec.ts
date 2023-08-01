94a5fae54f34ea7b339e0c4b8baa5f4e8b64ea95
Andrey Izman <andreyizman9@gmail.com>
2023-08-01 12:07:33 +0000

    [WAL-5946] Enabling and updating the openstack/volume/detail view specs in cypress test

--- cypress/e2e/openstack/volume/detail-view.spec.ts
+++ cypress/e2e/openstack/volume/detail-view.spec.ts
@@ -1,6 +1,7 @@
-xdescribe('OpenStack Volume detail view', () => {
+describe('OpenStack Volume detail view', () => {
   beforeEach(() => {
     cy.mockUser()
+      .mockCustomers()
       .mockChecklists()
       .setToken()
 
@@ -9,52 +10,60 @@ xdescribe('OpenStack Volume detail view', () => {
       })
       .intercept(
         'GET',
-        '/api/openstacktenant-volumes/df15b1f2dc7c445cadb2996594b0a6c3/',
-        {
-          fixture: 'openstack/openstacktenant-volume.json',
-        },
+        '/api/marketplace-resources/53b9bda915ab4dc0aac93a8f4fe83d30/',
+        { fixture: 'openstack/resource-openstack-volume.json' },
+      )
+      .intercept(
+        'GET',
+        '/api/marketplace-resources/53b9bda915ab4dc0aac93a8f4fe83d30/details/',
+        { fixture: 'openstack/openstacktenant-volume.json' },
       )
       .as('getOpenstackTenantVolume')
       .intercept('GET', '/api/openstacktenant-instances/**', {
         fixture: 'openstack/openstacktenant-instances.json',
       })
-      .intercept('GET', '/api/marketplace-cart-items/', {
+      .intercept(
+        'GET',
+        '/api/marketplace-resources/53b9bda915ab4dc0aac93a8f4fe83d30/offering',
+        { fixture: 'offerings/openstackVolume.json' },
+      )
+      .intercept('GET', '/api/marketplace-cart-items/**', {
         fixture: 'offerings/shoppingCartItem.json',
       })
       .intercept('GET', '/api/openstacktenant-snapshots/', [])
       .intercept('GET', '/api/projects/**', {
         fixture: 'projects/alice_azure.json',
       })
-      .intercept('GET', '/api/customers/', {
+      .intercept('GET', '/api/customers/bf6d515c9e6e445f9c339021b30fc96b', {
         fixture: 'marketplace/anderson_and_sons.json',
       })
 
       .log('Visit OpenStackTenant Volume detail view')
       .visit(
-        '/projects/5d3016240ccf4181b4cdc0baa0e41d5c/resources/OpenStackTenant.Volume/df15b1f2dc7c445cadb2996594b0a6c3/',
+        '/projects/5d3016240ccf4181b4cdc0baa0e41d5c/resource-details/53b9bda915ab4dc0aac93a8f4fe83d30',
       )
-      .get('.loading-title')
-      .should('not.exist')
-      .waitForSpinner();
+      .waitForPage();
   });
 
   it('Attaches OpenStack Volume to Instance', () => {
     cy.log('Open "Attach OpenStack Volume to Instance" dialog')
-      .get('[data-cy=actions-dropdown-btn]')
+      .get(
+        '#kt_content_container .public-dashboard-hero-body .dropdown button.btn-icon',
+      )
+      .first()
       .click({ force: true })
-      .wait('@getOpenstackTenantVolume')
-      .wait(500)
+      .next('.dropdown-menu')
       .contains('.dropdown-item', 'Attach')
       .click({ force: true })
 
       .log('Select an option of Instance field')
-      .get('div[class$="placeholder"]')
+      .get('.modal-dialog div[class$="placeholder"]')
       .first()
       .click()
       .selectTheFirstOptionOfDropdown()
 
       .log('Submit the form')
-      .get('button[type=submit]')
+      .get('.modal-dialog button[type=submit]')
       .click();
   });
 });
