b3417290afed86fbd9001c10dee0768ea4f86407
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-08-05 22:40:52 +0300

    Fix cost policy action on mobile [WAL-5994]

--- src/customer/cost-policies/CostPolicyCreateForm.tsx
+++ src/customer/cost-policies/CostPolicyCreateForm.tsx
@@ -5,7 +5,6 @@ import { Field, reduxForm } from 'redux-form';
 import { ENV } from '@waldur/configs/default';
 import { required } from '@waldur/core/validators';
 import {
-  SelectField,
   FormContainer,
   FieldError,
   SubmitButton,
@@ -19,6 +18,7 @@ import { getCustomer } from '@waldur/workspace/selectors';
 
 import { CostPolicyFormData } from './types';
 import { getCostPolicyActionOptions } from './utils';
+import { Select } from '@waldur/form/themed-select';
 
 export const CostPolicyCreateForm = reduxForm<
   CostPolicyFormData,
@@ -77,19 +77,23 @@ export const CostPolicyCreateForm = reduxForm<
             </InputGroup.Text>
           </InputGroup>
         </Form.Group>
-        <SelectField
-          name="actions"
-          label={
-            <>
-              3. {translate('Then')}
-              <span className="text-danger"> *</span>
-            </>
-          }
-          options={getCostPolicyActionOptions()}
-          getOptionValue={(option) => option.value}
-          getOptionLabel={(option) => option.label}
-          validate={required}
-        />
+        <Form.Group className="mb-7">
+          <Form.Label>3. {translate('Then')}</Form.Label>
+          <span className="text-danger"> *</span>
+          <Field
+            name="actions"
+            validate={required}
+            component={(fieldProps) => (
+              <Select
+                value={fieldProps.input.value}
+                onChange={(value) => fieldProps.input.onChange(value)}
+                options={getCostPolicyActionOptions()}
+                getOptionValue={(option) => option.value}
+                getOptionLabel={(option) => option.label}
+              />
+            )}
+          />
+        </Form.Group>
         <Form.Group className="text-end">
           <FieldError error={props.error} />
           <SubmitButton
