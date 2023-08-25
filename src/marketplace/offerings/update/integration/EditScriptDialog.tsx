b389914b233f87394c3144c7f34b477c93e58227
Adil Shirinov <dev.adil.shirinov@gmail.com>
2023-08-25 13:01:54 +0000

    [WAL-5894] Convert marketplace dry run to use async execution method

--- src/marketplace/offerings/update/integration/EditScriptDialog.tsx
+++ src/marketplace/offerings/update/integration/EditScriptDialog.tsx
@@ -1,18 +1,24 @@
+import delay from '@redux-saga/delay-p';
 import { useCallback, useState } from 'react';
 import { Button, Col, Modal, Row } from 'react-bootstrap';
-import { connect, useDispatch } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import { Field, getFormValues, initialize, reduxForm } from 'redux-form';
 
 import { Tip } from '@waldur/core/Tooltip';
 import { SubmitButton, SelectField } from '@waldur/form';
 import { MonacoField } from '@waldur/form/MonacoField';
 import { translate } from '@waldur/i18n';
 import {
-  runOfferingScript,
+  asyncRunOfferingScript,
+  getAsyncDryRun,
   updateProviderOfferingSecretOptions,
 } from '@waldur/marketplace/common/api';
 import { closeModalDialog } from '@waldur/modal/actions';
-import { showErrorResponse, showSuccess } from '@waldur/store/notify';
+import {
+  showError,
+  showErrorResponse,
+  showSuccess,
+} from '@waldur/store/notify';
 
 import { EDIT_SCRIPT_FORM_ID } from './constants';
 import { ScriptEditorProps } from './types';
@@ -39,7 +45,9 @@ export const EditScriptDialog = connect<{}, {}, OwnProps>((_, ownProps) => ({
     form: EDIT_SCRIPT_FORM_ID,
   })((props) => {
     const dispatch = useDispatch();
+    const [executing, setExecuting] = useState<boolean>(false);
     const [scriptExecutionResult, setScriptExecutionResult] = useState('');
+    const formValues = useSelector(getFormValues(EDIT_SCRIPT_FORM_ID));
     const update = useCallback(
       async (formData) => {
         try {
@@ -72,20 +80,39 @@ export const EditScriptDialog = connect<{}, {}, OwnProps>((_, ownProps) => ({
       dispatch(closeModalDialog());
     };
 
+    const pollAsyncDryRunResult = async (dryRunUuid: string) => {
+      let asyncDryRunResult: any;
+      setExecuting(true);
+      do {
+        asyncDryRunResult = await getAsyncDryRun(dryRunUuid);
+        if (asyncDryRunResult.data.get_state_display === 'erred') {
+          break;
+        }
+        await delay(3000);
+      } while (asyncDryRunResult.data.get_state_display !== 'done');
+      setExecuting(false);
+      return asyncDryRunResult;
+    };
+
     const handleSaveAndRunScriptButtonClick = async () => {
       const planUrl = props.resolve.offering?.plans?.length
         ? props.resolve.offering.plans[0].url
         : null;
       await props.handleSubmit(update)();
-      props.reset(); // "Clean" the form after saving new changes
       try {
-        const response: any = await runOfferingScript(
+        const response: any = await asyncRunOfferingScript(
           props.resolve.offering.uuid,
           planUrl,
           props.resolve.dry_run,
         );
 
-        setScriptExecutionResult(response.data.output);
+        const asyncDryRunResult: any = await pollAsyncDryRunResult(
+          response.data.uuid,
+        );
+        if (asyncDryRunResult.data.get_state_display === 'erred') {
+          dispatch(showError('An error occurred during script execution.'));
+        }
+        setScriptExecutionResult(asyncDryRunResult.data.output);
         dispatch(
           showSuccess(
             translate('{type} script was executed successfully', {
@@ -103,6 +130,7 @@ export const EditScriptDialog = connect<{}, {}, OwnProps>((_, ownProps) => ({
           ),
         );
       }
+      dispatch(initialize(EDIT_SCRIPT_FORM_ID, formValues));
     };
 
     return (
@@ -114,7 +142,13 @@ export const EditScriptDialog = connect<{}, {}, OwnProps>((_, ownProps) => ({
               <Button
                 variant="secondary"
                 onClick={handleSaveAndRunScriptButtonClick}
+                disabled={executing}
               >
+                {executing && (
+                  <>
+                    <i className="fa fa-spinner fa-spin me-1" />{' '}
+                  </>
+                )}
                 {translate('Save & dry run script')}
               </Button>
             ) : (
