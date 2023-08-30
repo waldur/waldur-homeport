93aa2d7be60addf7d71caba04ce59cebf4235d57
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-08-30 21:42:32 +0300

    Render access type in role edit dialog.

--- src/administration/roles/PermissionField.tsx
+++ src/administration/roles/PermissionField.tsx
@@ -1,41 +1,75 @@
-import { Accordion, Card } from 'react-bootstrap';
+import { useMemo } from 'react';
+import { Accordion, Badge, Card } from 'react-bootstrap';
 
 import { AwesomeCheckbox } from '@waldur/core/AwesomeCheckbox';
+import { translate } from '@waldur/i18n';
 
 import { PermissionOptions } from './PermissionOptions';
+
+const PermissionEntity = (props) => {
+  const accessType = useMemo(
+    () =>
+      props.entity.options.every((option) =>
+        props.input.value.includes(option.value),
+      )
+        ? 'FULL_ACCESS'
+        : props.entity.options.find((option) =>
+            props.input.value.includes(option.value),
+          )
+        ? 'PARTIAL_ACCESS'
+        : 'NO_ACCESS',
+    [props.entity, props.input.value],
+  );
+  return (
+    <Accordion className="my-2">
+      <Card>
+        <Accordion.Header>
+          <div className="flex-grow-1">{props.entity.label}</div>{' '}
+          <Badge
+            bg={
+              accessType === 'FULL_ACCESS'
+                ? 'success'
+                : accessType === 'PARTIAL_ACCESS'
+                ? 'primary'
+                : 'danger'
+            }
+            className="mx-2"
+          >
+            {accessType === 'FULL_ACCESS'
+              ? translate('Full access')
+              : accessType === 'PARTIAL_ACCESS'
+              ? translate('Partial access')
+              : translate('No access')}
+          </Badge>
+        </Accordion.Header>
+        <Accordion.Body>
+          {props.entity.options.map((option, optionIndex) => (
+            <div key={optionIndex} className="my-2">
+              <AwesomeCheckbox
+                label={option.label}
+                value={props.input.value.includes(option.value)}
+                onChange={(event: boolean) => {
+                  if (event) {
+                    props.input.onChange([...props.input.value, option.value]);
+                  } else {
+                    props.input.onChange(
+                      props.input.value.filter((item) => item !== option.value),
+                    );
+                  }
+                }}
+              />
+            </div>
+          ))}
+        </Accordion.Body>
+      </Card>
+    </Accordion>
+  );
+};
 
 export const PermissionField = (props) => (
   <>
     {PermissionOptions.map((entity, entityIndex) => (
-      <Accordion key={entityIndex} className="my-2">
-        <Card>
-          <Accordion.Header>{entity.label}</Accordion.Header>
-          <Accordion.Body>
-            {entity.options.map((option, optionIndex) => (
-              <div key={optionIndex} className="my-2">
-                <AwesomeCheckbox
-                  label={option.label}
-                  value={props.input.value.includes(option.value)}
-                  onChange={(event: boolean) => {
-                    if (event) {
-                      props.input.onChange([
-                        ...props.input.value,
-                        option.value,
-                      ]);
-                    } else {
-                      props.input.onChange(
-                        props.input.value.filter(
-                          (item) => item !== option.value,
-                        ),
-                      );
-                    }
-                  }}
-                />
-              </div>
-            ))}
-          </Accordion.Body>
-        </Card>
-      </Accordion>
+      <PermissionEntity entity={entity} key={entityIndex} input={props.input} />
     ))}
   </>
 );
