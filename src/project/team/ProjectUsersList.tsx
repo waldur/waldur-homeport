041301cd2a8f28855ab29dcd8eb3804d0c6ea542
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2023-10-16 09:27:07 +0000

    [WAL-6153] Fix query field name with existing one

--- src/project/team/ProjectUsersList.tsx
+++ src/project/team/ProjectUsersList.tsx
@@ -39,7 +39,7 @@ export const ProjectUsersList = () => {
   const tableProps = useTable({
     table: 'project-users',
     fetchData: createFetcher(`projects/${project.uuid}/list_users`),
-    queryField: 'full_name',
+    queryField: 'user_full_name',
   });
 
   return (
