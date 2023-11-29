d7ab3fd47335efd99b141fcba50a7b22e7a18db1
Andrey Izman <andreyizman9@gmail.com>
2023-11-29 07:35:00 +0000

    [WAL-6308] Migrate booking resource booking card to a new design on the resource details page

---
+++ src/booking/offering/OfferingBookingResourcesCalendarContainer.tsx
@@ -0,0 +1,78 @@
+import { useQuery } from '@tanstack/react-query';
+import { FunctionComponent } from 'react';
+import { Card } from 'react-bootstrap';
+
+import { getBookingsList } from '@waldur/booking/api';
+import { getStates } from '@waldur/booking/BookingStateFilter';
+import { OFFERING_TYPE_BOOKING } from '@waldur/booking/constants';
+import { LoadingErred } from '@waldur/core/LoadingErred';
+import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
+import { orderByFilter } from '@waldur/core/utils';
+import { translate } from '@waldur/i18n';
+
+import { BookingResourcesCalendar } from './BookingResourcesCalendar';
+
+async function loadBookingOfferings(offeringUuid: string) {
+  return await getBookingsList({
+    offering_uuid: offeringUuid,
+    offering_type: OFFERING_TYPE_BOOKING,
+    state: [getStates()[0], getStates()[1]].map(({ value }) => value),
+    o: orderByFilter({ field: 'created', mode: 'desc' }),
+  });
+}
+
+interface OfferingBookingResourcesCalendarContainerProps {
+  offeringUuid: string;
+}
+
+export const OfferingBookingResourcesCalendarContainer: FunctionComponent<OfferingBookingResourcesCalendarContainerProps> =
+  ({ offeringUuid }) => {
+    const {
+      data: calendarEvents,
+      isLoading,
+      isRefetching,
+      error,
+      refetch,
+    } = useQuery(['offeringBookings', offeringUuid], () =>
+      loadBookingOfferings(offeringUuid),
+    );
+
+    if (isLoading) {
+      return <LoadingSpinner />;
+    }
+
+    if (error) {
+      return (
+        <LoadingErred
+          loadData={refetch}
+          message={translate('Unable to load booking offerings.')}
+        />
+      );
+    }
+
+    return (
+      <Card className="offering-bookings">
+        <Card.Header>
+          <Card.Title>
+            <span className="me-2">{translate('Bookings')}</span>
+            {isRefetching ? (
+              <LoadingSpinner />
+            ) : (
+              <button
+                className="btn btn-icon btn-active-light"
+                onClick={() => refetch()}
+              >
+                <i className="fa fa-refresh fs-4"></i>
+              </button>
+            )}
+          </Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <BookingResourcesCalendar
+            bookingResources={calendarEvents}
+            refetch={refetch}
+          />
+        </Card.Body>
+      </Card>
+    );
+  };
