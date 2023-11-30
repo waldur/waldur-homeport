3d9929241e9f5b5d8ed6ad9c3a1c06e14db74267
Andrey Izman <andreyizman9@gmail.com>
2023-11-30 21:35:20 +0100

    WAL-6308 Migrating old calendar to a new design on the marketplace order item details

--- src/booking/BookingDetails.tsx
+++ src/booking/BookingDetails.tsx
@@ -1,13 +1,27 @@
-import { Calendar } from '@waldur/booking/components/calendar/Calendar';
+import { useContext } from 'react';
+import { AccordionContext } from 'react-bootstrap';
+
 import { OrderItemDetailsProps } from '@waldur/marketplace/types';
 import { OfferingConfigurationDetails } from '@waldur/support/OfferingConfigurationDetails';
 
+import { BookingResourcesCalendar } from './offering/BookingResourcesCalendar';
+
 export const BookingDetails = (props: OrderItemDetailsProps) => {
   const schedules = props.orderItem.attributes.schedules;
+
+  // We need to watch the activeEventKey of bootstrap accordion and use it as the calendar component key to re-render it.
+  // Because the bootstrap accordion will crush the calendar if it is closed on the first render.
+  const { activeEventKey } = useContext(AccordionContext);
+
   return (
     <>
       <OfferingConfigurationDetails {...props} />
-      {schedules && <Calendar events={schedules} />}
+      {schedules && (
+        <BookingResourcesCalendar
+          key={String(activeEventKey)}
+          bookingResources={[props.orderItem as any]}
+        />
+      )}
     </>
   );
 };
