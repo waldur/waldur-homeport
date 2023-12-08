d74d700e641665289d863e69da240f3719f0156c
Andrey Izman <andreyizman9@gmail.com>
2023-12-08 10:02:04 +0100

    WAL-6308 Fixed type typo

--- src/booking/BookingDetails.tsx
+++ src/booking/BookingDetails.tsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react';
 import { AccordionContext } from 'react-bootstrap';
 
-import { OrderItemDetailsProps } from '@waldur/marketplace/types';
+import { OrderDetailsProps } from '@waldur/marketplace/types';
 import { OfferingConfigurationDetails } from '@waldur/support/OfferingConfigurationDetails';
 
 import { BookingResourcesCalendar } from './offering/BookingResourcesCalendar';
 
-export const BookingDetails = (props: OrderItemDetailsProps) => {
-  const schedules = props.orderItem.attributes.schedules;
+export const BookingDetails = (props: OrderDetailsProps) => {
+  const schedules = props.order.attributes.schedules;
 
   // We need to watch the activeEventKey of bootstrap accordion and use it as the calendar component key to re-render it.
   // Because the bootstrap accordion will crush the calendar if it is closed on the first render.
@@ -19,7 +19,7 @@ export const BookingDetails = (props: OrderItemDetailsProps) => {
       {schedules && (
         <BookingResourcesCalendar
           key={String(activeEventKey)}
-          bookingResources={[props.orderItem as any]}
+          bookingResources={[props.order as any]}
         />
       )}
     </>
