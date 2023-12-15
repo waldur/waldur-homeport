205b2a1e658a86cee20f6573b86fcf8f767014cc
Adil Shirinov <dev.adil.shirinov@gmail.com>
2023-12-15 17:44:13 +0000

    [WAL-5421] Render list of discounted resources in campaign expandable

--- src/marketplace/service-providers/api.ts
+++ src/marketplace/service-providers/api.ts
@@ -1,7 +1,10 @@
-import { post, put } from '@waldur/core/api';
+import { get, post, put } from '@waldur/core/api';
 
 export const createCampaign = (payload) =>
   post('/promotions-campaigns/', payload);
 
 export const updateCampaign = (uuid, payload) =>
   put(`/promotions-campaigns/${uuid}/`, payload);
+
+export const getCampaignResources = (uuid) =>
+  get(`/promotions-campaigns/${uuid}/resources/`);
