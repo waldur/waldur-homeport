637e29d2227f5552167280a0881f429c6f75e0b5
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-01-22 11:11:50 +0200

    Fix resource events view.

--- src/resource/tabs/ResourceEvents.tsx
+++ src/resource/tabs/ResourceEvents.tsx
@@ -2,7 +2,9 @@ import { getEventsList } from '@waldur/events/BaseEventsList';
 
 export const ResourceEvents = getEventsList({
   mapPropsToFilter: (props) => ({
-    scope: props.resource.url,
+    scope: props.resource ? props.resource.url : props.marketplaceResource.url,
   }),
-  mapPropsToTableId: (props) => [props.resource?.uuid],
+  mapPropsToTableId: (props) => [
+    props.resource ? props.resource.uuid : props.marketplaceResource.uuid,
+  ],
 });
