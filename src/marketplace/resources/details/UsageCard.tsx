926b5f09aab0c273aeec4a69b0d47e9e0a47edc3
Andrey Izman <andreyizman9@gmail.com>
2024-01-25 16:56:40 +0000

    [WAL-6461] Extend usage graphs to show usage information from the start of the allocation on the resource details module

--- src/marketplace/resources/details/UsageCard.tsx
+++ src/marketplace/resources/details/UsageCard.tsx
@@ -1,9 +1,11 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Card } from 'react-bootstrap';
 
+import { SelectControl } from '@waldur/form/SelectControl';
 import { translate } from '@waldur/i18n';
 
 import { ResourceUsageTabsContainer } from '../usage/ResourceUsageTabsContainer';
+import { getUsageHistoryPeriodOptions } from '../usage/utils';
 
 export const UsageCard = ({ resource }) => {
   const resourceRef = useMemo(
@@ -13,12 +15,32 @@ export const UsageCard = ({ resource }) => {
     }),
     [resource],
   );
+  const periodOptions = useMemo(() => getUsageHistoryPeriodOptions(), []);
+  const [period, setPeriod] = useState(
+    periodOptions.find((op) => op.value === 12),
+  );
 
   return resource.is_usage_based || resource.is_limit_based ? (
     <Card className="mb-10" id="usage-history">
+      <Card.Header>
+        <Card.Title>
+          <h3>{translate('Usage history')}</h3>
+        </Card.Title>
+        <div className="card-toolbar">
+          <SelectControl
+            options={periodOptions}
+            value={period}
+            onChange={setPeriod}
+            className="w-150px"
+          />
+        </div>
+      </Card.Header>
       <Card.Body>
-        <h3>{translate('Usage history')}</h3>
-        <ResourceUsageTabsContainer resource={resourceRef} hideHeader={true} />
+        <ResourceUsageTabsContainer
+          resource={resourceRef}
+          months={period.value}
+          hideHeader={true}
+        />
       </Card.Body>
     </Card>
   ) : null;
--- src/marketplace/resources/usage/ResourceUsageChart.tsx
+++ src/marketplace/resources/usage/ResourceUsageChart.tsx
@@ -9,16 +9,18 @@ import { ComponentUsage } from './types';
 interface ResourceUsageChartProps {
   offeringComponent: OfferingComponent;
   usages: ComponentUsage[];
+  months: number;
   chartColor: string;
 }
 
 export const ResourceUsageChart: FunctionComponent<ResourceUsageChartProps> = ({
   offeringComponent,
   usages,
+  months,
   chartColor,
 }) => {
   const options = useMemo(
-    () => getEChartOptions(offeringComponent, usages, chartColor),
+    () => getEChartOptions(offeringComponent, usages, months, chartColor),
     [offeringComponent, usages, chartColor],
   );
   return <EChart options={options} height="400px" />;
