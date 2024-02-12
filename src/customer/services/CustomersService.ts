4708df9f3268c7c1d4030ebaddbb812bd501a498
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-02-13 00:08:51 +0200

    Remove leftovers

--- src/customer/services/CustomersService.ts
+++ src/customer/services/CustomersService.ts
@@ -1,12 +1,7 @@
 import { get } from '@waldur/core/api';
 import { getCustomer } from '@waldur/project/api';
 import store from '@waldur/store/store';
-import { UsersService } from '@waldur/user/UsersService';
 import { setCurrentCustomer } from '@waldur/workspace/actions';
-import {
-  checkCustomerUser,
-  getCustomer as getCustomerSelector,
-} from '@waldur/workspace/selectors';
 import { User } from '@waldur/workspace/types';
 
 class CustomersServiceClass {
@@ -16,19 +11,8 @@ class CustomersServiceClass {
     );
   }
 
-  get(customerUuid) {
-    return getCustomer(customerUuid);
-  }
-
-  isOwnerOrStaff() {
-    const customer = getCustomerSelector(store.getState());
-    return UsersService.getCurrentUser().then((user) => {
-      return checkCustomerUser(customer, user);
-    });
-  }
-
   refreshCurrentCustomer(customerUuid) {
-    return this.get(customerUuid).then((customer) => {
+    return getCustomer(customerUuid).then((customer) => {
       store.dispatch(setCurrentCustomer(customer));
       return customer;
     });
