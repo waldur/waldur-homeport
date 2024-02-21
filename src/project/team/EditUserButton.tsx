94966d21f0ab5474a29ebd936ff1ab50ea8fd43a
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-02-21 23:59:24 +0200

    Use roles and permissions in project users list.

--- src/project/team/EditUserButton.tsx
+++ src/project/team/EditUserButton.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { lazyComponent } from '@waldur/core/lazyComponent';
 import { translate } from '@waldur/i18n';
 import { openModalDialog } from '@waldur/modal/actions';
+import { PermissionEnum } from '@waldur/permissions/enums';
+import { hasPermission } from '@waldur/permissions/hasPermission';
 import { GenericPermission } from '@waldur/permissions/types';
 import { ActionButton } from '@waldur/table/ActionButton';
+import { getCustomer, getProject, getUser } from '@waldur/workspace/selectors';
 
 const EditUserDialog = lazyComponent(
   () => import('./EditUserDialog'),
@@ -22,6 +25,20 @@ export const EditUserButton: React.FC<EditUserButtonProps> = ({
   refetch,
 }) => {
   const dispatch = useDispatch();
+  const user = useSelector(getUser);
+  const project = useSelector(getProject);
+  const customer = useSelector(getCustomer);
+
+  if (
+    !hasPermission(user, {
+      permission: PermissionEnum.UPDATE_PROJECT_PERMISSION,
+      customerId: customer.uuid,
+      projectId: project.uuid,
+    })
+  ) {
+    return null;
+  }
+
   const callback = () =>
     dispatch(
       openModalDialog(EditUserDialog, {
--- src/project/team/UserRemoveButton.tsx
+++ src/project/team/UserRemoveButton.tsx
@@ -4,23 +4,37 @@ import { useDispatch, useSelector } from 'react-redux';
 import { translate } from '@waldur/i18n';
 import { waitForConfirmation } from '@waldur/modal/actions';
 import { deleteProjectUser } from '@waldur/permissions/api';
+import { PermissionEnum } from '@waldur/permissions/enums';
+import { hasPermission } from '@waldur/permissions/hasPermission';
+import { GenericPermission } from '@waldur/permissions/types';
 import { showErrorResponse, showSuccess } from '@waldur/store/notify';
 import { ActionButton } from '@waldur/table/ActionButton';
-import { getProject } from '@waldur/workspace/selectors';
+import { getCustomer, getProject, getUser } from '@waldur/workspace/selectors';
 
 interface UserRemoveButtonProps {
-  permission;
-  isDisabled: boolean;
+  permission: GenericPermission;
   refetch(): void;
 }
 
 export const UserRemoveButton: React.FC<UserRemoveButtonProps> = ({
   permission,
-  isDisabled,
   refetch,
 }) => {
   const dispatch = useDispatch();
+  const user = useSelector(getUser);
   const project = useSelector(getProject);
+  const customer = useSelector(getCustomer);
+
+  if (
+    !hasPermission(user, {
+      permission: PermissionEnum.DELETE_PROJECT_PERMISSION,
+      customerId: customer.uuid,
+      projectId: project.uuid,
+    })
+  ) {
+    return null;
+  }
+
   const callback = async () => {
     try {
       await waitForConfirmation(
@@ -50,12 +64,6 @@ export const UserRemoveButton: React.FC<UserRemoveButtonProps> = ({
   };
   return (
     <ActionButton
-      disabled={isDisabled}
-      tooltip={
-        isDisabled
-          ? translate(`You don't have permissions to delete this user.`)
-          : ''
-      }
       action={callback}
       title={translate('Remove')}
       icon="fa fa-trash"
