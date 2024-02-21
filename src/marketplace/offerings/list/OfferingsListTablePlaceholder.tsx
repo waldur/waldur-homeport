2c8ad30c5263e3cbd6bc585fda060265da33bb3c
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-02-21 23:27:12 +0200

    Use roles & permissions in offering list placeholder.

--- src/marketplace/offerings/list/OfferingsListTablePlaceholder.tsx
+++ src/marketplace/offerings/list/OfferingsListTablePlaceholder.tsx
@@ -1,11 +1,10 @@
 import { useSelector } from 'react-redux';
 
 import { translate } from '@waldur/i18n';
+import { PermissionEnum } from '@waldur/permissions/enums';
+import { hasPermission } from '@waldur/permissions/hasPermission';
 import { ImageTablePlaceholder } from '@waldur/table/ImageTablePlaceholder';
-import {
-  getCustomer,
-  isOwnerOrStaff as isOwnerOrStaffSelector,
-} from '@waldur/workspace/selectors';
+import { getCustomer, getUser } from '@waldur/workspace/selectors';
 
 import { CreateOfferingButton } from './CreateOfferingButton';
 
@@ -13,14 +12,20 @@ const TwoDocumentsIllustration: string = require('@waldur/images/table-placehold
 
 export const OfferingsListTablePlaceholder = ({ showActions }) => {
   const customer = useSelector(getCustomer);
-  const isOwnerOrStaff = useSelector(isOwnerOrStaffSelector);
-
+  const user = useSelector(getUser);
+  const showMessage =
+    showActions &&
+    customer?.is_service_provider &&
+    hasPermission(user, {
+      permission: PermissionEnum.CREATE_OFFERING,
+      customerId: customer.uuid,
+    });
   return (
     <ImageTablePlaceholder
       illustration={TwoDocumentsIllustration}
       title={translate('Nothing to see here')}
       description={
-        showActions && customer?.is_service_provider && isOwnerOrStaff
+        showMessage
           ? translate(
               'You can start filling this table by creating your first offering.',
             )
