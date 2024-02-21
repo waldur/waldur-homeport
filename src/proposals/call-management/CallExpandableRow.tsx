9d3b1969905e5413ca076af14cff13a3ba7fdb3f
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-02-22 01:06:56 +0200

    Render call details in one column layout.

--- src/proposals/call-management/CallExpandableRow.tsx
+++ src/proposals/call-management/CallExpandableRow.tsx
@@ -1,5 +1,4 @@
 import { FunctionComponent } from 'react';
-import { Col, Row } from 'react-bootstrap';
 
 import { formatDateTime } from '@waldur/core/dateUtils';
 import { translate } from '@waldur/i18n';
@@ -10,23 +9,19 @@ import { renderFieldOrDash } from '@waldur/table/utils';
 export const CallExpandableRow: FunctionComponent<{
   row: ProposalCall;
 }> = ({ row }) => (
-  <Row>
-    <Col sm={12} md={6}>
-      <Field
-        label={translate('Next cutoff')}
-        value={renderFieldOrDash(formatDateTime(row.end_time))}
-      />
-      <Field label={translate('Review strategy')} value={row.review_strategy} />
-      <Field label={translate('Round strategy')} value={row.round_strategy} />
-    </Col>
-    <Col sm={12} md={6}>
-      <Field label={translate('Submissions')} value="32" />
-      <Field label={translate('Pending review')} value="22" />
-      <Field
-        label={translate('Resource availability')}
-        value="44.54/100 % (i)"
-        className="text-nowrap"
-      />
-    </Col>
-  </Row>
+  <>
+    <Field
+      label={translate('Next cutoff')}
+      value={renderFieldOrDash(formatDateTime(row.end_time))}
+    />
+    <Field label={translate('Review strategy')} value={row.review_strategy} />
+    <Field label={translate('Round strategy')} value={row.round_strategy} />
+    <Field label={translate('Submissions')} value="32" />
+    <Field label={translate('Pending review')} value="22" />
+    <Field
+      label={translate('Resource availability')}
+      value="44.54/100 % (i)"
+      className="text-nowrap"
+    />
+  </>
 );
