434f710edc1d7d81f5671c9815f47dbb2079e377
Adil Shirinov <dev.adil.shirinov@gmail.com>
2024-02-22 13:31:28 +0000

    [WAL-6619] Change limits shows 2 times on resource detail page

--- src/openstack/openstack-tenant/actions/module.ts
+++ src/openstack/openstack-tenant/actions/module.ts
@@ -1,4 +1,3 @@
-import { ChangeLimitsAction } from '@waldur/marketplace/resources/change-limits/ChangeLimitsAction';
 import { ActionRegistry } from '@waldur/resource/actions/registry';
 
 import { RequestDirectAccessAction } from './RequestDirectAccessAction';
@@ -7,6 +6,5 @@ import actions from './index';
 
 ActionRegistry.register('OpenStack.Tenant', actions);
 ActionRegistry.registerQuickActions('OpenStack.Tenant', [
-  ChangeLimitsAction,
   RequestDirectAccessAction,
 ]);
