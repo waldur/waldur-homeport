9b3849f66800af9a06414cd811068e8636f5ecba
Andrey Izman <andreyizman9@gmail.com>
2024-03-06 08:51:06 +0000

    [WAL-6579] Add a list of offering requests to provider view

---
+++ src/proposals/offering-requests/OfferingRequestsList.tsx
@@ -0,0 +1,93 @@
+import { FC } from 'react';
+import { Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { getFormValues } from 'redux-form';
+import { createSelector } from 'reselect';
+
+import { translate } from '@waldur/i18n';
+import { ProposalCallRound } from '@waldur/proposals/types';
+import { Table, createFetcher } from '@waldur/table';
+import { DASH_ESCAPE_CODE } from '@waldur/table/constants';
+import { useTable } from '@waldur/table/utils';
+
+import { OFFERING_REQUESTS_FILTER_FORM_ID } from '../constants';
+
+import { OfferingRequestsListExpandableRow } from './OfferingRequestsListExpandableRow';
+import { OfferingRequestsListPlaceholder } from './OfferingRequestsListPlaceholder';
+import { OfferingRequestsTableFilter } from './OfferingRequestsTableFilter';
+
+interface OfferingRequestsListProps {
+  round: ProposalCallRound;
+}
+
+const filtersSelctor = createSelector(
+  getFormValues(OFFERING_REQUESTS_FILTER_FORM_ID),
+  (filters: any) => {
+    const result: Record<string, any> = {};
+    if (filters?.state) {
+      result.state = filters.state.map((option) => option.value);
+    }
+    if (filters?.organization) {
+      result.organization_uuid = filters.organization.uuid;
+    }
+    if (filters?.call) {
+      result.call = filters.call.url;
+    }
+    if (filters?.offering) {
+      result.offering = filters.offering.url;
+    }
+    return result;
+  },
+);
+
+export const OfferingRequestsList: FC<OfferingRequestsListProps> = () => {
+  const filter = useSelector(filtersSelctor);
+
+  const tableProps = useTable({
+    table: 'ProposalRequestedOfferingsList',
+    fetchData: createFetcher('proposal-requested-offerings'),
+    queryField: 'call_name',
+    filter,
+  });
+
+  return (
+    <Table
+      {...tableProps}
+      placeholderComponent={<OfferingRequestsListPlaceholder />}
+      columns={[
+        {
+          title: translate('Call'),
+          render: ({ row }) => <b>{row.call_name}</b>,
+        },
+        {
+          title: translate('Organization'),
+          render: ({ row }) => <>{row.provider_name}</>,
+        },
+        {
+          title: translate('Offering'),
+          render: ({ row }) => <>{row.offering_name}</>,
+        },
+        {
+          title: translate('Period'),
+          render: () => <>{DASH_ESCAPE_CODE}</>,
+        },
+        {
+          title: translate('State'),
+          render: ({ row }) => <>{row.state}</>,
+        },
+      ]}
+      title={translate('Offering requests')}
+      verboseName={translate('Offering requests')}
+      hasQuery={true}
+      actions={<Button variant="success">{translate('Accept request')}</Button>}
+      expandableRow={OfferingRequestsListExpandableRow}
+      hoverableRow={() => (
+        <>
+          <Button className="me-3">{translate('Accept')}</Button>
+          <Button variant="danger">{translate('Reject')}</Button>
+        </>
+      )}
+      filters={<OfferingRequestsTableFilter />}
+    />
+  );
+};
