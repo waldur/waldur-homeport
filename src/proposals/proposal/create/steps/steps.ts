81001e203483db530031e1bec058e43a091c54bc
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-03-18 14:34:50 +0200

    Drop deprecated proposal creation steps.

--- src/proposals/proposal/create/steps/steps.ts
+++ src/proposals/proposal/create/steps/steps.ts
@@ -3,8 +3,6 @@ import { ProposalCreationFormStep } from '@waldur/proposals/types';
 
 import { FormProjectDetailsStep } from './FormProjectDetailsStep';
 import { FormResourceRequestsStep } from './FormResourceRequestsStep';
-import { FormTeamStep } from './FormTeamStep';
-import { FormUserAgreementsStep } from './FormUserAgreementsStep';
 
 export const createProposalSteps: ProposalCreationFormStep[] = [
   {
@@ -13,18 +11,8 @@ export const createProposalSteps: ProposalCreationFormStep[] = [
     component: FormProjectDetailsStep,
   },
   {
-    label: translate('Team'),
-    id: 'step-team',
-    component: FormTeamStep,
-  },
-  {
     label: translate('Resource requests'),
     id: 'step-resource-requests',
     component: FormResourceRequestsStep,
-  },
-  {
-    label: translate('User agreements'),
-    id: 'step-user-agreements',
-    component: FormUserAgreementsStep,
   },
 ];
