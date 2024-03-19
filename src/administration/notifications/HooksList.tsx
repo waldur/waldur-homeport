cd88d56b4388758c7297f405d611fd67437cf0dd
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2024-03-19 12:55:48 +0000

    [WAL-6656] Enable search & add a filter for notifications

--- src/administration/notifications/HooksList.tsx
+++ src/administration/notifications/HooksList.tsx
@@ -1,4 +1,7 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import { getFormValues } from 'redux-form';
+import { createSelector } from 'reselect';
 
 import { formatDateTime } from '@waldur/core/dateUtils';
 import { titleCase } from '@waldur/core/utils';
@@ -8,10 +11,13 @@ import { useTable } from '@waldur/table/utils';
 import { HookListTablePlaceholder } from '@waldur/user/hooks/HookListTablePlaceholder';
 import { formatEventTitle } from '@waldur/user/hooks/utils';
 
-import { ADMIN_HOOK_LIST_ID } from './constants';
+import {
+  ADMIN_HOOKS_LIST_FILTER_FORM_ID,
+  ADMIN_HOOK_LIST_ID,
+} from './constants';
 import { HookRemoveButton } from './HookRemoveButton';
+import { HooksListFilter } from './HooksListFilter';
 import { HookUpdateButton } from './HookUpdateButton';
-
 import '@waldur/user/hooks/HookList.scss';
 
 const StateField = ({ row }) => {
@@ -26,11 +32,23 @@ const StateField = ({ row }) => {
   );
 };
 
+const filtersSelctor = createSelector(
+  getFormValues(ADMIN_HOOKS_LIST_FILTER_FORM_ID),
+  (filters: any) => {
+    const result: Record<string, any> = {};
+    if (filters?.state) {
+      result.is_active = filters.state.value;
+    }
+    return result;
+  },
+);
+
 const getDestinationField = (row) => row.destination_url || row.email || 'N/A';
 const getEventsField = (row) =>
   row.event_groups.map(formatEventTitle).join(', ');
 
 export const HooksList: FunctionComponent<any> = () => {
+  const filter = useSelector(filtersSelctor);
   const tableProps = useTable({
     table: ADMIN_HOOK_LIST_ID,
     fetchData: createFetcher('hooks'),
@@ -43,6 +61,7 @@ export const HooksList: FunctionComponent<any> = () => {
     exportAll: true,
     exportFields: ['Method', 'Destination', 'Events'],
     exportKeys: ['hook_type', 'destination_url', 'email', 'event_groups'],
+    filter,
   });
 
   return (
@@ -90,6 +109,8 @@ export const HooksList: FunctionComponent<any> = () => {
         </div>
       )}
       enableExport={true}
+      hasQuery={true}
+      filters={<HooksListFilter />}
       fullWidth
     />
   );
