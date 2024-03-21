d79cf4b556398dc527311e04a1d6d47555feb8a0
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-03-21 10:30:26 +0200

    Render existing uploaded file in proposal.

--- src/proposals/proposal/create/FormProjectDetailsStep.tsx
+++ src/proposals/proposal/create/FormProjectDetailsStep.tsx
@@ -2,16 +2,17 @@ import { useAsync } from 'react-use';
 import { Field } from 'redux-form';
 
 import { ENV } from '@waldur/configs/default';
-import { DropzoneFiles } from '@waldur/core/DropzoneFiles';
 import { required } from '@waldur/core/validators';
 import { isFeatureVisible } from '@waldur/features/connect';
 import { FormGroup, SelectField, StringField, TextField } from '@waldur/form';
 import { AwesomeCheckboxField } from '@waldur/form/AwesomeCheckboxField';
-import { formatJsx, translate } from '@waldur/i18n';
+import { translate } from '@waldur/i18n';
 import { StepCard } from '@waldur/marketplace/deploy/steps/StepCard';
 import { loadOecdCodes } from '@waldur/project/api';
 import { ProposalFormStepProps } from '@waldur/proposals/types';
 import { ActionButton } from '@waldur/table/ActionButton';
+
+import { UploadDocumentationFiles } from './UploadDocumentationFiles';
 
 const isCodeRequired = ENV.plugins.WALDUR_CORE.OECD_FOS_2007_CODE_MANDATORY;
 
@@ -113,37 +114,10 @@ export const FormProjectDetailsStep = (props: ProposalFormStepProps) => {
         name="supporting_documentation"
         className="mb-7"
         label={translate('Upload supporting documentation')}
-        component={(fieldProps) => (
-          <DropzoneFiles
-            multiple={false}
-            maxSize={25 * 1024 * 1024} // 25MB
-            accept={{
-              'application/pdf': ['.pdf'],
-              'image/jpeg': ['.jpg', '.jpeg'],
-              'image/png': ['.png'],
-              'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                ['.docx'],
-              'application/msword': ['.doc'],
-              'application/vnd.oasis.opendocument.text': ['.odt'],
-            }}
-            onDrop={(files) =>
-              fieldProps.input.onChange(files?.length ? files[0] : null)
-            }
-            message={translate(
-              'Drag and drop file here or <u>Choose file</u>',
-              {
-                u: (s) => <u className="text-link">{s}</u>,
-              },
-              formatJsx,
-            )}
-            footerLeft={
-              translate('Supported formats') +
-              ': PDF, PNG/JPG/JPEG, DOC/DOCX/ODT'
-            }
-            footerRight={translate('Maximum size') + ': 25MB'}
-          />
-        )}
-      ></Field>
+        component={FormGroup}
+      >
+        <UploadDocumentationFiles />
+      </Field>
     </StepCard>
   );
 };
---
+++ src/proposals/proposal/create/UploadDocumentationFiles.tsx
@@ -0,0 +1,37 @@
+import { DropzoneFiles } from '@waldur/core/DropzoneFiles';
+import { formatJsx, translate } from '@waldur/i18n';
+
+export const UploadDocumentationFiles = (props) => (
+  <>
+    {props.input.value?.length && (
+      <p className="text-muted">
+        {translate('Uploaded file')} {props.input.value[0].file}
+      </p>
+    )}
+    <DropzoneFiles
+      multiple={false}
+      maxSize={25 * 1024 * 1024} // 25MB
+      accept={{
+        'application/pdf': ['.pdf'],
+        'image/jpeg': ['.jpg', '.jpeg'],
+        'image/png': ['.png'],
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+          ['.docx'],
+        'application/msword': ['.doc'],
+        'application/vnd.oasis.opendocument.text': ['.odt'],
+      }}
+      onDrop={(files) => props.input.onChange(files?.length ? files[0] : null)}
+      message={translate(
+        'Drag and drop file here or <u>Choose file</u>',
+        {
+          u: (s) => <u className="text-link">{s}</u>,
+        },
+        formatJsx,
+      )}
+      footerLeft={
+        translate('Supported formats') + ': PDF, PNG/JPG/JPEG, DOC/DOCX/ODT'
+      }
+      footerRight={translate('Maximum size') + ': 25MB'}
+    />
+  </>
+);
