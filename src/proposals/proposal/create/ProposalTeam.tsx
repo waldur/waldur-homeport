87dc3070e9afe571d2008432e9d9fd66f82c42c2
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-03-21 10:32:29 +0200

    Extract proposal summary card to separate component.

--- src/proposals/proposal/create/ProposalTeam.tsx
+++ src/proposals/proposal/create/ProposalTeam.tsx
@@ -1,78 +1,12 @@
-import { Card } from 'react-bootstrap';
-
-import { ReadOnlyFormControl } from '@waldur/form/ReadOnlyFormControl';
-import { translate } from '@waldur/i18n';
-
 import { ProposalHeader } from './ProposalHeader';
+import { ProposalSummaryCard } from './ProposalSummaryCard';
 
 export const ProposalTeam = ({ proposal }) => {
   return (
     <div className="d-flex flex-column flex-xl-row gap-5 gap-lg-7 pb-10">
       <div className="container-xxl pe-xl-0 d-flex flex-column flex-lg-row-fluid gap-5 gap-lg-7">
         <ProposalHeader proposal={proposal} />
-        <Card>
-          <Card.Header>
-            <Card.Title>{translate('Proposal summary')}</Card.Title>
-          </Card.Header>
-          <Card.Body>
-            <ReadOnlyFormControl
-              label={translate('Project title')}
-              value={proposal.name}
-              className="col-12 col-md-6"
-              floating
-            />
-
-            <ReadOnlyFormControl
-              label={translate('Project summary')}
-              value={proposal.project_summary}
-              className="col-12 col-md-6"
-              floating
-            />
-
-            <ReadOnlyFormControl
-              label={translate('Detailed description')}
-              value={proposal.description || 'N/A'}
-              className="col-12 col-md-6"
-              floating
-            />
-
-            <ReadOnlyFormControl
-              label={translate('Project for civilian purpose?')}
-              value={
-                proposal.project_has_civilian_purpose
-                  ? translate('Yes')
-                  : translate('No')
-              }
-              className="col-12 col-md-6"
-              floating
-            />
-
-            <ReadOnlyFormControl
-              label={translate('Research field (OECD code)')}
-              value={proposal.oecd_fos_2007_label}
-              className="col-12 col-md-6"
-              floating
-            />
-
-            <ReadOnlyFormControl
-              label={translate('Is the project confidential?')}
-              value={
-                proposal.project_is_confidential
-                  ? translate('Yes')
-                  : translate('No')
-              }
-              className="col-12 col-md-6"
-              floating
-            />
-
-            <ReadOnlyFormControl
-              label={translate('Project duration in days')}
-              value={proposal.duration_in_days || 0}
-              className="col-12 col-md-6"
-              floating
-            />
-          </Card.Body>
-        </Card>
+        <ProposalSummaryCard proposal={proposal} />
       </div>
     </div>
   );
