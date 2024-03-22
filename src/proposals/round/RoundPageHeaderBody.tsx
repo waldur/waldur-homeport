888bd3a61035cfb98f19f8c630199aa928d40c3a
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2024-03-22 09:05:30 +0000

    [WAL-6703] Fix round end date info

--- src/proposals/round/RoundPageHeaderBody.tsx
+++ src/proposals/round/RoundPageHeaderBody.tsx
@@ -1,19 +1,41 @@
-import { formatRelative } from '@waldur/core/dateUtils';
+import { formatDateTime, formatRelativeWithHour } from '@waldur/core/dateUtils';
 import { translate } from '@waldur/i18n';
 
 import { Round } from '../types';
+import { getRoundStatus } from '../utils';
 
 interface RoundPageHeaderBodyProps {
   round: Round;
 }
 
 export const RoundPageHeaderBody = (props: RoundPageHeaderBodyProps) => {
+  const roundStatus = getRoundStatus(props.round);
   return (
     <p className="fw-bold">
-      {translate('Open round ends in')}:{' '}
-      <span className="text-danger">
-        {formatRelative(props.round.cutoff_time)}
-      </span>
+      {roundStatus.label === 'Open' && (
+        <p>
+          {translate('Open round ends')}:{' '}
+          <span className="text-danger">
+            {formatRelativeWithHour(props.round.cutoff_time)}
+          </span>
+        </p>
+      )}
+      {roundStatus.label === 'Ended' && (
+        <p>
+          {translate('The round has ended on: ')}
+          <span className="text-danger">
+            {formatDateTime(props.round.cutoff_time)}
+          </span>
+        </p>
+      )}
+      {roundStatus.label === 'Scheduled' && (
+        <p>
+          {translate('The round will be open on: ')}
+          <span className="text-danger">
+            {formatDateTime(props.round.start_time)}
+          </span>
+        </p>
+      )}
     </p>
   );
 };
