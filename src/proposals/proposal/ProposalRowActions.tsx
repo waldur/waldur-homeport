efbaa2d7d5362de68992540950818e95e418a453
Andrey Izman <andreyizman9@gmail.com>
2024-04-01 06:42:36 +0000

    [WAL-6687] Show a confirmation dialog when click on submit review on the review creation module

--- src/proposals/proposal/ProposalRowActions.tsx
+++ src/proposals/proposal/ProposalRowActions.tsx
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
+
 import { Link } from '@waldur/core/Link';
 import { translate } from '@waldur/i18n';
+import { isStaff as isStaffSelector } from '@waldur/workspace/selectors';
 
-export const ProposalRowActions = ({ row }) => (
-  <Link
-    state="proposal-create-review"
-    params={{ proposal_uuid: row.uuid }}
-    label={translate('Create review')}
-    className="btn btn-primary"
-  />
-);
+export const ProposalRowActions = ({ row }) => {
+  const isStaff = useSelector(isStaffSelector);
+  return isStaff ? (
+    <Link
+      state="proposal-create-review"
+      params={{ proposal_uuid: row.uuid }}
+      label={translate('Create review')}
+      className="btn btn-primary"
+    />
+  ) : null;
+};
