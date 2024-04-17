aadc42c2458f164d13685f3305581a8790f767d3
Andrey Izman <andreyizman9@gmail.com>
2024-04-17 17:24:32 +0000

    [WAL-6787] Remove tick & warning signs from documents section and added a message for an empty list

--- src/proposals/update/documents/CallDocumentsSection.tsx
+++ src/proposals/update/documents/CallDocumentsSection.tsx
@@ -10,51 +10,50 @@ export const CallDocumentsSection = ({ call, refetch }) => {
   return (
     <Card id="documents" className="mb-7">
       <Card.Header className="border-2 border-bottom">
-        <Card.Title>
-          {call.documents.length === 0 ? (
-            <i className="fa fa-warning text-danger me-3" />
-          ) : (
-            <i className="fa fa-check text-success me-3" />
-          )}
-          <span>{translate('Documents')}</span>
-        </Card.Title>
+        <Card.Title>{translate('Documents')}</Card.Title>
         <div className="card-toolbar">
           <AttachDocumentsButton call={call} refetch={refetch} />
         </div>
       </Card.Header>
       <Card.Body>
-        <Table bordered={true} hover={true} responsive={true}>
-          <tbody>
-            {call.documents.map((document, i) => (
-              <tr key={i}>
-                <td className="col-md-3">
-                  {decodeURIComponent(
-                    document.file
-                      .split('/')
-                      .pop()
-                      .replace(/_[^_]+\./, '.'),
-                  )}
-                </td>
-                <td className="col-md-9">
-                  <ExternalLink
-                    url={document.file}
-                    label={translate('Download')}
-                    iconless
-                  />
-                </td>
-                <td className="row-actions">
-                  <div>
-                    <RemoveDocumentButton
-                      fileUuid={document.uuid}
-                      call={call}
-                      refetch={refetch}
+        {call.documents.length > 0 ? (
+          <Table bordered={true} hover={true} responsive={true}>
+            <tbody>
+              {call.documents.map((document, i) => (
+                <tr key={i}>
+                  <td className="col-md-3">
+                    {decodeURIComponent(
+                      document.file
+                        .split('/')
+                        .pop()
+                        .replace(/_[^_]+\./, '.'),
+                    )}
+                  </td>
+                  <td className="col-md-9">
+                    <ExternalLink
+                      url={document.file}
+                      label={translate('Download')}
+                      iconless
                     />
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+                  </td>
+                  <td className="row-actions">
+                    <div>
+                      <RemoveDocumentButton
+                        fileUuid={document.uuid}
+                        call={call}
+                        refetch={refetch}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          <p className="text-center">
+            {translate('There are no documents uploaded.')}
+          </p>
+        )}
       </Card.Body>
     </Card>
   );
