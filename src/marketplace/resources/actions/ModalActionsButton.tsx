fe3e37507d480835ddbed7ca65d2e1dd65f332f0
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-04-18 12:36:11 +0300

    Fix dropdown does not close when pressing "See all" [WAL-6807]

--- src/marketplace/resources/actions/ModalActionsButton.tsx
+++ src/marketplace/resources/actions/ModalActionsButton.tsx
@@ -1,22 +1,11 @@
-import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
+import { DropdownButton } from 'react-bootstrap';
 
 import { translate } from '@waldur/i18n';
 
 import { ActionsPopover } from './ActionsPopover';
 
 export const ModalActionsButton = (props) => (
-  <OverlayTrigger
-    trigger="click"
-    placement="bottom"
-    rootClose
-    overlay={
-      <Popover id="popover-modal-action">
-        <ActionsPopover {...props} />
-      </Popover>
-    }
-  >
-    <Button variant="bg-light" className="dropdown-toggle">
-      {translate('Actions')}
-    </Button>
-  </OverlayTrigger>
+  <DropdownButton title={translate('Actions')} variant="light">
+    <ActionsPopover {...props} />
+  </DropdownButton>
 );
