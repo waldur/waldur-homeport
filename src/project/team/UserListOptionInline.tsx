c4a5ed2eda634dd74ceddff7abb31005a2a0b63f
Adil Shirinov <dev.adil.shirinov@gmail.com>
2024-04-24 13:26:19 +0000

    [WAL-6067] Staff user should see if a user is disabled or enabled while adding it to an org

--- src/project/team/UserListOptionInline.tsx
+++ src/project/team/UserListOptionInline.tsx
@@ -2,12 +2,20 @@ import { FunctionComponent } from 'react';
 import { components } from 'react-select';
 
 import { Tip } from '@waldur/core/Tooltip';
+import { translate } from '@waldur/i18n';
 
 export const UserListOptionInline: FunctionComponent<any> = (props) => (
   <components.Option {...props}>
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       {props.data.full_name}
       {props.data.email && <>&nbsp;({props.data.email})</>}
+      <span
+        style={{
+          marginLeft: '5px',
+        }}
+      >
+        {format_inactive_users(props.data.is_active)}
+      </span>
       <span
         style={{
           alignSelf: 'center',
@@ -28,11 +36,33 @@ const format_registration_method = (registration_method) => {
           #registration-method-tooltip {
             z-index: 9999;
           }
+          #inactive-user-tooltip {
+            z-index: 9999;
+          }
         `}
       </style>
       <Tip label={registration_method} id="registration-method-tooltip">
         <i className="fa fa-key" />
       </Tip>
+    </small>
+  );
+};
+
+const format_inactive_users = (is_active) => {
+  return (
+    <small>
+      <style>
+        {`
+          #inactive-user-tooltip {
+            z-index: 9999;
+          }
+        `}
+      </style>
+      {is_active === false && (
+        <Tip label={translate('Inactive')} id="inactive-user-tooltip">
+          <i className="fa fa-ban" />
+        </Tip>
+      )}
     </small>
   );
 };
