accc1ad17fc2ef8cf0382aabc325eca0a0593b2d
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-04-26 15:19:13 +0300

    Fix lint

--- src/customer/details/types.ts
+++ src/customer/details/types.ts
@@ -1,9 +1,5 @@
 import { Customer } from '@waldur/workspace/types';
 
-export interface CustomerLogoUpdateFormData {
-  image: HTMLImageElement;
-}
-
 export interface CustomerEditPanelProps {
   customer: Customer;
   callback(formData, dispatch): Promise<any>;
