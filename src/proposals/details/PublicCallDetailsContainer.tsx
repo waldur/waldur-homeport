2cc04b0bf8e70ce3dad205102b1cd6c3fefcf248
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-05-20 23:28:28 +0300

    Make flickering less vivid

--- src/proposals/details/PublicCallDetailsContainer.tsx
+++ src/proposals/details/PublicCallDetailsContainer.tsx
@@ -3,7 +3,6 @@ import { FunctionComponent } from 'react';
 import { useAsyncFn, useEffectOnce } from 'react-use';
 
 import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
-import { InvalidRoutePage } from '@waldur/error/InvalidRoutePage';
 import { translate } from '@waldur/i18n';
 import { useTitle } from '@waldur/navigation/title';
 
@@ -33,7 +32,5 @@ export const PublicCallDetailsContainer: FunctionComponent = () => {
     <h3>{translate('Unable to load call details.')}</h3>
   ) : value ? (
     <PublicCallDetails call={value} refreshCall={refreshCall} />
-  ) : (
-    <InvalidRoutePage />
-  );
+  ) : null;
 };
