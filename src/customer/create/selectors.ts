6043942a6f15669ee9e83dd4a5150dfafd8970cf
Andrey Izman <andreyizman9@gmail.com>
2024-06-03 08:21:11 +0000

    [WAL-6971] Hide create organization button if user is not staff

--- src/customer/create/selectors.ts
+++ src/customer/create/selectors.ts
@@ -1,10 +1,5 @@
-import { ENV } from '@waldur/configs/default';
-import { PermissionEnum, RoleEnum } from '@waldur/permissions/enums';
 import { RootState } from '@waldur/store/reducers';
 import { isStaff } from '@waldur/workspace/selectors';
 
 export const canCreateOrganization = (state: RootState): boolean =>
-  isStaff(state) ||
-  ENV.roles
-    .find(({ name }) => name === RoleEnum.CUSTOMER_OWNER)
-    .permissions.includes(PermissionEnum.CREATE_CUSTOMER);
+  isStaff(state);
