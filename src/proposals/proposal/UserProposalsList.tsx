0305f320ef9285a8c743d6f86eb901513456c5c9
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-06-17 13:42:29 +0300

    Expose all fields as separate columns in proposal list [WAL-6989]

--- src/proposals/proposal/UserProposalsList.tsx
+++ src/proposals/proposal/UserProposalsList.tsx
@@ -4,18 +4,20 @@ import { getFormValues } from 'redux-form';
 import { createSelector } from 'reselect';
 
 import { Link } from '@waldur/core/Link';
+import { isFeatureVisible } from '@waldur/features/connect';
+import { ProjectFeatures } from '@waldur/FeaturesEnums';
 import { translate } from '@waldur/i18n';
 import { PROPOSALS_FILTER_FORM_ID } from '@waldur/proposals/constants';
 import {
   formatProposalState,
   getProposalStateOptions,
 } from '@waldur/proposals/utils';
-import { Table, createFetcher } from '@waldur/table';
+import { createFetcher, Table } from '@waldur/table';
+import { DASH_ESCAPE_CODE } from '@waldur/table/constants';
 import { renderFieldOrDash, useTable } from '@waldur/table/utils';
 
 import { EndingField } from '../EndingField';
 
-import { ProposalsListExpandableRow } from './ProposalsListExpandableRow';
 import { ProposalsTableFilter } from './ProposalsTableFilter';
 
 const filtersSelector = createSelector(
@@ -52,31 +54,71 @@ export const UserProposalsList: FC = () => {
     [],
   );
 
+  const columns = [
+    {
+      title: translate('Proposal'),
+      render: ({ row }) => <>{row.name}</>,
+      keys: ['name'],
+    },
+    {
+      title: translate('Call'),
+      render: ({ row }) => <>{renderFieldOrDash(row.call_name)}</>,
+      keys: ['call_name'],
+    },
+    {
+      title: translate('Ending'),
+      render: ({ row }) => <EndingField endDate={row.round?.cutoff_time} />,
+      keys: ['round'],
+    },
+    {
+      title: translate('State'),
+      render: ({ row }) => <>{formatProposalState(row.state)}</>,
+      keys: ['state'],
+      orderField: 'state',
+    },
+    {
+      title: translate('UUID'),
+      render: ({ row }) => <>{row.uuid}</>,
+      keys: ['uuid'],
+      optional: true,
+    },
+    {
+      title: translate('Created'),
+      render: ({ row }) => <>{row.created}</>,
+      keys: ['created'],
+      orderField: 'created',
+      optional: true,
+    },
+    {
+      title: translate('Duration in days'),
+      render: ({ row }) => <>{row.duration_in_days || DASH_ESCAPE_CODE}</>,
+      keys: ['duration_in_days'],
+      optional: true,
+    },
+  ];
+  if (isFeatureVisible(ProjectFeatures.oecd_fos_2007_code)) {
+    columns.push({
+      title: translate('OECD FoS code'),
+      render: ({ row }) => (
+        <>
+          {row.oecd_fos_2007_code
+            ? `${row.oecd_fos_2007_code}. ${row.oecd_fos_2007_label}`
+            : DASH_ESCAPE_CODE}
+        </>
+      ),
+      optional: true,
+      keys: ['oecd_fos_2007_code', 'oecd_fos_2007_label'],
+    });
+  }
+
   return (
     <Table
       {...tableProps}
-      columns={[
-        {
-          title: translate('Proposal'),
-          render: ({ row }) => <>{row.name}</>,
-        },
-        {
-          title: translate('Call'),
-          render: ({ row }) => <>{renderFieldOrDash(row.call_name)}</>,
-        },
-        {
-          title: translate('Ending'),
-          render: ({ row }) => <EndingField endDate={row.round?.cutoff_time} />,
-        },
-        {
-          title: translate('State'),
-          render: ({ row }) => <>{formatProposalState(row.state)}</>,
-        },
-      ]}
+      columns={columns}
       title={translate('My proposals')}
       verboseName={translate('My proposals')}
       hasQuery={true}
-      expandableRow={ProposalsListExpandableRow}
+      hasOptionalColumns
       hoverableRow={({ row }) => (
         <Link
           state="proposals.manage-proposal"
