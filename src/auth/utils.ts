cdf9d2dcdff9aa53866669606dc653a59c728cc8
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-06-20 13:27:09 +0000

    Add support to enable direct authentication into Waldur without login page [WAL-6917]

--- src/auth/utils.ts
+++ src/auth/utils.ts
@@ -1,18 +1,21 @@
 import Qs from 'qs';
 
+import { EDUTEAMS_IDP } from './providers/constants';
+
 const getStateId = () =>
   encodeURIComponent(Math.random().toString(36).substr(2));
 
 export const getRedirectUri = (provider) =>
   `${document.baseURI}oauth_login_completed/${provider}/`;
 
-export const getOauthCallback = (options) => () => {
-  const params = {
+export const getOauthURL = (provider) =>
+  `${provider.auth_url}?${Qs.stringify({
     response_type: 'code',
-    client_id: options.clientId,
-    redirect_uri: getRedirectUri(options.name),
-    scope: options.scope,
+    client_id: provider.client_id,
+    redirect_uri: getRedirectUri(provider.provider),
+    scope:
+      provider.provider === EDUTEAMS_IDP
+        ? 'openid profile email eduperson_assurance ssh_public_key'
+        : 'openid',
     state: getStateId(),
-  };
-  window.location.href = `${options.authUrl}?${Qs.stringify(params)}`;
-};
+  })}`;
