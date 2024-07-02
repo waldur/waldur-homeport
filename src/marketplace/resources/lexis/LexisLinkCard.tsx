b8dd44bb21b7431dd557ef9641f034679cbd7781
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2024-07-02 10:26:47 +0000

    [WAL-7052] Drop double tables in lexis link view

--- src/marketplace/resources/lexis/LexisLinkCard.tsx
+++ src/marketplace/resources/lexis/LexisLinkCard.tsx
@@ -1,8 +1,6 @@
 import { useMemo } from 'react';
-import { Card } from 'react-bootstrap';
 import { useAsync } from 'react-use';
 
-import { translate } from '@waldur/i18n';
 import { countLexisLinks } from '@waldur/marketplace/common/api';
 
 import { BasicLexisLinkList } from './BasicLexisLinkList';
@@ -15,16 +13,5 @@ export const LexisLinkCard = ({ resource }) => {
   if (!result.value) {
     return null;
   }
-  return (
-    <Card>
-      <Card.Header>
-        <Card.Title>
-          <h3>{translate('LEXIS links')}</h3>
-        </Card.Title>
-      </Card.Header>
-      <Card.Body>
-        <BasicLexisLinkList filter={filter} />
-      </Card.Body>
-    </Card>
-  );
+  return <BasicLexisLinkList filter={filter} />;
 };
