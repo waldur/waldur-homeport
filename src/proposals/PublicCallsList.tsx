830131aea6905d22219676b95b657919643d9695
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2024-07-18 11:23:49 +0000

    Add reusable calls list & replace in call's page

---
+++ src/proposals/PublicCallsList.tsx
@@ -0,0 +1,118 @@
+import { FunctionComponent, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { getFormValues } from 'redux-form';
+
+import { formatDateTime } from '@waldur/core/dateUtils';
+import { Link } from '@waldur/core/Link';
+import { translate } from '@waldur/i18n';
+import { createFetcher, Table } from '@waldur/table';
+import { renderFieldOrDash, useTable } from '@waldur/table/utils';
+
+import { CallAllFiltersWithDefaultState } from './call-management/CallAllFilters';
+import { CallCard } from './CallCard';
+import { CALL_FILTER_FORM_ID } from './constants';
+import { PublicCallExpandableRow } from './PublicCallExpandableRow';
+import { Call } from './types';
+import { formatCallState, getRoundsWithStatus } from './utils';
+
+interface PublicCallsListProps {
+  offering_uuid: string;
+}
+
+const CallColumns = [
+  {
+    title: translate('Name'),
+    orderField: 'name',
+    render: ({ row }) => (
+      <Link
+        state="public-call.details"
+        params={{ call_uuid: row.uuid }}
+        label={row.name}
+      />
+    ),
+  },
+  {
+    title: translate('Organization'),
+    render: ({ row }) => <>{row.customer_name}</>,
+  },
+  {
+    title: translate('Start'),
+    render: ({ row }) => (
+      <>{renderFieldOrDash(formatDateTime(row.start_date))}</>
+    ),
+  },
+  {
+    title: translate('End'),
+    render: ({ row }) => <>{renderFieldOrDash(formatDateTime(row.end_date))}</>,
+  },
+  {
+    title: translate('State'),
+    render: ({ row }) => <>{formatCallState(row.state)}</>,
+  },
+  {
+    title: translate('Active round'),
+    render: ({ row }) => {
+      const activeRound = useMemo(() => {
+        const items = getRoundsWithStatus(row.rounds);
+        const first = items[0];
+        if (
+          first &&
+          (first.status.value === 'open' || first.status.value === 'scheduled')
+        ) {
+          return first;
+        }
+        return null;
+      }, [row.rounds]);
+      return (
+        <>
+          {activeRound ? formatDateTime(activeRound.cutoff_time) : <>&mdash;</>}
+        </>
+      );
+    },
+  },
+];
+
+export const PublicCallsList: FunctionComponent<PublicCallsListProps> = (
+  props,
+) => {
+  const usePublicCallsFilter = (offering_uuid?: string) => {
+    const filters = useSelector(getFormValues(CALL_FILTER_FORM_ID)) as any;
+
+    return useMemo(() => {
+      const result: Record<string, any> = {};
+      if (filters) {
+        if (filters.state) {
+          result.state = filters.state.map((option) => option.value);
+        }
+        if (filters.has_active_round) {
+          result.has_active_round = filters.has_active_round;
+        }
+      }
+      if (offering_uuid) {
+        result.offering_uuid = offering_uuid;
+      }
+      return result;
+    }, [filters, offering_uuid]);
+  };
+  const filter = usePublicCallsFilter(props.offering_uuid);
+  const tableProps = useTable({
+    table: 'PublicCallsList',
+    fetchData: createFetcher('proposal-public-calls'),
+    filter,
+    queryField: 'name',
+  });
+  return (
+    <Table<Call>
+      title={translate('Calls for proposals')}
+      {...tableProps}
+      columns={CallColumns}
+      gridItem={({ row }) => <CallCard call={row} />}
+      gridSize={{ lg: 6, xl: 4 }}
+      verboseName={translate('Public calls')}
+      initialSorting={{ field: 'name', mode: 'desc' }}
+      hasQuery={true}
+      expandableRow={PublicCallExpandableRow}
+      filters={<CallAllFiltersWithDefaultState />}
+    />
+  );
+};
--- src/proposals/PublicCallsPage.tsx
+++ src/proposals/PublicCallsPage.tsx
@@ -1,59 +1,19 @@
 import { useCurrentStateAndParams } from '@uirouter/react';
-import { FunctionComponent, useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { getFormValues } from 'redux-form';
+import { FunctionComponent } from 'react';
 
 import { ENV } from '@waldur/configs/default';
-import { formatDateTime } from '@waldur/core/dateUtils';
-import { Link } from '@waldur/core/Link';
 import { LandingHeroSection } from '@waldur/dashboard/hero/LandingHeroSection';
 import { translate } from '@waldur/i18n';
 import { useFullPage } from '@waldur/navigation/context';
-import { CallAllFiltersWithDefaultState } from '@waldur/proposals/call-management/CallAllFilters';
-import { CALL_FILTER_FORM_ID } from '@waldur/proposals/constants';
-import { Call } from '@waldur/proposals/types';
-import { createFetcher, Table } from '@waldur/table';
-import { renderFieldOrDash, useTable } from '@waldur/table/utils';
 
-import { CallCard } from './CallCard';
 import background from './proposal-calls.png';
-import { PublicCallExpandableRow } from './PublicCallExpandableRow';
-import { formatCallState, getRoundsWithStatus } from './utils';
-
-const usePublicCallsFilter = (offering_uuid?: string) => {
-  const filters = useSelector(getFormValues(CALL_FILTER_FORM_ID)) as any;
-
-  return useMemo(() => {
-    const result: Record<string, any> = {};
-    if (filters) {
-      if (filters.state) {
-        result.state = filters.state.map((option) => option.value);
-      }
-      if (filters.has_active_round) {
-        result.has_active_round = filters.has_active_round;
-      }
-    }
-    if (offering_uuid) {
-      result.offering_uuid = offering_uuid;
-    }
-    return result;
-  }, [filters, offering_uuid]);
-};
+import { PublicCallsList } from './PublicCallsList';
 
 export const PublicCallsPage: FunctionComponent = () => {
   const {
     params: { offering_uuid },
   } = useCurrentStateAndParams();
   useFullPage();
-
-  const filter = usePublicCallsFilter(offering_uuid);
-
-  const tableProps = useTable({
-    table: 'PublicCallsList',
-    fetchData: createFetcher('proposal-public-calls'),
-    filter,
-    queryField: 'name',
-  });
 
   return (
     <>
@@ -63,76 +23,7 @@ export const PublicCallsPage: FunctionComponent = () => {
         backgroundImage={background}
       />
       <div className="container-fluid mt-20 mb-10">
-        <Table<Call>
-          title={translate('Calls for proposals')}
-          {...tableProps}
-          columns={[
-            {
-              title: translate('Name'),
-              orderField: 'name',
-              render: ({ row }) => (
-                <Link
-                  state="public-call.details"
-                  params={{ call_uuid: row.uuid }}
-                  label={row.name}
-                />
-              ),
-            },
-            {
-              title: translate('Organization'),
-              render: ({ row }) => <>{row.customer_name}</>,
-            },
-            {
-              title: translate('Start'),
-              render: ({ row }) => (
-                <>{renderFieldOrDash(formatDateTime(row.start_date))}</>
-              ),
-            },
-            {
-              title: translate('End'),
-              render: ({ row }) => (
-                <>{renderFieldOrDash(formatDateTime(row.end_date))}</>
-              ),
-            },
-            {
-              title: translate('State'),
-              render: ({ row }) => <>{formatCallState(row.state)}</>,
-            },
-            {
-              title: translate('Active round'),
-              render: ({ row }) => {
-                const activeRound = useMemo(() => {
-                  const items = getRoundsWithStatus(row.rounds);
-                  const first = items[0];
-                  if (
-                    first &&
-                    (first.status.value === 'open' ||
-                      first.status.value === 'scheduled')
-                  ) {
-                    return first;
-                  }
-                  return null;
-                }, [row.rounds]);
-                return (
-                  <>
-                    {activeRound ? (
-                      formatDateTime(activeRound.cutoff_time)
-                    ) : (
-                      <>&mdash;</>
-                    )}
-                  </>
-                );
-              },
-            },
-          ]}
-          gridItem={({ row }) => <CallCard call={row} />}
-          gridSize={{ lg: 6, xl: 4 }}
-          verboseName={translate('Public calls')}
-          initialSorting={{ field: 'name', mode: 'desc' }}
-          hasQuery={true}
-          expandableRow={PublicCallExpandableRow}
-          filters={<CallAllFiltersWithDefaultState />}
-        />
+        <PublicCallsList offering_uuid={offering_uuid} />
       </div>
     </>
   );
