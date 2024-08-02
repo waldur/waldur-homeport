ff60af7f497e765a36af0692b4f142f7669bfeea
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2024-08-02 09:43:48 +0000

    [WAL-7222] Add mass cancel action for invitations

--- src/invitations/InvitationsMultiSelectActions.tsx
+++ src/invitations/InvitationsMultiSelectActions.tsx
@@ -2,16 +2,19 @@ import { DropdownButton } from 'react-bootstrap';
 
 import { translate } from '@waldur/i18n';
 
+import { MultiCancelAction } from './MultiCancelAction';
 import { MultiResendAction } from './MultiResendAction';
+import { Invitation } from './types';
 
 export const InvitationsMultiSelectActions = ({
   rows,
   refetch,
 }: {
-  rows: any[];
+  rows: Invitation[];
   refetch(): void;
 }) => (
   <DropdownButton variant="primary" title={translate('All actions')}>
     <MultiResendAction rows={rows} refetch={refetch} />
+    <MultiCancelAction rows={rows} refetch={refetch} />
   </DropdownButton>
 );
