75fb9ba08f3d204738b276036ae8caec1212703b
Ilja Livenson <ilja.livenson@gmail.com>
2024-08-24 22:35:47 +0300

    Drop deprecated resource summary for sec groups

--- src/openstack/openstack-security-groups/SecurityGroupExpandableRow.tsx
+++ src/openstack/openstack-security-groups/SecurityGroupExpandableRow.tsx
@@ -1,12 +1,5 @@
-import { ResourceSummary } from '@waldur/resource/summary/ResourceSummary';
-
 import { SecurityGroupRulesList } from './SecurityGroupRulesList';
 
 export const SecurityGroupExpandableRow = ({ row }) => {
-  return (
-    <>
-      <ResourceSummary resource={row} />
-      <SecurityGroupRulesList resource={row} />
-    </>
-  );
+  return <SecurityGroupRulesList resource={row} />;
 };
