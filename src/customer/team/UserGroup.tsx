c0c7121bc7a0054932c3bca845acba33786ac37a
Andrey Izman <andreyizman9@gmail.com>
2024-09-13 08:59:17 +0000

    [WAL-7400] Add email and username on the user edit popup for organization and project users

--- src/customer/team/UserGroup.tsx
+++ src/customer/team/UserGroup.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react';
 import { Form } from 'react-bootstrap';
 
 import { translate } from '@waldur/i18n';
+import { DASH_ESCAPE_CODE } from '@waldur/table/constants';
 
 export const UserGroup: FunctionComponent<{ permission }> = ({
   permission,
@@ -9,7 +10,15 @@ export const UserGroup: FunctionComponent<{ permission }> = ({
   <Form.Group>
     <p>
       <strong>{translate('User')}</strong>:{' '}
-      {permission.full_name || permission.username}
+      {permission.full_name || DASH_ESCAPE_CODE}
+    </p>
+    {Boolean(permission.email) && (
+      <p>
+        <strong>{translate('Email')}</strong>: {permission.email}
+      </p>
+    )}
+    <p>
+      <strong>{translate('Username')}</strong>: {permission.username}
     </p>
   </Form.Group>
 );
