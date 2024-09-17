af15d71cb9a15c9d72f0ca3812fe1a2ee888a263
Ilja Livenson <ilja.livenson@gmail.com>
2024-09-17 15:48:59 +0100

    Avoid showing set slug action to non-staff users [WAL-7430]

--- src/marketplace/resources/SetSlugAction.tsx
+++ src/marketplace/resources/SetSlugAction.tsx
@@ -1,9 +1,11 @@
 import { PencilSimpleLine } from '@phosphor-icons/react';
+import { useSelector } from 'react-redux';
 
 import { lazyComponent } from '@waldur/core/lazyComponent';
 import { translate } from '@waldur/i18n';
 import { DialogActionItem } from '@waldur/resource/actions/DialogActionItem';
 import { ActionItemType } from '@waldur/resource/actions/types';
+import { getUser } from '@waldur/workspace/selectors';
 
 const SetSlugDialog = lazyComponent(
   () => import('./SetSlugDialog'),
@@ -11,6 +13,10 @@ const SetSlugDialog = lazyComponent(
 );
 
 export const SetSlugAction: ActionItemType = ({ resource, refetch }) => {
+  const user = useSelector(getUser);
+  if (!user.is_staff) {
+    return null;
+  }
   return (
     <DialogActionItem
       title={translate('Set slug')}
