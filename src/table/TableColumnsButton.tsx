412733f3c44c49bec9664f705edd6d8ed23f0c89
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-09-26 11:42:49 +0300

    Make icon more vivid: gray on white

--- src/table/TableColumnsButton.tsx
+++ src/table/TableColumnsButton.tsx
@@ -42,7 +42,7 @@ const SortableItem = (props) => {
       {...attributes}
       {...listeners}
     >
-      <span className="svg-icon svg-icon-2 svg-icon-white me-3">
+      <span className="svg-icon svg-icon-2 svg-icon-gray me-3">
         <DotsSixVertical size={32} />
       </span>{' '}
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
