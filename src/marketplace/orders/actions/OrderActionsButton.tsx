7a1961d0f30ff23b37f031e7c0bb3417c81e9e9a
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-09-26 09:39:34 +0300

    Avoid rendering empty order actions dropdown

--- src/marketplace/orders/actions/OrderActionsButton.tsx
+++ src/marketplace/orders/actions/OrderActionsButton.tsx
@@ -5,13 +5,12 @@ import { CancelOrderButton } from '../details/CancelOrderButton';
 
 import { OrderConsumerActions } from './OrderConsumerActions';
 
-export const OrderActionsButton = ({ order, loadData }) => {
-  return (
+export const OrderActionsButton = ({ order, loadData }) =>
+  order.can_terminate || order.state === 'pending-consumer' ? (
     <ActionsDropdownComponent label={translate('All actions')} labeled={true}>
       {order.can_terminate && (
         <CancelOrderButton uuid={order.uuid} loadData={loadData} />
       )}
       <OrderConsumerActions order={order} />
     </ActionsDropdownComponent>
-  );
-};
+  ) : null;
