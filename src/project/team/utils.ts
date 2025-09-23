83560b9145c03f71723464dad5c1241d3c8d4b8f
Andrey Izman <andreyizman9@gmail.com>
2025-09-23 21:36:43 +0000

    [WAL-9215] Conceal active and invitations tabs from project team for course projects

--- src/project/team/utils.ts
+++ src/project/team/utils.ts
@@ -1,4 +1,7 @@
+import { useRouter } from '@uirouter/react';
+import { useEffect } from 'react';
 import { createSelector } from 'reselect';
+import { Project } from 'waldur-js-client';
 
 import { getCustomer, getUser } from '@waldur/workspace/selectors';
 
@@ -11,3 +14,16 @@ export const hasCurrentCustomerPermission = createSelector(
         scope_uuid === customer?.uuid && scope_type === 'customer',
     ),
 );
+
+export const useRedirectCourseProjects = (project: Project) => {
+  // Check if project is course kind, then redirect
+  const router = useRouter();
+  useEffect(() => {
+    const redirect = ['project-users', 'project-invitations'].includes(
+      router.globals.$current.name,
+    );
+    if (redirect && project.kind === 'course') {
+      router.stateService.go('project-course-accounts');
+    }
+  }, []);
+};
