27612c72920aa2430bdc195d6b1843feec1b1ea1
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-09-26 20:29:30 +0000

    Update enums and descriptions from Waldur Mastermind

--- src/SettingsDescription.ts
+++ src/SettingsDescription.ts
@@ -800,6 +800,12 @@ export const SettingsDescription = [
         default: false,
         type: 'boolean',
       },
+      {
+        key: 'OIDC_BLOCK_CREATION_OF_UNINVITED_USERS',
+        description: translate('If true, block creation of an account on OIDC login if user email is not provided or provided and is not in the list of one of the active invitations.'),
+        default: false,
+        type: 'boolean',
+      },
     ],
   },
   {
