91e455b169d523487cdb7f788477ac477af673ab
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-10-01 12:13:35 +0300

    Add plan resources tab for service providers [WAL-9243]

---
+++ src/marketplace/offerings/update/plans/PlanComponentsTable.tsx
@@ -0,0 +1,49 @@
+import { InfoIcon } from '@phosphor-icons/react';
+import { FC } from 'react';
+import { OfferingComponent, ProviderPlanDetails } from 'waldur-js-client';
+
+import { Tip } from '@waldur/core/Tooltip';
+import { translate } from '@waldur/i18n';
+
+interface OwnProps {
+  row: ProviderPlanDetails;
+  components: OfferingComponent[];
+}
+
+export const PlanComponentsTable: FC<OwnProps> = (props) => (
+  <table className="table align-middle">
+    <thead>
+      <tr className="align-middle">
+        <th>{translate('Name')}</th>
+        <th>{translate('Current price')}</th>
+        <th>{translate('Price update next month')}</th>
+        <th>{translate('Units')}</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.components.map((component, index) => (
+        <tr key={index}>
+          <td>
+            {component.name}{' '}
+            <Tip
+              id={`tip-component-${props.row.name}-${component.name}`}
+              label={component.type}
+              placement="right"
+            >
+              <InfoIcon weight="bold" />
+            </Tip>
+          </td>
+          <td>
+            {parseFloat(props.row.prices[component.type] as unknown as string)}
+          </td>
+          <td>
+            {props.row.future_prices[component.type] ?? translate('No update')}
+          </td>
+          <td>
+            <div className="form-control-static">{component.measured_unit}</div>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
