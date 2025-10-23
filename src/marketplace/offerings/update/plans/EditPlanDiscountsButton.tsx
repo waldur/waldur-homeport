9b423bfa6d0abeafcc1bed42e125c928e1fba2d6
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-10-23 10:26:10 +0000

    Allow to edit plan discounts [WAL-9316]

---
+++ src/marketplace/offerings/update/plans/EditPlanDiscountsButton.tsx
@@ -0,0 +1,38 @@
+import { TagIcon } from '@phosphor-icons/react';
+import { FunctionComponent } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+
+import { lazyComponent } from '@waldur/core/lazyComponent';
+import { translate } from '@waldur/i18n';
+import { openModalDialog } from '@waldur/modal/actions';
+
+import { EDIT_PLAN_DISCOUNTS_FORM_ID } from './constants';
+
+const EditPlanDiscountsDialog = lazyComponent(() =>
+  import('./EditPlanDiscountsDialog').then((module) => ({
+    default: module.EditPlanDiscountsDialog,
+  })),
+);
+
+export const EditPlanDiscountsButton: FunctionComponent<{
+  offering;
+  plan;
+  refetch;
+}> = ({ offering, plan, refetch }) => {
+  const dispatch = useDispatch();
+  const callback = () => {
+    dispatch(
+      openModalDialog(EditPlanDiscountsDialog, {
+        resolve: { offering, plan, refetch },
+        formId: EDIT_PLAN_DISCOUNTS_FORM_ID,
+        size: 'lg',
+      }),
+    );
+  };
+  return (
+    <Dropdown.Item onClick={callback}>
+      <TagIcon size={18} /> {translate('Edit discounts')}
+    </Dropdown.Item>
+  );
+};
---
+++ src/marketplace/offerings/update/plans/EditPlanDiscountsDialog.tsx
@@ -0,0 +1,76 @@
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import { marketplacePlansUpdateDiscounts } from 'waldur-js-client';
+
+import { SubmitButton } from '@waldur/form';
+import { translate } from '@waldur/i18n';
+import { Plan } from '@waldur/marketplace/types';
+import { useModal } from '@waldur/modal/hooks';
+import { ModalDialog } from '@waldur/modal/ModalDialog';
+import { useNotify } from '@waldur/store/hooks';
+
+import { EDIT_PLAN_DISCOUNTS_FORM_ID } from './constants';
+import { DiscountsTable } from './DiscountsTable';
+
+const getInitialValues = (plan: Plan) => {
+  const initialDiscounts = {};
+  if (plan.components) {
+    for (const planComponent of plan.components) {
+      initialDiscounts[planComponent.type] = {
+        discount_threshold: planComponent.discount_threshold,
+        discount_rate: planComponent.discount_rate,
+      };
+    }
+  }
+  return { discounts: initialDiscounts };
+};
+
+export const EditPlanDiscountsDialog = connect<
+  {},
+  {},
+  { resolve: { plan: Plan } }
+>((_, ownProps) => ({
+  initialValues: getInitialValues(ownProps.resolve.plan),
+}))(
+  reduxForm<{}, { resolve: { offering; plan; refetch } }>({
+    form: EDIT_PLAN_DISCOUNTS_FORM_ID,
+  })((props) => {
+    const { showErrorResponse, showSuccess } = useNotify();
+    const { closeDialog } = useModal();
+
+    const update = async (formData) => {
+      try {
+        await marketplacePlansUpdateDiscounts({
+          path: { uuid: props.resolve.plan.uuid },
+          body: {
+            discounts: formData.discounts,
+          },
+        });
+        showSuccess(translate('Discounts have been updated successfully.'));
+        await props.resolve.refetch();
+        closeDialog();
+      } catch (error) {
+        showErrorResponse(error, translate('Unable to update discounts.'));
+      }
+    };
+
+    return (
+      <form onSubmit={props.handleSubmit(update)}>
+        <ModalDialog
+          title={translate('Edit discounts for plan {planName}', {
+            planName: props.resolve.plan.name,
+          })}
+          footer={
+            <SubmitButton
+              disabled={props.invalid}
+              submitting={props.submitting}
+              label={translate('Save')}
+            />
+          }
+        >
+          <DiscountsTable components={props.resolve.offering.components} />
+        </ModalDialog>
+      </form>
+    );
+  }),
+);
