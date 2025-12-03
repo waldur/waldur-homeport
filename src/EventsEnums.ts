a7304d81523d0baa30383ff53e162d2a71046abc
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-12-03 21:11:45 +0000

    Update enums and descriptions from Waldur Mastermind

--- src/EventsEnums.ts
+++ src/EventsEnums.ts
@@ -309,6 +309,8 @@ export const UsersEnum = {
   user_deletion_succeeded: 'user_deletion_succeeded',
   user_details_update_succeeded: 'user_details_update_succeeded',
   user_has_been_created_by_staff: 'user_has_been_created_by_staff',
+  user_invitation_deleted: 'user_invitation_deleted',
+  user_invitation_updated: 'user_invitation_updated',
   user_password_updated: 'user_password_updated',
   user_password_updated_by_staff: 'user_password_updated_by_staff',
   user_update_succeeded: 'user_update_succeeded',
