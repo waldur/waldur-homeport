ef321285a3c90501d639c8457897a514bb8b89f4
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2025-12-03 13:17:17 +0000

    [WAL-9457] Fix OfferingUserRoles list component

--- src/marketplace/offerings/update/roles/RolesSection.tsx
+++ src/marketplace/offerings/update/roles/RolesSection.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
-import { Card } from 'react-bootstrap';
+import { marketplaceOfferingUserRolesList } from 'waldur-js-client';
 
 import { translate } from '@waldur/i18n';
 import { NoResult } from '@waldur/navigation/header/search/NoResult';
+import { createFetcher } from '@waldur/table/api';
 import Table from '@waldur/table/Table';
 import { useTable } from '@waldur/table/useTable';
 
-import { RefreshButton } from '../components/RefreshButton';
 import { OfferingSectionProps } from '../types';
 
 import { AddRoleButton } from './AddRoleButton';
@@ -15,47 +15,35 @@ import { DeleteRoleButton } from './DeleteRoleButton';
 export const RolesSection: FC<OfferingSectionProps> = (props) => {
   const tableProps = useTable({
     table: 'OfferingRolesList',
-    fetchData: () =>
-      Promise.resolve({
-        rows: props.offering.roles,
-      }),
+    fetchData: createFetcher(marketplaceOfferingUserRolesList),
   });
 
   return (
-    <Card className="card-bordered">
-      <Card.Header>
-        <Card.Title className="h5">
-          <span className="me-2">{translate('Roles')}</span>
-          <RefreshButton refetch={props.refetch} loading={props.loading} />
-        </Card.Title>
-        <div className="card-toolbar">
-          <AddRoleButton {...props} />
-        </div>
-      </Card.Header>
-      <Table
-        {...tableProps}
-        cardBordered={false}
-        columns={[
-          {
-            title: translate('Role'),
-            render: ({ row }) => row.name,
-          },
-        ]}
-        verboseName={translate('Roles')}
-        placeholderComponent={
-          <NoResult
-            callback={props.refetch}
-            title={translate('No roles found')}
-            message={translate("Offering doesn't have roles.")}
-            buttonTitle={translate('Search again')}
-            className="mt-n5"
-          />
-        }
-        hasActionBar={false}
-        rowActions={({ row }) => (
-          <DeleteRoleButton role={row} refetch={props.refetch} />
-        )}
-      />
-    </Card>
+    <Table
+      {...tableProps}
+      cardBordered={false}
+      title={translate('Roles')}
+      columns={[
+        {
+          title: translate('Role'),
+          render: ({ row }) => row.name,
+        },
+      ]}
+      verboseName={translate('Roles')}
+      placeholderComponent={
+        <NoResult
+          callback={props.refetch}
+          title={translate('No roles found')}
+          message={translate("Offering doesn't have roles.")}
+          buttonTitle={translate('Search again')}
+          className="mt-n5"
+        />
+      }
+      hasQuery={false}
+      rowActions={({ row }) => (
+        <DeleteRoleButton role={row} refetch={tableProps.fetch} />
+      )}
+      tableActions={<AddRoleButton {...props} refetch={tableProps.fetch} />}
+    />
   );
 };
