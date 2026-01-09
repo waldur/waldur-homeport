f92406a74127566d3f39c2c65db1c57852981173
Ilja Livenson <ilja@opennodecloud.com>
2026-01-09 19:22:46 +0000

    Fix TypeError when user is undefined in hasCurrentCustomerPermission selector

--- src/project/team/utils.ts
+++ src/project/team/utils.ts
@@ -9,7 +9,7 @@ export const hasCurrentCustomerPermission = createSelector(
   getUser,
   getCustomer,
   (user, customer) =>
-    user.permissions?.find(
+    user?.permissions?.find(
       ({ scope_uuid, scope_type }) =>
         scope_uuid === customer?.uuid && scope_type === 'customer',
     ),
