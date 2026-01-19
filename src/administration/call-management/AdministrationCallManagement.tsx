05379be3f840f10a1bfbc8933d96623030117603
Ilja Livenson <ilja.livenson@gmail.com>
2026-01-19 21:06:23 +0200

    Expose constance settings in admin

---
+++ src/administration/call-management/AdministrationCallManagement.tsx
@@ -0,0 +1,31 @@
+import { translate } from '@waldur/i18n';
+
+import { SettingsTab, SettingsWithTabs } from '../settings/SettingsWithTabs';
+
+const CALL_MANAGEMENT_TABS: SettingsTab[] = [
+  {
+    key: 'proposal',
+    title: translate('Proposal'),
+    groupName: translate('Proposal settings'),
+  },
+  {
+    key: 'orcid',
+    title: translate('ORCID integration'),
+    groupName: translate('ORCID integration settings'),
+  },
+  {
+    key: 'publication',
+    title: translate('Publication API'),
+    groupName: translate('Publication API settings'),
+  },
+];
+
+export const AdministrationCallManagement = () => {
+  return (
+    <SettingsWithTabs
+      title={translate('Call management settings')}
+      tabs={CALL_MANAGEMENT_TABS}
+      queryKey="AdministrationCallManagement"
+    />
+  );
+};
