c8705e1ffda638411725c628869f4ce6e3c6b066
Maxim Prokopenko <maxim.prokopenko@gmail.com>
2026-01-26 14:34:48 +0000

    [WAL-9579] Use only latest image if duplicate names are retrieved from openstack.

--- src/openstack/openstack-tenant/TenantImagesTable.tsx
+++ src/openstack/openstack-tenant/TenantImagesTable.tsx
@@ -1,16 +1,28 @@
 import { FunctionComponent, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { getFormValues } from 'redux-form';
 import { OpenstackImagesListData } from 'waldur-js-client';
 
+import {
+  TenantImagesFilter,
+  TENANT_IMAGES_FILTER_FORM_ID,
+} from './TenantImagesFilter';
 import { TenantImagesList } from './TenantImagesList';
 
 export const TenantImagesTable: FunctionComponent<{ resourceScope }> = ({
   resourceScope,
 }) => {
+  const filterValues = useSelector(
+    getFormValues(TENANT_IMAGES_FILTER_FORM_ID),
+  ) as OpenstackImagesListData['query'];
   const filter = useMemo(
     (): OpenstackImagesListData['query'] => ({
       tenant_uuid: resourceScope.uuid,
+      ...(filterValues?.show_duplicate_names
+        ? { show_duplicate_names: true }
+        : {}),
     }),
-    [resourceScope],
+    [resourceScope?.uuid, filterValues?.show_duplicate_names],
   );
-  return <TenantImagesList filter={filter} />;
+  return <TenantImagesList filter={filter} filters={<TenantImagesFilter />} />;
 };
