a98271f323148b69467c8cccb6e983cf64331fa2
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2026-01-29 22:46:26 +0200

    fix: Change Marketplace breadcrumb from "Marketplace" to "Settings".

--- src/administration/routes.ts
+++ src/administration/routes.ts
@@ -283,7 +283,7 @@ export const states: StateDeclaration[] = [
       })),
     ),
     data: {
-      breadcrumb: () => translate('Marketplace'),
+      breadcrumb: () => translate('Settings'),
     },
   },
 
