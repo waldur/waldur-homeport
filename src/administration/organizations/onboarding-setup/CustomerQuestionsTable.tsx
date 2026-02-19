cf0f091a7c04f7844351737e68ac83da93bb164b
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2026-02-19 17:46:58 +0000

    [WAL-9728] Redesign onboarding setup view

---
+++ src/administration/organizations/onboarding-setup/CustomerQuestionsTable.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+
+import { translate } from '@waldur/i18n';
+import { TableWithPortal } from '@waldur/table/types';
+
+import { BaseQuestionsTable } from './BaseQuestionsTable';
+import {
+  CUSTOMER_CHECKLIST_QUESTIONS,
+  PredefinedQuestion,
+} from './predefinedQuestions';
+
+export const CustomerQuestionsTable: FC<TableWithPortal> = ({ portal }) => {
+  const getMappingDisplay = (row: PredefinedQuestion) => {
+    if (row.maps_to_customer_field) {
+      return (
+        <div>
+          <span className="text-gray-600 fs-7">{translate('Customer')}: </span>
+          <code className="text-primary">{row.maps_to_customer_field}</code>
+        </div>
+      );
+    }
+    return <span className="text-muted">—</span>;
+  };
+
+  return (
+    <BaseQuestionsTable
+      portal={portal}
+      checklistType="customer"
+      checklistTypeLabel={translate('customer data')}
+      predefinedQuestions={CUSTOMER_CHECKLIST_QUESTIONS}
+      getMappingDisplay={getMappingDisplay}
+    />
+  );
+};
