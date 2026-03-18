29d64c1c28f899e452ef633e79a089aa3b4f222e
Sergei Zaiaev <serjzv@gmail.com>
2026-03-18 16:43:26 +0200

    Add LDAP agent .env file generator dialog for SLURM offerings

---
+++ src/site-agent/SlurmOfferingActions.tsx
@@ -0,0 +1,113 @@
+import { useQuery } from '@tanstack/react-query';
+import { FC } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { ProviderOfferingDetails } from 'waldur-js-client';
+
+import { lazyComponent } from '@waldur/core/lazyComponent';
+import { Tip } from '@waldur/core/Tooltip';
+import { translate } from '@waldur/i18n';
+import { getServiceProviderByCustomer } from '@waldur/marketplace/common/api';
+import { openModalDialog } from '@waldur/modal/actions';
+import { ActionDropdownButton } from '@waldur/table/ActionDropdownButton';
+
+import { SITE_AGENT_PLUGIN } from './constants';
+
+const SiteAgentConfigDialog = lazyComponent(() =>
+  import('./SiteAgentConfigDialog').then((m) => ({
+    default: m.SiteAgentConfigDialog,
+  })),
+);
+
+const LdapAgentEnvDialog = lazyComponent(() =>
+  import('./LdapAgentEnvDialog').then((m) => ({
+    default: m.LdapAgentEnvDialog,
+  })),
+);
+
+interface SlurmOfferingActionsProps {
+  offering: ProviderOfferingDetails;
+}
+
+export const SlurmOfferingActions: FC<SlurmOfferingActionsProps> = ({
+  offering,
+}) => {
+  const dispatch = useDispatch();
+
+  const { data: serviceProvider, isLoading } = useQuery({
+    queryKey: ['service-provider', offering.customer_uuid],
+    queryFn: () =>
+      getServiceProviderByCustomer({ customer_uuid: offering.customer_uuid }),
+    enabled: !!offering.customer_uuid,
+  });
+
+  const openSiteAgentConfig = () => {
+    if (!serviceProvider) return;
+    dispatch(
+      openModalDialog(SiteAgentConfigDialog, {
+        resolve: {
+          provider: {
+            uuid: serviceProvider.uuid,
+            customer_name: serviceProvider.customer_name,
+          },
+          fixedOffering: {
+            uuid: offering.uuid,
+            name: offering.name,
+          },
+        },
+        size: 'lg',
+      }),
+    );
+  };
+
+  const openLdapAgentEnv = () => {
+    dispatch(
+      openModalDialog(LdapAgentEnvDialog, {
+        resolve: {
+          offering: {
+            uuid: offering.uuid,
+            name: offering.name,
+          },
+        },
+        size: 'lg',
+      }),
+    );
+  };
+
+  const showSiteAgentConfig = offering.type === SITE_AGENT_PLUGIN;
+  const showLdapAgentEnv =
+    !!offering.plugin_options?.service_provider_can_create_offering_user;
+
+  if (!showSiteAgentConfig && !showLdapAgentEnv) return null;
+
+  return (
+    <ActionDropdownButton
+      title={translate('Actions')}
+      disabled={isLoading}
+      align="end"
+    >
+      {showSiteAgentConfig && (
+        <Tip
+          id="site-agent-config-item"
+          label={
+            !serviceProvider
+              ? translate('Service provider not found for this offering.')
+              : null
+          }
+        >
+          <Dropdown.Item
+            onClick={openSiteAgentConfig}
+            disabled={!serviceProvider}
+          >
+            {translate('Generate Site Agent Config')}
+          </Dropdown.Item>
+        </Tip>
+      )}
+      {showLdapAgentEnv && (
+        <Dropdown.Item onClick={openLdapAgentEnv}>
+          {translate('Generate LDAP Agent Env')}
+        </Dropdown.Item>
+      )}
+    </ActionDropdownButton>
+  );
+};
