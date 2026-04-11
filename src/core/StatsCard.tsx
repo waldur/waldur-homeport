a16b8c0847da1158e12c2326f79dc1e32dbe7420
Darina <kitadk04@gmail.com>
2026-04-11 07:19:28 +0000

    fix: reporting chart small fixes

--- src/core/StatsCard.tsx
+++ src/core/StatsCard.tsx
@@ -8,7 +8,7 @@ interface StatsCardProps {
 export const StatsCard: FC<StatsCardProps> = ({ label, value }) => (
   <div className="card card-flush card-bordered h-100">
     <div className="card-body d-flex py-5 flex-column">
-      <div className="fs-4 fw-bold text-quaternary">{label}</div>
+      <div className="fs-6 fw-bold text-quaternary">{label}</div>
       <div className="flex-grow-1 mt-3">
         <h1 style={{ fontSize: '32px' }}>{value}</h1>
       </div>
