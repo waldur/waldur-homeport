3213e0b6fdd0f8e6f58aeef92d55fba204c3f9ca
Ilja Livenson <ilja@opennodecloud.com>
2026-05-14 13:22:55 +0000

    Show helpful hint when order error traceback is hidden

--- src/marketplace/resources/resource-pending/ResourceOrderErrorDialog.tsx
+++ src/marketplace/resources/resource-pending/ResourceOrderErrorDialog.tsx
@@ -3,16 +3,25 @@ import { ModalDialog } from '@/modal/ModalDialog';
 import { Field } from '@/resource/summary';
 
 export const ResourceOrderErrorDialog = ({ resolve }) => {
+  const { error_message, error_traceback } = resolve.resource.creation_order;
   return (
     <ModalDialog title={translate('Order errors')}>
       <Field label={translate('Error message')}>
-        {resolve.resource.creation_order.error_message}
+        {error_message || translate('No error message reported.')}
       </Field>
-      <Field label={translate('Error traceback')} valueClass="text-pre">
-        <div style={{ height: 300, overflow: 'scroll' }}>
-          {resolve.resource.creation_order.error_traceback}
-        </div>
-      </Field>
+      {error_traceback ? (
+        <Field label={translate('Error traceback')} valueClass="text-pre">
+          <div style={{ height: 300, overflow: 'scroll' }}>
+            {error_traceback}
+          </div>
+        </Field>
+      ) : (
+        <Field label={translate('Error traceback')}>
+          {translate(
+            'Detailed traceback is visible to support staff only. Please contact support if you need more information.',
+          )}
+        </Field>
+      )}
     </ModalDialog>
   );
 };
