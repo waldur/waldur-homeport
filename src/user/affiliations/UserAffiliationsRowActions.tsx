cb6af7d3fdc2e5ca29ac80e46c4f0a0b0e4131fd
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2026-05-25 00:22:08 +0300

    refactor: migrate UserAffiliationsRowActions to useManagedMutation for permission revocation

--- src/user/affiliations/UserAffiliationsRowActions.tsx
+++ src/user/affiliations/UserAffiliationsRowActions.tsx
@@ -2,9 +2,8 @@ import { FC, useMemo } from 'react';
 import { Permission } from 'waldur-js-client';
 
 import { translate } from '@/i18n';
-import { useModal } from '@/modal/actions';
+import { useManagedMutation } from '@/modal/useManagedMutation';
 import { RemovalActionItem } from '@/resource/actions/RemovalActionItem';
-import { useNotify } from '@/store/notify';
 import { ActionsDropdownComponent } from '@/table/ActionsDropdown';
 import { useUser } from '@/workspace/hooks';
 
@@ -21,7 +20,6 @@ interface UserAffiliationsRowActionsProps {
 export const UserAffiliationsRowActions: FC<UserAffiliationsRowActionsProps> = (
   props,
 ) => {
-  const { confirm } = useModal();
   const user = useUser();
 
   // Check user permission based on the scope type
@@ -30,31 +28,23 @@ export const UserAffiliationsRowActions: FC<UserAffiliationsRowActionsProps> = (
     [user, props.row],
   );
 
-  const { showErrorResponse, showSuccess } = useNotify();
+  const { mutate: callback, isPending } = useManagedMutation<any, any, void>({
+    mutationFn: () => revokePermission(props.row),
+    confirmation: {
+      title: translate('Confirmation'),
+      body: translate('Are you sure you want to revoke this permission?'),
+    },
+    successMessage: translate('Permission has been revoked.'),
+    errorMessage: translate('Unable to revoke permission.'),
+    refetch: props.fetch,
+  });
 
-  const callback = async () => {
-    try {
-      await confirm(
-        translate('Confirmation'),
-        translate('Are you sure you want to revoke this permission?'),
-      );
-    } catch {
-      return;
-    }
-    try {
-      await revokePermission(props.row);
-      showSuccess(translate('Permission has been revoked.'));
-      await props.fetch();
-    } catch (e) {
-      showErrorResponse(e, translate('Unable to revoke permission.'));
-    }
-  };
   return (
     <ActionsDropdownComponent>
       <RemovalActionItem
         action={callback}
         title={translate('Revoke')}
-        disabled={!canDeletePermission}
+        disabled={!canDeletePermission || isPending}
         tooltip={
           !canDeletePermission &&
           translate(
