35f121c743248da1d8781f9c2aa5ce02dc4d9cb2
Ilja Livenson <ilja@opennodecloud.com>
2026-07-08 15:13:25 +0000

    Update enums and descriptions from Waldur Mastermind

--- src/permissions/enums.ts
+++ src/permissions/enums.ts
@@ -21,7 +21,7 @@ export const PermissionMap = {
   offering: 'OFFERING.CREATE_PERMISSION',
   call: 'CALL.CREATE_PERMISSION',
   proposal: 'PROPOSAL.MANAGE',
-  call_organizer: 'CALL.CREATE_PERMISSION',
+  callmanagingorganisation: 'CALL.CREATE_PERMISSION',
   service_provider: 'CUSTOMER.CREATE_PERMISSION',
   resource: 'RESOURCE.CREATE_PERMISSION',
   resource_project: 'RESOURCE_PROJECT.CREATE_PERMISSION',
