79f4c7f1852c38442dd6c1dc6fd5f01dacf21de1
Ilja Livenson <ilja@opennodecloud.com>
2026-07-10 10:06:27 +0000

    Show Restore type in resource order history

--- src/marketplace/orders/list/OrderTypeCell.tsx
+++ src/marketplace/orders/list/OrderTypeCell.tsx
@@ -1,8 +1,12 @@
 import { translate } from '@/i18n';
+import { renderFieldOrDash } from '@/table/utils';
 
 export const OrderTypeCell = ({ row }) =>
-  ({
-    Create: translate('Create'),
-    Update: translate('Update'),
-    Terminate: translate('Terminate'),
-  })[row.type];
+  renderFieldOrDash(
+    {
+      Create: translate('Create'),
+      Update: translate('Update'),
+      Terminate: translate('Terminate'),
+      Restore: translate('Restore'),
+    }[row.type],
+  );
