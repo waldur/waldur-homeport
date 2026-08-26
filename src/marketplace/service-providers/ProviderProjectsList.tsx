fe4aeef953c39ae61556a77ae46f588eb8145de4
Ojagverdiyev <ojagverdiyev13@gmail.com>
2026-08-26 16:12:55 +0300

    Add filters to organization and service provider project lists

--- src/marketplace/service-providers/ProviderProjectsList.tsx
+++ src/marketplace/service-providers/ProviderProjectsList.tsx
@@ -1,8 +1,14 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { marketplaceServiceProvidersProjectsList } from 'waldur-js-client';
 
 import { ProjectsListTable } from '@/project/ProjectsList';
 import { createFetcher } from '@/table/api';
+import {
+  MarketplaceServiceProvidersProjectsFilter,
+  MarketplaceServiceProvidersProjectsFilterFormId,
+  selectMarketplaceServiceProvidersProjectsFilter,
+} from '@/table/generated/MarketplaceServiceProvidersProjectsFilter';
+import { useFilterValues } from '@/table/useFilterValues';
 import { useTable } from '@/table/useTable';
 
 import { CustomerResourcesListPlaceholder } from '../resources/list/CustomerResourcesListPlaceholder';
@@ -10,13 +16,21 @@ import { CustomerResourcesListPlaceholder } from '../resources/list/CustomerReso
 import { ProviderProjectExpandableRow } from './ProviderProjectExpandableRow';
 import { PROVIDER_CUSTOMERS_TABLE_TABS } from './utils';
 
+const TABLE_ID = 'marketplace-provider-projects';
+
 const ProviderProjectsListComponent = ({ provider }) => {
+  const filterValues = useFilterValues(TABLE_ID);
+  const filter = useMemo(
+    () => selectMarketplaceServiceProvidersProjectsFilter(filterValues),
+    [filterValues],
+  );
   const tableProps = useTable({
-    table: 'marketplace-provider-projects',
+    table: TABLE_ID,
     fetchData: createFetcher(marketplaceServiceProvidersProjectsList, {
       path: { service_provider_uuid: provider.uuid },
     }),
     queryField: 'query',
+    filter,
     mandatoryFields: ['project_metadata'], // This is required to display the metadata tab in the expandable row
   });
 
@@ -34,6 +48,8 @@ const ProviderProjectsListComponent = ({ provider }) => {
       tableActions={null}
       rowActions={null}
       expandableRow={ExpandableRow}
+      filters={<MarketplaceServiceProvidersProjectsFilter />}
+      formId={MarketplaceServiceProvidersProjectsFilterFormId}
     />
   );
 };
